df0c5fc9c422ffa91882cdf45d7c0fa37575ab0b
Ofer Cohen <oc666@netvision.net.il>
2013-01-29 18:39:52 +0200

    Create separate configuration for each environment

--- public/index.php
+++ public/index.php
@@ -8,7 +8,29 @@
 defined('APPLICATION_PATH')
 	|| define("APPLICATION_PATH", dirname(__DIR__));
 
-$conf_path = APPLICATION_PATH . "/conf/application.ini";
+$config = array(
+	'servers' => array(
+		'dev' => array('127.0.0.1', '127.0.1.1'),
+		'test' => array('192.168.36.10'),
+		'prod' => '192.168.37.10',
+	)
+);
+
+$current_server = gethostbyname(gethostname()); // we cannot use $_SERVER cause we are on CLI on most cases
+foreach ($config['servers'] as $key => $server) {
+	if (is_array($server) && in_array($current_server, $server)) {
+		$config_env = $key;
+		break;
+	} elseif ($current_server == $server) {
+		$config_env = $key;
+		break;
+	}
+}
+
+$conf_path = APPLICATION_PATH . "/conf/" . $config_env . ".ini";
+if (!file_exists($conf_path)) {
+	die("no config file found");
+}
 $app = new Yaf_Application($conf_path);
 
 if (!isset($_SERVER['HTTP_USER_AGENT'])) {
