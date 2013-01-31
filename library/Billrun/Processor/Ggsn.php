0b63cf9e1a38ff5f22413ada0bffe06565c97b04
eran <eran@eran-Vostro-3360.(none)>
2013-01-31 19:40:04 +0200

    started refactroting egsn to ggsn

---
+++ library/Billrun/Processor/Ggsn.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Ggsn
+ *
+ * @author eran
+ */
+class Billrun_Processor_Ggsn extends Billrun_Processor_Egsn {
+	/**
+	 * the type of the object
+	 *
+	 * @var string
+	 */
+	static protected $type = 'ggsn';
+}
+
+?>
