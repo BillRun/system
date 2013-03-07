8cf5a647ff2bea27585f6cb830c0cc5f2c8662bd
Ofer Cohen <oc666@netvision.net.il>
2013-03-07 12:26:41 +0200

    rearrange config envs

--- public/index.php
+++ public/index.php
@@ -8,30 +8,9 @@
 defined('APPLICATION_PATH')
 	|| define("APPLICATION_PATH", dirname(__DIR__));
 
-$config = array(
-	'servers' => array(
-		'dev' => array('127.0.0.1', '127.0.1.1', '::1'),
-		'test' => array('192.168.36.10'),
-		'prod' => array('192.168.37.10'),
-	)
-);
+require_once APPLICATION_PATH . DIRECTORY_SEPARATOR . 'conf' . DIRECTORY_SEPARATOR . 'config.php';
 
-$current_server = gethostbyname(gethostname()); // we cannot use $_SERVER cause we are on CLI on most cases
-foreach ($config['servers'] as $key => $server) {
-	if (is_array($server) && in_array($current_server, $server)) {
-		$config_env = $key;
-		break;
-	} elseif ($current_server == $server) {
-		$config_env = $key;
-		break;
-	}
-}
-
-$conf_path = APPLICATION_PATH . "/conf/" . $config_env . ".ini";
-if (!file_exists($conf_path)) {
-	die("no config file found" . PHP_EOL);
-}
-$app = new Yaf_Application($conf_path);
+$app = new Yaf_Application(BILLRUN_CONFIG_PATH);
 
 if (!isset($_SERVER['HTTP_USER_AGENT'])) {
 	$app->getDispatcher()->setDefaultAction('Cli');
