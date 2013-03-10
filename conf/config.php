54fefa3f844c80a73eceb684ba1e1687f1ade7fb
eran <eran@eran-Vostro-3360.(none)>
2013-03-10 18:51:46 +0200

    changed the config loading to allow for  runtime  configuration selection

--- conf/config.php
+++ conf/config.php
@@ -25,16 +25,27 @@ if (!defined('BILLRUN_CONFIG_PATH')) {
 		)
 	);
 
-	$current_server = gethostbyname(gethostname()); // we cannot use $_SERVER cause we are on CLI on some cases
+	$envOpts = getopt('',array('environment:'));
+	$envArg = isset($envOpts['environment']) ?  $envOpts['environment'] : false;
+	$current_server = gethostbyname(gethostname()); // we cannot use $_SERVER cause we are on CLI on most cases
 	foreach ($config['servers'] as $key => $server) {
-		if (is_array($server) && in_array($current_server, $server)) {
-			$config_env = $key;
-			break;
-		} elseif ($current_server == $server) {
-			$config_env = $key;
-			break;
-		}
+		if ( is_array($server) && in_array($current_server, $server) ||
+			 $current_server == $server ||
+			 $key == $envArg ) {
+
+				$config_env = $key;
+
+			}	
 	}
+//	foreach ($config['servers'] as $key => $server) {
+//		if (is_array($server) && in_array($current_server, $server)) {
+//			$config_env = $key;
+//			break;
+//		} elseif ($current_server == $server) {
+//			$config_env = $key;
+//			break;
+//		}
+//	}
 
 	$conf_path = APPLICATION_PATH . "/conf/" . $config_env . ".ini";
 	if (!file_exists($conf_path)) {
