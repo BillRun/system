5889b4afb6550a7725516baba848a7704948c0a5
Ofer Cohen <oc666@netvision.net.il>
2013-03-16 14:57:18 +0200

    Mongodloid code conventions

--- library/Mongodloid/Exception.php
+++ library/Mongodloid/Exception.php
@@ -5,7 +5,6 @@
  * @copyright       Copyright (C) 2012 S.D.O.C. LTD. All rights reserved.
  * @license         GNU General Public License version 2 or later; see LICENSE.txt
  */
-class Mongodloid_Exception extends Exception
-{
+class Mongodloid_Exception extends Exception {
 	
 }
