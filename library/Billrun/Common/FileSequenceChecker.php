92ceea6062cd582fd1da19f2942121d0a6046d89
Ofer Cohen <oc666@netvision.net.il>
2013-03-18 10:36:45 +0200

    code conventions

--- library/Billrun/Common/FileSequenceChecker.php
+++ library/Billrun/Common/FileSequenceChecker.php
@@ -18,43 +18,42 @@
 class Billrun_Common_FileSequenceChecker {
 
 	public $lastLogFile = false;
-	
 	protected $getFileSequenceDataCallable = false;
 	protected $hostname = '';
 	protected $type = false;
 	protected $sequencers = array();
-	
-	public function __construct( $getFileSequenceDataFunc, $host, $type) {
+
+	public function __construct($getFileSequenceDataFunc, $host, $type) {
 		$this->getFileSequenceDataCallable = $getFileSequenceDataFunc;
 		$this->hostname = $host;
 		$this->type = $type;
 		$this->loadLastFileDataFromHost();
 	}
-	
+
 	/**
 	 * Check that the received files are in the proper order.
 	 * @param $filename the recieve filename.
 	 */
 	public function addFileToSequence($filename) {
 		$msg = FALSE;
-		if(!$this->getFileSequenceDataCallable) {
+		if (!$this->getFileSequenceDataCallable) {
 			throw new Exception('getFileSequenceData Function wasn`t set on construction!');
 		}
-		
-		if(!($sequenceData = call_user_func($this->getFileSequenceDataCallable, $filename))) {
+
+		if (!($sequenceData = call_user_func($this->getFileSequenceDataCallable, $filename))) {
 			$msg = "GGSN Reciever : Couldnt parse received file : $filename !!!!";
-			Billrun_Factory::log()->log($msg,  Zend_Log::ALERT);			
+			Billrun_Factory::log()->log($msg, Zend_Log::ALERT);
 			return $msg;
 		}
-		
-		if(!isset($this->sequencers[$sequenceData['date']])) {
+
+		if (!isset($this->sequencers[$sequenceData['date']])) {
 			$this->sequencers[$sequenceData['date']] = new Billrun_Common_SequenceChecker();
 		}
-		$this->sequencers[$sequenceData['date']]->addSequence($sequenceData['seq'],  $filename);
-	
+		$this->sequencers[$sequenceData['date']]->addSequence($sequenceData['seq'], $filename);
+
 		return $msg;
 	}
-	
+
 	/**
 	 * Check if the files that were added has a  missing or out if syn sequence.
 	 * @return bool|array false if the sequence is ok or anarry containg the files that were checked.
@@ -62,25 +61,25 @@ class Billrun_Common_FileSequenceChecker {
 	public function hasSequenceMissing() {
 		$ret = false;
 
-		foreach($this->sequencers as $sequencer) {
-			if(!$sequencer->isSequenceValid()) {
+		foreach ($this->sequencers as $sequencer) {
+			if (!$sequencer->isSequenceValid()) {
 				$ret = array_values($sequencer->sequence);
 			}
 		}
-		
+
 		return $ret;
 	}
-	
+
 	/**
 	 * load the last sequence number for the files of the current type from the data base.
 	 */
 	protected function loadLastFileDataFromHost() {
 		$db = Billrun_Factory::db();
 		$log = $db->getCollection($db::log_table);
-		$lastLogFile = $log->query()->equals('source',$this->type)->exists('received_time')
-									->equals('retrieved_from',$this->hostname)->
-									cursor()->sort(array('received_time' => -1,'file_name' => -1))->limit(1)->rewind()->current();
-		if( isset($lastLogFile['file_name']) ) {
+		$lastLogFile = $log->query()->equals('source', $this->type)->exists('received_time')
+				->equals('retrieved_from', $this->hostname)->
+				cursor()->sort(array('received_time' => -1, 'file_name' => -1))->limit(1)->rewind()->current();
+		if (isset($lastLogFile['file_name'])) {
 			$this->lastLogFile = $lastLogFile;
 			$lastSequenceData = call_user_func($this->getFileSequenceDataCallable, $lastLogFile->get('file_name'));
 			$this->sequencers[$lastSequenceData['date']] = new Billrun_Common_SequenceChecker();
