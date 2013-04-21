901560381d6de224bfac4653e3dc01878821af1e
Ofer Cohen <oc666@netvision.net.il>
2013-04-21 12:15:28 +0300

    continue with tariff module

--- library/Billrun/Tariff.php
+++ library/Billrun/Tariff.php
@@ -14,7 +14,7 @@
  * @package  Billing
  * @since    0.5
  */
-abstract class Billrun_Tariff {
+class Billrun_Tariff {
 
 	/**
 	 * Instance of the Tariff
@@ -22,42 +22,113 @@ abstract class Billrun_Tariff {
 	 * @var self instance (Billrun_Tariff)
 	 */
 	static protected $instance = null;
-	
+
 	/**
 	 * Options of the Tariff
 	 *
 	 * @var array
 	 */
-	static protected $options = null;
+	protected $options = null;
 
+	/**
+	 * Rates data of the Tariff
+	 *
+	 * @var array
+	 */
+	protected $rates = null;
+
+	/**
+	 * constructor
+	 * 
+	 * @param array $options the options to preset for the class
+	 */
 	protected function __construct(array $options = array()) {
 		$this->options = $options;
+		// todo: support lazy load
 		$this->load();
 	}
+
 	/**
 	 * load the tariff from DB
 	 */
 	protected function load() {
-		
+		$this->rates = Billrun_Factory::db()->ratesCollection();
 	}
 
+	/**
+	 * get tariff rate
+	 */
 	public function get() {
 		$args = func_get_args();
+		if (!is_array($args[0])) {
+			return;
+		}
+
+		$params = $args[0];
+
+		if (isset($params['searchValue'])) {
+			$searchValue = $params['searchValue'];
+		} else {
+			return;
+		}
+
+		if (isset($params['searchBy'])) {
+			$searchField = $params['searchBy'];
+		} else {
+			$searchField = 'key';
+		}
+
+		if (isset($params['findColumn'])) {
+			$returnField = $params['findColumn'];
+		} else {
+			$returnField = false;
+		}
+
+		if (isset($params['callback'])) {
+			$callback = $params['callback'];
+		} else {
+			$callback = false;
+		}
+
+		if ($callback) {
+//			call_user_func_array($callback, $param_arr);
+		}
+
+		$data = $this->rates->query()->equal($searchField, $searchValue);
+		if ($returnField) {
+			
+		}
 	}
 
+	/**
+	 * set the tariff rate
+	 */
 	public function set() {
-		$args = func_get_args();
+		return false;
 	}
 
+	/**
+	 * delete tariff rate
+	 */
 	public function delete() {
-		$args = func_get_args();
+		return false;
 	}
 
+	/**
+	 * save tariff to database
+	 */
 	public function save() {
-		$args = func_get_args();
+		return false;
 	}
-	
-	public function getInstance(array $options = array()) {
+
+	/**
+	 * Singleton of tariff class
+	 * 
+	 * @param array $options the options of the instance
+	 * 
+	 * @return type
+	 */
+	static public function getInstance(array $options = array()) {
 		if (!isset(self::$instance)) {
 			self::$instance = new self($options);
 		}
