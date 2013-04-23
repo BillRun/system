c2d77407e5adca1831568af21344f58e832b2f9a
Ofer Cohen <oc666@netvision.net.il>
2013-04-23 20:20:11 +0300

    fix issue - email alerts does not contain stats

--- application/plugins/emailAlerts.php
+++ application/plugins/emailAlerts.php
@@ -179,7 +179,7 @@ class emailAlertsPlugin extends Billrun_Plugin_BillrunPluginBase {
 			"Count of success: $successful" . PHP_EOL;
 
 		if ($failed || $successful) {
-			$msg = PHP_EOL . "This mail contain 1 attachment for libreoffice and ms-office" . PHP_EOL;
+			$msg .= PHP_EOL . "This mail contain 1 attachment for libreoffice and ms-office" . PHP_EOL;
 			$attachmentPath = '/tmp/' . date('YmdHi') . '_alert_status.csv';
 			$attachment = $this->generateMailCSV($attachmentPath, $events);
 		}
