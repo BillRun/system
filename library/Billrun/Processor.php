4c8798683a86a19800a327018c8e57faaaab4efa
Ofer Cohen <oc666@netvision.net.il>
2013-04-25 16:53:16 +0300

    make the processor stamp log in debug log level

--- library/Billrun/Processor.php
+++ library/Billrun/Processor.php
@@ -248,7 +248,7 @@ abstract class Billrun_Processor extends Billrun_Base {
 			// old method of processing => receiver did not logged, so it's the first time the file logged into DB
 			$entity = new Mongodloid_Entity($trailer);
 			if ($log->query('stamp', $entity->get('stamp'))->count() > 0) {
-				Billrun_Factory::log()->log("Billrun_Processor::logDB - DUPLICATE! trying to insert duplicate log file with stamp of : {$entity->get('stamp')}", Zend_Log::NOTICE);
+				Billrun_Factory::log()->log("Billrun_Processor::logDB - DUPLICATE! trying to insert duplicate log file with stamp of : {$entity->get('stamp')}", Zend_Log::DEBUG);
 				return FALSE;
 			}
 			return $entity->save($log, true);
