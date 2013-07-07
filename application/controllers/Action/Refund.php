c86b33a05df5b0bdbbc9e8a17883630250963f96
Ofer Cohen <oc666@netvision.net.il>
2013-07-07 17:15:33 +0300

    change api response error property to desc to keep on uniformity with success

--- application/controllers/Action/Refund.php
+++ application/controllers/Action/Refund.php
@@ -36,7 +36,7 @@ class RefundAction extends Action_Base {
 		if (!empty($missing_fields)) {
 			$this->getController()->setOutput(array(
 				'status' => 0,
-				'error' => 'required field(s) missing: ' . implode(', ', $missing_fields),
+				'desc' => 'required field(s) missing: ' . implode(', ', $missing_fields),
 			));
 			return;
 		}
@@ -61,7 +61,7 @@ class RefundAction extends Action_Base {
 		if ($linesCollection->query('stamp', $post['stamp'])->count() > 0) {
 			$this->getController()->setOutput(array(
 				'status' => 0,
-				'error' => 'Transcation already exists in the DB',
+				'desc' => 'Transcation already exists in the DB',
 				'input' => $post,
 			));
 			return;
@@ -71,7 +71,7 @@ class RefundAction extends Action_Base {
 		if ($entity->save($linesCollection) === false) {
 			$this->getController()->setOutput(array(
 				'status' => 0,
-				'error' => 'failed to store into DB',
+				'desc' => 'failed to store into DB',
 				'input' => $post,
 			));
 		}
