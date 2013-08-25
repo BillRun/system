787438d4f76759f327525fa2b14b92ab65c75d05
Ofer Cohen <oc666@netvision.net.il>
2013-08-25 13:27:24 +0300

    log server details on crash

--- public/index.php
+++ public/index.php
@@ -5,8 +5,7 @@
  * @copyright       Copyright (C) 2012 S.D.O.C. LTD. All rights reserved.
  * @license         GNU General Public License version 2 or later; see LICENSE.txt
  */
-defined('APPLICATION_PATH')
-	|| define("APPLICATION_PATH", dirname(__DIR__));
+defined('APPLICATION_PATH') || define("APPLICATION_PATH", dirname(__DIR__));
 
 require_once APPLICATION_PATH . DIRECTORY_SEPARATOR . 'conf' . DIRECTORY_SEPARATOR . 'config.php';
 
@@ -18,6 +17,7 @@ if (!isset($_SERVER['HTTP_USER_AGENT'])) {
 }
 try {
 	$app->bootstrap()->run();
-} catch(Exception $e){
-	Billrun_Factory::log()->log("Crashed When running... exception details are as follow : \n".print_r($e,1), Zend_Log::CRIT);			
+} catch (Exception $e) {
+	$log = print_R($_SERVER, TRUE) . PHP_EOL . print_R($e, TRUE);
+	Billrun_Factory::log()->log("Crashed When running... exception details are as follow : " . PHP_EOL . $log , Zend_Log::CRIT);
 }
