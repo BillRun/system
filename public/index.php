9e7243aa3cf705c4c50903763a87c3b5baba31d7
Ofer Cohen <oc666@netvision.net.il>
2013-08-25 13:28:49 +0300

    log server details on crash

--- public/index.php
+++ public/index.php
@@ -5,18 +5,19 @@
  * @copyright       Copyright (C) 2012 S.D.O.C. LTD. All rights reserved.
  * @license         GNU General Public License version 2 or later; see LICENSE.txt
  */
-defined('APPLICATION_PATH')
-	|| define("APPLICATION_PATH", dirname(__DIR__));
+defined('APPLICATION_PATH') || define("APPLICATION_PATH", dirname(__DIR__));
 
 require_once APPLICATION_PATH . DIRECTORY_SEPARATOR . 'conf' . DIRECTORY_SEPARATOR . 'config.php';
 
 $app = new Yaf_Application(BILLRUN_CONFIG_PATH);
 
+// TODO: move this into bootstrap
 if (!isset($_SERVER['HTTP_USER_AGENT'])) {
 	$app->getDispatcher()->setDefaultController('Cli');
 }
 try {
 	$app->bootstrap()->run();
-} catch(Exception $e){
-	Billrun_Factory::log()->log("Crashed When running... exception details are as follow : \n".print_r($e,1), Zend_Log::CRIT);			
+} catch (Exception $e) {
+	$log = print_R($_SERVER, TRUE) . PHP_EOL . print_R($e, TRUE);
+	Billrun_Factory::log()->log("Crashed When running... exception details are as follow : " . PHP_EOL . $log , Zend_Log::CRIT);
 }
