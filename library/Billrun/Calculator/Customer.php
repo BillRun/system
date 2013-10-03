0f730c22d6e759c3582aedb8a5c2a8886f888969
vbass <vbass@golantelecom.co.il>
2013-10-03 13:53:14 +0300

    Customer calc proccess

--- library/Billrun/Calculator/Customer.php
+++ library/Billrun/Calculator/Customer.php
@@ -35,8 +35,9 @@ class Billrun_Calculator_Customer extends Billrun_Calculator {
 					'$or' => array(
 						array('account_id' => array('$exists' => false)),
 						array('subscriber_id' => array('$exists' => false))
-					)
-		));
+					),
+					'callEventStartTimeStamp' => array('$gt' => '20130929022502'),
+		))->cursor()->limit('500');
 	}
 
 	/**
@@ -46,31 +47,82 @@ class Billrun_Calculator_Customer extends Billrun_Calculator {
 	 * @return boolean true on success else false
 	 */
 	protected function updateRow($row) {
+		$imsi = null;
+		$phone_number = null;
+		
 		if ($row['source'] == 'api' && $row['type'] == 'refund') {
 			$time = date("YmtHis", $row->get('unified_record_time')->sec);
-			$phone_number = $row->get('NDC_SN');
-		} else {
-			$time = $row->get('call_start_dt');
-			$phone_number = $row->get('caller_phone_no');
+			
+			if($row->get('NDC_SN')) {
+				$phone_number = $row->get('NDC_SN');
+				$time = $row->get('call_start_dt');
+			}
+			else {
+				$imsi = $row->get('imsi');
+				$time = $row->get('callEventStartTimeStamp');
+			}
+			
+		} else {	
+			if($row->get('NDC_SN')) {
+				$phone_number = $row->get('caller_phone_no');
+				$time = $row->get('call_start_dt');
+			}
+			else {
+				$imsi = $row->get('imsi');
+				$time = $row->get('callEventStartTimeStamp');
+			}
 		}
 
 		// load subscriber
-		$subscriber = golan_subscriber::get($phone_number, $time);
+		$subscriber = golan_subscriber::get($time, $phone_number, $imsi);
+		
 		if (!$subscriber) {
-			Billrun_Factory::log()->log("subscriber not found. phone:" . $phone_number . " time: " . $time, Zend_Log::INFO);
+			if (!isset($row['subscriber_not_found']) || (isset($row['subscriber_not_found']) && $row['subscriber_not_found'] == false)) {
+				$msg = "Failed  when sending event to subscriber_plan_by_date.rpc.php". PHP_EOL . "subscriber not found, phone_number: ". $phone_number . "imsi: ". $imsi;
+				$this->sendEmailOnFailure($msg);
+				
+				// update all rows with same subscriber detials - subscriber_not_found:true
+				$status = true;
+				$result = $this->update_same_subscriber($status, $imsi, $phone_number);
+			}
+			
+			Billrun_Factory::log()->log("subscriber not found. phone:" . $phone_number . " imsi: ". $imsi. " time: " . $time, Zend_Log::INFO);
 			return false;
+		}
+		
+		if (isset($row['subscriber_not_found']) && $row['subscriber_not_found'] == true) {
+			// update all rows with same subscriber detials - subscriber_not_found:false
+			$status = false;
+			$result = $this->update_same_subscriber($status, $imsi, $phone_number);
 		}
 		
 		$current = $row->getRawData();
 		
-		if (!isset($subscriber['id']) || !isset($subscriber['account_id'])) {
-			Billrun_Factory::log()->log("subscriber_id or account_id not found. phone:" . $phone_number . " time: " . $time, Zend_Log::WARN);
+		$subscriber_id = $subscriber['subscriber_id'];
+		if(empty($imsi)) {
+			$subscriber_id = $subscriber['id'];
+		}
+		
+		if (!isset($subscriber_id) || !isset($subscriber['account_id'])) {
+			if (!isset($row['subscriber_not_found']) || (isset($row['subscriber_not_found']) && $row['subscriber_not_found'] == false)) {
+				$msg = "Did not receive one of necessary params - phone_number: ". $phone_number . "imsi: ". $imsi. PHP_EOL;
+				$this->sendEmailOnFailure($msgi);
+				
+				// update all rows with this imsi - imsi_not_found:true
+				$status = true;
+				$result = $this->update_same_subscriber($status, $imsi, $phone_number);
+			}
+			
+			Billrun_Factory::log()->log("subscriber_id or account_id not found. phone:" . $phone_number . " imsi: ". $imsi. " time: " . $time, Zend_Log::WARN);
 			return false;
 		}
 		
-		$added_values = array('subscriber_id' => $subscriber['id'], 'account_id' => $subscriber['account_id']);
+		$added_values = array('subscriber_id' => $subscriber_id, 'account_id' => $subscriber['account_id']);
 		$newData = array_merge($current, $added_values);
 		$row->setRawData($newData);
+		
+		print_r($newData);
+		
 		return true;
 	}
 
@@ -85,6 +137,42 @@ class Billrun_Calculator_Customer extends Billrun_Calculator {
 				continue;
 			}
 		}
+	}
+	
+	protected function update_same_subscriber($status, $imsi, $phone_number) {
+		$lines = Billrun_Factory::db()->linesCollection();
+		
+		if (!empty($imsi)) {
+			$val = $imsi;
+			$key = 'imsi';
+		}
+		else {
+			$val = $phone_number;
+			$key = 'caller_phone_no';
+		}
+		
+		$rows = array();
+		$results = $lines->query(array(
+						'source' => 'nrtrde',
+						$key => $val
+					));
+		
+		if(empty($results)) {
+			exit;
+		}
+		
+		foreach ($results as $result) {
+			$added_values = array('subscriber_not_found' => $status);
+			$result_a = $result->getRawData();
+			$newData = array_merge($result_a, $added_values);
+			$result->setRawData($newData);
+			$result->save($lines);
+		}
+	}
+	
+	protected function sendEmailOnFailure($msg) {
+		Billrun_Factory::log()->log($msg, Zend_Log::ALERT);
+		return Billrun_Util::sendMail("Failed Fraud Alert, subscriber not found" . date(Billrun_Base::base_dateformat), $msg, Billrun_Factory::config()->getConfigValue('fraudAlert.failed_alerts.recipients', array()) );
 	}
 	
 	/**
