95e26f13bfbafd62731dac79fd1ca0603e0c8df4
Ofer Cohen <oc666@netvision.net.il>
2013-10-28 16:11:54 +0200

    code convention

--- conf/config.php
+++ conf/config.php
@@ -5,17 +5,15 @@
  * @copyright       Copyright (C) 2012 S.D.O.C. LTD. All rights reserved.
  * @license         GNU General Public License version 2 or later; see LICENSE.txt
  */
-
 /**
  * this file configured the environments and configuration of each one
  * if you have only one environment please set it directly as follow:
  * 
  * define('BILLRUN_CONFIG_PATH', APPLICATION_PATH . "/conf/configuration.ini");
  * 
- *  * put the above line in the head of this file and then create configuration.ini in application/conf directory
+ * put the above line in the head of this file and then create configuration.ini in application/conf directory
  * all the below code will be ignored
  */
-
 define('BILLRUN_CONFIG_PATH', APPLICATION_PATH . "/conf/prod.ini");
 
 
@@ -23,21 +21,20 @@ if (!defined('BILLRUN_CONFIG_PATH')) {
 	$config = array(
 		'servers' => array(
 			'dev' => array('127.0.0.1', '127.0.1.1', '::1'),
-			'test' => array('sdocserver','10.162.10.63'),
-			'prod' => array('192.168.37.10', 'fraud.golan.local', '192.168.30.20','rp02_hfa.gt.local','rp02.gt.local','rp01.gt.local','rp01_hfa.gt.local'),
+			'test' => array('sdocserver', '10.162.10.63'),
+			'prod' => array('192.168.37.10', 'fraud.golan.local', '192.168.30.20', 'rp02_hfa.gt.local', 'rp02.gt.local', 'rp01.gt.local', 'rp01_hfa.gt.local'),
 		)
 	);
 
-	$envOpts = getopt('',array('environment:'));
+	$envOpts = getopt('', array('environment:'));
 	if (isset($envOpts['environment'])) {
 		$config_env = (string) $envOpts['environment'];
 	} else {
 		$current_server = gethostbyname(gethostname()); // we cannot use $_SERVER cause we are on CLI on most cases
 		foreach ($config['servers'] as $key => $server) {
-			if ( is_array($server) && in_array($current_server, $server) ||
-				 $current_server == $server ) {
-					$config_env = $key;
-				}	
+			if (is_array($server) && in_array($current_server, $server) || $current_server == $server) {
+				$config_env = $key;
+			}
 		}
 	}
 
