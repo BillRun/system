fc257cc4d8dcd42cb5c78057cf1fd939ae00e41f
Eran <etherpulse@gmail.com>
2013-11-18 15:55:05 +0200

    switched from modem reset to modem hang up

--- library/Billrun/Generator/Calls.php
+++ library/Billrun/Generator/Calls.php
@@ -145,7 +145,7 @@ class Billrun_Generator_Calls extends Billrun_Generator {
 		Billrun_Factory::log("Calls killed.");
 
 		foreach($this->modemDevices as $device) {
-			if(!$device->resetModem()) {
+			if($device->hangUp() == Gsmodem_Gsmodem::UNKNOWN) {
 				Billrun_Factory::log()->log("Failed when trying to reset the modem with number:". $device->getModemNumber(),Zend_Log::ERR);
 			}
 		}		
