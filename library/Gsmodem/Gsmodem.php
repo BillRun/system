18fc2fe2b6105402b04737bf61639f0acd5b409a
Eran <etherpulse@gmail.com>
2013-11-28 16:55:52 +0200

    More fixes to make the modem work correctly

--- library/Gsmodem/Gsmodem.php
+++ library/Gsmodem/Gsmodem.php
@@ -196,6 +196,9 @@ class Gsmodem_Gsmodem  {
 		sleep(5);
 		$this->doCmd($this->getATcmd('echo_mode',array(0)), false, false);
 		sleep(5);
+		$this->doCmd("AT+CFUN=0 ;\r\n", true,true,false,  static::COMMAND_RESPONSE_TIME);
+		sleep(2);
+		$this->doCmd("AT+CFUN=1 ;\r\n", true,true,false,  static::COMMAND_RESPONSE_TIME);
 		$this->doCmd($this->getATcmd('register_reporting',array(2)), true ,true,false, static::COMMAND_RESPONSE_TIME);
 		sleep(1);
 		$this->doCmd($this->getATcmd('incoming_call_id',array(1)), true ,true,false, static::COMMAND_RESPONSE_TIME);
@@ -209,7 +212,7 @@ class Gsmodem_Gsmodem  {
 	 */
 	public function resetModem() {
 		$ret = true;
-		$ret &= $this->doCmd("AT+CVHU=0 ;\r", true,true,false,  static::COMMAND_RESPONSE_TIME) != FALSE;
+		$ret &= $this->doCmd("AT+CVHU=0 ;\r\n", true,true,false,  static::COMMAND_RESPONSE_TIME) != FALSE;
 		sleep(1);
 		$this->hangUp();
 		sleep(1);
