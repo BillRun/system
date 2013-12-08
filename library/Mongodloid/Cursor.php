949df4405ba9eeb5171cd90f37005e3c69bb1b2f
Ofer Cohen <oc666@netvision.net.il>
2013-12-08 18:25:49 +0200

    add timeout option to mongo cursor

--- library/Mongodloid/Cursor.php
+++ library/Mongodloid/Cursor.php
@@ -73,5 +73,9 @@ class Mongodloid_Cursor implements Iterator, Countable {
 		$this->_cursor->setReadPreference($read_preference, $tags);
 		return $this;
 	}
+	
+	public function timeout($ms) {
+		$this->_cursor->timeout($ms);
+	}
 
 }
