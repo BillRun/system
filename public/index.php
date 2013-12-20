e122c773c33fb7ec5a9ba2f0f3a59587d157c319
Ofer Cohen <oc666@netvision.net.il>
2013-12-21 01:46:49 +0200

    change the way the environment is declared

--- public/index.php
+++ public/index.php
@@ -7,14 +7,26 @@
  */
 defined('APPLICATION_PATH') || define("APPLICATION_PATH", dirname(__DIR__));
 
-require_once APPLICATION_PATH . DIRECTORY_SEPARATOR . 'conf' . DIRECTORY_SEPARATOR . 'config.php';
-
-$app = new Yaf_Application(BILLRUN_CONFIG_PATH);
-
 // TODO: move this into bootstrap
 if (!isset($_SERVER['HTTP_USER_AGENT'])) {
-    $app->getDispatcher()->setDefaultController('Cli');
+	$envOpts = getopt('', array('environment:'));
+	$env = isset($envOpts['environment'])?$envOpts['environment']:null;
+} else {
+	if (!defined('APPLICATION_ENV')) {
+		$env = getenv('APPLICATION_ENV');
+		define('APPLICATION_ENV', $env);
+	}
 }
+
+if (!isset($env)) {
+	die('Environment did not setup!');
+}
+
+define('BILLRUN_CONFIG_PATH', APPLICATION_PATH . "/conf/" . $env . ".ini");
+
+// TODO build one config ini with declaration for each environment
+$app = new Yaf_Application(BILLRUN_CONFIG_PATH);
+
 try {
     $app->bootstrap()->run();
 } catch (Exception $e) {
