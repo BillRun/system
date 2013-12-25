8fa1882fdf55fc1c2c154496acee142631b820f2
killawatt <foxession@gmail.com>
2013-12-25 22:13:45 +0200

    change csv import from credit to bulkcredit mid dev

--- application/controllers/Import.php
+++ application/controllers/Import.php
@@ -17,15 +17,15 @@ class ImportController extends Yaf_Controller_Abstract {
 	public function indexAction() {
 		die();
 		$parser = Billrun_Parser::getInstance(array(
-				'type' => 'separator',
-				'separator' => ",",
+					'type' => 'separator',
+					'separator' => ",",
 		));
 
 		$parser->setSeparator(",");
 		$import = Billrun_Processor::getInstance(array(
-				'type' => 'importzones',
-				'parser' => $parser,
-				'path' => '/home/shani/Documents/S.D.O.C/BillRun/backups/zone.csv'
+					'type' => 'importzones',
+					'parser' => $parser,
+					'path' => '/home/shani/Documents/S.D.O.C/BillRun/backups/zone.csv'
 		));
 
 		if ($import === FALSE) {
@@ -36,9 +36,9 @@ class ImportController extends Yaf_Controller_Abstract {
 		$importData = $import->process();
 
 		$merge = Billrun_Processor::getInstance(array(
-				'type' => 'mergerates',
-				'parser' => $parser,
-				'path' => '/home/shani/Documents/S.D.O.C/BillRun/backups/tariff_v2_filtered.csv'
+					'type' => 'mergerates',
+					'parser' => $parser,
+					'path' => '/home/shani/Documents/S.D.O.C/BillRun/backups/tariff_v2_filtered.csv'
 		));
 
 		if ($merge === FALSE) {
@@ -49,9 +49,9 @@ class ImportController extends Yaf_Controller_Abstract {
 		$mergeData = $merge->process();
 
 		$mergePackage = Billrun_Processor::getInstance(array(
-				'type' => 'mergezonepackage',
-				'parser' => $parser,
-				'path' => '/home/shani/Documents/S.D.O.C/BillRun/backups/zone_group_element.csv'
+					'type' => 'mergezonepackage',
+					'parser' => $parser,
+					'path' => '/home/shani/Documents/S.D.O.C/BillRun/backups/zone_group_element.csv'
 		));
 
 		if ($mergePackage === FALSE) {
@@ -62,9 +62,9 @@ class ImportController extends Yaf_Controller_Abstract {
 		$mergePackageData = $mergePackage->process();
 
 		$merge_intl_networks = Billrun_Processor::getInstance(array(
-				'type' => 'mergeintlnetworks',
-				'parser' => $parser,
-				'path' => '/home/shani/Documents/S.D.O.C/BillRun/backups/mobile_network.csv'
+					'type' => 'mergeintlnetworks',
+					'parser' => $parser,
+					'path' => '/home/shani/Documents/S.D.O.C/BillRun/backups/mobile_network.csv'
 		));
 
 		if ($merge_intl_networks === FALSE) {
@@ -75,9 +75,9 @@ class ImportController extends Yaf_Controller_Abstract {
 		$importMapData = $merge_intl_networks->process();
 
 		$wholesale = Billrun_Processor::getInstance(array(
-				'type' => 'wholesaleoutrates',
-				'parser' => $parser,
-				'path' => '/home/shani/Documents/S.D.O.C/BillRun/backups/wholesale/wsalein_tariff_out_v2.csv'
+					'type' => 'wholesaleoutrates',
+					'parser' => $parser,
+					'path' => '/home/shani/Documents/S.D.O.C/BillRun/backups/wholesale/wsalein_tariff_out_v2.csv'
 		));
 
 		if ($wholesale === FALSE) {
@@ -88,9 +88,9 @@ class ImportController extends Yaf_Controller_Abstract {
 		$importWholesaleZones = $wholesale->process();
 
 		$wholesalein = Billrun_Processor::getInstance(array(
-				'type' => 'wholesaleinrates',
-				'parser' => $parser,
-				'path' => '/home/shani/Documents/S.D.O.C/BillRun/backups/wholesale/wsalein_tariff_in_v2.csv'
+					'type' => 'wholesaleinrates',
+					'parser' => $parser,
+					'path' => '/home/shani/Documents/S.D.O.C/BillRun/backups/wholesale/wsalein_tariff_in_v2.csv'
 		));
 
 		if ($wholesalein === FALSE) {
@@ -102,16 +102,16 @@ class ImportController extends Yaf_Controller_Abstract {
 
 		$this->getView()->title = "BillRun | The best open source billing system";
 		$this->getView()->content = "Data import count: " . count($importWholesaleZones)
-			. "<br />" . PHP_EOL
-			. "Data merge count: " . count($mergeData) . "<br />"
-			. "Data merge package count: " . count($mergePackageData) . "<br />"
-			. "Data merge package count: " . count($mergePackageData) . "<br />"
-			. "Merge intl. networks count: " . $importMapData . "<br />" . PHP_EOL;
+				. "<br />" . PHP_EOL
+				. "Data merge count: " . count($mergeData) . "<br />"
+				. "Data merge package count: " . count($mergePackageData) . "<br />"
+				. "Data merge package count: " . count($mergePackageData) . "<br />"
+				. "Merge intl. networks count: " . $importMapData . "<br />" . PHP_EOL;
 		;
 	}
 
 	public function csvAction() {
-		$path = ''; // TODO: setup by config cli input
+		$path = '/home/shani/Desktop/refunds_201312.csv'; // TODO: setup by config cli input
 		if (!file_exists($path) || is_dir($path)) {
 			Billrun_Factory::log("file not exists or path is directory");
 			return FALSE;
@@ -122,11 +122,12 @@ class ImportController extends Yaf_Controller_Abstract {
 			$limit = 0; // TODO: setup by config cli input
 			$enclosure = '"'; // TODO: setup by config cli input
 			$escape = '\\'; // TODO: setup by config cli input
-			$uri = 'http://rp01.gt/api/credit'; // TODO: setup by config cli input
+			$uri = 'http://billrun/api/bulkcredit'; // TODO: setup by config cli input
 			$curl = new Zend_Http_Client_Adapter_Curl();
 			$client = new Zend_Http_Client($uri);
 			$client->setAdapter($curl);
 			$client->setMethod(Zend_Http_Client::POST);
+			$send_arr = array();
 			while (($data = fgetcsv($handle, $limit, $delimiter)) !== FALSE) {
 				$urt = new Zend_Date(strtotime($data[2] . " " . $data[3]), 'he_IL');
 				$send = array(
@@ -135,13 +136,18 @@ class ImportController extends Yaf_Controller_Abstract {
 					'credit_time' => $urt->getTimestamp(),
 					'amount_without_vat' => $data[4],
 					'reason' => $data[5],
-					'credit_type' => isset($data[6])?$data[6]:'refund',
+					'credit_type' => isset($data[6]) ? $data[6] : 'refund',
 				);
-				$client->setParameterPost($send);
-				$response = $client->request();
-				print $response->getBody();
-				Billrun_Factory::log("API response with: " . print_R($response->getBody(), true) . "<br />");
+				$send_arr[] = $send;
 			}
+			$params = array(
+				'operation' => 'credit',
+				'credits' => json_encode($send_arr),
+			);
+			$client->setParameterPost($params);
+			$response = $client->request();
+			print $response->getBody();
+			Billrun_Factory::log("API response with: " . print_R($response->getBody(), true) . "<br />");
 		}
 		die(" end...");
 	}
