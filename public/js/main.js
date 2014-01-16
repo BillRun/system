df6c42195d30eb94a7468e3274e5f67d5b287bab
Vitali Bass <vbass@gt-dev-lt002.(none)>
2014-01-16 10:58:19 +0200

    more fix on admin

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,19 @@
 $('body').on('hidden', '.modal', function() {
 	$(this).removeData('modal');
 });
-
+var checkItems = false;
 $(function() {
 	$("#close_and_new,#duplicate").click(function() {
 		var items_checked = $('#data_table :checked');
-                
+                checkItems=true;
 		if (items_checked.length) {
 			$(this).data('remote', edit_url_prefix + items_checked.eq(0).val() + '&type=' + $(this).data('type'));
-		}
+		} 
 	});
 
 	$("#remove").click(function() {
 		var items_checked = $('#data_table :checked');
+                checkItems=true;
 		var output = $.map(items_checked, function(n, i) {
 			return n.value;
 		}).join(',');
@@ -21,13 +22,15 @@ $(function() {
 		}
 	});
 
-	$("#popupModal,#confirmModal").on('show', function(event) {
-		var items_checked = $('#data_table :checked');
-		if (!items_checked.length || (items_checked.length != 1 && (coll != 'lines' || $(this).attr('id') != 'confirmModal'))) {
-			alert('Please check exactly one item from the list');
-			$(this).removeData('modal');
-			event.preventDefault();
-		}
+	$("#popupModal,#confirmModal").on('show', function(event) {           
+                 if(checkItems) {
+                    var items_checked = $('#data_table :checked');
+                    if (!items_checked.length || (items_checked.length != 1 && (coll != 'lines' || $(this).attr('id') != 'confirmModal'))) {
+                            alert('Please check exactly one item from the list');
+                            $(this).removeData('modal');
+                            event.preventDefault();
+                    }
+                }
 	});
 
 	$('.multiselect').multiselect({
@@ -103,7 +106,7 @@ function addFilter(button) {
 function update_current(obj){
 
         var items_checked = $(obj).next("input[type=checkbox]");
-
+        checkItems=false;
         if (items_checked.length) {
                 $(obj).data('remote', edit_url_prefix + items_checked.eq(0).val() + '&type=' + $(obj).data('type'));
         }
