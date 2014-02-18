cd4f79d72b02f4b5751f91d0d30b627e1774ec75
Ofer Cohen <oc666@netvision.net.il>
2014-02-18 19:16:05 +0200

    refactoring the fraud calculations

--- application/plugins/fraud.php
+++ application/plugins/fraud.php
@@ -26,78 +26,106 @@ class fraudPlugin extends Billrun_Plugin_BillrunPluginBase {
 	 * Method to save fraud events on the fraud system once event triggered
 	 * 
 	 * @param array $row the line from lines collection
-	 * @param array $balance
+	 * @param Mongodloid_Entity $balance 
 	 * @param string $usage_type, the usage type could be: call, data, mms, sms
 	 * @param array $rate
 	 * @param string $volume
 	 * @param Billrun_Calculator $calculator
 	 * 
 	 */
-	public function afterUpdateSubscriberBalance($row, $balance, $usage_type, $rate, $volume, $price, $calculator) {
+	public function afterUpdateSubscriberBalance($row, $balance, $rowPrice, $calculator) {
+		
+		// if not plan to row - cannot do anything
+		if (!isset($row['plan'])) {
+			Billrun_Factory::log("Fraud plugin - plan not exists for line " . $row['stamp'], Zend_Log::INFO);
+			return true;
+		}
 
-		$sub_balance = $balance->getRawData();
-//		$sub_row = $row->getRawData();
-//		$events = Billrun_Factory::db()->eventsCollection();
+		// first check we are not on tap3, because we prevent intl fraud on nrtrde
+		if ($row['type'] == 'tap3') {
+			return true;
+		}
+		
 
-		$properties = Billrun_Factory::config()->getConfigValue('fraud.usage_types', array());
+		$thresholds = Billrun_Factory::config()->getConfigValue('fraud.thresholds', array());
 
-		foreach ($properties as $value) {
-
-			if (preg_match("/$usage_type/", $value) !== 0) {
-				$sum_usagev += $sub_balance['balance']['totals'][$value]['usagev'];
+		foreach ($thresholds as $type => $limits) {
+			$method = $type . 'Check';
+			if (method_exists($this, $method)) {
+				call_user_func_array(array($this, $method), array($limits, $row, $balance, $rowPrice));
+			} else {
+				Billrun_Factory::log("Fraud plugin - method doesn't exists " . $method, Zend_Log::WARN);
 			}
-
-			$sum_cost += $sub_balance['balance']['totals'][$value]['cost'];
 		}
 
-		$usage_type_properties = array('call' => array('threshold_usagev' => Billrun_Factory::config()->getConfigValue('fraud.thresholds.call'), 'units' => 'SEC'),
-			'sms' => array('threshold_usagev' => Billrun_Factory::config()->getConfigValue('fraud.thresholds.sms'), 'units' => 'SMS'),
-			'mms' => array('threshold_usagev' => Billrun_Factory::config()->getConfigValue('fraud.thresholds.mms'), 'units' => 'NUMBER'),
-			'data' => array('threshold_usagev' => Billrun_Factory::config()->getConfigValue('fraud.thresholds.data'), 'units' => 'BYTES'));
-
-		$fraud_connection = Billrun_Factory::db(Billrun_Factory::config()->getConfigValue('fraud.db'))->eventsCollection();
-		$fraud_connection_options = array();
-		$fraud_connection_options['w'] = 1;
-
-		$value = $row['usagev'];
-		$value_before = $sum_usagev;
-		// TODO !! check if small
-		// TODO !! check current balance
-		if ($usage_type == 'data') {
-			foreach ($usage_type_properties[$usage_type]['threshold_usagev'] as $threshold_plan => $threshold_name) {
-
-				if ($threshold_plan == 'small' && $row['plan'] != 'small') {
-					continue;
-				}
-
-				$threshold_value = $usage_type_properties[$usage_type]['threshold_usagev'][$threshold_plan][$threshold_name];
-
-				if ($this->over_threshold($value_before, $value, $threshold_value)) {
-					$this->insert_fraud_event('usagev', $value, $value_before, $row, $threshold_value, $usage_type_properties[$usage_type]['units'], $threshold_name, $fraud_connection, $fraud_connection_options);
-				}
-			}
-		} else if (over_threshold($value_before, $value, $threshold)) {
-			$this->insert_fraud_event('usagev', $value, $value_before, $row, $threshold, $usage_type_properties[$usage_type]['units'], 'FP_NATIONAL2', $fraud_connection, $fraud_connection_options);
-		}
-
-		$current_plan_name = $row['plan'];
-
-		if (empty($current_plan_name)) {
-			$current_plan_name = 'NO_GIFT';
-		}
-
-		$threshold = Billrun_Factory::config()->getConfigValue('fraud.thresholds.cost.' . $current_plan_name);
-		$value_before = $sum_cost;
-		$value = $price;
-
-		if ($this->over_threshold($value_before, $value, $threshold)) {
-			$this->insert_fraud_event('cost', $value, $value_before, $row, $threshold, 'NIS', 'FP_NATIONAL1', $fraud_connection, $fraud_connection_options);
-		}
 	}
 
-	protected function insert_fraud_event($filter_by, $value, $value_before, $row, $threshold, $units, $event_type, $fraud_connection, $fraud_connection_options) {
+	protected function costCheck($limits, $row, $balance, $rowPrice) {
+		$price_before = 0;// TODO sum all except TAP3
+		$price_after = $price_before + $price;
+		$this->checkLimits($limits, $row, $price_before, $price_after);
+	}
 
-		Billrun_Factory::log()->log('Marking down' . $event_type . 'lines For fraud plugin', Zend_Log::INFO);
+	protected function usageCheck($limits, $row, $balance, $rowPrice) {
+		// if the limit is not for the line type
+		if (!isset($row['usaget']) || !isset($limits[$row['usaget']])) {
+			return true;
+		}
+
+		$usaget = $row['usaget'];
+
+		if (!isset($balance->balance['totals'][$usaget]['usagev'])) {
+			Billrun_Factory::log("Fraud plugin - balance not exists for subscriber " . $row['sid'], Zend_Log::INFO);
+			return true;
+		}
+		$balance_before_change = $balance->balance['totals'][$usaget]['usagev'];
+		$balance_after_change = $balance_before_change + $row['usagev'];
+
+		$this->checkLimits($limits, $row, $balance_before_change, $balance_after_change);
+		
+		return true;
+	}
+	
+	protected function checkLimits($limits, $row, $before, $after) {
+		foreach ($limits as $limit) {
+			// if the limit for specific plans
+			if (isset($limit['limitPlans']) &&
+				(is_array($limit['limitPlans']) && !in_array($row['plan'], $limit['limitPlans']))) {
+				continue;
+			}
+			
+			$threshold = $limit['threshold'];
+			$recurring = isset($limit['recurring']) && $limit['recurring'];
+			if ($this->isThresholdTriggered($before, $after, $threshold, $recurring)) {
+				// insert event
+				$name = $limit['name'];
+				Billrun_Factory::log("Fraud plugin - trigger event " . $row['stamp'] . ' with event name ' . $name, Zend_Log::INFO);
+			}
+		}
+
+	}
+
+	/**
+	 * method to check if threshold passed between two value
+	 * 
+	 * @param float $usage_before the usage before
+	 * @param float $usage_after the usage after
+	 * @param float $threshold the threshold to pass
+	 * @param boolean $recurring if true it will check with iterating of the threshold
+	 * 
+	 * @return boolean true if the threshold passed the value
+	 */
+	protected function isThresholdTriggered($usage_before, $usage_after, $threshold, $recurring = false) {
+		if ($recurring) {
+//			return (floor($usage_before / $threshold) < floor($usage_after / $threshold));
+			return ($usage_before % $threshold > $usage_after % $threshold || ($usage_after-$usage_before) > $threshold);
+		}
+		return ($usage_before < $threshold) && ($threshold < $usage_after);
+	}
+
+	protected function insert_fraud_event($value, $value_before, $row, $threshold, $units, $event_type, $fraud_connection, $fraud_connection_options) {
+
+		$fraud_connection = Billrun_Factory::db(Billrun_Factory::config()->getConfigValue('fraud.db'))->eventsCollection();
 
 		$newEvent = new Mongodloid_Entity();
 		$newEvent['value_usagev'] = $row['usagev'];
@@ -112,7 +140,7 @@ class fraudPlugin extends Billrun_Plugin_BillrunPluginBase {
 		$newEvent['stamp'] = md5(serialize($newEvent));
 
 		try {
-			$insertResult = $fraud_connection->insert($newEvent, $fraud_connection_options);
+			$insertResult = $fraud_connection->insert($newEvent, array('w' => 1));
 
 			if ($insertResult['ok'] == 1) {
 				Billrun_Factory::log()->log("line with the stamp: " . $newEvent['stamp'] . " inserted to the fraud events", Zend_Log::INFO);
