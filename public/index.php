3f6031eef43e64e0fa4dcd4cc75db251eafc1ed8
killawatt <foxession@gmail.com>
2014-04-03 19:11:42 +0300

    Added host name to exceptions log message (BILLRUN-470)

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@
  * @copyright       Copyright (C) 2012 S.D.O.C. LTD. All rights reserved.
  * @license         GNU General Public License version 2 or later; see LICENSE.txt
  */
-defined('APPLICATION_PATH') || define("APPLICATION_PATH", dirname(__DIR__));
+defined('APPLICATION_PATH') || define('APPLICATION_PATH', dirname(__DIR__));
 
 require_once(APPLICATION_PATH . DIRECTORY_SEPARATOR . 'conf' . DIRECTORY_SEPARATOR . 'config.php');
 
@@ -14,6 +14,6 @@ $app = new Yaf_Application(BILLRUN_CONFIG_PATH);
 try {
 	$app->bootstrap()->run();
 } catch (Exception $e) {
-	$log = print_R($_SERVER, TRUE) . PHP_EOL . print_R("Error code : " . $e->getCode() . PHP_EOL . "Error message: " . $e->getMessage() . PHP_EOL . $e->getTraceAsString(), TRUE); // we don't cast the exception to string because Yaf_Exception could cause a segmentation fault
-	Billrun_Factory::log()->log("Crashed When running... exception details are as follow : " . PHP_EOL . $log, Zend_Log::CRIT);
+	$log = print_R($_SERVER, TRUE) . PHP_EOL . print_R('Error code : ' . $e->getCode() . PHP_EOL . 'Error message: ' . $e->getMessage() . PHP_EOL . 'Host: ' . gethostname() . PHP_EOL . $e->getTraceAsString(), TRUE); // we don't cast the exception to string because Yaf_Exception could cause a segmentation fault
+	Billrun_Factory::log()->log('Crashed When running... exception details are as follow : ' . PHP_EOL . $log, Zend_Log::CRIT);
 }
