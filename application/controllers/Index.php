72825ed6a7e59a38ac17314e4533790f7d2a25ad
Ofer Cohen <oc666@netvision.net.il>
2014-04-07 15:01:44 +0300

    remove debugging code

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -15,13 +15,6 @@
 class IndexController extends Yaf_Controller_Abstract {
 
 	public function indexAction() {
-		foreach (array("12342355678", "1800123456", "0501234567", "501234567", "972546918666", "1-234-235-5678", "+44 (0) 1522 814010") as $a) {
-			print $a . "<br />" . PHP_EOL;
-			$r = Billrun_Util::msisdn($a);
-			print "Return: " . $r . "<br />" . PHP_EOL;
-			print "<br />" . PHP_EOL;
-		}
-		die;
 		$this->redirect('admin');
 		$this->getView()->title = "BillRun | The best open source billing system";
 		$this->getView()->content = "Open Source Last Forever!";
