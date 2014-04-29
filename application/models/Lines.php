86c115fd9c98d7c74fdd0d0a433ee51d7efe25b8
Ofer Cohen <oc666@netvision.net.il>
2014-04-29 16:51:57 +0300

    limit the count cursor of lines

--- application/models/Lines.php
+++ application/models/Lines.php
@@ -114,9 +114,9 @@ class LinesModel extends TableModel {
 			$size = $this->size;
 		}
 		
-//		$limit = Billrun_Factory::config()->getConfigValue('admin_panel.lines.limit', 100);
+		$limit = Billrun_Factory::config()->getConfigValue('admin_panel.lines.limit', 10000);
 
-		$cursor = $this->collection->query($filter_query)->cursor()->setReadPreference(Billrun_Factory::config()->getConfigValue('read_only_db_pref'));//->limit($limit);
+		$cursor = $this->collection->query($filter_query)->cursor()->setReadPreference(Billrun_Factory::config()->getConfigValue('read_only_db_pref'))->limit($limit);
 		$this->_count = $cursor->count();
 		$resource = $cursor->sort($this->sort)->skip($skip)->limit($size);
 		$ret = array();
