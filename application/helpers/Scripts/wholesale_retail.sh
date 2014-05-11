e34afc834c8b0cec41aa511afc01d323dbb416fe
killawatt <foxession@gmail.com>
2014-05-11 12:11:37 +0300

    Added wholesale_retail script

---
+++ application/helpers/Scripts/wholesale_retail.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+###  script arguments as follow:
+###  1) report name
+###  2) day (YYYY-MM-dd format)
+###  3) output directory
+
+if [ $1 ]; then
+        report_name=$1;
+else
+	echo "please supply a report name"
+	exit 2
+fi
+
+if [ $2 ]; then
+        day=$2;
+else
+	echo "please supply day (format: YYYY-MM-dd)"
+	exit 2
+fi
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+output_dir="${script_dir}/../../../files/csvs";
+
+if [ $3 ]; then
+	output_dir=$3;
+fi
+
+js_code='db.getMongo().setReadPref("secondaryPreferred");'
+
+#compatibility with 2.6 - aggregate is cursor
+mongo_main_version=`mongo --version | awk '//{split($4,a,"."); print a[1]"."a[2]}'`
+
+case $report_name in
+
+	"extra" )
+	js_code=$js_code'db.lines.aggregate({$match:{urt:{$gte:ISODate("'$day'T00:00:00+03:00"),$lt:ISODate("'$day'T23:59:59+03:00")},$or:[{over_plan:{$exists:1}},{out_plan:{$exists:1}}]}},{$project:{_id:0,over_aprice:{$cond: [{$gt: ["$over_plan", 0]}, "$aprice", 0]},out_aprice:{$cond: [{$gt: ["$out_plan", 0]}, "$aprice", 0]}}},{$group:{_id:0,over:{$sum:"$over_aprice"},out:{$sum:"$out_aprice"}}}).result.forEach(function (obj){print("'$day'" + "," + obj.over + "," + obj.out)});';;
+
+	*)
+	echo "Unrecognized report name";
+	exit;
+	;;
+esac
+
+if [ $mongo_main_version == "2.6" ] ; then
+	js_code=${js_code/\.result/}
+fi
+
+if [[ -n "$js_code" ]]; then	
+	mongo billing -ureading -pguprgri --quiet --eval "$js_code" > "$output_dir/$report_name""_""$day.csv" ;
+fi
+
+
+exit;
