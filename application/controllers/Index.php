299ef45f472af8d4a706d86fd82fc03d8a36ae5c
Ofer Cohen <oc666@netvision.net.il>
2014-05-15 23:25:43 +0300

    clean testing code

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -15,12 +15,6 @@
 class IndexController extends Yaf_Controller_Abstract {
 
 	public function indexAction() {
-		echo "<pre>";
-		echo Billrun_Util::msisdn('000972576677003') . PHP_EOL;
-		echo Billrun_Util::msisdn('972542080308') . PHP_EOL;
-		echo Billrun_Util::msisdn('0577304005') . PHP_EOL;
-		echo Billrun_Util::msisdn('000033622359887') . PHP_EOL;
-		die;
 		$this->redirect('admin');
 		$this->getView()->title = "BillRun | The best open source billing system";
 		$this->getView()->content = "Open Source Last Forever!";
