cf2cd990668527dd49da83a4cf8ea59d727f9ed4
killawatt <foxession@gmail.com>
2014-05-27 13:56:53 +0300

    Avoid running same page at the same time on monthly billrun using flock command

--- scripts/run_billrun.sh
+++ scripts/run_billrun.sh
@@ -45,7 +45,7 @@ billrun_dir="/var/www/billrun";
 
 for i in `seq 0 $instances`; do
         page=`expr $start_instance \+ $i`;
-        screen -d -m php -t $billrun_dir $billrun_dir/public/index.php  --env prod --aggregate --type customer --stamp $month --page $page --size $size
+		screen -d -m flock -n $billrun_dir"/workspace/aggregate_lock_"$page"_"$size".tmp" php -t $billrun_dir $billrun_dir/public/index.php  --env prod --aggregate --type customer --stamp $month --page $page --size $size
         sleep $sleeptime;
 done
 
