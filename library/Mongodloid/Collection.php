cd0f2dc9c756b13d6812169d5406b3178d640a98
Ofer Cohen <oc666@netvision.net.il>
2014-06-02 15:49:54 +0200

    change collection setReadPreference to return the self instance instead of the set return status

--- library/Mongodloid/Collection.php
+++ library/Mongodloid/Collection.php
@@ -190,11 +190,11 @@ class Mongodloid_Collection {
 	 */
 	public function setReadPreference($readPreference, array $tags = array()) {
 		if (defined('MongoClient::' . $readPreference)) {
-			return $this->_collection->setReadPreference(constant('MongoClient::' . $readPreference), $tags);
+			$this->_collection->setReadPreference(constant('MongoClient::' . $readPreference), $tags);
 		} else if (in_array($readPreference, Mongodloid_Connection::$availableReadPreferences)) {
-			return $this->_collection->setReadPreference($readPreference, $tags);
+			$this->_collection->setReadPreference($readPreference, $tags);
 		}
-		return FALSE;
+		return $this;
 	}
 
 	/**
