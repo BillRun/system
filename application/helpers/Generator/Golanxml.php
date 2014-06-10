09d57ac57cbfe75e293cda7d4916b5c9cd93711f
Ofer Cohen <oc666@netvision.net.il>
2014-06-10 17:29:13 +0300

    fix issue with presentation of calling to abroad in generate xml

--- application/helpers/Generator/Golanxml.php
+++ application/helpers/Generator/Golanxml.php
@@ -859,7 +859,7 @@ class Generator_Golanxml extends Billrun_Generator {
 	protected function getCalledNo($line) {
 		$called_number = '';
 		if ($line['type'] == 'tap3' // on tap3
-			|| (isset($line['out_circuit_group']) && ($line['out_circuit_group'] == "2100" && $line['out_circuit_group'] == "2101" || $line['out_circuit_group'] == "2499"))) // or call to abroad
+			|| (isset($line['out_circuit_group']) && ($line['out_circuit_group'] == "2100" || $line['out_circuit_group'] == "2101" || $line['out_circuit_group'] == "2499"))) // or call to abroad
 		{
 			if ($line['usaget'] == 'incoming_call') {
 				$called_number = $line['calling_number'];
