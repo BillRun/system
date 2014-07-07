3cba79735e00a8b129ab3b1ec60bcf4ae79eccb6
killawatt <foxession@gmail.com>
2014-07-07 18:18:44 +0300

    Wholesale: Fixed hidden tabs graphs' legend

--- public/js/main.js
+++ public/js/main.js
@@ -251,13 +251,10 @@ $(function() {
 		$('.wholesale-table').stickyTableHeaders({fixedOffset: $('.navbar-fixed-top')});
 	}
 
-	//for bootstrap 3 use 'shown.bs.tab' instead of 'shown' in the next line
 	$('a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
-		//save the latest tab; use cookies if you like 'em better:
 		localStorage.setItem('lastTab', $(e.target).attr('id'));
 	});
 
-	//go to the latest tab, if it exists:
 	var lastTab = localStorage.getItem('lastTab');
 	if (lastTab) {
 		$('#' + lastTab).tab('show');
