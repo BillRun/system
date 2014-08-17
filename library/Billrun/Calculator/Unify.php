bcf62d0c991386064201c9598aac0e535021094b
Ofer Cohen <oc666@netvision.net.il>
2014-08-17 15:02:35 +0300

    minor fix to nsn unify is legitimate method

--- library/Billrun/Calculator/Unify.php
+++ library/Billrun/Calculator/Unify.php
@@ -316,7 +316,7 @@ class Billrun_Calculator_Unify extends Billrun_Calculator {
 	}
 
 	public function isNsnLineLegitimate($line) {
-		if ((isset($line['arate']) && $line['arate'] != false) || ($line['usaget'] == 'incoming_call' && isset($line['sid']))) {
+		if ((isset($line['arate']) && $line['arate'] !== false) || (isset($line['usaget']) && $line['usaget'] == 'incoming_call' && isset($line['sid']))) {
 			return false;
 		}
 		return true;
