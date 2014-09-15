b7f8374fbeecc14dedfae41b745a1091b0531f60
Ofer Cohen <oc666@netvision.net.il>
2014-09-15 12:28:15 +0300

    minor fix to datausage api

--- application/controllers/Action/Datausage.php
+++ application/controllers/Action/Datausage.php
@@ -48,7 +48,7 @@ class DatausageAction extends Action_Base {
 		$counter = 0;
 		$accounts = array();
 		foreach ($results as $result) {
-			$accounts['aid'][$result['aid']]['subs'][$result['sid']] = Billrun_Util::byteFormat($result['balance']['totals']['data']['usagev'], 'MB', false, '', '');
+			$accounts['aid'][$result['aid']]['subs'][$result['sid']] = Billrun_Util::byteFormat($result['balance']['totals']['data']['usagev'], 'MB', 2, false, '.', '');
 			$counter++;
 		}
 
