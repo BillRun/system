a3c4b5cbca9b1d7b967f0301793135dd64efecb1
Ofer Cohen <oc666@netvision.net.il>
2014-09-20 04:07:38 +0300

    add ird support

---
+++ application/plugins/ird.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * @package         Billing
+ * @copyright       Copyright (C) 2012-2013 S.D.O.C. LTD. All rights reserved.
+ * @license         GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+/**
+ * IRD plugin for daily international roaming data (IRD)
+ *
+ * @package  Application
+ * @subpackage Plugins
+ * @since    2.8
+ */
+class irdPlugin extends Billrun_Plugin_BillrunPluginBase {
+
+	protected $ird_daily = null;
+
+	public function beforeUpdateSubscriberBalance($balance, $row, $rate, $calculator) {
+		if (isset($row['daily_ird']) || $row['daily_ird']) {
+			$this->daily_ird = true;
+		} else {
+			$this->daily_ird = false;
+		}
+	}
+
+	/**
+	 * method to override the plan group limits
+	 * 
+	 * @param type $rateUsageIncluded
+	 * @param type $groupSelected
+	 * @param type $limits
+	 * @param type $plan
+	 * @param type $usageType
+	 * @param type $rate
+	 * @param type $subscriberBalance
+	 * 
+	 * @todo need to verify when lines does not come in chronological order
+	 */
+	public function triggerPlanGroupRateRule(&$rateUsageIncluded, $groupSelected, $limits, $plan, $usageType, $rate, $subscriberBalance) {
+		if ($groupSelected != 'IRD') {
+			return;
+		}
+		if ($this->daily_ird) {
+			$rateUsageIncluded = 'UNLIMITED';
+		}
+	}
+
+}
