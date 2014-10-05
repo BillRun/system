85ef21eb91e8c0d5576d2dd485eb2c2b798849ba
Eran <etherpulse@gmail.com>
2014-10-05 18:26:19 +0300

    change the  tax  field name to sdr_tax

--- application/plugins/tap3.php
+++ application/plugins/tap3.php
@@ -203,8 +203,8 @@ class tap3Plugin extends Billrun_Plugin_BillrunPluginBase implements Billrun_Plu
 			$cdrLine['exchange_rate'] = $this->exchangeRates[Billrun_Util::getNestedArrayVal($cdrLine, $mapping['exchange_rate_code'],0)];
 		}
 		
-		if (Billrun_Util::getNestedArrayVal($cdrLine, $mapping['tax']) !== null) {
-			$cdrLine['tax'] = Billrun_Util::getNestedArrayVal($cdrLine, $mapping['tax']) / $this->sdr_division_value;			
+		if (Billrun_Util::getNestedArrayVal($cdrLine, $mapping['sdr_tax']) !== null) {
+			$cdrLine['sdr_tax'] = Billrun_Util::getNestedArrayVal($cdrLine, $mapping['sdr_tax']) / $this->sdr_division_value;			
 		}
 
 		//save the sending source in each of the lines
