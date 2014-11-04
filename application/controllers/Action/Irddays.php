fde774bba453c63c45e5d6931af928ea5b13860f
Ilya <ilya@billrun.net>
2014-11-04 11:17:02 +0200

    BILLRUN-669 api vodafone days based on fraud lines

--- application/controllers/Action/Irddays.php
+++ application/controllers/Action/Irddays.php
@@ -20,13 +20,75 @@ class IrddaysAction extends Action_Base {
          */
 public function execute() {
                 Billrun_Factory::log()->log("Execute ird days API call", Zend_Log::INFO);
-
+				$request = $this->getRequest();
+				$sid = intval($request->get("sid"));
+				$this_year = date("Y");
+				$ggsn_fields = Billrun_Factory::config()->getConfigValue('ggsn.fraud.groups.vodafone15');
+				$sender = Billrun_Factory::config()->getConfigValue('nrtrde.fraud.groups.vodafone15');
+				$match = array(
+					'$match' => array(
+						'subscriber_id' => $sid,
+						
+						'plan' => array('$in' => array('LARGE_PREMIUM','LARGE_KOSHER_PREMIUM')),
+						'$or' => array(
+									array_merge(
+										array(
+											'type' => "ggsn" , 
+											'record_opening_time' => new MongoRegex("/^$this_year/"),
+										),
+										$ggsn_fields
+									), 
+									array(
+										'type' => "nrtrde",
+										'callEventStartTimeStamp' => new MongoRegex("/^$this_year/"),
+										'sender' => array('$in' => $sender),
+										'$or' => array(
+											array(
+												'record_type' => "MTC"
+											),
+											array(
+												'record_type' => "MOC",
+													'connectedNumber' => new MongoRegex('/^972/')
+											)
+										)
+									),
+						),
+						
+							
+					)
+				);
+				$limit = array(
+					'$limit' => 10,
+				);
+				
+				$group = array (
+					'$group' => array(
+						'_id' => array('$substr' => 
+											array(
+												array('$ifNull' => array('$record_opening_time','$callEventStartTimeStamp')),
+												4,
+												4
+											)
+									),
+						count => array('$sum' => 1 ),
+					)
+				);
+				
+				$group2 = array (
+					'$group' => array(
+						'_id' => null,
+						count => array('$sum' => 1 ),
+					)
+				);
+				
+				$res = Billrun_Factory::db()->linesCollection()->aggregate($match,$group,$group2);
+				
                 $this->getController()->setOutput(array(
                         'status' => 1,
                         'desc' => 'success',
                         'input' => $post,
                         'details' => array(
-                                'days' => 0,
+                                'days' => $res[0]["count"],
                                 'min_day' => 40,
                                 'max_day' => 45,
                         )
