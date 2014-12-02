ec4a4ac81f0dbe5a8969221efa8e75ccae4d506b
Ofer Cohen <oc666@netvision.net.il>
2014-12-02 18:04:14 +0200

    add rat_type to ggsn unify (used to identify 3G/4G)

--- library/Billrun/Calculator/Unify.php
+++ library/Billrun/Calculator/Unify.php
@@ -50,12 +50,12 @@ class Billrun_Calculator_Unify extends Billrun_Calculator {
 					),
 					'date_seperation' => 'Ymd',
 					'stamp' => array(
-						'value' => array('sgsn_address', 'ggsn_address', 'sid', 'aid', 'arate', 'imsi', 'plan', 'rating_group', 'billrun'), 
+						'value' => array('sgsn_address', 'ggsn_address', 'sid', 'aid', 'arate', 'imsi', 'plan', 'rating_group', 'billrun', 'rat_type'), 
 						'field' => array('in_plan', 'out_plan', 'over_plan', 'aprice'),
 					),
 					'fields' => array(
 						'$set' => array('process_time'),
-						'$setOnInsert' => array('urt', 'imsi', 'usagesb', 'usaget', 'aid', 'sid', 'ggsn_address', 'sgsn_address', 'rating_group', 'arate', 'plan', 'billrun'),
+						'$setOnInsert' => array('urt', 'imsi', 'usagesb', 'usaget', 'aid', 'sid', 'ggsn_address', 'sgsn_address', 'rating_group', 'arate', 'plan', 'billrun', 'rat_type'),
 						'$inc' => array('usagev', 'aprice', 'apr', 'fbc_downlink_volume', 'fbc_uplink_volume', 'duration', 'in_plan', 'out_plan', 'over_plan'),
 					),
 				),
