d73aeec324b17f52f8c7924a9544ca88f9e02d59
Ilya <ilya@billrun.net>
2014-12-04 10:40:57 +0200

    removing unneeded files

--- scripts/maintenance/clean_cache.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-## script to clean all golan memcached servers
-
-echo 'flush_all' | nc con01.gt.local 11211
-echo 'flush_all' | nc con02.gt.local 11211
-echo 'flush_all' | nc con03.gt.local 11211 
