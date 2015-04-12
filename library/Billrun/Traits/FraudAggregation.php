a1610bacde73ea21a2ad8f6692b6a238d0d21798
Ofer Cohen <oc666@netvision.net.il>
2015-04-13 02:37:13 +0300

    revert last change

--- library/Billrun/Traits/FraudAggregation.php
+++ library/Billrun/Traits/FraudAggregation.php
@@ -64,8 +64,7 @@ trait Billrun_Traits_FraudAggregation {
 					$query['where']['$match'] = array_merge($query['where']['$match'], (isset($eventQuery['query']) ? $this->parseEventQuery($eventQuery['query']) : array()), (isset($eventRules['group_rules'][$groupName]) ? $this->parseEventQuery($eventRules['group_rules'][$groupName]) : array()));
 					$ruleMatch = array('$match' => (isset($eventQuery['match']) ? $eventQuery['match'] : array('value' => array('$gte' => intval($eventQuery['threshold']))) ));
 					
-					$pipeline = array($query['base_match'], $query['where'], $query['group_match'], $query['group'], $query['translate'], $query['project'], $ruleMatch);
-					$ret = $lines->aggregate($pipeline, array("allowDiskUse" => 1));
+					$ret = $lines->aggregate($query['base_match'], $query['where'], $query['group_match'], $query['group'], $query['translate'], $query['project'], $ruleMatch);
 
 					if ($this->postProcessEventResults($events, $ret, $eventQuery, $key)) {
 						$events = array_merge($events, $ret);
