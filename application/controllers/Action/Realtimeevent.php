925b9f51fbaed561926adfaef30e214d16041180
Ofer Cohen <oc666@netvision.net.il>
2015-07-20 00:13:14 +0300

    improve real-time processor

--- application/controllers/Action/Realtimeevent.php
+++ application/controllers/Action/Realtimeevent.php
@@ -25,51 +25,73 @@ class RealtimeeventAction extends ApiAction {
 		Billrun_Factory::log()->log("Execute realtime event", Zend_Log::INFO);
 //		$this->event = $this->getRequest()->getRequest();
 		$a = '{
-			"sessionId": "GyOCS.sasnlbumtsma0-0.pelephone.gy.lab;1378620500;536872634",
-				"ccRequestType": 1 ,
-				"ccRequestNumber": 415,
-			   "destinationHost": "dgu1.pelephone.gy.lab",
-			   "startTime": "20130908060820",
-				"MSCC" : {
-				   "used": 1048576 ,
-					   "requested": 1048576,
-					   "serviceIdentifier" : 400700,
-					   "ratingGroup": 92
-				   } ,
-				   "imsi":"425030024380232",
-				   "msisdn": "972502182928",
-				  "userEquipment" : {
-												 "type" : 0  ,
-												 "value" : "33353833393430343137363030323032"
-											  },
-				  "serviceContextId": "32251@3gpp.org",
-				 "Service-Information" : {
-					"calledStationId": "sphone.labpelephone.net.il",
-					"3GPPChargingCharacteristics": "0400",
-					"3GPPPDPType": "IPv4 (0)",
-					"3GPPGPRSNegotiatedQoSprofile":  "9-13921f73968db044524859",
-					"SGSNAddress": "00015b876003",
-					"3GPPNSAPI" : 5,
-					"3GPPSelection-Mode": 1,
-					"CGAddress": "000100000000",
-		"GGSNAddressFamily": 1,
-					"GGSNAddress": "91.135.96.162",
-		"3GPPIMSIMCCMNC": 42503,
-		"3GPPSGSNMCCMNC": 42503,
-		"3GPPMSTimeZone": 2101,
-		"PDPAddress": "00010aa12d0",
-		"3GPPUserLocationInfo": {
-						  "geographicLocationType":1,
-						  "MCC" : "425",
-						 "MNC" :"03" ,
-						 "locationAreaCode": 6101,
-						 "serviceAreaCode": 1046        
-		 },
-		"3GPPRATType": "01"
-        }}';
+			"sessionId":"GyOCS.sasnlbumtsma0-0.pelephone.gy.lab;1378620500;536872634",
+			"ccRequestType":1,
+			"ccRequestNumber":415,
+			"destinationHost":"dgu1.pelephone.gy.lab",
+			"startTime":"20130908060820",
+			"MSCC":{
+				"used":1048576,
+				"requested":1048576,
+				"serviceIdentifier":400700,
+				"ratingGroup":92
+			},
+			"imsi":"425030024380232",
+			"msisdn":"972502182928",
+			"userEquipment":{
+				"type":0,
+				"value":"33353833393430343137363030323032"
+			},
+			"serviceContextId":"32251@3gpp.org",
+			"Service-Information":{
+				"calledStationId":"sphone.labpelephone.net.il",
+				"3GPPChargingCharacteristics":"0400",
+				"3GPPPDPType":"IPv4 (0)",
+				"3GPPGPRSNegotiatedQoSprofile":"9-13921f73968db044524859",
+				"SGSNAddress":"00015b876003",
+				"3GPPNSAPI":5,
+				"3GPPSelection-Mode":1,
+				"CGAddress":"000100000000",
+				"GGSNAddressFamily":1,
+				"GGSNAddress":"91.135.96.162",
+				"3GPPIMSIMCCMNC":42503,
+				"3GPPSGSNMCCMNC":42503,
+				"3GPPMSTimeZone":2101,
+				"PDPAddress":"00010aa12d0",
+				"3GPPUserLocationInfo":{
+					"geographicLocationType":1,
+					"MCC":"425",
+					"MNC":"03",
+					"locationAreaCode":6101,
+					"serviceAreaCode":1046
+				},
+				"3GPPRATType":"01"
+			}
+		}';
 		$this->event = @json_decode($a, JSON_OBJECT_AS_ARRAY);
 		$this->event['source'] = 'realtime';
-		$this->event['type'] = 'data';
+		$this->event['type'] = 'gy';
+		
+		if (isset($this->event['Service-Information']['SGSNAddress'])) {
+			$this->event['sgsn_address'] = long2ip(hexdec($this->event['Service-Information']['SGSNAddress']));
+		} else {
+			$sgsn_dec = hexdec($this->event['SGSNAddress']);
+			if (is_numeric($sgsn_dec)) {
+				$this->event['sgsn_address'] = long2ip($sgsn_dec);
+			}
+		}
+		
+		if (isset($this->event['GGSNAddress'])) {
+			$this->event['ggsn_address'] = $this->event['GGSNAddress'];
+			unset($this->event['GGSNAddress']);
+		}
+
+		if (isset($this->event['startTime'])) {
+			$this->event['record_opening_time'] = $this->event['startTime'];
+			unset($this->event['startTime']);
+		}
+		
+		$this->event['urt'] = new MongoDate(strtotime($this->event['record_opening_time']));
 
 		$options = array(
 			'type' => 'Realtime',
---
+++ library/Billrun/Calculator/Rate/Gy.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * @package         Billing
+ * @copyright       Copyright (C) 2012-2015 S.D.O.C. LTD. All rights reserved.
+ * @license         GNU Affero General Public License Version 3; see LICENSE.txt
+ */
+
+/**
+ * Billing calculator rate class
+ * The class is basic rate that can evaluate record rate by different factors
+ * 
+ * @package  calculator
+ * @since    4.0
+ * @todo Merge to general internet data rate calculator
+ *
+ */
+class Billrun_Calculator_Rate_Gy extends Billrun_Calculator_Rate_Ggsn {
+
+	/**
+	 * the type of the object
+	 *
+	 * @var string
+	 */
+	static protected $type = 'gy';
+
+	/**
+	 * @see Billrun_Calculator_Rate::getLineVolume
+	 */
+	protected function getLineVolume($row, $usage_type) {
+		return $row['MSCC']['used'];
+	}
+
+	/**
+	 * @see Billrun_Calculator_Rate::getLineUsageType
+	 */
+	protected function getLineUsageType($row) {
+		return 'data';
+	}
+
+	/**
+	 * @see Billrun_Calculator_Rate::getLineRate
+	 */
+	protected function getLineRate($row, $usage_type) {
+		$line_time = $row['urt'];
+		foreach ($this->rates as $rate) {
+			if (preg_match($rate['params']['sgsn_addresses'], $row['sgsn_address']) && $rate['from'] <= $line_time && $line_time <= $rate['to']) {
+				return $rate;
+			}
+		}
+		Billrun_Factory::log()->log("Couldn't find rate for row : " . print_r($row['stamp'], 1), Zend_Log::DEBUG);
+		return FALSE;
+	}
+
+}
