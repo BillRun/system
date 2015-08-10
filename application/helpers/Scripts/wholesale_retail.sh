ef570b7856c179b56e4ccc4b2c2153ca3d62c061
Ofer Cohen <oc666@netvision.net.il>
2015-08-10 10:35:20 +0300

    fix minor regression

--- application/helpers/Scripts/wholesale_retail.sh
+++ application/helpers/Scripts/wholesale_retail.sh
@@ -18,6 +18,8 @@ else
 	output_dir="${script_dir}/../../../files/wholesale";
 fi
 
+js_code='db.getMongo().setReadPref("secondaryPreferred");'
+
 js_code=$js_code'db.lines.aggregate({$match:{urt:{$gte:ISODate("'$day'T00:00:00+03:00"),$lt:ISODate("'$day'T23:59:59+03:00")},$or:[{over_plan:{$exists:1}},{out_plan:{$exists:1}}]}},{$project:{_id:0,over_aprice:{$cond: [{$gt: ["$over_plan", 0]}, "$aprice", 0]},out_aprice:{$cond: [{$gt: ["$out_plan", 0]}, "$aprice", 0]}}},{$group:{_id:0,over:{$sum:"$over_aprice"},out:{$sum:"$out_aprice"}}}).forEach(function (obj){print("'$day'" + "\t" + obj.over + "\t" + obj.out)});';
 
 mongo billing -ureading -pguprgri --quiet --eval "$js_code" >> "$output_dir/extra.csv" ;
