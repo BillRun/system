b10a3f5ed25ffee48a1476b1d355e09544b0bfb8
Ofer Cohen <oc666@netvision.net.il>
2015-08-13 20:23:21 +0300

    remove debug code

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -15,17 +15,6 @@
 class IndexController extends Yaf_Controller_Abstract {
 
 	public function indexAction() {
-		$a = range("1", "1000");
-		$c = array();
-		foreach ($a as $b) {
-			$c["a"] = $b;
-		}
-		$t = microtime(true);
-		for ($i=0; $i<=1; $i++) {
-			serialize(unserialize($b));
-		}
-		
-		die((microtime(true) - $t) . "<br />END");
 		$this->redirect('admin');
 		$this->getView()->title = "BillRun | The best open source billing system";
 		$this->getView()->content = "Open Source Last Forever!";
