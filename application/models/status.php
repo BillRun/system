61c76e65bacc8fa5070921e229f4546d4f2cce7a
Ilya <ilya@billrun.net>
2015-08-20 19:56:18 +0300

    BILLRUN-742 add admin page with nrtrde status and last file received

---
+++ application/models/status.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * @package         Billing
+ * @copyright       Copyright (C) 2015 S.D.O.C. LTD. All rights reserved.
+ * @license         GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+/**
+ * Status model class
+ *
+ * @package     Models
+ * @since       1.0
+ */
+class statusModel {
+
+/*
+ * method to test the fraud ftp connection
+ */
+	
+	public function testFtpConnection() {
+			$config= Billrun_Factory::config()->getConfigValue('nrtrde.ftp');
+			
+			$this->ftp = Zend_Ftp::connect($config['host'], $config['user'], $config['password']);
+			$this->ftp->setPassive(isset($config['passive']) ? $config['passive'] : false);
+			return $this->ftp->isConnected();
+	}
+/*
+ * method to query for the last file recieved
+ */	
+	public function lastFile() {
+		$last_file = Billrun_Factory::db()->logCollection()->
+					query(array('source' => 'nrtrde', 'received_time' => array('$exists' => true)))->cursor()->
+					sort(array('received_time' => -1))->limit(1)->current();
+		return array('file_name' => $last_file['file_name'],'received_time' => $last_file['received_time']) ;
+	}
+}
