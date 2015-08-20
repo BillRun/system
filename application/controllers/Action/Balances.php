8feaf257f78f98d4bcbf9e68e0446d8d862eb5b4
tomf <tom@billrun.net>
2015-08-20 19:00:26 +0300

    Added the Balances module for the API action

---
+++ application/controllers/Action/Balances.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @package         Billing
+ * @copyright       Copyright (C) 2012-2013 S.D.O.C. LTD. All rights reserved.
+ * @license         GNU Affero General Public License Version 3; see LICENSE.txt
+ */
+require_once APPLICATION_PATH . '/application/controllers/Action/Api.php';
+
+/**
+ * This class holds the balances logic for the subscribers.
+ *
+ * @package     Controllers
+ * @subpackage  Action
+ * @since       4.0
+*/
+class BalancesAction extends ApiAction{
+	
+	/**
+	 * The logic to be executed when this API plugin is called.
+	 */
+	public function execute() {
+		$request = $this->getRequest();
+
+		// This is the method which is going to be executed.
+		$action = Billrun_Balances_Actions_Manager::getSubscriberAction($request);
+		
+		// Check that received a valid action.
+		if(!$action) {
+			// TODO: Report failed action. What do i write to the output if this happens?
+			Billrun_Factory::log("Failed to get balances action instance for received input", Zend_Log::ALERT);
+			return;
+		}
+		
+		$output = $action->execute();
+		
+		// Set the raw input.
+		$output['input'] = $request->getRequest();
+		
+		$this->getController()->setOutput(array($output));
+	}
+}
