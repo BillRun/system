e7f25dae224ec32dfc36ef4128e1c6682ecde44d
tomf <tom@billrun.net>
2015-08-25 14:44:31 +0300

    Added support in getting balance by subscriber+prepaid include

--- library/Billrun/ActionManagers/Balances/Query.php
+++ library/Billrun/ActionManagers/Balances/Query.php
@@ -26,6 +26,12 @@ class Billrun_ActionManagers_Balances_Query extends Billrun_ActionManagers_Balan
 	protected $queryInRange = false;
 	
 	/**
+	 * Query for projecting the balance.
+	 * @var type 
+	 */
+	protected $balancesProjection = array();
+		
+	/**
 	 */
 	public function __construct() {
 		parent::__construct();
@@ -86,12 +92,23 @@ class Billrun_ActionManagers_Balances_Query extends Billrun_ActionManagers_Balan
 		$to = $input->get('to');
 		$from = $input->get('from');
 		if($to && $from) {
-			$this->balancesQuery['to'] =
-				array('$lte' => new MongoTimestamp($to));
-			$this->balancesQuery['from'] = 
-				array('$gte' => new MongoTimestamp($from));
+			$this->setDateParameters($to, $from, $this->balancesQuery);
 			$this->queryInRange = true;
 		}
+	}
+	
+	/**
+	 * Set date parameters to a query.
+	 * are not null.
+	 * @param type $to - To pramter.
+	 * @param type $from - From parameter.
+	 * @param type $query - Query to set the date in.
+	 */
+	protected function setDateParameters($to, $from, $query) {
+		$query['to'] =
+			array('$lte' => new MongoTimestamp($to));
+		$query['from'] = 
+			array('$gte' => new MongoTimestamp($from));
 	}
 	
 	/**
@@ -106,16 +123,85 @@ class Billrun_ActionManagers_Balances_Query extends Billrun_ActionManagers_Balan
 			return false;
 		}
 		
-//		$accountName = $input->get('name');
-//		if(empty($sid)) {
-//			Billrun_Factory::log("Balances Query receieved no account name!", Zend_Log::NOTICE);
-//			return false;
-//		}
-		
 		$this->balancesQuery = 
 			array('sid'	=> $sid);
 		
 		$this->parseDateParameters($input);
+				
+		// Set the prepaid filter data.
+		if(!$this->createFieldFilterQuery($input)) {
+			return false;
+		}
+		
+		return true;
+	}
+	
+	/**
+	 * Create the query to filter only the required fields from the record.
+	 * @param type $input
+	 */
+	protected function createFieldFilterQuery($input) {
+		$prepaidQuery = $this->getPrepaidQuery($input);
+		
+		// Check if received both external_id and name.
+		if(count($prepaidQuery) > 1) {
+			Billrun_Factory::log("Received both external id and name in balances query, specify one or none.", Zend_Log::ERR);
+			return false;
+		}
+		// If empty it means that there is no filtering to be done.
+		else if(empty($prepaidQuery)) {
+			return true;
+		}
+		
+		// Set to and from if exists.
+		if(isset($this->balancesQuery['to']) && isset($this->balancesQuery['from'])) {
+			$this->setDateParameters($this->balancesQuery['to'], $this->balancesQuery['from'], $prepaidQuery);
+		}
+		
+		if(!$this->setPrepaidDataToQuery($prepaidQuery)) {
+			return false;
+		}
+		
+		return true;
+	}
+	
+	/**
+	 * Get the mongo query to run on the prepaid collection.
+	 * @param type $input
+	 * @return type
+	 */
+	protected function getPrepaidQuery($input) {
+		$prepaidQuery = array();
+		
+		$accountName = $input->get('pp_includes_name');
+		if(!empty($accountName)) {
+			$prepaidQuery['name'] = $accountName;
+		}
+		$accountExtrenalId = $input->get('pp_includes_external_id');
+		if(!empty($accountExtrenalId)) {
+			$prepaidQuery['external_id '] = $accountExtrenalId;
+		}
+		
+		return $prepaidQuery;
+	}
+	
+	protected function setPrepaidDataToQuery($prepaidQuery) {
+		// Get the prepaid record.
+		$prepaidCollection = Billrun_Factory::db()->prepaidIncludesCollection();
+		
+		// TODO: Use the prepaid DB/API proxy.
+		$prepaidRecord = $prepaidCollection->query($prepaidQuery)->cursor()->current();
+		if(!$prepaidRecord || $prepaidRecord->isEmpty()) {
+			Billrun_Factory::log("Failed to get prepaid record.", Zend_Log::NOTICE);
+			return false;
+		}
+		
+		// TODO: Check if they are set? Better to have a prepaid record object with this functionallity.
+		$chargingBy = $prepaidRecord['charging_by'];
+		$chargingByUsegt = $prepaidRecord['charging_by_usaget'];
+
+		$this->balancesQuery['charging_by'] = $chargingBy;
+		$this->balancesQuery['charging_by_usaget'] = $chargingByUsegt;
 		
 		return true;
 	}
