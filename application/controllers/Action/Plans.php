bf6339ebcf9073b1767cb217938449cb8934b76f
tomf <tom@billrun.net>
2015-08-27 17:37:16 +0300

    Reverted the compound param changes

--- application/controllers/Action/Plans.php
+++ application/controllers/Action/Plans.php
@@ -14,14 +14,68 @@ require_once APPLICATION_PATH . '/application/controllers/Action/Api.php';
  * 
  * @since    2.6
  */
-class PlansAction extends Billrun_API_CompoundParamAction {
+class PlansAction extends ApiAction {
+
+	public function execute() {
+		Billrun_Factory::log("Execute plans api call", Zend_Log::INFO);
+		$request = $this->getRequest();
+
+		// If no query received, using empty array as default. 
+		// TODO: Is this correct? or should an error be raised if no query received?
+		$requestedQuery = $request->get('query', array());
+		$query = $this->processQuery($requestedQuery);
+		$strip = $this->getCompundParam($request->get('strip', false), false);
+		$filter = !empty($strip) ? $strip : array('name');
+
+		$cacheParams = array(
+			'fetchParams' => array(
+				'query' => $query,
+				'filter' => $filter,
+				'strip' => $strip,
+			),
+			'stampParams' => array($requestedQuery, $filter, $strip),
+		);
+
+		$this->setCacheLifeTime(Billrun_Utils_TimerUtils::daysToSeconds(1)); 
+		$results = $this->cache($cacheParams);
+		
+		$this->getController()->setOutput(array(array(
+				'status' => 1,
+				'desc' => 'success',
+				'details' => $results,
+				'input' => $request->getRequest(),
+			)));
+	}
+	
 	/**
-	 * Return data from received resource.
-	 * @param Mongodloid_Cursor $resource - Resource to extract data from.
-	 * @return array Array of results.
+	 * basic fetch data method used by the cache
+	 * 
+	 * @param array $params parameters to fetch the data
+	 * 
+	 * @return boolean
 	 */
-	protected function getModelDataFromResource($resource) {
-		$results = null;
+	protected function fetchData($params) {
+		if (is_null($params)) {
+			$params = array();
+		}
+		if (!isset($params['query'])) {
+			$params['query'] = array();
+		}
+		$params['query']['$or'] = array(
+				array(
+					'hiddenFromApi' => array(
+						'$exists' => 0,
+					)
+				),
+				array(
+					'hiddenFromApi' => false
+				),
+				array(
+					'hiddenFromApi' => 0
+				)
+		);
+		$model = new PlansModel(array('sort' => array('from' => 1)));
+		$resource = $model->getData($params['query'], $params['filter']);
 		if (is_resource($resource)) {
 			$results = iterator_to_array($resource);
 		} else if ($resource instanceof Mongodloid_Cursor) {
@@ -30,27 +84,96 @@ class PlansAction extends Billrun_API_CompoundParamAction {
 				$results[] = $item->getRawData();
 			}
 		}
-		
+		if (isset($params['strip']) && !empty($params['strip'])) {
+			$results = $this->stripResults($results, $params['strip']);
+		}
 		return $results;
-	}
-	
-	/**
-	 * Fetch results from the related model using the fetch data params.
-	 * @param array $params - Fetch data params.
-	 * @return array of results from the model.
-	 */
-	protected function fetchDataFromModel($params) {
-		$model = new PlansModel(array('sort' => array('from' => 1)));
-		$resource = $model->getData($params['query'], $params['filter']);
-		
-		return $this->getModelDataFromResource($resource);
+
 	}
 
 	/**
-	 * This function is called prior to the execute function.
+	 * Process the query and prepere it for usage by the Plans model
+	 * @param type $query the query that was recevied from the http request.
+	 * @return array containing the processed query.
 	 */
-	protected function preExecute() {
-		Billrun_Factory::log("Execute plans api call", Zend_Log::INFO);
+	protected function processQuery($query) {
+		$retQuery = $this->getCompundParam($query, array());
+
+		// TODO: This code appears multiple times in the project, 
+		// should be moved to a more general class.
+		if (!isset($retQuery['from'])) {
+			$retQuery['from']['$lte'] = new MongoDate();
+		} else {
+			$retQuery['from'] = $this->intToMongoDate($retQuery['from']);
+		}
+		if (!isset($retQuery['to'])) {
+			$retQuery['to']['$gte'] = new MongoDate();
+		} else {
+			$retQuery['to'] = $this->intToMongoDate($retQuery['to']);
+		}
+		
+		return $retQuery;
+	}
+
+	/**
+	 * Change numeric references to MongoDate object in a given filed in an array.
+	 * @param MongoDate $arr 
+	 * @param type $fieldName the filed in the array to alter
+	 * @return the translated array
+	 */
+	protected function intToMongoDate($arr) {
+		if (is_array($arr)) {
+			foreach ($arr as $key => $value) {
+				if (is_numeric($value)) {
+					$arr[$key] = new MongoDate((int) $value);
+				}
+			}
+		} else if (is_numeric($arr)) {
+			$arr = new MongoDate((int) $arr);
+		}
+		return $arr;
+	}
+
+	/**
+	 * 
+	 * @param type $results
+	 * @param type $strip
+	 * @return type
+	 * TODO: This function is found in the project multiple times, should be moved to a better location.
+	 */
+	protected function stripResults($results, $strip) {
+		$stripped = array();
+		foreach ($strip as $field) {
+			foreach ($results as $rate) {
+				if (isset($rate[$field])) {
+					if (is_array($rate[$field])) {
+						$stripped[$field] = array_merge(isset($stripped[$field]) ? $stripped[$field] : array(), $rate[$field]);
+					} else {
+						$stripped[$field][] = $rate[$field];
+					}
+				}
+			}
+		}
+		return $stripped;
+	}
+
+	/**
+	 * process a compund http parameter (an array)
+	 * @param type $param the parameter that was passed by the http;
+	 * @return type
+	 */
+	protected function getCompundParam($param, $retParam = array()) {
+		if(isset($param)) {
+			$retParam =  $param ;
+			if ($param !== FALSE) {
+				if (is_string($param)) {
+					$retParam = json_decode($param, true);
+				} else {
+					$retParam = (array) $param;
+				}
+			}
+		}
+		return $retParam;
 	}
 
 }
