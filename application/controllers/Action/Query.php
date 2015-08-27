a08db2038713807b8d669a96bbca2656f7c16665
tomf <tom@billrun.net>
2015-08-27 10:26:42 +0300

    Refactored the ActionQuery API to remove TODOs

--- application/controllers/Action/Query.php
+++ application/controllers/Action/Query.php
@@ -16,6 +16,134 @@ require_once APPLICATION_PATH . '/application/controllers/Action/Api.php';
  */
 class QueryAction extends ApiAction {
 
+	const MAX_LIST = 1000;
+	
+	/**
+	 * Get the count limit query part.
+	 * @param array $request - Input request.
+	 * @return boolean Array of queries according to received max size, false if invalid.
+	 */
+	protected function getMaxListQuery($request) {
+		$returnQuery = array();
+		
+		$fieldsToCheck = array('aid', 'sid');
+		
+		foreach ($fieldsToCheck as $field) {
+			if (!isset($request[$field])) {
+				continue;
+			}
+			
+			$queryLimitResults = $this->getQueryLimitForField($request, $field);
+
+			// Error occured.
+			if($queryLimitResults === false) {
+				return false;
+			}
+
+			$returnQuery[$field] = $queryLimitResults;
+		}
+		
+		return $returnQuery;
+	}
+	
+	/**
+	 * Get the query for limiting results of list.
+	 * @param array $request - Input request.
+	 * @param string $paramName - Param name to limit by.
+	 * @return array - Query for limiting, or false if error occured.
+	 */
+	protected function getQueryLimitForField($request, $paramName) {
+		$verifiedArray = Billrun_Util::verify_array($request[$paramName], 'int');
+		if (count($verifiedArray) > self::MAX_LIST) {
+			$this->setError('Maximum of '. $paramName . ' is ' . self::MAX_LIST, $request);
+			return false;
+		}
+		
+		return array('$in' => $verifiedArray);
+	}
+	
+	/**
+	 * Build the query for the api exectie based on the input request.
+	 * @param array $request - Input request array.
+	 * @return array The array to use for the query execute, false if error occured.
+	 */
+	protected function buildQuery($request) {
+		$executeQuery = $this->getMaxListQuery($request);
+		// Error occured.
+		if($executeQuery === false) {
+			// TODO: Return true on purpose? 
+			return false;
+		}
+		
+		if (isset($request['billrun'])) {
+			$executeQuery['billrun'] = $this->getBillrunQuery($request['billrun']);
+		}
+
+		if (isset($request['query'])) {
+			$inputRequestQuery = $this->getArrayParam($request['query']);
+			$executeQuery = array_merge($executeQuery, (array) $inputRequestQuery);
+		}
+		
+		return $executeQuery;
+	}
+	
+	/**
+	 * Get all the lines for the input query.
+	 * @param array $request - Input request array.
+	 * @return array of lines to return as result, false if error occurred.
+	 */
+	protected function getResultLines($request) {
+		$executeQuery = $this->buildQuery($request);
+		// Error occured.
+		if($executeQuery === false) {
+			// TODO: Return true on purpose? 
+			return false;
+		}
+		
+		$model = new LinesModel($this->getQueryOptions($request));
+		$lines = null;
+		if (isset($request['distinct'])) {
+			$lines = $model->getDistinctField((string) $request['distinct'], $executeQuery);
+		} else {
+			$lines = $model->getData($executeQuery);
+			foreach ($lines as &$line) {
+				$line = $line->getRawData();
+			}
+		}
+		
+		return $lines;
+	}
+	
+	/**
+	 * Get the array of options to use for the query.
+	 * @param array $request - Input request array.
+	 * @return array Options array for the query.
+	 */
+	protected function getQueryOptions($request) {
+		return array(
+			'sort' => array('urt'),
+			'page' => isset($request['page']) && $request['page'] > 0 ? (int) $request['page']: 0,
+			'size' =>isset($request['size']) && $request['size'] > 0 ? (int) $request['size']: 1000,
+			);
+	}
+	
+	/**
+	 * Send the results to the controller.
+	 * @param array $request - Input array request.
+	 * @param array $result - Array of results for the requested query.
+	 */
+	protected function sendResults($request, $result) {
+		$ret = array(
+			array(
+				'status' => 1,
+				'desc' => 'success',
+				'input' => $request,
+				'details' => $result,
+			)
+		);
+		$this->getController()->setOutput($ret);
+	}
+	
 	/**
 	 * method to execute the query
 	 * it's called automatically by the api main controller
@@ -27,67 +155,17 @@ class QueryAction extends ApiAction {
 
 		if (!isset($request['aid']) && !isset($request['sid'])) {
 			$this->setError('Require to supply aid or sid', $request);
-			// TODO: Return true on purpose? 
-			return true;
+			return false;
 		}
 		
-		$find = array();
-		$max_list = 1000;
-		
-		if (isset($request['aid'])) {
-			$aids = Billrun_Util::verify_array($request['aid'], 'int');
-			if (count($aids) > $max_list) {
-				$this->setError('Maximum of aid is ' . $max_list, $request);
-				return true;
-			}
-			$find['aid'] = array('$in' => $aids);
-		}
-
-		if (isset($request['sid'])) {
-			$sids = Billrun_Util::verify_array($request['sid'], 'int');
-			if (count($sids) > $max_list) {
-				$this->setError('Maximum of sid is ' . $max_list, $request);
-				return true;
-			}
-			$find['sid'] = array('$in' => $sids);
-		}
-
-		if (isset($request['billrun'])) {
-			$find['billrun'] = $this->getBillrunQuery($request['billrun']);
-		}
-
-		if (isset($request['query'])) {
-			$query = $this->getArrayParam($request['query']);
-			$find = array_merge($find, (array) $query);
-		}
-
-		$options = array(
-			'sort' => array('urt'),
-			'page' => isset($request['page']) && $request['page'] > 0 ? (int) $request['page']: 0,
-			'size' =>isset($request['size']) && $request['size'] > 0 ? (int) $request['size']: 1000,
-		);
-		$model = new LinesModel($options);
-		
-		if (isset($request['distinct'])) {
-			$lines = $model->getDistinctField((string) $request['distinct'], $find);
-		} else {
-			$lines = $model->getData($find);
-
-			foreach ($lines as &$line) {
-				$line = $line->getRawData();
-			}
+		$lines = $this->getResultLines($request);
+		// Error occured.
+		if($lines === false) {
+			return false;
 		}
 
 		Billrun_Factory::log("query success", Zend_Log::INFO);
-		$ret = array(
-			array(
-				'status' => 1,
-				'desc' => 'success',
-				'input' => $request,
-				'details' => $lines,
-			)
-		);
-		$this->getController()->setOutput($ret);
+		$this->sendResults($request, $lines);
 	}
 	
 	/**
@@ -113,6 +191,11 @@ class QueryAction extends ApiAction {
 		return $ret;
 	}
 	
+	/**
+	 * Get query based on an array of values.
+	 * @param array $billrun - Array of values to build the query from.
+	 * @return array Result query.
+	 */
 	protected function getBillrunQuery($billrun) {
 		return array('$in' => Billrun_Util::verify_array($this->getArrayParam($billrun), 'str'));
 	}
