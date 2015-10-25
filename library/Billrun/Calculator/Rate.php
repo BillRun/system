2fa5ac16edddfb805683b43e7c75e7a9a4a1c529
Ilya <ilya@billrun.net>
2015-10-25 11:47:41 +0200

    BILLRUN-801 add rate_key to lines

--- library/Billrun/Calculator/Rate.php
+++ library/Billrun/Calculator/Rate.php
@@ -13,6 +13,7 @@
 abstract class Billrun_Calculator_Rate extends Billrun_Calculator {
 
 	const DEF_CALC_DB_FIELD = 'arate';
+	const DEF_RATE_KEY_DB_FIELD = 'arate_key';
 	const DEF_APR_DB_FIELD = 'apr';
 	
 
@@ -35,6 +36,7 @@ abstract class Billrun_Calculator_Rate extends Billrun_Calculator {
 	 * @var string
 	 */
 	protected $ratingField = self::DEF_CALC_DB_FIELD;
+	protected $ratingKyeField = self::DEF_RATE_KEY_DB_FIELD;
 	protected $pricingField = Billrun_Calculator_CustomerPricing::DEF_CALC_DB_FIELD;
 	protected $aprField = self::DEF_APR_DB_FIELD;
 
@@ -96,7 +98,7 @@ abstract class Billrun_Calculator_Rate extends Billrun_Calculator {
 	public function writeLine($line, $dataKey) {
 		Billrun_Factory::dispatcher()->trigger('beforeCalculatorWriteLine', array('data' => $line, 'calculator' => $this));
 		$save = array();
-		$saveProperties = array($this->ratingField, 'usaget', 'usagev', $this->pricingField, $this->aprField);
+		$saveProperties = array($this->ratingField,$this->ratingKyeField, 'usaget', 'usagev', $this->pricingField, $this->aprField,);
 		foreach ($saveProperties as $p) {
 			if (!is_null($val = $line->get($p, true))) {
 				$save['$set'][$p] = $val;
@@ -149,6 +151,7 @@ abstract class Billrun_Calculator_Rate extends Billrun_Calculator {
 			'usaget' => $usage_type,
 			'usagev' => $volume,
 			$this->ratingField => $rate ? $rate->createRef() : $rate,
+			$this->ratingKyeField => $rate ? $rate['key'] : null,
 		);
 		if ($rate) {
 			$added_values[$this->aprField] = Billrun_Calculator_CustomerPricing::getPriceByRate($rate, $usage_type, $volume);
