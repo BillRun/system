7f6ab08c354551c9f365121e69c685c10ccecefb
Almog Lewis Nitzberg <lewis@billrun.net>
2015-11-29 18:28:50 +0200

    replace underscore

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -798,7 +798,7 @@ class AdminController extends Yaf_Controller_Abstract {
 	}
 
 	protected function buildTableComponent($table, $filter_query, $options = array()) {
-		$this->title = preg_replace('/_/', ' ', ucfirst($table));
+		$this->title = str_replace('_', ' ', ucfirst($table));
 
 		// TODO: use ready pager/paginiation class (zend? joomla?) with auto print
 		$basic_params = array(
