4926015c2d07919a78b9bef9ff1e152861af9c2b
Ofer Cohen <oc666@netvision.net.il>
2015-12-19 20:56:36 +0200

    fix pricing script and export

---
+++ mongo/pl/plans.js
@@ -0,0 +1,14 @@
+// migrate customer plans (COS)
+db.tmp_COS.find().forEach(
+	function(obj20) {
+		db.plans.insert({
+			name:obj20.COS_NAME,
+			from:ISODate('2015-12-01'),
+			to:ISODate('2099-12-31 23:59:59'),
+			type:'customer',
+			external_id: obj20.COS_ID,
+			external_code: obj20.COS_CODE,
+		});
+	}
+);
+
