189cd79de2b3337aaeb482c4bc73e00bdea9e6f3
Ofer Cohen <oc666@netvision.net.il>
2015-12-30 23:00:08 +0200

    change write concern to be majority

--- library/Billrun/Calculator/CustomerPricing.php
+++ library/Billrun/Calculator/CustomerPricing.php
@@ -574,7 +574,7 @@ class Billrun_Calculator_CustomerPricing extends Billrun_Calculator {
 		if ($row['charging_type'] === 'postpaid') {
 			$update['$set']['balance.cost'] = $balanceRaw['balance']['cost'] + $pricingData[$this->pricingField];
 		}
-		$options = array('w' => 1);
+//		$options = array('w' => 'majority');
 		Billrun_Factory::log("Updating balance " . $balance_id . " of subscriber " . $row['sid'], Zend_Log::DEBUG);
 		Billrun_Factory::dispatcher()->trigger('beforeCommitSubscriberBalance', array(&$row, &$pricingData, &$query, &$update, $rate, $this));
 		$ret = $this->balances->update($query, $update, $options);
