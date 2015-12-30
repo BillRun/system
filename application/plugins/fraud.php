a311bda295a8f9a03764be2d758a178104325144
Eran <etherpulse@gmail.com>
2015-12-30 13:42:00 +0200

    Fix for updated ips that shuuld be deleivered to prod

--- application/plugins/fraud.php
+++ application/plugins/fraud.php
@@ -469,7 +469,7 @@ class fraudPlugin extends Billrun_Plugin_BillrunPluginBase {
 	protected function insertRoamingGgsn($lines) {
 		$roamingLines = array();
 		foreach ($lines as $line) {
-			if (!preg_match('/^(?=62\.90\.|37\.26\.|176\.12\.158\.(\d$|[1]\d$|2[10]$))/', $line['sgsn_address'])) {
+			if (!preg_match('/^(?=62\.90\.|37\.26\.|85\.64\.|172\.28\.|176\.12\.158\.)/', $line['sgsn_address'])) {
 				$roamingLines[] = $line;
 			}
 		}
