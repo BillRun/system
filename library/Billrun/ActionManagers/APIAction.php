1dba442339d64c569dd0a6312627881eda00cb52
tomf <tom@billrun.net>
2016-01-03 15:36:18 +0200

    Now using the errorReporter trait

--- library/Billrun/ActionManagers/APIAction.php
+++ library/Billrun/ActionManagers/APIAction.php
@@ -13,12 +13,7 @@
  */
 abstract class Billrun_ActionManagers_APIAction {
 	
-	/**
-	 * This members holds the error message to be reported.
-	 */
-	protected $error = "Successful";
-	protected $errorCode = 0;
-	protected $errors = array();
+	use Billrun_ActionManagers_ErrorReporter;
 
 	protected function __construct($params) {
 		if (isset($params['error'])) {
@@ -26,44 +21,4 @@ abstract class Billrun_ActionManagers_APIAction {
 		}
 		$this->errors = Billrun_Factory::config()->getConfigValue('errors', array());
 	}
-	
-	/**
-	 * Get the current error of the action.
-	 * @return string the description for the current error.
-	 */
-	public function getError() {
-		return $this->error;
-	}
-	
-	public function getErrorCode() {
-		return $this->errorCode;
-	}
-
-	
-	/**
-	 * Report an error.
-	 * @param int $errorCode - Error index to report.
-	 * @param Zend_Log_Filter_Priority $errorLevel
-	 */
-	protected function reportError($errorCode, $errorLevel=Zend_Log::INFO, array $args = array()) {
-		if (!is_numeric($errorCode)) {
-			$this->error = $errorCode;
-			$this->errorCode = -1;
-		} else {
-			if (isset($this->errors[$errorCode])) {
-				$this->error = $this->errors[$errorCode];
-				
-				if(!empty($args)) {
-					$this->error=vsprintf($this->error, $args);
-				}
-				
-				$this->errorCode = $errorCode;
-			} else {
-				$this->error = 'Unknown issue';
-				$this->errorCode = 999999;
-			}
-		}
-		Billrun_Factory::log($this->errorCode . ": " . $this->error, $errorLevel);
-	}
-	
 }
