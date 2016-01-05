495baba64fa1c0784c1a46e1234d023c101efbfb
Roman <roman@billrun.net>
2016-01-05 21:47:11 +0200

    Utest for AddBalance + Call + Data + Bootstrap UI

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -20,6 +20,7 @@ class TestController extends Yaf_Controller_Abstract {
 	 * @var string
 	 */
 	protected $baseUrl = '';
+	protected $baseApiUrl = '';
 	
     /**
 	 * unique ref for Data and API calls
@@ -37,8 +38,9 @@ class TestController extends Yaf_Controller_Abstract {
 			Billrun_Factory::log('Exit Unit testing. Unit testing not allowed on production');
 			die();
 		}
-		$this->baseUrl = "http://" . gethostname() . $this->getRequest()->getBaseUri() . '/api';
+		$this->baseApiUrl = "http://" . gethostname() . $this->getRequest()->getBaseUri() . '/api';
 		$this->reference = rand(1000000000, 9999999999);
+		$this->baseUrl = $this->getRequest()->getBaseUri();
 	}
 
 	/**
@@ -47,23 +49,37 @@ class TestController extends Yaf_Controller_Abstract {
 	 * @return void
 	 */
 	public function indexAction() {
+		$this->getView()->baseUrl = $this->baseUrl;
+		$formParams = $this->_getTestTestingData();
+		foreach ($formParams as $name => $value) {
+			$this->getView()->{$name} = $value;
+		}
+	}
+	
+	/**
+	 * Test Result Page
+	 * 
+	 * @return void
+	 */
+	public function resultAction() {
+		//redirect if to test page if data not exist
+		if(empty($_SERVER['QUERY_STRING'])){
+			$url = "http://" . $_SERVER['HTTP_HOST'] . "/" . $this->baseUrl . "/test";
+			header("Location: $url");
+			die();
+		}
+		
 		$imsi = (string) filter_input(INPUT_GET, 'imsi'); //'425030002438039';
 		$type = (string) filter_input(INPUT_GET, 'type');
 		$scenarioData = (string) filter_input(INPUT_GET, 'scenario');
-		$scenario = array_map('trim', explode(',', $scenarioData));
+		$scenario = array_map('trim', explode("\n", trim($scenarioData)));
 		$sid = (int) filter_input(INPUT_GET, 'sid');
 		$amount = (int) filter_input(INPUT_GET, 'amount');
+		$balanceType = (string) filter_input(INPUT_GET, 'balanceType');
 		$removeLines = filter_input(INPUT_GET, 'removelines');
-		if (empty($type)) {
-			$this->getView()->testForm = true;
-			return;
-		}
-
 
 		if (empty($sid)) {
 			$sid = $this->_getSid($imsi);
-		} else {
-			$sid = array($sid);
 		}
 		if($removeLines == 1){
 		// Reset - remove all linese before test
@@ -80,16 +96,19 @@ class TestController extends Yaf_Controller_Abstract {
 				$this->_callScenario($scenario, $imsi);
 				break;
 			case 'addBalance':
-				$this->_addBalance($sid, $amount);
+				$this->_addBalance($sid, $amount, $balanceType);
 				break;
 		}
+		
 		// Get all lines created during scenarion
-		$lines = $this->_getLines($imsi);
+		$lines = $this->_getLines($sid, (addBalance == $type));
+		
 		// Get balance after scenario
 		$balance['after'] = $this->_getBalance($sid);
 
+		$this->getView()->baseUrl = $this->baseUrl;
 		$this->getView()->lines = $lines;
-		$this->getView()->balance = $balance;
+		$this->getView()->balances = $balance;
 	}
 
 	/**
@@ -99,8 +118,14 @@ class TestController extends Yaf_Controller_Abstract {
 	 */
 	public function _dataScenario($scenario, $imsi) {
 		foreach ($scenario as $index => $name) {
-			$args = array('imsi' => $imsi, 'requestNum' => ($index + 1));
-			$data = $this->_getDataData($name, $args);
+			$nameAndUssage = explode("|", $name);
+			$args = array(
+				'imsi' => $imsi,
+				'requestNum' => ($index + 1),
+				'type' => $nameAndUssage[0],
+				'usedUnits' => ($nameAndUssage[1]) ? $nameAndUssage[1] : 1000
+			);
+			$data = $this->_getDataData($args);
 			$this->sendRequest(array('usaget' => 'data', 'request' => $data));
 		}
 	}
@@ -112,8 +137,13 @@ class TestController extends Yaf_Controller_Abstract {
 	 */
 	public function _callScenario($scenario, $imsi) {
 		foreach ($scenario as $index => $name) {
-			$args = array('imsi' => $imsi);
-			$data = $this->_getCallData($name, $args);
+			$nameAndUssage = explode("|", $name);
+			$args = array(
+				'imsi' => $imsi,
+				'type' => $nameAndUssage[0],
+				'duration' => ($nameAndUssage[1]) ? $nameAndUssage[1] : 4000
+			);
+			$data = $this->_getCallData($args);
 			$this->sendRequest(array('usaget' => 'call', 'request' => $data));
 		}
 	}
@@ -128,7 +158,7 @@ class TestController extends Yaf_Controller_Abstract {
 		//$data['XDEBUG_SESSION_START'] = 'netbeans-xdebug';
 		$params = http_build_query($data);
 		$endpoint = trim("/" . $endpoint); // 'realtimeevent' / 'balances'
-		$URL = $this->baseUrl . $endpoint . '?' . $params;
+		$URL = $this->baseApiUrl . $endpoint . '?' . $params;
 		Billrun_Factory::log('SEND REQUEST' . $URL);
 		Billrun_Util::sendRequest($URL);
 	}
@@ -139,8 +169,10 @@ class TestController extends Yaf_Controller_Abstract {
 	 * @param Array $data : imsi, requestNum
 	 * @return JSON string
 	 */
-	protected function _getDataData($type, $data) {
+	protected function _getDataData($data) {
+		$type = $data['type'];
 		$imsi = $data['imsi'];
+		$usedUnits = (int)$data['usedUnits'];
 		$requestNum = $data['requestNum'];
 
 		$request = array(
@@ -193,12 +225,12 @@ class TestController extends Yaf_Controller_Abstract {
 			case 'update':
 				$request['requestType'] = "2";
 				$request['requestNum'] = $requestNum; //"2";
-				$request['msccData'][0]['usedUnits'] = "1000";
+				$request['msccData'][0]['usedUnits'] = $usedUnits;
 				break;
 			case 'final':
 				$request['requestType'] = "3";
 				$request['requestNum'] = $requestNum; //"3";
-				$request['msccData'][0]['usedUnits'] = "800";
+				$request['msccData'][0]['usedUnits'] = $usedUnits;
 				break;
 			default: return NULL;
 				break;
@@ -213,8 +245,10 @@ class TestController extends Yaf_Controller_Abstract {
 	 * @param Array $data : imsi
 	 * @return XML string
 	 */
-	protected function _getCallData($type, $data) {
+	protected function _getCallData($data) {
 		//Billrun_Util::msisdn('509889899');
+		$type = $data['type'];
+		$duration = $data['duration'];
 		$imsi = $data['imsi'];
 		$request = '<?xml version = "1.0" encoding = "UTF-8"?>';
 		switch ($type) {
@@ -224,7 +258,7 @@ class TestController extends Yaf_Controller_Abstract {
 				break;
 			case 'reservation_time': $request .= '<request><api_name>reservation_time</api_name><calling_number>972502145131</calling_number><call_reference>' . $this->reference . '</call_reference><call_id>rm7123123123</call_id><imsi>' . $imsi . '</imsi><connected_number>0390222222</connected_number><time_date>2015/08/13 11:59:03.423</time_date></request>';
 				break;
-			case 'release_call': $request = '<request><api_name>release_call</api_name><calling_number>972502145131</calling_number><call_reference>' . $this->reference . '</call_reference><call_id>rm7123123123</call_id><imsi>' . $imsi . '</imsi><connected_number>0390222222</connected_number><time_date>2015/08/13 11:59:03.543</time_date><duration>4000</duration><scp_release_cause>mmm</scp_release_cause><isup_release_cause>nnn</isup_release_cause><call_leg>x</call_leg></request>';
+			case 'release_call': $request = '<request><api_name>release_call</api_name><calling_number>972502145131</calling_number><call_reference>' . $this->reference . '</call_reference><call_id>rm7123123123</call_id><imsi>' . $imsi . '</imsi><connected_number>0390222222</connected_number><time_date>2015/08/13 11:59:03.543</time_date><duration>' . $duration . '</duration><scp_release_cause>mmm</scp_release_cause><isup_release_cause>nnn</isup_release_cause><call_leg>x</call_leg></request>';
 				break;
 			default: $request = NULL;
 				break;
@@ -240,11 +274,12 @@ class TestController extends Yaf_Controller_Abstract {
 	 */
 	protected function _getAddBalanceData($data) {
 		$sid = $data['sid'];
+		$balance_type = $data['balance_type'];
 		$amount = (-1) * $data['amount'];
 		$request = array(
 			'method' => 'update',
 			'sid' => $sid,
-			'query' => json_encode(["pp_includes_name" => "CORE BALANCE"]),
+			'query' => json_encode(["pp_includes_name" => $balance_type]),
 			'upsert' => json_encode(["value" => $amount, "expiration_date" => "2016-07-01T00:00:00+02:00"])
 		);
 		return $request;
@@ -261,54 +296,88 @@ class TestController extends Yaf_Controller_Abstract {
 	}
 
 	protected function _getSid($imsi) {
-		$sid = array();
 		$searchQuery = ['imsi' => $imsi];
 		$cursor = Billrun_Factory::db()->subscribersCollection()->query($searchQuery)->cursor()->limit(100000);
 		foreach ($cursor as $row) {
-			$sid[] = $row['sid'];
+			return $row['sid'];
 		}
-		return $sid;
+		return NULL;
 	}
 
 	protected function _getBalance($sid) {
-		$amount = array();
-		$searchQuery = ["sid" => ['$in' => $sid]];
+		$balances = array();
+		$searchQuery = ["sid" => $sid];
 		$cursor = Billrun_Factory::db()->balancesCollection()->query($searchQuery)->cursor()->limit(100000);
 		foreach ($cursor as $row) {
-			$amount[(string) $row['_id']] = floatval($row['balance']['cost']);
+			if($row['charging_by_usaget'] == 'total_cost'){
+				$amount = floatval($row['balance']['cost']);
+			} else {
+				$amount = $row['balance']['totals'][$row["charging_by_usaget"]][$row["charging_by"]];
+			}
+			$balances[(string) $row['_id']] = array(
+				'amount' => -1 * number_format($amount,3),
+				'charging_by_usaget' => $row["charging_by_usaget"],
+				'charging_by' => $row["charging_by"]
+			);
 		}
-		return $amount;
+		return $balances;
 	}
 
-	protected function _getLines($imsi) {
+	protected function _getLines($sid, $charging = false) {
 		$lines = array();
 		$amount = 0;
 		
-		$searchQuery = array(
-			"imsi" => $imsi,
-			'$or' => array(
-				array("session_id" => (int)$this->reference),
-				array("call_reference" => (string)$this->reference)),
-		);
+		if($charging){
+			$searchQuery = array(
+				"sid" => $sid,
+				"type" => 'charging'
+			);
+		} else {
+			$searchQuery = array(
+				"sid" => $sid,
+				'$or' => array(
+					array("session_id" => (int)$this->reference),
+					array("call_reference" => (string)$this->reference)),
+			);
+		}
 		
-		error_log(__LINE__ . ":" . __FUNCTION__ . ":: Data : " . print_r(json_encode($searchQuery), 1));
-		//$searchQuery = ['imsi' => $imsi];
 		$cursor = Billrun_Factory::db()->linesCollection()->query($searchQuery)->cursor()->limit(100000)->sort(['urt' => 1]);
 		foreach ($cursor as $row) {
 			$amount += $row['aprice'];
 			$lines['rows'][] = array(
+				'time_date' => date('d/m/Y H:i:s', $row['urt']->sec),
 				'record_type' => $row['record_type'],
-				'aprice' => $row['aprice']
+				'aprice' => $row['aprice'],
+				'usaget' => $row['usaget'],
+				'usagev' => $row['usagev'],
+				'balance_before' => number_format($row['balance_before'],3),
+				'balance_after' => number_format($row['balance_after'],3),
+				'arate' => (string)$row['arate']['$id']
 			);
 		}
 		$lines['total'] = $amount;
+		$lines['ref'] = $charging ? "Charging" : $this->reference;
 		return $lines;
 	}
 
-	protected function _addBalance($sids, $amount) {
-		$args = array('sid' => current($sids), 'amount' => $amount);
+	protected function _addBalance($sid, $amount, $type) {
+		$args = array('sid' => $sid, 'amount' => $amount, 'balance_type' => $type);
 		$data = $this->_getAddBalanceData($args);
 		$this->sendRequest($data, 'balances');
+	}
+	
+	protected function _getTestTestingData(){
+		$output = array();
+		$cursor = Billrun_Factory::db()->prepaidincludesCollection()->query()->cursor()->limit(100000)->sort(['urt' => 1]);
+		foreach ($cursor as $row) {
+			$output['balance_types'][] = $row['name'];
+		}
+		
+		$output['call_scenario'] = "start_call\nanswer_call\nreservation_time\nrelease_call|5000";
+		$output['data_scenario'] = "init\nupdate|1000\nupdate|800\nfinal|500";
+		$output['imsi'] = "425030002438039";
+		$output['sid'] = "546918666";
+		return $output;
 	}
 
 }
