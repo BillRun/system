ea84ef06721d4a70e7d1eded3672934126e68b23
Ilya <ilya@billrun.net>
2016-01-07 21:09:04 +0200

    wholesale auto verification, alert  for differences compared to week before

--- application/controllers/Cron.php
+++ application/controllers/Cron.php
@@ -49,6 +49,10 @@ class CronController extends Yaf_Controller_Abstract {
 		}
 	}
 
+	public function checkWholesaleAction() {
+		$this->weeklyTestWholesale();
+	}
+
 	protected function locate($process) {
 		$logsModel = new LogModel();
 		$empty_types = array();
@@ -69,7 +73,7 @@ class CronController extends Yaf_Controller_Abstract {
 
 	protected function sendAlerts($process, $empty_types) {
 		if (empty($empty_types)) {
-			return ;
+			return;
 		}
 		$events_string = implode(', ', $empty_types);
 		Billrun_Factory::log("Send alerts for " . $process, Zend_Log::INFO);
@@ -115,6 +119,58 @@ class CronController extends Yaf_Controller_Abstract {
 
 	protected function getSmsList() {
 		return Billrun_Factory::config()->getConfigValue('cron.log.sms_recipients', array());
+	}
+
+	/**
+	 * method test wholesale data looking for inconsistencies
+	 * 
+	 */
+	protected function weeklyTestWholesale() {
+		$db = Billrun_Factory::config()->getConfigValue('wholesale.db');
+		$settings = Billrun_Factory::config()->getConfigValue('cron.wholesale');
+		$this->db = Zend_Db::factory('Pdo_Mysql', array(
+				'host' => $db['host'],
+				'username' => $db['username'],
+				'password' => $db['password'],
+				'dbname' => $db['name']
+		));
+		$base_query = 'SELECT * FROM wholesale where product REGEXP "^IL" AND carrier REGEXP "^(M|N)" AND network = "all" '
+			. 'AND duration > "' . $settings['duration']['minimum'] . '" ';
+		$check_day_of_month = date("Y-m-d", strtotime($settings['checkingDay'] . ' days ago'));
+		$ref_day_of_month = date("Y-m-d", strtotime(($settings['checkingDay'] + 7) . ' days ago'));
+
+		$check_day_query = $base_query . ' AND dayofmonth ="' . $check_day_of_month . '"';
+		$ref_day_query = $base_query . ' AND dayofmonth ="' . $ref_day_of_month . '"';
+		$check_day_data = $this->db->fetchAll($check_day_query);
+		if (empty($check_day_data)) {
+			Billrun_Factory::log("wholesale error: " . $check_day_of_month . " whoelsale data is missing", Zend_Log::ERR);
+		}
+		$ref_day_data = $this->db->fetchAll($ref_day_query);
+
+		$rearranged_check_day = $this->reorder_wholesale_data($check_day_data);
+		$rearranged_ref_day = $this->reorder_wholesale_data($ref_day_data);
+		foreach ($rearranged_check_day as $carrier => $carrier_lines) {
+			foreach ($carrier_lines as $product => $line) {
+				if (!isset($rearranged_ref_day[$carrier][$product])) {
+					continue;
+				}
+				$ref_duration = $rearranged_ref_day[$carrier][$product]['duration'];
+				$diff = abs($line['duration'] / $ref_duration - 1);
+				if ($diff > $settings['duration']['diff']) {
+					$rpMessage = 'wholesale warning: carrier ' . $carrier . ' product ' . $product . ' on ' . $check_day_of_month
+						. ' differs by ' . round($diff * 100) . '%' . ' compared to ' . $ref_day_of_month;
+					Billrun_Factory::log($rpMessage, Zend_Log::ERR);
+				}
+			}
+		}
+	}
+
+	protected function reorder_wholesale_data($wholesale_data) {
+		$rearanged = array();
+		foreach ($wholesale_data as $wholesale_line) {
+			$rearanged[$wholesale_line['carrier']][$wholesale_line['product']] = $wholesale_line;
+		}
+		return $rearanged;
 	}
 
 }
