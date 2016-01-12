e02eddd3ba89c21e74e01065bb2fd3811abfbeee
Spyes <lewis@billrun.net>
2016-01-12 17:10:15 +0200

    plans and balances fixes

--- public/js/controllers/BalancesController.js
+++ public/js/controllers/BalancesController.js
@@ -2,11 +2,12 @@ angular
   .module('BillrunApp')
   .controller('BalancesController', BalancesController);
 
-function BalancesController($controller) {
+function BalancesController($controller, Utils) {
   'use strict';
 
   var vm = this;
   $controller('EditController', {$scope: vm});
+  vm.utils = Utils;
 
   vm.init = function () {
     vm.initEdit();
