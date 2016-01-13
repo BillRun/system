3bae60ff1065be8ba222a559a04cd4bf320c4558
Roman <roman@billrun.net>
2016-01-13 18:07:24 +0200

    uTest : new Test Charge Balance By Card Secret

---
+++ application/models/utest/BalanceCard.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * @package         Billing
+ * @copyright       Copyright (C) 2012-2015 S.D.O.C. LTD. All rights reserved.
+ * @license         GNU Affero General Public License Version 3; see LICENSE.txt
+ */
+
+/**
+ * Add balance model class
+ *
+ * @package  Models
+ * @subpackage uTest
+ * @since    4.0
+ */
+class utest_BalanceCardModel extends utest_AbstractUtestModel {
+
+	public function __construct(\UtestController $controller) {
+		parent::__construct($controller);
+		$this->result = array('balance_before', 'balance_after', 'lines');
+		$this->label = 'Update balance by card';
+	}
+	
+	function doTest() {
+		$sid = (int) Billrun_Util::filter_var($this->controller->getRequest()->get('sid'), FILTER_VALIDATE_INT);
+		$secret = Billrun_Util::filter_var($this->controller->getRequest()->get('secret'), FILTER_SANITIZE_STRING);
+
+		$params = array(
+			'sid' => $sid,
+			'secret' => $secret,
+		);
+
+		$data = $this->getRequestData($params);
+		$this->controller->sendRequest($data, 'balances');
+	}
+
+	/**
+	 * Get data for AddBalance request
+	 * @param String $type start_call / answer_call / reservation_time / release_call
+	 * @param Array $data : imsi
+	 * @return XML string
+	 */
+	protected function getRequestData($params) {
+		$request = array(
+			'method' => 'update',
+			'sid' => $params['sid'],
+			'query' => json_encode(["secret" => $params['secret']]),
+		);
+		return $request;
+	}
+
+}
