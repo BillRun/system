ebf15baf0d0f9755e0c5665594ba3b9e369a50b0
tomf <tom@billrun.net>
2016-01-19 09:59:25 +0200

    Including the total cost in the autorenew query (After Eli's request)

--- library/Billrun/ActionManagers/Subscribersautorenew/Query.php
+++ library/Billrun/ActionManagers/Subscribersautorenew/Query.php
@@ -84,7 +84,11 @@ class Billrun_ActionManagers_Subscribersautorenew_Query extends Billrun_ActionMa
 		// TODO: Is this filtered by priority?
 		// TODO: Should this include the total_cost??
 		foreach ($includeList as $includeRoot => $includeValues) {
-			if(!isset($includeValues['pp_includes_name'])) {
+			if(($includeRoot === 'cost') && (is_int($includeValues))) {
+				$includeRoot = "total_cost";
+				$includeValues['pp_includes_name'] = "Total";
+				$includeValues = array('usagev' => $includeValues);
+			} else if(!isset($includeValues['pp_includes_name'])) {
 				continue;
 			}
 			
