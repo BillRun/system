146cb582a83e8aeac13b3660458210b2f77d967a
Ofer Cohen <oc666@netvision.net.il>
2016-01-24 17:14:42 +0200

    fix issue on rate rt calculator - wrong usage of row

--- library/Billrun/Calculator/Rate/Callrt.php
+++ library/Billrun/Calculator/Rate/Callrt.php
@@ -43,11 +43,12 @@ class Billrun_Calculator_Rate_Callrt extends Billrun_Calculator_Rate {
 	 * @see Billrun_Calculator_Rate::getLineRate
 	 */
 	protected function getLineRate($row) {
-		$called_number = $this->get_called_number($row);
-		$line_time = $row->get('urt');
-		$usage_type = $row->get('usaget');
+//		$called_number = $this->get_called_number($row);
+//		$line_time = $row->get('urt');
+//		$usage_type = $row->get('usaget');
 		$this->setRowDataForQuery($row);
-		$matchedRate = $this->getRateByParams($called_number, $usage_type, $line_time);
+//		$matchedRate = $this->getRateByParams($called_number, $usage_type, $line_time);
+		$matchedRate = $this->getRateByParams($row);
 
 		return $matchedRate;
 	}
