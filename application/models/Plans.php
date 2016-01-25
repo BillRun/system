834b72c01b04acd256454ef26a91c3c7e6303b31
Spyes <lewis@billrun.net>
2016-01-25 17:31:17 +0200

    service providers in customer plans

--- application/models/Plans.php
+++ application/models/Plans.php
@@ -24,10 +24,10 @@ class PlansModel extends TabledateModel {
 	public function getTableColumns() {
 		$columns = array(
 			'name' => 'Name',
+			'service_provider' => 'Service Provider'
 		);
 		if ($this->type === 'charging') {
 			$columns['desc'] = "Description";
-			$columns['service_provider'] = 'Service Provider';
 		}
 		$columns['from'] = 'From'; $columns['to'] = 'Expiration';
 		return $columns;
