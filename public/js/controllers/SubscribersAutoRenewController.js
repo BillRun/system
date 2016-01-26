55cc5193859b5739e9e2e2994e9d4c8bf6b88db9
Spyes <lewis@billrun.net>
2016-01-26 09:37:29 +0200

    fix dates in auto renew

--- public/js/controllers/SubscribersAutoRenewController.js
+++ public/js/controllers/SubscribersAutoRenewController.js
@@ -5,7 +5,15 @@ app.controller('SubscribersAutoRenewController', ['$scope', '$controller', 'Data
     $controller('EditController', {$scope: $scope});
 
     $scope.init = function () {
-      $scope.initEdit();
+      $scope.initEdit(function (entity) {
+        if (_.isObject(entity.last_renew_date)) {
+          entity.last_renew_date = new Date(entity.last_renew_date.sec * 1000);
+        }
+        if (_.isObject(entity.creation_date)) {
+          entity.creation_date = new Date(entity.creation_date.sec * 1000);
+        }
+        console.log(entity);
+      });
       $scope.intervals = ["month", "day"];
       Database.getAvailableServiceProviders().then(function (res) {
         $scope.availableServiceProviders = res.data;
