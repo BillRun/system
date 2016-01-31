619b80de88fb6efd2a08e7ced28deaf9f19843ad
Spyes <lewis@billrun.net>
2016-01-31 12:55:56 +0200

    global session in getSetVar

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1040,6 +1040,12 @@ class AdminController extends Yaf_Controller_Abstract {
 	 * @return type
 	 */
 	protected function getSetVar($session, $source_name, $target_name = null, $default = null) {
+		$global_session_vars = Billrun_Factory::config()->getConfigValue('admin_panel.global_session_vars', array());
+		if (in_array($source_name, $global_session_vars)) {
+			$getsetvar_session = Yaf_session::getInstance();
+		} else {
+			$getsetvar_session = $session;
+		}
 		if (is_null($target_name)) {
 			$target_name = $source_name;
 		}
@@ -1053,16 +1059,16 @@ class AdminController extends Yaf_Controller_Abstract {
 		$var = $request->get($key);
 		if ($new_search) {
 			if (is_string($var) || is_array($var)) {
-				$session->$target_name = $var;
+				$getsetvar_session->$target_name = $var;
 			} else {
-				$session->$target_name = $default;
+				$getsetvar_session->$target_name = $default;
 			}
 		} else if (is_string($var) || is_array($var)) {
-			$session->$target_name = $var;
+			$getsetvar_session->$target_name = $var;
 		} else if (!isset($session->$target_name)) {
-			$session->$target_name = $default;
+			$getsetvar_session->$target_name = $default;
 		}
-		return $session->$target_name;
+		return $getsetvar_session->$target_name;
 	}
 
 	protected function applyFilters($table, $session = false) {
