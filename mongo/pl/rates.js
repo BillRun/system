0724ba3738220545ae82161e7f3893c3b4f31353
Ofer Cohen <oc666@netvision.net.il>
2016-02-16 00:27:47 +0200

    rates migration - WIP - start to compress the rates for simplication. Use reference to interconnect

--- mongo/pl/rates.js
+++ mongo/pl/rates.js
@@ -24,10 +24,16 @@ function create_tariff(tariff, interconnect) {
 			_access = Number(interconnect.INITIAL_CHARGE);
 		} else {
 			_access = 0;
-			_interconnect = interconnect.ADD_CHARGE;
-			tariff.INITIAL_CHARGE += Number(interconnect.INITIAL_CHARGE);
-			tariff.ADD_CHARGE     += Number(interconnect.ADD_CHARGE);
+//			_interconnect = interconnect.ADD_CHARGE;
+//			tariff.INITIAL_CHARGE += Number(interconnect.INITIAL_CHARGE);
+//			tariff.ADD_CHARGE     += Number(interconnect.ADD_CHARGE);
 		}
+	} 
+	if (typeof interconnect.PP_TARIFF_NAME != 'undefined') {
+		_interconnect_name = standardKey(interconnect.PP_TARIFF_NAME);	
+	} else {
+		print("No interconnect!!!");
+		_interconnect_name = null;
 	}
 
 	print("create_tariff usaget : " + _usaget);
@@ -44,6 +50,7 @@ function create_tariff(tariff, interconnect) {
 		return {
 			'access': _access,
 			'unit' : _unit,
+			'interconnect': _interconnect_name,
 			'rate':     [{
 				'to': 2147483647,
 				'price': Number(tariff.ADD_CHARGE),
@@ -54,6 +61,7 @@ function create_tariff(tariff, interconnect) {
 	return {
 		'access':   _access,
 		'unit' : _unit,
+		'interconnect': _interconnect_name,
 		'rate':     [{
 			'to': Number(_amount),
 			'price': Number(tariff.INITIAL_CHARGE),
@@ -96,7 +104,7 @@ function getUsageType(app_id) {
 	return _usaget;
 }
 
-function _plan(plan_id, plan_name, usaget, shabbat, tariffs, tariffs_interconnect) {
+function _plan(plan_id, plan_name, usaget, shabbat, tariffs) {
 	var unit_type
 	if (shabbat) {
 		unit_type = "13";
@@ -115,34 +123,39 @@ function _plan(plan_id, plan_name, usaget, shabbat, tariffs, tariffs_interconnec
 		}
 	}
 	if (typeof _interconnect != 'undefined') {
+		print('tariffs ids: ' + _tariff_ids.join());
+		print("plan id: " + plan_id);
+		print("plan : " + plan_name);
+		print("interconnect name: " + _interconnect.PP_TARIFF_NAME);
 		_t = create_tariff(obj10, _interconnect);
-		if (tariffs_interconnect[usaget] === undefined)
-			tariffs_interconnect[usaget] = {};
-		tariffs_interconnect[usaget][plan_name] = _t;
+		if (tariffs[usaget] === undefined)
+			tariffs[usaget] = {};
+		tariffs[usaget][plan_name] = _t;
+	} else {
+		print('tariffs ids: ' + _tariff_ids.join());
+		print("plan id: " + plan_id);
+		print("plan : " + plan_name);
+		_t = create_tariff(obj10, {});
+		if (tariffs[usaget] === undefined)
+			tariffs[usaget] = {};
+		tariffs[usaget][plan_name] = _t;
+		
 	}
-
-	print('tariffs ids: ' + _tariff_ids.join());
-	print("plan id: " + plan_id);
-	print("plan : " + plan_name);
-	_t = create_tariff(obj10);
-	if (tariffs[usaget] === undefined)
-		tariffs[usaget] = {};
-	tariffs[usaget][plan_name] = _t;
 }
 
-function activity_and_plan(subtype, appid, usaget, shabbat, tariffs, tariffs_interconnect) {
+function activity_and_plan(subtype, appid, usaget, shabbat, tariffs) {
 	db.tmp_ACTIVITY_AND_PP_PLAN.find({"NEW_SUBTYPE_ID":subtype, "APPLICATION_ID" : appid, PP_PLAN_ID:{$exists:1, $ne:''}}).forEach(
 		function(obj8) {
-			_plan(obj8.PP_PLAN_ID, 'BASE', usaget, shabbat, tariffs, tariffs_interconnect);
+			_plan(obj8.PP_PLAN_ID, 'BASE', usaget, shabbat, tariffs);
 		}
 	);
 }
 
-function cos_plans(subtype, appid, usaget, shabbat, tariffs, tariffs_interconnect) {
+function cos_plans(subtype, appid, usaget, shabbat, tariffs) {
 	db.tmp_COS_PLANS.find({"SUBTYPE_ID":subtype, "APPLICATION_ID" : appid, PP_PLAN_ID:{$exists:1, $ne:''}}).forEach(
 		function(obj4) {
 			db.tmp_COS.find({"COS_ID" : obj4.COS_ID}).forEach(function(obj5) {_plan_name = obj5.COS_NAME;});
-			_plan(obj4.PP_PLAN_ID, _plan_name, usaget, shabbat, tariffs, tariffs_interconnect);
+			_plan(obj4.PP_PLAN_ID, _plan_name, usaget, shabbat, tariffs);
 		}
 	);
 }
@@ -159,16 +172,14 @@ function getUniqueArray(arr) {
 }
 
 function isEmpty(obj) {
-//   for(var key in obj) {
-//      if (obj.hasOwnProperty(key)) {
-//         return false;
-//      }
-//   }
-//   return true;
 	return Object.keys(obj).length == 0;
 }
 
-//db.tmp_PPS_PREFIXES.aggregate({$match:{BILLING_ALLOCATION:/FreeCall/}}, {$group:{_id:"$BILLING_ALLOCATION", prefixes:{$addToSet:"$PPS_PREFIXES"}}}).forEach(
+function standardKey(_rate_name) {
+	return _rate_name.replace(/ |-/g, "_").toUpperCase();
+}
+
+//db.tmp_PPS_PREFIXES.aggregate({$match:{BILLING_ALLOCATION:/Bezeq144/}}, {$group:{_id:"$BILLING_ALLOCATION", prefixes:{$addToSet:"$PPS_PREFIXES"}}}).forEach(
 db.tmp_PPS_PREFIXES.aggregate({$group:{_id:"$BILLING_ALLOCATION", prefixes:{$addToSet:"$PPS_PREFIXES"}}}).forEach(
 	function(obj1) {
 		_rate_name = obj1._id;
@@ -200,41 +211,9 @@ db.tmp_PPS_PREFIXES.aggregate({$group:{_id:"$BILLING_ALLOCATION", prefixes:{$add
 						print("app id: " + _appid);
 						print("usaget: " + _usaget);
 						
-						activity_and_plan(_subtype, _appid, _usaget, false, _tariffs, _tariffs_interconnect);
-						cos_plans(_subtype, _appid, _usaget, false, _tariffs, _tariffs_interconnect);
+						activity_and_plan(_subtype, _appid, _usaget, false, _tariffs);
+						cos_plans(_subtype, _appid, _usaget, false, _tariffs);
 				
-					}
-				);
-			}
-		);
-
-		_tariffs_shabbat = {};
-		_tariffs_shabbat_interconnect = {};
-//		======================================================================================================================================================
-//		shabbat
-		db.tmp_Prefix_Allocation_ID_clear.find({ALLOCATION_B: _rate_name}).forEach(
-			function(obj2) {
-				if (obj2.HOME_OPPS_ID == '0' && obj2.HOME_TPPS_ID == '0') {
-					return;
-				}
-				if (obj2.ALLOCATION_B == 'Anywhere') {
-					_location_id = obj2.HOME_TPPS_ID;
-				} else {
-					_location_id = obj2.HOME_OPPS_ID;
-				}
-				print("location id: " + _location_id);
-				db.tmp_SUBTYPE_TRANSLATION.find({"LOCATION_ID" : _location_id, SPECIAL_FEATURE:"0"}).forEach(
-					function(obj3) {
-						_subtype = obj3.NEW_SUBTYPE;
-						_appid = obj3.APPLICATION_ID;
-						_usaget = getUsageType(_appid);
-						print("sub type: " + _subtype);
-						print("app id: " + _appid);
-						print("usaget: " + _usaget);
-						
-						activity_and_plan(_subtype, _appid, _usaget, false, _tariffs_shabbat, _tariffs_shabbat_interconnect);
-						cos_plans(_subtype, _appid, _usaget, false, _tariffs_shabbat, _tariffs_shabbat_interconnect);
-
 					}
 				);
 			}
@@ -245,64 +224,14 @@ db.tmp_PPS_PREFIXES.aggregate({$group:{_id:"$BILLING_ALLOCATION", prefixes:{$add
 		}
 		_prefixes = getUniqueArray(_prefixes);
 
-		if (isEmpty(_tariffs_interconnect)) {
-			_tariffs_interconnect = _tariffs;
-		}
-		if (isEmpty(_tariffs_shabbat)) {
-			_tariffs_shabbat = _tariffs;
-		}
-		if (isEmpty(_tariffs_shabbat_interconnect)) {
-			_tariffs_shabbat_interconnect = _tariffs;
-		}
 		_rate = {
-			'from':    ISODate('2016-01-01'),
+			'from':    ISODate('2016-02-01'),
 			'to':      ISODate('2099-12-31 23:59:59'),
-			'key':     _rate_name.replace(/ |-/g, "_").toUpperCase(),
+			'key':     standardKey(_rate_name),
 			'params':  {
-				'prefix': _prefixes,
-				'shabbat': false,
-				'interconnect': false,
+				'prefix': _prefixes
 			},
 			'rates': _tariffs
-		};
-		db.rates.insert(_rate);
-
-		_rate = {
-			'from':    ISODate('2016-01-01'),
-			'to':      ISODate('2099-12-31 23:59:59'),
-			'key':     _rate_name.replace(/ |-/g, "_").toUpperCase() + '_INTERCONNECT',
-			'params':  {
-				'prefix': _prefixes,
-				'shabbat': false,
-				'interconnect': true,
-			},
-			'rates': _tariffs_interconnect
-		};
-		db.rates.insert(_rate);
-
-		_rate = {
-			'from':    ISODate('2016-01-01'),
-			'to':      ISODate('2099-12-31 23:59:59'),
-			'key':     _rate_name.replace(/ |-/g, "_").toUpperCase() + '_SHABBAT',
-			'params':  {
-				'prefix': _prefixes,
-				'shabbat': true,
-				'interconnect': false,
-			},
-			'rates': _tariffs_shabbat
-		};
-		db.rates.insert(_rate);
-
-		_rate = {
-			'from':    ISODate('2016-01-01'),
-			'to':      ISODate('2099-12-31 23:59:59'),
-			'key':     _rate_name.replace(/ |-/g, "_").toUpperCase() + '_SHABBAT_INTERCONNECT',
-			'params':  {
-				'prefix': _prefixes,
-				'shabbat': true,
-				'interconnect': true,
-			},
-			'rates': _tariffs_shabbat_interconnect
 		};
 		db.rates.insert(_rate);
 
@@ -340,5 +269,5 @@ db.rates.insert({
 			}
 		}
 	},
-	"to" : ISODate("2113-08-28T17:23:55Z")
+	"to" : ISODate("2099-08-28T17:23:55Z")
 })
