320447cb8eb2fb27215d5f9bec0a4e9a9d8c5d4d
tomf <tom@billrun.net>
2016-02-18 18:32:43 +0200

    Added the normalized report to the lines when only partialy loading balance

--- library/Billrun/ActionManagers/Balances/Updaters/PrepaidInclude.php
+++ library/Billrun/ActionManagers/Balances/Updaters/PrepaidInclude.php
@@ -82,8 +82,18 @@ class Billrun_ActionManagers_Balances_Updaters_PrepaidInclude extends Billrun_Ac
 		$findQuery['pp_includes_external_id'] = $chargingPlan->getPPID();
 		
 		$updateResult = $this->updateBalance($chargingPlan, $findQuery, $defaultBalance, $recordToSet['to']);
-		if($this->normalizeBalance($findQuery, $subscriber['plan'], $chargingPlan) === false) {
+		$normalizeResult = $this->normalizeBalance($findQuery, $subscriber['plan'], $chargingPlan);
+		if($normalizeResult === false) {
 			return false;
+		}
+		
+		// Report on changes
+		if($normalizeResult['nModified'] > 0) {
+			$valueName = $chargingPlan->getFieldName();
+			$beforeNormalizing = $updateResult[0]['balance'][$valueName];
+			$updateResult[0]['balance'][$valueName] = $normalizeResult['max'];
+			$updateResult[0]['balance']['normalized'] = $beforeNormalizing + $chargingPlan->getValue() . "=>" . $beforeNormalizing . '=>' . $normalizeResult['max'];
+			
 		}
 		
 		$updateResult[0]['source'] = $prepaidIncludes->createRefByEntity($prepaidRecord);
