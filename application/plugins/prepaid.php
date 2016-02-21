eb6b31a56f670e3170840896cde3e82f6dcaf2b9
yonatanr <yonatan@billrun.net>
2016-02-21 14:18:34 +0200

    Does not set call_offset for any real-time event except call

--- application/plugins/prepaid.php
+++ application/plugins/prepaid.php
@@ -74,6 +74,9 @@ class prepaidPlugin extends Billrun_Plugin_BillrunPluginBase {
 
 	protected function getRowCurrentUsagev($row) {
 		try {
+			if ($row['type'] != "callrt") {
+				return 0;
+			}
 			$lines_coll = Billrun_Factory::db()->linesCollection();
 			$query = $this->getRowCurrentUsagevQuery($row);
 			$line = $lines_coll->aggregate($query)->current();
