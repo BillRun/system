7d8fca7c42541066aceb7effd4a03efc53ad6c1b
Ofer Cohen <oc666@netvision.net.il>
2016-02-21 10:53:38 +0200

    update build version date

--- library/Billrun/Version.php
+++ library/Billrun/Version.php
@@ -30,7 +30,7 @@ class Billrun_Version
 	public static $BUILD = '20';
 
 	/** @var  string  Release date. */
-	public static $RELDATE = '14-February-2016';
+	public static $RELDATE = '20-February-2016';
 
 	/** @var  string  Link text. */
 	public static $URL = '<a href="https://bill.run">BillRun</a>';
