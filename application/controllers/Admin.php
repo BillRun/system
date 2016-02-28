8be47b19a2b371face66cf6e4aa9e8fc89fe94e4
Spyes <lewis@billrun.net>
2016-02-28 15:44:36 +0200

    improvements to subscriber side panel

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -85,6 +85,7 @@ class AdminController extends Yaf_Controller_Abstract {
 		$this->addJs($this->baseUrl . '/js/vendor/xeditable.min.js');
 		$this->addJs($this->baseUrl . '/js/vendor/bootstrap-table.js');
 		$this->addJs($this->baseUrl . '/js/vendor/angular-pageslide-directive.js');
+		$this->addJs($this->baseUrl . '/js/vendor/angular-sanitize.min.js');
 
 		$this->addJs($this->baseUrl . '/js/main.js');
 		$this->addJs($this->baseUrl . '/js/app.js');
