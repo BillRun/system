210579b64a7b1240f32317eb1e718c2fb61663d4
Ofer Cohen <oc666@netvision.net.il>
2016-02-28 23:51:05 +0200

    improve logging and remove deprecated code of Billrun_Log

--- library/Billrun/Log.php
+++ library/Billrun/Log.php
@@ -15,40 +15,6 @@
  */
 class Billrun_Log extends Zend_Log {
 	
-	const DUPLICATE_MESSAGE_SUPPRESS_COUNTER = 50;
-	const DUPLICATE_MESSAGE_PRIORITY_THRESHOLD = Zend_Log::DEBUG;
-	const LOG_MSG_PREFIX_SIZE = 7;
-	
-	const LOG_COLOR_RED = 1;
-	const LOG_COLOR_GREEN = 2;
-	const LOG_COLOR_YELLOW = 3;
-	
-	/**
-	 * Array for the log messages prefixes based on priority.
-	 */
-	private static $PRIORITY_COLORS = array
-		(
-			Zend_Log::EMERG => Billrun_Log::LOG_COLOR_RED,
-			Zend_Log::ERR => Billrun_Log::LOG_COLOR_RED,
-			Zend_Log::CRIT => Billrun_Log::LOG_COLOR_RED,
-			Zend_Log::WARN => Billrun_Log::LOG_COLOR_YELLOW,
-//			Zend_Log::DEBUG => Billrun_Log::LOG_COLOR_YELLOW,
-//			Zend_Log::INFO => Billrun_Log::LOG_COLOR_RED,
-//			Zend_Log::NOTICE => "NOTIC ",
-//			Zend_Log::ALERT => "ALERT "		
-		);
-	
-	/**
-	 * Add color to a string.
-	 * 
-	 * @string - String to put color on.
-	 * @color - int representing the requested.
-	 */
-	protected function addColor($string, $color) {
-		$fixedColor = $color %10;
-		return "\033[1;3" . $fixedColor . "m" . $string . "\033[0m";
-	}
-	
 	/**
 	 * the log instances for bridged singletones
 	 * 
@@ -63,49 +29,21 @@ class Billrun_Log extends Zend_Log {
 	protected $stamp = '';
 
 	/**
-	 * If true log is printed in color.
-	 * @var type Boolean
-	 */
-	protected $colored = true;
-	
-	/**
-	 * This stores the last message that was logged for log suppressing.
+	 * login user
 	 * 
-	 * @var string - Last message that was logged.
+	 * @var string
 	 */
-	private $lastMessage = null;
-	
+	protected $user = null;
+
 	/**
-	 * This stores the priority of the last message that was logged for log suppressing.
-	 * 
-	 * @var priority - Priority of the last message that was logged.
-	 */
-	private $lastMessagePriority = Zend_Log::DEBUG;
-	
-	/**
-	 * This stores the extras of the last message that was logged for log suppressing.
-	 * 
-	 * @var extras - Extras of the last message that was logged.
-	 */
-	private $lastMessageExtras = null;
-	
-	/**
-	 * This is to print how many duplicate messages were suppressed.
-	 * @var int - Counter for how many duplicate messages were printed.
-	 */
-	private $duplicateMessageCounter = 0;
-	
-    /**
      * Class constructor.  Create a new logger.
 	 * Generate an ID for the logger for later filtering.
      *
      * @param Zend_Log_Writer_Abstract|null  $writer  default writer
-	 * @param Boolean $colored If true log is printed in color.
      * @return void
      */
-    public function __construct(Zend_Log_Writer_Abstract $writer = null, $colored=true) {
+    public function __construct(Zend_Log_Writer_Abstract $writer = null) {
 		parent::__construct($writer);
-		$this->colored = $colored;
 		
 		if ($pid = getmypid()) {
 			$this->stamp = Billrun_Util::getHostName() .  ':p' . $pid;
@@ -113,6 +51,11 @@ class Billrun_Log extends Zend_Log {
 			// Make a unique log stamp for each run of the application
 			$this->stamp = substr(md5($_SERVER['REQUEST_TIME'] . rand(0, 100)), 0, 7);
 		}
+		
+		if (($user = Billrun_Factory::user()) !== FALSE) {
+			$this->user = $user->getUsername();
+		}
+
 	}
 	
 	public static function getInstance(array $options = array()) {
@@ -128,25 +71,6 @@ class Billrun_Log extends Zend_Log {
 
 		return self::$instances[$stamp];
 	}
-
-//	/**
-//	 * Overriding the Zend log _packEvent function to print with colors.
-//	 * @param type $message
-//	 * @param type $priority
-//	 * @return type
-//	 */
-//	protected function _packEvent($message, $priority) {
-//		$logRecord = parent::_packEvent($message, $priority);
-//		if($this->colored && array_key_exists($priority, Billrun_Log::$PRIORITY_COLORS)){
-//			// item is in the hastable
-//			$logRecord['priorityName'] = 
-//				str_pad($this->addColor($logRecord['priorityName'], 
-//										Billrun_Log::$PRIORITY_COLORS[$priority]), 
-//					    Billrun_Log::LOG_MSG_PREFIX_SIZE, " ");
-//		}
-//		
-//		return $logRecord;
-//	}
 
 	/**
 	 * Log a crash using raised exception as crash details.
@@ -164,85 +88,27 @@ class Billrun_Log extends Zend_Log {
 	}
 	
 	/**
-	 * This function executes logic for suppressing duplicate log prints, 
-	 * return true if there is a message to print and false if there is non to be printed.
-	 * 
-	 * @param  string   $message   Message to log
-	 * @param  integer  $priority  Priority of message
-	 * @param  mixed    $extras    Extra information to log in event
-	 * 
-	 * @return boolean false if nothing to be printed.
-	 */
-	protected function logSuppressDuplicates($message, $priority, $extras) {
-		// Check if the priority is to be suppressed.
-		if($priority < Billrun_Log::DUPLICATE_MESSAGE_PRIORITY_THRESHOLD){
-			return true;
-		}
-		
-		$duplicateMessage = false;
-		
-		// Check if this message is the same as the last one logged.
-		if($message  == $this->lastMessage		   && 
-		   $priority == $this->lastMessagePriority &&
-		   $extras   == $this->lastMessageExtras){
-			$this->duplicateMessageCounter++;
-			
-			// Check if to suppress the message.
-			if($this->duplicateMessageCounter < Billrun_Log::DUPLICATE_MESSAGE_SUPPRESS_COUNTER) {
-				return false;
-			}
-			
-			$duplicateMessage = true;
-		}
-		
-		$tempLastMessage = $this->lastMessage;
-					
-		if($this->duplicateMessageCounter > 0) {
-			$suppressedMessage = $this->lastMessage . " (Suppressed $this->duplicateMessageCounter)";
-			$this->duplicateMessageCounter = 0;
-			
-			// Print the last message if exists.
-			$this->log($suppressedMessage, $this->lastMessagePriority, $this->lastMessageExtras);
-		}
-		
-		// Set the last message to be the last logged not including the 'Suppressed' suffix.
-		if ($duplicateMessage) {
-			$this->lastMessage = $tempLastMessage;
-			return false;
-		}
-		
-		// There is no need to print again, if this is also a duplicated message.
-		$this->lastMessage = $message;
-		$this->lastMessagePriority = $priority;
-		$this->lastMessageExtras = $extras;
-		
-		return true;
-	}
-	
-	/**
 	 * Log a message at a priority
 	 *
 	 * @param  string   $message   Message to log
 	 * @param  integer  $priority  Priority of message
 	 * @param  mixed    $extras    Extra information to log in event
-	 * @param  color	$color	   Number for ASCII color for the message to be printed in, NULL if no color.
 	 * @return void
 	 * @throws Zend_Log_Exception
 	 */
-	public function log($message, $priority = Zend_Log::DEBUG, $extras = null, $color=null) {
-		if($color !== null){
-			$message = $this->addColor($message, $color);
+	public function log($message, $priority = Zend_Log::DEBUG, $extras = null) {
+		$prefixes = array();
+		if ($this->stamp) {
+			$prefixes[] = $this->stamp;
+		}
+		
+		if ($this->user) {
+			$prefixes[] = $this->user;
 		}
 
-		// If returned false, nothing to be printed.
-		if(!$this->logSuppressDuplicates($message, $priority, $extras)) {
-			return;
+		if (!empty($prefixes)) {
+			$message = '[' . implode('|', $prefixes). '] ' . $message;			
 		}
-		
-		if ($this->stamp) {
-			$message = '[' . $this->stamp . '] ' . $message;
-		}
-		
 		parent::log($message, $priority, $extras);
 	}
 
