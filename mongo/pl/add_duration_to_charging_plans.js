636dbc2cdf8314436b2f133520efff043570d570
Spyes <lewis@billrun.net>
2016-03-07 12:57:39 +0200

    fixes add duration to charging plans script and exported new plans

--- mongo/pl/add_duration_to_charging_plans.js
+++ mongo/pl/add_duration_to_charging_plans.js
@@ -1,12 +1,17 @@
-db.plans.find({"type": "charging", "$or": [{"recurring": 0}, {"recurring": {"$exists": 0}}]}).forEach(function (plan) {
-  db.plans.update({"_id": plan._id},
-                  {"$set":
-                   {
-                     "period":
-                     {
-                       "unit": "months",
-                       "duration": 12
-                     }
-                   }
-                  });
-});
+var count = db.plans.find({"type": "charging", "$or": [{"recurring": 0}, {"recurring": {"$exists": 0}}]}).count();
+var res = db.plans.update({"type": "charging", "$or": [{"recurring": 0}, {"recurring": {"$exists": 0}}]},
+                          {"$set":
+                           {
+                             "period":
+                             {
+                               "unit": "months",
+                               "duration": 12
+                             }
+                           }
+                          },
+                          {multi: true});
+if (res.nModified !== count) {
+  print("Found " + count + " but only updated " + res.nModified + "!!");
+} else {
+  print("Modified " + res.nModified + " of " + count + " records!");
+}
