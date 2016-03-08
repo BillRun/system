39f666201a5d777309a3e28033624a14ee22e22a
Spyes <lewis@billrun.net>
2016-03-08 11:04:13 +0200

    get latest subscriber info when filtering balances

--- application/models/Balances.php
+++ application/models/Balances.php
@@ -219,13 +219,14 @@ class BalancesModel extends TableModel {
 				$item['units'] = implode(',', $units);
 				$subscriber = Billrun_Factory::db()->subscribersCollection()
 					->query(array('sid' => $item['sid'],
-						'from' => array('$lte' => $item['from']),
 						'to' => array('$gte' => $item['from'])))
 					->cursor()
+					->sort(array('from' => -1))
 					->limit(1)
 					->current()
 					->getRawData();
-				$item['service_provider'] = $subscriber['service_provider'];
+				if (isset($subscriber['service_provider']))
+					$item['service_provider'] = $subscriber['service_provider'];
 			}
 			if ($current_plan = $this->getDBRefField($item, 'current_plan')) {
 				$item['current_plan'] = $current_plan['name'];
