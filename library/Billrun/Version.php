532e8d39a1742845e2e99ad4c6668ae5da38c7e8
Ofer Cohen <oc666@netvision.net.il>
2016-03-20 17:53:49 +0200

    update version dev build

--- library/Billrun/Version.php
+++ library/Billrun/Version.php
@@ -27,10 +27,10 @@ class Billrun_Version
 	public static $DEV_STATUS = 'Beta';
 
 	/** @var  string  Build number. */
-	public static $BUILD = '2';
+	public static $BUILD = '3';
 
 	/** @var  string  Release date. */
-	public static $RELDATE = '14-March-2016';
+	public static $RELDATE = '21-March-2016';
 
 	/** @var  string  Link text. */
 	public static $URL = '<a href="https://bill.run">BillRun</a>';
