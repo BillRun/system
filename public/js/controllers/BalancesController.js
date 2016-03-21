03f078766aa9852d627a95c13a8d1bbf4b558371
Ofer Cohen <oc666@netvision.net.il>
2016-03-21 18:56:38 +0200

    change balance type on balance update to MTR

--- public/js/controllers/BalancesController.js
+++ public/js/controllers/BalancesController.js
@@ -45,7 +45,7 @@ function BalancesController($controller, Utils, $http, $window, Database, $route
       }),
       additional: JSON.stringify({
         balance_source: "AdminPanel",
-        balance_type: "BalanceUpdate",
+        balance_type: "MTR",
         balance_info: username
       })
     };
