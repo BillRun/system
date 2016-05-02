f149ea793601b9ea4a0d12142eb3c6c9c7402097
Idan Daniel <idan.daniel@billrun.net>
2016-05-02 13:10:31 +0300

    fixing the range of ild's_one_way ocg to be only 2102. attached a script that updates all ocg of  KT rates to be only 2102.

--- mongo/maintenance/fix_range_of _out_circuit_group_for_llds_1_way.js
+++ mongo/maintenance/fix_range_of _out_circuit_group_for_llds_1_way.js
@@ -2,5 +2,5 @@
 var relevant_key = /^KT/;
 var today = new Date();
 
-db.rates.update({key: relevant_key, from: {$lte: today}, to: {$gt: today}}, {$set:{'params.out_circuit_group':[{from:"2100" , to: "2499"}]}}, {multi: true})
+db.rates.update({key: relevant_key, from: {$lte: today}, to: {$gt: today}}, {$set:{'params.out_circuit_group': "2102"}}, {multi: true})
 
