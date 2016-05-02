b5c6180a992282ffa0de5fa74a3d8f0c86aa04c0
Ofer Cohen <ofer@billrun.net>
2016-05-02 17:10:04 +0300

    refactor count months method to be based on getNextRenewDate method (auto-renew logic)

--- library/Billrun/Utils/Autorenew.php
+++ library/Billrun/Utils/Autorenew.php
@@ -14,13 +14,16 @@
  */
 class Billrun_Utils_Autorenew {
 
-	public static function getNextRenewDate($from, $retMongoDate = true) {
+	public static function getNextRenewDate($from, $retMongoDate = true, $relative_datetime = null) {
+		if (is_null($relative_datetime)) { // default is current time
+			$relative_datetime = time();
+		}
 		$from_dom = (int) date('d', $from);
-		$current_dom = (int) date('d');
-		if ($current_dom < $from_dom && $from_dom <= date('t')) { // check if we have the next renew date in the current month in the next following days
+		$current_dom = (int) date('d', $relative_datetime);
+		if ($current_dom < $from_dom && $from_dom <= date('t', $relative_datetime)) { // check if we have the next renew date in the current month in the next following days
 			 $ret_ts = strtotime(date('Y-m-' . self::pad_date_part($from_dom)));
 		} else {
-			$first_day_of_next_month = strtotime('tomorrow', strtotime('last day of this month'));
+			$first_day_of_next_month = strtotime('tomorrow', strtotime('last day of this month', $relative_datetime));
 			$last_day_of_next_month = strtotime('last day of this month', $first_day_of_next_month); // this will be the last day of next month midnight
 			$last_day_of_next_month_dom = (int) date('d', $last_day_of_next_month);
 			if ($from_dom > $last_day_of_next_month_dom) { // if next month is less than from date need to handle this
@@ -38,6 +41,18 @@ class Billrun_Utils_Autorenew {
 	
 	protected static function pad_date_part($date_part) {
 		return str_pad($date_part, 2, '0', STR_PAD_LEFT);
+	}
+	
+	public static function countMonths($d1, $d2) {
+		$min_date = min($d1, $d2);
+		$max_date = max($d1, $d2);
+		$count = 0;
+		while ($min_date < $max_date && $count < 9999) { // second arg avoid infinite loop
+			$min_date = self::getNextRenewDate($min_date, false, $min_date);
+			$count++;
+//			print $count . " " . date('Y-m-d H:i:s', $min_date) . "<br />" . PHP_EOL;
+		}
+		return $count;
 	}
 
 }
