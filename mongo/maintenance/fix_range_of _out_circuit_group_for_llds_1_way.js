c7c637a1ad5a41256eb7b6abccd5f60f3d4d501b
Idan Daniel <idan.daniel@billrun.net>
2016-05-02 17:28:26 +0300

    script for updating KT rates ocg range.

--- mongo/maintenance/fix_range_of _out_circuit_group_for_llds_1_way.js
+++ mongo/maintenance/fix_range_of _out_circuit_group_for_llds_1_way.js
@@ -2,5 +2,4 @@
 var relevant_key = /^KT/;
 var today = new Date();
 
-db.rates.update({key: relevant_key, from: {$lte: today}, to: {$gt: today}}, {$set:{'params.out_circuit_group': "2102"}}, {multi: true})
-
+db.rates.update({key: relevant_key, from: {$lte: today}, to: {$gt: today}}, {$set:{'params.out_circuit_group': [{from: "2100", to: "2102"}, {from: "2499", to: "2499"}, {from: "2120", to: "2122"}]}}, {multi: true})
