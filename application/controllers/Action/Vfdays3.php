167496e898e4b60fd4239b1229f126d273e178bc
Ofer Cohen <ofer@billrun.net>
2016-05-11 14:29:42 +0300

    add vfdays3 API

---
+++ application/controllers/Action/Vfdays3.php
@@ -0,0 +1,120 @@
+<?php
+
+/**
+ * @package         Billing
+ * @copyright       Copyright (C) 2012 S.D.O.C. LTD. All rights reserved.
+ * @license         GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+/**
+ * Refund action class
+ *
+ * @package  Action
+ * @since    1.0
+ */
+class Vfdays3Action extends Action_Base {
+
+	protected $plans = null;
+
+	/**
+	 * method to execute the refund
+	 * it's called automatically by the api main controller
+	 * on vadofone
+	 */
+	public function execute() {
+		Billrun_Factory::log()->log("Execute ird days API call", Zend_Log::INFO);
+		$request = $this->getRequest();
+		$min_days = intval($request->get("min_days"));
+		if (empty($min_days)) {
+			$this->getController()->setOutput(array(array(
+					'status' => 0,
+					'desc' => 'need to supply min_days arguments',
+					'input' => $request->getRequest(),
+			)));
+			return;
+		}
+		$max_datetime = $request->get("max_datetime");
+		$list = $this->count_days_by_lines($min_days, $max_datetime);
+		$this->getController()->setOutput(array(array(
+				'status' => 1,
+				'desc' => 'success',
+				'input' => $request->getRequest(),
+				'details' => $list
+		)));
+	}
+
+	/**
+	 * 
+	 * @param int $min_days Return only subscribers exceeding this number of days
+	 * @param string $max_datetime A PHP date/time string to be used as an upper limit
+	 * @return array
+	 */
+	public function count_days_by_lines($min_days = 35, $max_datetime = null) {
+		$from = strtotime('January 1st');
+
+		$elements[] = array(
+			'$match' => array(
+				'unified_record_time' => array(
+					'$gte' => new MongoDate($from),
+				),
+				'arategroup' => 'VF',
+			),
+		);
+		if ($max_datetime) {
+			$elements[0]['$match']['unified_record_time']['$lte'] = new MongoDate(strtotime($max_datetime));
+		}
+		$elements[] = array(
+			'$group' => array(
+				'_id' => array(
+					'day_key' => array('$substr' =>
+						array(
+							array('$ifNull' => array('$record_opening_time', '$callEventStartTimeStamp')),
+							4,
+							4
+						)
+					),
+					'sid' => '$sid'
+				),
+			),
+		);
+		$elements[] = array(
+			'$sort' => array(
+				'_id.sid' => 1,
+				'_id.day_key' => -1,
+			),
+		);
+		$elements[] = array(
+			'$group' => array(
+				'_id' => '$_id.sid',
+				'days_num' => array(
+					'$sum' => 1,
+				),
+				'last_day' => array(
+					'$first' => '$_id.day_key',
+				)
+			),
+		);
+		$elements[] = array(
+			'$match' => array(
+				'days_num' => array(
+					'$gte' => $min_days,
+				)
+			)
+		);
+		$elements[] = array(
+			'$project' => array(
+				'_id' => 0,
+				'sid' => '$_id',
+				'count_days' => '$days_num',
+				'last_date' => '$last_day',
+				'max_days' => 45,
+			)
+		);
+		
+		MongoCursor::$timeout = -1;
+		$res = Billrun_Factory::db()->linesCollection()->getMongoCollection()->aggregateCursor($elements, array('allowDiskUse' => TRUE, 'maxTimeMS' => 10*60*1000))->setReadPreference(MongoClient::RP_SECONDARY_PREFERRED)->timeout(-1);
+		$it = iterator_to_array($res);
+		return $it;
+	}
+
+}
