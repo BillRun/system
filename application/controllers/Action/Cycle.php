5357dd4cc1e938ab28005ae63c4b14e7f95393bc
Ofer Cohen <ofer@billrun.net>
2016-05-15 19:07:07 +0300

    code conventions and test env settings for automatic cycle

--- application/controllers/Action/Cycle.php
+++ application/controllers/Action/Cycle.php
@@ -1,21 +1,21 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
 class CycleAction extends Action_Base {
-	
-	
+
 	protected $billing_cycle = null;
+
 	/**
 	 * method to execute the aggregate process
 	 * it's called automatically by the cli main controller
 	 */
 	public function execute() {
-	
+
 		$possibleOptions = array(
 			'type' => false,
 			'stamp' => true,
@@ -26,42 +26,42 @@ class CycleAction extends Action_Base {
 
 		if (($options = $this->_controller->getInstanceOptions($possibleOptions)) === FALSE) {
 			return;
-		}	
+		}
 
-		if (is_null($options['stamp'])){
+		if (is_null($options['stamp'])) {
 			$next_billrun_key = Billrun_Util::getBillrunKey(time());
 			$current_billrun_key = Billrun_Util::getPreviousBillrunKey($next_billrun_key);
 			$options['stamp'] = $current_billrun_key;
 		}
 
-		if (!$options['size']){
+		if (!$options['size']) {
 			// default value for size
 			$options['size'] = Billrun_Factory::config()->getConfigValue('customer.aggregator.size');
 		}
 
 		$this->billing_cycle = Billrun_Factory::db()->billing_cycleCollection();
-		$process_interval =  (int)Billrun_Factory::config()->getConfigValue('cycle.processes.interval');
-		if (Billrun_Factory::config()->isProd()){
-			if ($process_interval < 60){	  // 1 minute is minimum sleep time 
-				$process_interval = 60;		
+		$process_interval = (int) Billrun_Factory::config()->getConfigValue('cycle.processes.interval');
+		if (Billrun_Factory::config()->isProd()) {
+			if ($process_interval < 60) {   // 1 minute is minimum sleep time 
+				$process_interval = 60;
 			}
 		}
-		
-		do{
-			$pid = pcntl_fork();	
+
+		do {
+			$pid = pcntl_fork();
 			if ($pid == -1) {
 				die('could not fork');
 			} else if ($pid) {
-					sleep($process_interval);
-					pcntl_wait($status);
-			} else {		
+				sleep($process_interval);
+				pcntl_wait($status);
+			} else {
 				$this->_controller->addOutput("Loading aggregator");
 				try {
 					$aggregator = Billrun_Aggregator::getInstance($options);
 				} catch (Exception $e) {
 					Billrun_Factory::log()->log($e->getMessage(), Zend_Log::NOTICE);
 					$aggregator = FALSE;
-				}	
+				}
 				$this->_controller->addOutput("Aggregator loaded");
 				if ($aggregator) {
 					$this->_controller->addOutput("Loading data to Aggregate...");
@@ -78,12 +78,7 @@ class CycleAction extends Action_Base {
 				}
 				break;
 			}
-		} 
-		while (Billrun_Aggregator_Customer::isBillingCycleOver($this->billing_cycle, $options['stamp'], (int)$options['size']) === FALSE);
-	} 
-	
-	
-	
+		} while (Billrun_Aggregator_Customer::isBillingCycleOver($this->billing_cycle, $options['stamp'], (int) $options['size']) === FALSE);
+	}
 
-	
 }
