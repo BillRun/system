d10cbf403c58fb7ea1d1b2099f0c08a81770f070
Ofer Cohen <ofer@billrun.net>
2016-05-15 20:20:07 +0300

    minor issue: max process on new automatic cycle get config value + 1

--- library/Billrun/Calculator/CustomerPricing.php
+++ library/Billrun/Calculator/CustomerPricing.php
@@ -113,7 +113,9 @@ class Billrun_Calculator_CustomerPricing extends Billrun_Calculator {
 		$this->loadRates();
 		$this->loadPlans();
 		$this->balances = Billrun_Factory::db(array('name' => 'balances'))->balancesCollection()->setReadPreference('RP_PRIMARY');
-		$this->active_billrun = Billrun_Billrun::getActiveBillrun();
+		$cached_active_billrun = Billrun_Factory::cache()->get('active_billrun', __CLASS__);
+		if (empty($cached_active_billrun))
+		$this->getActiveBillrun();//active_billrun = Billrun_Billrun::getActiveBillrun();
 		$this->active_billrun_end_time = Billrun_Util::getEndTime($this->active_billrun);
 		$this->next_active_billrun = Billrun_Util::getFollowingBillrunKey($this->active_billrun);
 		// max recursive retrues for value=oldValue tactic
