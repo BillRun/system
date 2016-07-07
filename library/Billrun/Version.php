bba684752c8dc2ca33641347cbce85d03a40aabb
Ofer Cohen <ofer@billrun.net>
2016-07-07 21:58:51 +0300

    update dev build

--- library/Billrun/Version.php
+++ library/Billrun/Version.php
@@ -27,10 +27,10 @@ class Billrun_Version {
 	public static $DEV_STATUS = 'Stable';
 
 	/** @var  string  Build number. */
-	public static $BUILD = '11';
+	public static $BUILD = '12';
 
 	/** @var  string  Release date. */
-	public static $RELDATE = '05-July-2016';
+	public static $RELDATE = '07-July-2016';
 
 	/** @var  string  Link text. */
 	public static $URL = '<a href="https://bill.run">BillRun</a>';
