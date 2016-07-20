3c59aeb2bed6ca27973e2144d8991e773295e31a
Tom <tom@billrun.net>
2016-07-20 16:48:40 +0300

    Added simpletest to version 28

---
+++ application/controllers/Test.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @package         Billing
+ * @copyright       Copyright (C) 2012-2016 S.D.O.C. LTD. All rights reserved.
+ * @license         GNU Affero General Public License Version 3; see LICENSE.txt
+ */
+
+/**
+ * Billing test controller class
+ *
+ * @package  Controller
+ * @since    4.4
+ */
+class TestController extends Yaf_Controller_Abstract {
+
+	public function init() {
+		if (Billrun_Factory::config()->isProd()) {
+			die("Cannot run on production environment");
+		}
+		Billrun_Test::getInstance($this->getRequest()->action);
+		$this->getRequest()->action = 'index';
+	}
+
+}
