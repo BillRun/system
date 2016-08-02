91fc8b222116cf0b7a885ea36c229dcd2319f965
yonatanr <yonatan@billrun.net>
2016-08-02 13:03:12 +0300

    add tenant support

--- conf/config.php
+++ conf/config.php
@@ -8,27 +8,91 @@
 $env = null;
 
 if (!defined('APPLICATION_ENV')) {
-	$env = getenv('APPLICATION_ENV');
+	defineEnvironment();
+}
+define('BILLRUN_CONFIG_PATH', APPLICATION_PATH . "/conf/" . APPLICATION_ENV . ".ini");
+validateEnvironment();
+
+if (!defined('APPLICATION_TENANT')) {
+	$a = getenv('APPLICATION_MULTITENANT');
+	$b = php_sapi_name();
+	if (getenv('APPLICATION_MULTITENANT') && php_sapi_name() === 'cli') { 
+		defineTenant();
+	}
+}
+
+/**
+ * Get a value from the CLI context
+ * @return string valu to set up
+ */
+function getValueFromCLI($opt) {
+	if (!is_array($opt)) {
+		$opt = arra($opt);
+	}
+	$envs = getopt('', $opt);
+	if (!$envs) {
+		return null;
+	}
 	
+	$opts = array_values($envs);
+	if (isset($opts[0])) {
+		return $opts[0];
+	}
+	
+	return null;
+}
+
+/**
+ * Define the environment if not yet defined
+ */
+function defineEnvironment() {
+	$env = getenv('APPLICATION_ENV');
+
 	// if APPLICATION_ENV not defined and the getenv not find it (not through web server), let's take it by cli opt
-	if (empty($env) && ($envs = getopt('', array('env:', 'environment:')))) {
-		$envOpts = array_values($envs);
-		if (isset($envOpts[0])) {
-			$env = $envOpts[0];
-		}
+	if (empty($env)) {
+		$env = getValueFromCLI(array('env:', 'environment:'));
+	}
+	
+	// Failed to get environment
+	if (empty($env)) {
+		$errorMessage = 'Environment could not be setup';
+		error_log($errorMessage);
+		die($errorMessage . PHP_EOL);
 	}
 
-	if (empty($env)) {
-		error_log('Environment did not setup!');
-		die('Environment did not setup!' . PHP_EOL);
-	}
-	
 	define('APPLICATION_ENV', $env);
 }
 
-define('BILLRUN_CONFIG_PATH', APPLICATION_PATH . "/conf/" . APPLICATION_ENV . ".ini");
+/**
+ * Validate the invironment name
+ */
+function validateEnvironment() {
+	if (file_exists(BILLRUN_CONFIG_PATH)) {
+		return;
+	}
 
-if (!file_exists(BILLRUN_CONFIG_PATH)) {
-	error_log('Configuration file did not found');
-	die('Configuration file did not found');
+	$errorMessage = 'Configuration file did not found';
+	error_log($errorMessage);
+	die($errorMessage);
+}
+
+/**
+ * Define the tenant if not yet defined
+ */
+function defineTenant() {
+	$tenant = getenv('APPLICATION_TENANT');
+
+	// if APPLICATION_TENANT not defined and the getenv not find it (not through web server), let's take it by cli opt
+	if (empty($tenant)) {
+		$tenant = getValueFromCLI('tenant');
+	}
+	
+	// Failed to get tenant
+	if (empty($tenant)) {
+		$errorMessage = 'Tenant could not be setup';
+		error_log($errorMessage);
+		die($errorMessage . PHP_EOL);
+	}
+
+	define('APPLICATION_TENANT', $tenant);
 }
