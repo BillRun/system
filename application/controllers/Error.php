1bc09c4d5e8132657bb75d192a08db64e69fdf7f
Tom <tom@billrun.net>
2016-08-07 18:54:19 +0300

    Using the Yaf error handler to alert user on permission error

---
+++ application/controllers/Error.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Controller for error handling
+ * 
+ * @package  Controller
+ * @since    4
+ */
+class ErrorController extends Yaf_Controller_Abstract {
+	
+     /** 
+      * you can also call to Yaf_Request_Abstract::getException to get the 
+      * un-caught exception.
+      */
+     public function errorAction($exception) {
+        /* error occurs */
+        switch ($exception->getCode()) {
+            case YAF_ERR_NOTFOUND_MODULE:
+            case YAF_ERR_NOTFOUND_CONTROLLER:
+            case YAF_ERR_NOTFOUND_ACTION:
+            case YAF_ERR_NOTFOUND_VIEW:
+                echo 404, ":", $exception->getMessage();
+                break;
+            default :
+                echo "INVALID ACTION";
+                break;
+        }
+     } 
+}
