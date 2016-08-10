053a279b9c822149e008f3d9877ef17660f9eec2
Tom <tom@billrun.net>
2016-08-10 13:35:08 +0300

    Applied code conventions

--- library/Billrun/DataTypes/Conf/List.php
+++ library/Billrun/DataTypes/Conf/List.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @package         Billing
  * @copyright       Copyright (C) 2012-2016 S.D.O.C. LTD. All rights reserved.
@@ -9,53 +10,55 @@
  * Wrapper class for a complex list value object
  */
 class Billrun_DataTypes_Conf_List extends Billrun_DataTypes_Conf_Base {
+
 	use Billrun_Traits_Api_UserPermissions;
-	
+
 	protected $list = array();
 	protected $template = array();
-	
+
 	/**
 	 * Unique indicator for list memeber, to check if adding new 
 	 * value or editting existing.
 	 * @var string 
 	 */
 	protected $matchKey = "";
+
 	public function __construct(&$obj) {
 		$this->val = &$obj['v'];
 		$this->list = &$obj['list'];
 		$this->matchKey = $obj['k'];
-		if(isset($obj['template'])) {
+		if (isset($obj['template'])) {
 			$this->template = $obj['template'];
 		}
 	}
-	
+
 	public function validate() {
-		if( empty($this->val)				   || 
-		   !is_array($this->val)			   || 
-		    empty($this->matchKey)			   || 
-		   !is_string($this->matchKey)		   || 
-		   !is_array($this->list)) {
+		if (empty($this->val) ||
+			!is_array($this->val) ||
+			empty($this->matchKey) ||
+			!is_string($this->matchKey) ||
+			!is_array($this->list)) {
 			return false;
 		}
-		
-		if(!$this->validateMendatoryFields()) {
+
+		if (!$this->validateMendatoryFields()) {
 			return false;
 		}
-		
-		if(!$this->validateKnownFields()) {
+
+		if (!$this->validateKnownFields()) {
 			return false;
 		}
-		
-		if(!$this->validateEditable()) {
+
+		if (!$this->validateEditable()) {
 			return false;
 		}
-				
+
 		// Set in the list.
-		if($this->val !== null) {
+		if ($this->val !== null) {
 			$this->list[] = $this->val;
 			$this->val = null;
 		}
-		
+
 		return true;
 	}
 
@@ -68,7 +71,7 @@ class Billrun_DataTypes_Conf_List extends Billrun_DataTypes_Conf_Base {
 			$found = array_filter($block, function($v, $k) use($name) {
 				return $k == $this->matchKey && $v == $name;
 			}, ARRAY_FILTER_USE_BOTH);
-			if(!empty($found)) {
+			if (!empty($found)) {
 				// TODO: Do something with the 'system' magic value, magic values
 				// are bad.
 				$editing['block'] = (isset($block['system']) && $block['system']);
@@ -76,12 +79,12 @@ class Billrun_DataTypes_Conf_List extends Billrun_DataTypes_Conf_Base {
 				break;
 			}
 		}
-		
-		if(empty($editing)) {
+
+		if (empty($editing)) {
 			return true;
 		}
-		
-		if(isset($editing['block']) && $editing['block']) {
+
+		if (isset($editing['block']) && $editing['block']) {
 			return false;
 		}
 		// Remove the existing object.
@@ -90,20 +93,21 @@ class Billrun_DataTypes_Conf_List extends Billrun_DataTypes_Conf_Base {
 
 		return true;
 	}
-	
+
 	protected function validateMendatoryFields() {
 		foreach ($this->template as $field => $mendatory) {
-			if($mendatory && !isset($this->val[$field])) {
+			if ($mendatory && !isset($this->val[$field])) {
 				return false;
 			}
 		}
 		return true;
 	}
+
 	protected function validateKnownFields() {
 		$diff = array_diff(array_keys($this->val), array_keys($this->template));
 		return empty($diff);
 	}
-	
+
 	public function value() {
 		return $this->list;
 	}
