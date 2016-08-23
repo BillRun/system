73577f149893a64622045a1554200c5805a1280a
Tom <tom@billrun.net>
2016-08-23 10:04:53 +0300

    Added file header

--- library/Billrun/Billingcycle.php
+++ library/Billrun/Billingcycle.php
@@ -1,9 +1,9 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * @package         Billing
+ * @copyright       Copyright (C) 2012-2016 BillRun Technologies Ltd. All rights reserved.
+ * @license         GNU Affero General Public License Version 3; see LICENSE.txt
  */
 
 /**
