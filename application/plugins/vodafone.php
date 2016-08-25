989acfac6b49c425cb2cfd1e0380ecb97380be5c
Idan Daniel <idan.daniel@billrun.net>
2016-08-25 10:46:59 +0300

    change vodafone tap3 to Israel timezone from local timezone

--- application/plugins/vodafone.php
+++ application/plugins/vodafone.php
@@ -22,13 +22,14 @@ class vodafonePlugin extends Billrun_Plugin_BillrunPluginBase {
 
 	public function beforeUpdateSubscriberBalance($balance, $row, $rate, $calculator) {
 		if ($row['type'] == 'tap3') {
-			if (isset($row['basicCallInformation']['CallEventStartTimeStamp']['localTimeStamp'])) {
+			if (isset($row['urt'])) {
+				$timestamp = $row['urt']->sec;
 				$this->line_type = $row['type'];
-				$this->line_time = $row['basicCallInformation']['CallEventStartTimeStamp']['localTimeStamp'];
+				$this->line_time = date("YmdHis",  $timestamp);	
 			} else {
-				Billrun_Factory::log()->log('localTimeStamp wasn\'t found for line ' . $row['stamp'] . '.', Zend_Log::ALERT);
+				Billrun_Factory::log()->log('urt wasn\'t found for line ' . $row['stamp'] . '.', Zend_Log::ALERT);
 			}
-		}
+		}	
 	}
 
 	public function afterUpdateSubscriberBalance($row, $balance, &$pricingData, $calculator) {
@@ -59,7 +60,7 @@ class vodafonePlugin extends Billrun_Plugin_BillrunPluginBase {
 		$line_month = substr($this->line_time, 4, 2);
 		$line_day = substr($this->line_time, 6, 2);
 		$dayKey = $line_year . $line_month . $line_day;
-		$results = $this->loadSidLines($sid, $limits, $plan, $groupSelected, $dayKey);
+		$results = $this->loadSidLines($sid, $limits, $plan, $groupSelected);
 		if (!isset($this->cached_results[$sid]) || !in_array($dayKey, $this->cached_results[$sid])) {
 			$this->cached_results[$sid][] = $dayKey;
 		}
@@ -79,18 +80,64 @@ class vodafonePlugin extends Billrun_Plugin_BillrunPluginBase {
 		$groupSelected = FALSE; // we will cancel the usage as group plan when set to false groupSelected
 	}
 
-	protected function loadSidLines($sid, $limits, $plan, $groupSelected, $dayKey) {
-		$line_year = date('Y', strtotime($this->line_time));
-		$from = date('YmdHis', strtotime(str_replace('%Y', $line_year, $limits['period']['from']) . ' 00:00:00'));
-		$to = date('YmdHis', strtotime(str_replace('%Y', $line_year, $limits['period']['to']) . ' 23:59:59'));
+protected function loadSidLines($sid, $limits, $plan, $groupSelected) {
+		$year = date('Y', strtotime($this->line_time));
+		$line_month = intval(substr($this->line_time, 4, 2));
+		$line_day = intval(substr($this->line_time, 6, 2));
+		$from = strtotime(date('YmdHis', strtotime(str_replace('%Y', $year, $limits['period']['from']) . ' 00:00:00')));
+		$to = strtotime(date('YmdHis', strtotime(str_replace('%Y', $year, $limits['period']['to']) . ' 23:59:59')));
+		$line_year = intval($year);
+		$start_of_year = new MongoDate($from);
+		$end_of_year = new MongoDate($to);
+		$isr_transitions = timezone_transitions_get(new DateTimeZone('Asia/Jerusalem'), strtotime('January 1st'), strtotime('December 31'));
+		$summer_transition = $isr_transitions['1']['time'];
+		$winter_transition = $isr_transitions['2']['time'];	
+		$summer_offset = $isr_transitions['1']['offset'];
+		$winter_offset = $isr_transitions['2']['offset'];
+		$summer_date = new DateTime($summer_transition);
+		$winter_date = new DateTime($winter_transition);
+		$transition_date_summer = new MongoDate($summer_date->getTimestamp());
+		$transition_date_winter = new MongoDate($winter_date->getTimestamp());
+			
+		$project = array(
+			'$project' => array(
+				'sid' => 1,
+				'urt' => 1,
+				'type' => 1,    
+				'plan' => 1,
+				'arategroup' => 1,
+				'billrun' => 1,
+				'in_group' => 1,
+				'aprice' => 1,
+				'isr_time' => array(
+					'$cond' => array(
+						'if' => array(
+							'$and' => array(
+								array('$gte' => array('$urt', $transition_date_summer)),
+								array('$lt' => array('$urt', $transition_date_winter)),
+							),
+								
+						),
+						'then' => array(
+							'$add' => array('$urt', $summer_offset * 1000)
+						),
+						'else' => array(
+							'$add' => array('$urt', $winter_offset  * 1000) 
+						),
+				 
+					),		
+				),
+			),
+		);
+		
 		$match = array(
 			'$match' => array(
 				'sid' => $sid,
 				'type' => 'tap3',
 				'plan' => $plan->getData()->get('name'),
-				'basicCallInformation.CallEventStartTimeStamp.localTimeStamp' => array(
-					'$gte' => $from,
-					'$lte' => $to,
+				'urt' => array(
+					'$gte' => $start_of_year,
+					'$lte' => $end_of_year,
 				),
 				'arategroup' => $groupSelected,
 				'in_group' => array(
@@ -101,26 +148,49 @@ class vodafonePlugin extends Billrun_Plugin_BillrunPluginBase {
 				),
 			),
 		);
+
 		$group = array(
 			'$group' => array(
 				'_id' => array(
-					'day_key' => array(
-						'$substr' => array('$basicCallInformation.CallEventStartTimeStamp.localTimeStamp', 0, 8),
+					'day_key' => array( 
+						'$dayOfMonth' => '$isr_time', 
+					),
+					'month_key' => array(
+						'$month' => '$isr_time', 
+					),
+					'year_key' => array(
+						'$year' => '$isr_time', 
 					),
 				),
 			),
 		);
+		
 		$match2 = array(
 			'$match' => array(
 				'_id.day_key' => array(
-					'$lte' => $dayKey,
+					'$lte' => $line_day, 
+				),
+				'_id.month_key' => array(
+					'$lte' => $line_month, 
+				),
+				'_id.year_key' => array(
+					'$lte' => $line_year, 
 				),
 			),
 		);
 
-		$results = Billrun_Factory::db()->linesCollection()->aggregate($match, $group, $match2);
+		$results = Billrun_Factory::db()->linesCollection()->aggregate($project, $match, $group, $match2);
 		return array_map(function($res) {
-					return $res['_id']['day_key'];
+					$month_day = "";
+					if (strlen($res['_id']['month_key']) < 2) {
+						$month_day .= "0";
+					}
+					$month_day .= $res['_id']['month_key'];
+					if (strlen($res['_id']['day_key']) < 2) {
+						$month_day .= "0";
+					}
+					$month_day .= $res['_id']['day_key'];
+					return  $res['_id']['year_key'] . $month_day;
 				}, $results);
 	}
 
