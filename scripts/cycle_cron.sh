5ff5c0b7818db79a1274ccd6eda254e222be2ece
Tom <tom@billrun.net>
2016-08-31 12:53:36 +0300

    Fixed the cycle cron bash script

--- scripts/cycle_cron.sh
+++ scripts/cycle_cron.sh
@@ -4,27 +4,62 @@
 ###  1) Path to the billrun installation directory
 ###  2) Environment (dev / prod etc.)
 
-if [ $1 ]; then
-	cd $1;
-	export APPLICATION_MULTITENANT=1;
-	INDEX=public/index.php
+usage() {
+        usage="$(basename "$0") -d PATH -e ENVIRONMENT -t AGGREGATOR TYPE -- this scripts runs the cycle action for all tenants
 
-else
-	echo "please supply the path to the billrun installation directory"
-	exit 1
-fi
+        where:
+            -d DIRECTORY path to billrun directory.
+            -e ENV current billrun environment
+            -t TYPE aggregator type
+            -h print help"
 
-if [ -z "$APPLICATION_MULTITENANT" ] || [ "$APPLICATION_MULTITENANT" != 1 ]; then
-    echo "Not running in multi-tenant mode. Exiting."
-    exit 1
-fi  
+        echo "$usage"
+}
 
-if [ $2 ]; then
-	ENV=$2
-else
-	echo "please supply an environment"
-	exit 2
-fi
+TYPE=""
+ENV=""
+INDEX=""
+
+parseInput() {
+	OPTIND=1
+	optionMask=0
+        while getopts "hd:e:t:" option; do
+          case $option in
+            h) 
+	       usage
+               exit
+               ;;
+            d) ((optionMask^=1))
+	       cd $OPTARG || { usage; exit 1; }
+               export APPLICATION_MULTITENANT=1;
+               INDEX=public/index.php
+               ;;
+            e) ((optionMask^=2))
+	       ENV="$OPTARG"
+               ;;
+            t) ((optionMask^=4))
+	       TYPE="$OPTARG"
+               ;;
+            :) printf "missing argument for -%s\n" "$OPTARG" >&2
+               usage
+               exit 1
+               ;;
+           \?) printf "illegal option: -%s\n" "$OPTARG" >&2
+               usage
+               exit 1
+               ;;
+          esac
+        done
+	
+	if [ "$optionMask" != 7 ]; then
+		usage
+		exit 1;
+	fi
+ 	
+       shift $((OPTIND - 1))
+}
+
+parseInput "${@}"
 
 CLIENTS=$1/conf/tenants/*.ini
 LOCKS=$1/scripts/locks/
@@ -36,10 +71,11 @@ do
 		TEMP=$(basename $f)
 		TENANT=${TEMP%.*}
 
-		flock -n $LOCKS"/pseudo_cron_"$TENANT"_cycle.lock" -c "php $INDEX --env $ENV --tenant $TENANT --cycle" &
+		flock -n $LOCKS"/pseudo_cron_"$TENANT"_cycle.lock" -c "php $INDEX --env $ENV --tenant $TENANT --type $TYPE --cycle" &
 		echo "Invoked cycling for $TENANT"
 
 	else
 		echo "No tenants found"
 	fi	
 done
+
