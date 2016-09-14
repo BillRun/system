df508fe7febf0b740d947048eabbf72c21fb4b46
ariel.halperin <ariel.halperin@billrun.com>
2016-09-14 14:08:27 +0300

    fix merge issues

--- public/index.php
+++ public/index.php
@@ -6,8 +6,6 @@
  * @license         GNU Affero General Public License Version 3; see LICENSE.txt
  */
 defined('APPLICATION_PATH') || define('APPLICATION_PATH', dirname(__DIR__));
-header('Access-Control-Allow-Credentials: true');
-header("Access-Control-Allow-Origin: ".$_SERVER['HTTP_ORIGIN']);
 require_once(APPLICATION_PATH . DIRECTORY_SEPARATOR . 'conf' . DIRECTORY_SEPARATOR . 'config.php');
 $app = new Yaf_Application(BILLRUN_CONFIG_PATH);
 $app->bootstrap()->run();
