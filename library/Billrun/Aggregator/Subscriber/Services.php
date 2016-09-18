9931b58030ef10933364155c99d6a5a17faad7d0
Tom <tom@billrun.net>
2016-09-18 13:22:03 +0300

    Created the subscriber service aggregator logic

--- library/Billrun/Aggregator/Subscriber/Services.php
+++ library/Billrun/Aggregator/Subscriber/Services.php
@@ -39,6 +39,44 @@ class Billrun_Aggregator_Subscriber_Services extends Billrun_Aggregator_Subscrib
 	 * @return type
 	 */
 	protected function getData($billrunKey, Billrun_Subscriber $subscriber) {
-		return array();
+		$serviceEntries = array();
+		$services = $subscriber->getServices($billrunKey);
+		foreach ($services as $service) {
+			$servicesChargeEntries = array_merge($serviceEntries, $this->getChargeServicesEntries($subscriber, $billrunKey, $service));
+			$serviceEntries = $servicesChargeEntries;
+		}
+
+		return $serviceEntries;
+	}
+	
+	protected function getChargeServicesEntries($subscriber, $billrunKey, $service) {
+		$charge = $service->getPrice($billrunKey);
+		if (isset($charge)) {
+			$flatEntries = array($this->getServiceEntry($subscriber, $billrunKey, $service, $charge));
+		} else {
+			$flatEntries = array();
+		}
+		return $flatEntries;
+	}
+	
+	protected function getServiceEntry(Billrun_Subscriber $subscriber, $billrunKey, $service, $charge) {
+		$start = Billrun_Billrun::getStartTime($billrunKey);
+		$startTimestamp = strtotime($start);
+		$flatEntry = new Mongodloid_Entity(array(
+			'aid' => $subscriber->aid,
+			'sid' => $subscriber->sid,
+			'source' => 'billrun',
+			'billrun' => $billrunKey,
+			'type' => 'service',
+			'usaget' => 'service',
+			'urt' => new MongoDate($startTimestamp),
+			'aprice' => $charge,
+			'service' => $service->getName(),
+//			'plan_ref' => $plan->createRef(),
+			'process_time' => new MongoDate(),
+		));
+		$stamp = md5($subscriber->aid . '_' . $subscriber->sid . $service->getName() . '_' . $start . $billrunKey);
+		$flatEntry['stamp'] = $stamp;
+		return $flatEntry;
 	}
 }
