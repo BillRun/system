8139f28981276f61dfef92a9ab41dce3f80ca2e9
Tom <tom@billrun.net>
2016-09-25 12:14:55 +0300

    Continued implementing

--- library/Billrun/Aggregator/Customer.php
+++ library/Billrun/Aggregator/Customer.php
@@ -208,19 +208,75 @@ class Billrun_Aggregator_Customer extends Billrun_Aggregator {
 	 */
 	public function load() {
 		$cycle = new Billrun_DataTypes_CycleTime($this->getStamp());
+		$data = $this->loadRawData($cycle);
+		$aggregatedData = $this->translateRawData($data);
+		return $data;
+	}
+
+	/**
+	 * Get the raw data
+	 * @param Billrun_DataTypes_CycleTime $cycle
+	 * @return array of raw data
+	 */
+	protected function loadRawData($cycle) {
+		if (!$this->overrideAccountIds) {
+			return $this->aggregateMongo($cycle, $this->page, $this->size);
+		}
+		
 		$data = array();
-		if ($this->overrideAccountIds) {
-			$data = array();
-			foreach ($this->overrideAccountIds as $account_id) {
-				$data = $data + $this->aggregateMongo($cycle, 0, 1, $account_id);
+		foreach ($this->overrideAccountIds as $account_id) {
+			$data = $data + $this->aggregateMongo($cycle, 0, 1, $account_id);
+		}
+		return $data;
+	}
+	
+	/**
+	 * Translate the raw data to aggregateable data
+	 * @param array $rawData
+	 * @return array Array of aggregateable data.
+	 */
+	protected function translateRawData($rawData) {
+		$data = array();
+		$billruns_count = 0;
+		$dataKeys = array_keys($rawData);
+		foreach ($rawData as $accid => $account) {
+			Billrun_Factory::log("Aggregate loop");
+			if ($this->memory_limit > -1 && memory_get_usage() > $this->memory_limit) {
+				// TODO: Memory limit should not be here as magic number.
+				Billrun_Factory::log('Customer aggregator memory limit of ' . $this->memory_limit / 1048576 . 'M has reached. Exiting (page: ' . $this->page . ', size: ' . $this->size . ').', Zend_Log::ALERT);
+				break;
 			}
-		} else {
-			$data = $this->aggregateMongo($cycle, $this->page, $this->size);
+			//pre-load  account lines 
+			if ($this->bulkAccountPreload && !($billruns_count % $this->bulkAccountPreload) && count($dataKeys) > $billruns_count) {
+				$aidsToLoad = array_slice($dataKeys, $billruns_count, $this->bulkAccountPreload);
+				Billrun_Billrun::preloadAccountsLines($aidsToLoad, $billrun_key);
+			}
+			Billrun_Factory::dispatcher()->trigger('beforeAggregateAccount', array($accid, $account, &$this));
+			Billrun_Factory::log('Current account index: ' . ++$billruns_count, Zend_Log::INFO);
+
+			if (!$this->overrideAccountIds && Billrun_Billrun::exists($accid, $billrun_key)) {
+				Billrun_Factory::log("Billrun " . $billrun_key . " already exists for account " . $accid, Zend_Log::ALERT);
+				$skipped_billruns_count++;
+				continue;
+			}
+			$params = array(
+				'aid' => $accid,
+				'billrun_key' => $billrun_key,
+				'autoload' => !empty($this->overrideAccountIds),
+			);
+			$account_billrun = Billrun_Factory::billrun($params);
+			if ($this->overrideAccountIds) {
+				$account_billrun->resetBillrun();
+			}
+			
+			$this->addAccountFieldsToBillrun($account_billrun, $account);
+			
+			$this->data[] = new Billrun_Cycle_Account($account);
 		}
 		
 		return $data;
 	}
-
+	
 	protected function handleInvoices($data) {
 		$query = array('billrun_key' => $this->stamp, 'page_number' => (int)$this->page, 'page_size' => $this->size);
 		$dataCount = count($data);
@@ -261,21 +317,6 @@ class Billrun_Aggregator_Customer extends Billrun_Aggregator {
 	 * execute aggregate
 	 */
 	public function aggregate() {
-		$account_billrun = false;
-		$billrun_key = $this->getStamp();
-		$billruns_count = 0;
-		$skipped_billruns_count = 0;
-		if ($this->bulkAccountPreload) {
-			Billrun_Factory::log('loading accounts that will be needed to be preloaded...', Zend_Log::INFO);
-			$dataKeys = array_keys($this->data);
-			//$existingAccounts = array();			
-			foreach ($dataKeys as $key => $aid) {
-				if (!$this->overrideAccountIds && Billrun_Billrun::exists($aid, $billrun_key)) {
-					unset($dataKeys[$key]);
-					//$existingAccounts[$aid]  = $this->data[$aid];
-				}
-			}
-		}
 		foreach ($this->data as $accid => $account) {
 			Billrun_Factory::log("Aggregate loop");
 			if ($this->memory_limit > -1 && memory_get_usage() > $this->memory_limit) {
--- library/Billrun/Cycle/Account.php
+++ library/Billrun/Cycle/Account.php
@@ -14,6 +14,12 @@
  */
 class Billrun_Cycle_Account extends Billrun_Cycle_Common {
 	
+	/**
+	 * Array of account attributes
+	 * @var array 
+	 */
+	protected $attributes = array();
+	
 	public function __construct($data) {
 		parent::__construct($data);
 	}
@@ -28,10 +34,35 @@ class Billrun_Cycle_Account extends Billrun_Cycle_Common {
 		return isset($input['subscribers']) && is_array($input['subscribers']);
 	}
 
+	/**
+	 * Construct the subscriber records
+	 * @param type $data
+	 */
 	protected function constructRecords($data) {
-		foreach ($data as $subscriber) {
+		$this->populateBillrunWithAccountData($data);
+		$subscribers = $data['subscribers'];
+		foreach ($subscribers as $subscriber) {
 			$this->records[] = new Billrun_Cycle_Subscriber($subscriber);
 		}
 	}
 
+	/**
+	 * Get an empty billrun account entry structure.
+	 * @param int $aid the account id of the billrun document
+	 * @param string $billrun_key the billrun key of the billrun document
+	 * @return array an empty billrun document
+	 */
+	protected function populateBillrunWithAccountData($account) {
+		$attr = array();
+		foreach (Billrun_Factory::config()->getConfigValue('billrun.passthrough_data', array()) as $key => $remoteKey) {
+			if (isset($account['attributes'][$remoteKey])) {
+				$attr[$key] = $account['attributes'][$remoteKey];
+			}
+		}
+		if (isset($account['attributes']['first_name']) && isset($account['attributes']['last_name'])) {
+			$attr['full_name'] = $account['attributes']['first_name'] . ' ' . $account['attributes']['last_name'];
+		}
+
+		$this->attributes = $attr;
+	}
 }
