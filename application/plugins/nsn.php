f07900aaef66adea888b94fb9e9e7f49d62df5f2
Idan Daniel <idan.daniel@billrun.net>
2016-09-29 13:38:23 +0300

    adding isset cause_for_termination

--- application/plugins/nsn.php
+++ application/plugins/nsn.php
@@ -507,7 +507,7 @@ class nsnPlugin extends Billrun_Plugin_BillrunPluginFraud implements Billrun_Plu
 	public function beforeProcessorStore($processor){
 		$data = &$processor->getData();
 		foreach ($data['data'] as $line) {
-			if (isset($line['type']) && $line['type'] == 'nsn' && ((!isset($line['duration']) || ($line['duration'] <= 0)) && ($line['cause_for_termination'] != '00000000'))){
+			if (isset($line['type']) && $line['type'] == 'nsn' && ((!isset($line['duration']) || ($line['duration'] <= 0)) && (isset($line['cause_for_termination']) && $line['cause_for_termination'] != '00000000'))){
 				$processor->unsetQueueRow($line['stamp']);
 			}
 		}
