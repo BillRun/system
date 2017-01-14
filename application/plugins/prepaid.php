8072e478b46ab5346f0b5bbea8b6311fb46bde86
Ofer Cohen <ofer.cohen@billrun.com>
2017-01-14 23:29:16 +0200

    limit the range of reuqest_num field when query line to rebalance (previous line). this was raised on session with hundreds of requests

--- application/plugins/prepaid.php
+++ application/plugins/prepaid.php
@@ -224,7 +224,10 @@ class prepaidPlugin extends Billrun_Plugin_BillrunPluginBase {
 			$findQuery = array(
 				"sid" => $row['sid'],
 				"session_id" => $row['session_id'],
-				"request_num" => array('$lt' => $row['request_num']),
+				"request_num" => array(
+					'$lt' => $row['request_num'],
+					'$gte' => ($row['request_num'] > 10) ? $row['request_num'] - 10 : 0,
+				),
 				"mscc_data.service_id" => $row['mscc_data'][0]['service_id'],
 			);
 			$sort = array(
