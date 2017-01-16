950e662be1050bc8dbd063fb8544b5dc08c72b3b
Roman <roman@billrun.net>
2017-01-16 19:32:05 +0200

    BRCD-217 : Change Payment Gateway

--- application/controllers/Externalpaypage.php
+++ application/controllers/Externalpaypage.php
@@ -20,6 +20,7 @@ class ExternalPaypageController extends Yaf_Controller_Abstract {
 	public function indexAction() {
 		$view = new Yaf_View_Simple(Billrun_Factory::config()->getConfigValue('application.directory') . '/views/paypage');
 		$request = $this->getRequest()->getRequest();
+		$action = $this->getRequest()->get('action', 'create');
 		$query = array(
 			'type' => 'account',
 			'aid' => intval($request['aid'])
@@ -37,12 +38,11 @@ class ExternalPaypageController extends Yaf_Controller_Abstract {
 		$this->getView()->assign('account', $account);
 		$this->getView()->assign('account_config', $config['subscribers']['account']['fields']);
 		$this->getView()->assign('subscriber_config', $config['subscribers']['subscriber']['fields']);
-		if(empty($account['payment_gateway'])){
-			$this->getView()->assign('payment_gateways', $config['payment_gateways']);
-		}
+		$this->getView()->assign('payment_gateways', $config['payment_gateways']);
 		$this->getView()->assign('plans', $planNames);
 		$this->getView()->assign('plan', $selectedPlan);
 		$this->getView()->assign('return_url', $request['return_url']);
+		$this->getView()->assign('action', $action);
 		return $view->render();
 	}
 
--- application/controllers/Internalpaypage.php
+++ application/controllers/Internalpaypage.php
@@ -21,52 +21,55 @@ class InternalPaypageController extends ExternalPaypageController {
 	public function init() {
 		Billrun_Factory::db();
 	}
-	
+
 	public function createAction() {
 		$this->allowed();
 		$request = $this->getRequest()->getRequest();
-		$create = new Billrun_ActionManagers_Subscribers_Create();
 		$type = empty($request['aid']) ? 'account' : 'subscriber';
 		if (empty($request['aid'])) {
 			unset($request['aid']);
 		} else {
 			$request['aid'] = intval($request['aid']);
 		}
-		$query = array(
-			"type" => $type,
-			"subscriber" => json_encode($request)
-		);
-		$jsonObject = new Billrun_AnObj($query);
-		if (!$create->parse($jsonObject)) {
-			/* TODO: HANDLE ERROR! */
-			return false;
-		}
-		if (!($res = $create->execute())) {
-			/* TODO: HANDLE ERROR! */
-			return false;
-		}
+
+		if ($request['action'] !== 'updatePaymentGateway') {
+			$create = new Billrun_ActionManagers_Subscribers_Create();
+			$query = array(
+				"type" => $type,
+				"subscriber" => json_encode($request)
+			);
+			$jsonObject = new Billrun_AnObj($query);
+			if (!$create->parse($jsonObject)) {
+				/* TODO: HANDLE ERROR! */
+				return false;
+			}
+			if (!($res = $create->execute())) {
+				/* TODO: HANDLE ERROR! */
+				return false;
+			}
 		
-		//payment_gateway already exist, redirect to return url
-		if(empty($request['payment_gateway'])){
-			header("Location: " . $request['return_url']);
-			return false;
+			//payment_gateway already exist, redirect to return url
+			if (empty($request['payment_gateway'])) {
+				header("Location: " . $request['return_url']);
+				return false;
+			}
 		}
-		
+
 		$secret = Billrun_Factory::config()->getConfigValue("shared_secret.key");
 		$data = array(
-			"aid" => $res['details']['aid'],
-            "name" => $request['payment_gateway'],
+			"aid" => $request['aid'],
+			"name" => $request['payment_gateway'],
 			"type" => $type
 		);
-        $signed = Billrun_Utils_Security::addSignature($data, $secret);
+		$signed = Billrun_Utils_Security::addSignature($data, $secret);
 		$sendData = array(
 			"data" => $signed,
 		);
 
-		header("Location: /paymentgateways/getRequest?data=".json_encode($signed));
+		header("Location: /paymentgateways/getRequest?data=" . json_encode($signed));
 		return false;
 	}
-	
+
 	protected function getPermissionLevel() {
 		return Billrun_Traits_Api_IUserPermissions::PERMISSION_ADMIN;
 	}
