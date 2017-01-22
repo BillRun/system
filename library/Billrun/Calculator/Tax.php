c0ea079ee1074c2843b3ab608cc523b2e8838b0b
Eran Uzan <eran@billrun.net>
2017-01-22 14:15:50 +0200

    Implementation of Usage tax rating

---
+++ library/Billrun/Calculator/Tax.php
@@ -0,0 +1,77 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Tax
+ *
+ * @author eran
+ */
+abstract class Billrun_Calculator_Tax extends Billrun_Calculator {
+
+	protected $nonTaxableTypes = array();
+
+	public function __construct($options = array()) {
+		parent::__construct($options);
+		$this->nonTaxableTypes = Billrun_Factory::config('tax.non_taxable_types', array());
+	}
+
+	public function updateRow($row) {
+		Billrun_Factory::dispatcher()->trigger('beforeCalculatorUpdateRow', array(&$row, $this));
+		$current = $row->getRawData();
+		if( $problemField = $this->isLineDataComplete($current) ) {
+			Billrun_Factory::log("Line {$current['stamp']} is missing/has illigeal value in fields ".  implode(',', $problemField). ' For calcaulator '.$this->getType() );
+			return FALSE;
+		}
+		
+		$subscriber = (new Billrun_Subscriber_Db())->load(array('sid'=>$current['sid'],'time'=>date('Ymd H:i:sP',$current['urt']->sec)));
+		$newData = $this->updateRowTaxInforamtion($current, $subscriber);
+		$row->setRawData($newData);
+
+		Billrun_Factory::dispatcher()->trigger('afterCalculatorUpdateRow', array(&$row, $this));
+		return $row;;
+	}
+
+	/**
+	 * stab function The  will probably  be no need to prepare data for taxing
+	 * @param type $lines
+	 * @return nothing
+	 */
+	public function prepareData($lines) { }
+
+	
+	//================================ Protected ===============================	
+
+	/**
+	 * Retrive all queued lines except from those that are configured not to be retrived.
+	 * @return type
+	 */
+	protected function getLines() {
+		return $this->getQueuedLines( array( 'type' => array( '$nin' => $this->nonTaxableTypes ) ) );
+	}
+
+	public function getCalculatorQueueType() {
+		return 'tax';
+	}
+
+	protected function isLineLegitimate($line) {
+		return true|| !empty($line[Billrun_Calculator_Rate::DEF_CALC_DB_FIELD]); //in general all rated lines are taxable
+	}	
+	
+	protected function isLineDataComplete($line) {
+		$missingFields = array_diff( array('sid'), array_keys($line) );
+		return empty($missingFields) ? FALSE : $missingFields;
+	}
+
+	/**
+	 * Update the line/row with it related taxing data.
+	 * @param array $line The line to update it data.
+	 * @param array $subscriber  the subscriber that is associated with the line
+	 * @return array updated line/row with the tax data
+	 */
+	abstract protected function updateRowTaxInforamtion($line,$subscriber);
+}
