722dafb9eaeebe842ad7b1babce5d4edbe197e58
Idan Daniel <idan.daniel@billrun.net>
2017-01-30 12:11:01 +0200

    fixing vodafone range by urt and not Israel time.

--- application/plugins/vodafone.php
+++ application/plugins/vodafone.php
@@ -148,7 +148,7 @@ class vodafonePlugin extends Billrun_Plugin_BillrunPluginBase {
 		
 		$match2 = array(
 			'$match' => array(
-				'isr_time' => array(
+				'urt' => array(
 					'$gte' => $start_of_year,
 					'$lte' => $end_of_year,
 				),
