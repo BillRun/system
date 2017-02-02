fc814bf8b86b371d5fd94ff1fcdbc9f3068e15a4
Eran Uzan <eran@billrun.net>
2017-02-02 15:59:34 +0200

    Changes to fit invoice to highcomm spcifications

--- library/Billrun/Util.php
+++ library/Billrun/Util.php
@@ -190,6 +190,18 @@ class Billrun_Util {
 		$cycleData = new Billrun_DataTypes_CycleTime($billrunKey);
 		return date('F Y', $cycleData->start());
 	}
+	
+	/**
+	 * Returns a readable date from billrun key.
+	 * example: converts "201607" to : "July 2016"
+	 * 
+	 * @param type $billrunKey
+	 * @return type
+	 */
+	public static function billrunKeyToPeriodSpan($billrunKey,$format) {
+		$cycleData = new Billrun_DataTypes_CycleTime($billrunKey);
+		return date($format, $cycleData->start()) .' - '. date($format, $cycleData->end()-1);
+	}
 
 	/**
 	 * convert corrency.  
