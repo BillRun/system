cb8cb90e679d86e0d84515265cc2814768d07c04
Ofer Cohen <ofer.cohen@billrun.com>
2017-03-26 13:07:36 +0300

    update FE with latest features

--- public/globalSettings.js
+++ public/globalSettings.js
@@ -73,5 +73,19 @@ var globalSetting = {
       itemType: 'discount',
       itemsType: 'discounts',
     },
+    subscription: {
+      collection: 'subscribers',
+      uniqueField: 'sid',
+      itemName: 'subscription',
+      itemType: 'subscription',
+      itemsType: 'subscriptions',
+    },
+    customer: {
+      collection: 'subscribers',
+      uniqueField: 'aid',
+      itemName: 'customer',
+      itemType: 'customer',
+      itemsType: 'customers',
+    },
   },
 };
