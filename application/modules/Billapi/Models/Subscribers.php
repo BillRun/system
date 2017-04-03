349406720b839bfb6020cb1ca015481da6772bda
Ofer Cohen <ofer.cohen@billrun.com>
2017-04-04 00:01:33 +0300

    on subscribers move update also plan activation and services from/to if needed

--- application/modules/Billapi/Models/Subscribers.php
+++ application/modules/Billapi/Models/Subscribers.php
@@ -69,5 +69,72 @@ class Models_Subscribers extends Models_Entity {
 			$service['to'] = isset($service['to']) && $service['to'] <= $this->update['to'] ? $service['to'] : $this->update['to'];
 		}
 	}
+	
+	/**
+	 * move from date of entity including change the previous entity to field
+	 * 
+	 * @return boolean true on success else false
+	 */
+	public function move() {
+		$this->action = 'move';
+		if (!$this->query || empty($this->query) || !isset($this->query['_id'])) { // currently must have some query
+			return;
+		}
+
+		if (!isset($this->update['from'])) {
+			$this->update = array(
+				'from' => new MongoDate()
+			);
+		}
+
+		if ($this->update['from']->sec > $this->before['to']->sec) {
+			throw new Billrun_Exceptions_Api(0, array(), 'Requested start date greater than end date');
+		}
+
+		$this->checkMinimumDate($this->update, 'from');
+
+		$keyField = $this->getKeyField();
+		$query = array(
+			$keyField => $this->before[$keyField],
+			'to' => array(
+				'$lte' => $this->before['from'],
+			)
+		);
+		$previousEntry = $this->collection->query($query)->cursor()->sort(array('to' => -1))->current();
+
+		if (!empty($previousEntry) && !$previousEntry->isEmpty() && $previousEntry['from']->sec > $this->update['from']->sec) {
+			throw new Billrun_Exceptions_Api(0, array(), 'Requested start date is less than previous end date');
+		}
+		
+		if ($this->before['plan_activation']->sec == $this->before['from']->sec) {
+			$this->update['plan_activation'] = $this->update['from'];
+		}
+		
+		foreach($this->before['services'] as $key => $service) {
+			if ($service['from']->sec == $this->before['from']->sec) {
+				$this->update['services'][$key]['from'] = $this->update['from'];
+			}
+		}
+
+		$status = $this->dbUpdate($this->query, $this->update);
+		if (!isset($status['nModified']) || !$status['nModified']) {
+			return false;
+		}
+		$this->trackChanges($this->query['_id']);
+
+		if (!empty($previousEntry) && !$previousEntry->isEmpty()) {
+			$update = array('to' => new MongoDate($this->update['from']->sec - 1));
+			foreach($previousEntry['services'] as $key => $service) {
+				if ($service['to']->sec == $update['to']) {
+					$this->update['services'][$key]['to'] = $update['to'];
+				}
+			}
+			$this->setQuery(array('_id' => $previousEntry['_id']->getMongoID()));
+			$this->setUpdate($update);
+			$this->setBefore($previousEntry);
+			return $this->update();
+		}
+		return true;
+	}
 
 }
