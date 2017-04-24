7ec2839c0b894dad60af5a6ab7e9b25b7cbcfa22
Ofer Cohen <ofer.cohen@billrun.com>
2017-04-24 11:17:56 +0300

    fix the way remain is calculated

--- library/Billrun/UpdateByDelta/Subscribersautorenew.php
+++ library/Billrun/UpdateByDelta/Subscribersautorenew.php
@@ -68,8 +68,8 @@ class Billrun_UpdateByDelta_Subscribersautorenew extends Billrun_UpdateByDelta_U
 			!empty($existing['from']->sec) && !empty($existing['to']->sec) && 
 			($expected['from']->sec != $existing['from']->sec || $expected['to']->sec != $existing['to']->sec)) {
 			$expected['next_renew_date'] = Billrun_Utils_Autorenew::getNextRenewDate($expected['from']->sec);
-			$expected['remain'] = Billrun_Utils_Autorenew::countMonths(time(), $expected['to']->sec);
 			$expected['done'] = Billrun_Utils_Autorenew::countMonths($expected['from']->sec, time());
+			$expected['remain'] = Billrun_Utils_Autorenew::countMonths($expected['from']->sec, $expected['to']->sec) - $expected['done'];
 		}
 
 		// Update the record.
