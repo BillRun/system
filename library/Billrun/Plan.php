9ed8f0d18bb2a572678f501d48c8c7a14ee60658
Eran Uzan <eran.uzan@billrun.com>
2017-04-26 19:51:55 +0300

    Removed manual (debug) timezone adjusment

--- library/Billrun/Plan.php
+++ library/Billrun/Plan.php
@@ -337,7 +337,7 @@ class Billrun_Plan extends Billrun_Service {
 			// HACK :  fix for the month length differance between the  activation and the  plan change
 			if(round($endOffset -1,6) == round($startOffset,6) && $activation && $startOffset > 0) {
 				$startFratcion = 1 -($startOffset-floor($startOffset));
-				$currentDays = date('t',Billrun_Plan::monthDiffToDate($endOffset, $activation)-3601);
+				$currentDays = date('t',Billrun_Plan::monthDiffToDate($endOffset, $activation));
 				$startPricing += (($startFratcion * date('t',$activation)) /  $currentDays - $startFratcion) ;
 			}
 		}
@@ -346,7 +346,7 @@ class Billrun_Plan extends Billrun_Service {
 			// HACK :  fix for the month length differance between the  activation and the  plan change
 			if(round($endOffset -1,6) == round($startOffset,6) && $activation && $startOffset > 0) {
 				$endFratcion = 1 -($startOffset-floor($startOffset));
-				$currentDays = date('t',Billrun_Plan::monthDiffToDate($endOffset, $activation)-3601);
+				$currentDays = date('t',Billrun_Plan::monthDiffToDate($endOffset, $activation));
 				$endPricing += (($endFratcion * date('t',$activation)) / $currentDays - $endFratcion) ;
 			}
 		}
