2daa9f48a12ff9a0601e9f45b4fe276bd79bdbb2
yonatanr <yonatan@billrun.net>
2017-05-08 18:23:47 +0300

    add comments for some of the changes

--- application/controllers/Action/Realtimeevent.php
+++ application/controllers/Action/Realtimeevent.php
@@ -138,7 +138,7 @@ class RealtimeeventAction extends ApiAction {
 			$line["mscc_data"][0]['usagev'] = ( isset($line['usagev']) ? $line['usagev'] : 0);
 			$current["mscc_data"][] = $line["mscc_data"][0];
 		}
-		$current['session_id'] = $this->event['session_id'];
+		$current['session_id'] = $this->event['session_id']; // returns the original session_id of the request to the unified response (and not the modified one)
 		return $current;
 	}
 
@@ -148,7 +148,7 @@ class RealtimeeventAction extends ApiAction {
 				$new_event = $this->event;
 				$new_event["mscc_data"] = array(0 => $mscc_service);
 				$new_event['usaget'] = $this->usaget;
-				$new_event['session_id'] .= $mscc_service['rating_group']; 
+				$new_event['session_id'] .= $mscc_service['rating_group'];  // each one of the splitting events should have different session_id (for rebalance and unify purposes)
 				$new_event['stamp'] = Billrun_Util::generateArrayStamp($new_event);
 
 				$this->events[] = $new_event;
