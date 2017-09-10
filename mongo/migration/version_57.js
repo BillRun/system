7ad35179ef1947e8d6872890fe97257405e5bb72
Snir Cohen <snir.cohen@billrun.com>
2017-09-10 17:01:30 +0300

    BRCD-1002 - Adding weight property type to units of measure. added fields to config.export, updated migration script accordingly. moved to here from version_56.js

--- mongo/migration/version_57.js
+++ mongo/migration/version_57.js
@@ -3,3 +3,17 @@
  * Please try to avoid using migration script and instead make special treatment in the code!
  */
 
+
+// BRCD-1002- Add weight property type to units of measure
+var lastConfig = db.config.find().sort({_id: -1}).limit(1).pretty()[0];
+if (lastConfig.property_types && lastConfig.property_types.indexOf('weight') === -1) {
+	delete lastConfig['_id'];
+	lastConfig.property_types.push(
+			{
+				"system": true,
+				"type": "weight",
+				"uom": [{"unit": 1, "name": "mg", "label": "mg"}, {"unit": 1000, "name": "g", "label": "g"}, {"unit": 1000000, "name": "kg", "label": "kg"}, {"unit": 1000000000, "name": "ton", "label": "ton"}],
+				"invoice_uom": "kg"
+			});
+	db.config.insert(lastConfig);
+}
