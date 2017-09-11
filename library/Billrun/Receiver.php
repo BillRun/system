14dd831e3d351c1be806ae5ca5c61fbbb117cd94
Snir Cohen <snir.cohen@billrun.com>
2017-09-11 15:25:27 +0300

    BRCD-1047 - Verify regex in input processor. Changed condition a bit - previous version accepted: filename_regex == "" Which is an unwanted option.

--- library/Billrun/Receiver.php
+++ library/Billrun/Receiver.php
@@ -41,7 +41,7 @@ abstract class Billrun_Receiver extends Billrun_Base {
 	public function __construct($options = array()) {
 		parent::__construct($options);
 
-		if (isset($options['filename_regex']) || isset($options['receiver']['connections'][0]['filename_regex'])) {
+		if (isset($options['filename_regex']) || !empty($options['receiver']['connections'][0]['filename_regex'])) {
 			$this->filenameRegex = isset($options['receiver']['connections'][0]['filename_regex']) ? $options['receiver']['connections'][0]['filename_regex'] : $options['filename_regex'];
 		}
 		if (isset($options['receiver']['limit']) && $options['receiver']['limit']) {
