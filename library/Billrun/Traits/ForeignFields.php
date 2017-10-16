d4c603853ab267f3222818bbd58fe65013beb715
Eran Uzan <eran.uzan@billrun.com>
2017-10-16 13:23:32 +0300

    Added Missed Trait file (BRCD-1094)

---
+++ library/Billrun/Traits/ForeignFields.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * @package         Billing
+ * @copyright       Copyright (C) 2012-2016 BillRun Technologies Ltd. All rights reserved.
+ * @license         GNU Affero General Public License Version 3; see LICENSE.txt
+ */
+
+/**
+ * Billing tarit to add foreign fields logic to generated cdr/line
+ *
+ * @package  Trait
+ * @since    0.5
+ */
+
+trait Billrun_Traits_ForeignFields  {
+	/**
+	 * This array  will hold all the  added foregin fields that  were added to the CDR/row/line.
+	 */
+	protected $addedForeignFields = array();
+	
+	
+	protected function getAddedFoerignFields() {
+		return $this->addedForeignFields;
+	}
+	
+	protected function clearAddedForeignFields() {
+		$this->addedForeignFields = array();
+	}
+	
+
+	protected function getForeignFields($foreignEntities,$existsingFields = array()) {
+		$this->clearAddedForeignFields();
+		$foreignFieldsData = !empty($existsingFields) ? $existsingFields : array();
+		$forignFieldsConf = array_filter(Billrun_Factory::config()->getConfigValue('lines.fields', array()), function($value) {
+			return isset($value['foreign']);	
+		});
+		
+		foreach ($forignFieldsConf as $fieldConf) {
+			if (!empty($foreignEntities[$fieldConf['foreign']['entity']]) ) {
+				if(!is_array($foreignEntities[$fieldConf['foreign']['entity']]) || Billrun_Util::isAssoc($foreignEntities[$fieldConf['foreign']['entity']])) {
+					Billrun_Util::setIn($foreignFieldsData, $fieldConf['field_name'], $this->getForeginEntityFieldValue($foreignEntities[$fieldConf['foreign']['entity']], $fieldConf['foreign']['field']));
+				} else {
+					foreach ($foreignEntities[$fieldConf['foreign']['entity']] as $idx => $foreignEntity) {
+						Billrun_Util::setIn($foreignFieldsData, $fieldConf['field_name'].'.'.$idx, $this->getForeginEntityFieldValue($foreignEntity, $fieldConf['foreign']['field']));
+					}
+				}
+				$this->addedForeignFields[] = preg_replace('/\..+$/','',$fieldConf['field_name']);
+			}
+		}
+		return $foreignFieldsData;
+	}
+	
+	protected function getForeginEntityFieldValue($foreignEntity, $field) {
+		if(is_object($foreignEntity) && method_exists($foreignEntity, 'getData')) {
+			$foreignEntity = $foreignEntity->getData();
+		}
+		return $foreignEntity[$field];
+	}
+
+}
