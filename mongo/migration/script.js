6c3ca3cd0d09c13d11d2a2e8b5ce9f1b43b8fdad
idan <idan.daniel@billrun.com>
2018-02-04 16:02:42 +0200

    BRCD-1324 - Save CreditGuard last 4 digits in the account active payment gateway field.

--- mongo/migration/script.js
+++ mongo/migration/script.js
@@ -83,3 +83,14 @@ for (var i in lastConfig['file_types']) {
 	}
 }
 db.config.insert(lastConfig);
+
+//BRCD-1324 - Save CreditGuard last 4 digits in the account active payment gateway field
+db.subscribers.find({type:"account", 'payment_gateway.active.name':"CreditGuard"}).forEach(
+		function(obj) {
+			var activeGateway = obj.payment_gateway.active;
+			var token = activeGateway.card_token;
+			var fourDigits = token.substring(token.length - 4, token.length);
+			activeGateway.four_digits = fourDigits;
+			db.subscribers.save(obj)
+		}
+)
