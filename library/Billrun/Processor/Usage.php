58fc09c176c4bf1ef45c5c231458fd9e045e2754
Snir Cohen <snir.cohen@billrun.com>
2018-05-06 10:31:59 +0300

    BRCD-1405 - Fully support hierarchical structure in input processor - Fixes.

--- library/Billrun/Processor/Usage.php
+++ library/Billrun/Processor/Usage.php
@@ -262,8 +262,9 @@ class Billrun_Processor_Usage extends Billrun_Processor {
 		$usagevFields = is_array($this->volumeSrc) ? $this->volumeSrc : array($this->volumeSrc);
 		if (!empty($usagevFields)) {
 			foreach ($usagevFields as $usagevField) {
-				if (!is_null(Billrun_util::getIn($userFields, $usagevField)) && is_numeric(Billrun_util::getIn($userFields, $usagevField))) {
-					$volume += floatval(Billrun_util::getIn($userFields, $usagevField));
+				$usagev = Billrun_util::getIn($userFields, $usagevField);
+				if (!is_null($usagev) && is_numeric($usagev)) {
+					$volume += floatval($usagev);
 				}
 				else {
 					Billrun_Factory::log('Usage volume field ' . $usagevField . ' is missing or invalid for file ' . basename($this->filePath), Zend_Log::ALERT);
