e1ebb70bebdfa0a7da241c4d487b5bc3f7a4fecb
Roman <roman@billrun.net>
2018-06-05 18:29:18 +0300

    BRCD-1224 feat(RatesImpotrer) - Base import create and update alloed only by KEY without multi-values fields

--- application/modules/Billapi/Models/Action/Import/Rates.php
+++ application/modules/Billapi/Models/Action/Import/Rates.php
@@ -19,12 +19,12 @@ class Models_Action_Import_Rates extends Models_Action_Import {
 	/**
 	 * @var array
 	 */
-	protected $exists_rates = array();
+	protected $rates_by_plan = array();
 	
 	/**
 	 * On Create action check for:
 	 * 1. Create Rate  - OK
-	 * 2. Create rate revision
+	 * 2. Not allow rate revision
 	 * 3. Update PLAN price
 	 *  or on Update actin check for:
 	 * 1. Update rate revision
@@ -32,26 +32,70 @@ class Models_Action_Import_Rates extends Models_Action_Import {
 	 * 
 	 */
 	
-	/**
-	 * Allow to import rates revisiona in CREATE import type
-	 */
-//	protected function getImportParams($entity) {
-//		if($this->isExistingEntity($entity)) {
-//			$this->setImportOperation('permanentchange');
-//		}
-//		return parent::getImportParams($entity);
-//	}
+	protected function runQuery() {
+		$rates = array();
+		foreach ($this->update as $rate) {
+			if (empty($rates[$rate['key']])) {
+				$rates[$rate['key']] = array();
+			}
+			if(!empty($rate['rates'])) {
+				foreach ($rate['rates'] as $usaget => $pricing) {
+					$rates[$rate['key']] = array_merge($rates[$rate['key']], array_keys($pricing));
+				}
+			}
+		}
+		$this->rates_by_plan = $rates;
+		return parent::runQuery();
+	}
 	
-//	protected function isExistingEntity($entity) {
-//		$key = $entity['key'];
-//		if (in_array($key, $this->exists_rates)) {
-//			return true;
-//		} else {
-//			$query = array('key' => $key);
-//			if(Billrun_Factory::db()->ratesCollection()->query($query)->count() > 0) {
-//				$this->exists_rates[] = $key;
-//				return true;
-//			}
-//		}
-//	}
+	protected function importEntity($entity) {
+		$unique_plan_rates = count(array_unique($this->rates_by_plan[$entity['key']]));
+		$plan_rates = count($this->rates_by_plan[$entity['key']]);
+		if($this->getImportOperation() == 'create' && $unique_plan_rates != $plan_rates){
+			return 'Create revision not allowd with import action Create, please use Update';
+		}
+		foreach ($entity['rates'] as $usaget => $rates) {
+			if($rates) {
+				$plans = array_keys($rates);
+				$withBase = in_array('BASE',$plans);
+				if (count($plans) > 1 || !$withBase) {
+					foreach ($plans as $plan_name) {
+						if($plan_name !== 'BASE'){
+							$query = array(
+								'effective_date' => empty($entity['effective_date']) ? $entity['from'] : $entity['effective_date'],
+								'name' => $plan_name
+							);
+							$update = array(
+								"rates.{$entity['key']}" => array(
+									$usaget => $rates[$plan_name]
+								),
+								'from' => $entity['from']
+							);
+							$params = array(
+								'collection' => 'plans',
+								'request' => array(
+									'action' => 'permanentchange',
+									'update' => json_encode($update),
+									'query' => json_encode($query)
+								)
+							);
+							$entityModel = $this->getEntityModel($params);
+							$entityModel->permanentchange();
+							
+							// Remove plan rates
+							unset($entity['rates'][$usaget][$plan_name]);
+							if (empty($entity['rates'][$usaget])) {
+								unset($entity['rates'][$usaget]);
+							}
+							if(empty($entity['rates'])) {
+								unset($entity['rates']);
+							}
+						}
+					}
+				}
+			}
+		}
+		return parent::importEntity($entity);
+	}
+	
 }
