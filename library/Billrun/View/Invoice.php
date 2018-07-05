cf86bc2e01f0c2fc80abdcc7ff175ac183303dfd
Eran Uzan <eran.uzan@billrun.com>
2018-07-05 16:22:31 +0300

    BRCD-1505 : Removed default value from the code.

--- library/Billrun/View/Invoice.php
+++ library/Billrun/View/Invoice.php
@@ -226,6 +226,6 @@ class Billrun_View_Invoice extends Yaf_View_Simple {
 	}
 	
 	public function shouldRatebeDisplayed($usageData,$section='all') {
-		return !Billrun_Util::regexArrMatch(Billrun_Factory::config()->getConfigValue('invoice_export.'.$section.'.hide_rates',array('/PREPRICED.*CALL.*TO_ABROAD/')),$usageData['rate']);
+		return !Billrun_Util::regexArrMatch(Billrun_Factory::config()->getConfigValue('invoice_export.hide_rates.'.$section,array()),$usageData['rate']);
 	}
 }
