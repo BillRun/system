d37e9bef7b267b687cebfe444f1c567afb005c2e
killawatt <shani@billrun.net>
2018-07-12 14:17:22 +0300

    Add <coll, key> counters index to migration script (BRCD-1455)

--- mongo/migration/script.js
+++ mongo/migration/script.js
@@ -298,5 +298,6 @@ for (var i in propertyTypes) {
 db.billrun.update({'attributes.invoice_type':{$ne:'immediate'}, billrun_key:{$regex:/^[0-9]{14}$/}},{$set:{'attributes.invoice_type': 'immediate'}},{multi:1});
 
 db.rebalance_queue.ensureIndex({"creation_date": 1}, {unique: false, "background": true})
+db.counters.ensureIndex({coll: 1, key: 1}, { sparse: false, background: true});
 
 db.config.insert(lastConfig);
