6fba02a7491ed2610950138564ea2dea60582a5c
Eran Uzan <eran.uzan@billrun.com>
2018-09-03 16:33:37 +0300

    Added upper limit to the messages that will be shwon

--- library/Billrun/View/Invoice.php
+++ library/Billrun/View/Invoice.php
@@ -237,7 +237,8 @@ class Billrun_View_Invoice extends Yaf_View_Simple {
 		$msgs = !$sub->isEmpty() && !empty($sub['invoice_messages']) ? $sub['invoice_messages'] : [];
 		$retMsgs = [];
 		foreach($msgs as $msg) {
-			if(strtotime($msg['entry_time']) > $this->data['start_date']->sec) {
+			$entryTime = strtotime($msg['entry_time']);
+			if( $this->data['start_date']->sec <= $entryTime && $entryTime < $this->data['end_date']->sec ) {
 				$retMsgs [] = $msg;
 			}
 		}
