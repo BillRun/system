8462b281ba87f09f2fed64101b7503b15e8d40cb
Shani <shani@billrun.net>
2019-01-08 15:47:33 +0000

    Merge branch 'brcd_1725_b' into 'release-5.9-beta2'
    BRCD-1725 - Reopened subscriber is charged for an old service
    
    See merge request sdoc/billrun!2790
    
    (cherry picked from commit 412a63e9cc971eecf4db1950a7bccd2f94964188)
    
    501e1491 BRCD-1725 - Reopened subscriber is charged for an old service
    474e22b3 Removed unnecessary code.

--- library/Billrun/Cycle/Subscriber.php
+++ library/Billrun/Cycle/Subscriber.php
@@ -434,14 +434,9 @@ class Billrun_Cycle_Subscriber extends Billrun_Cycle_Common {
 
 		$subscriberPlans = array();
 		$services = array();
-		$substart = PHP_INT_MAX;
 		$subend = 0;
 		foreach ($current as $subscriber) {
 			$subscriber = $this->handleSubscriberDates($subscriber, $endTime);
-			//Find the earliest instance of the subscriber
-			foreach(Billrun_Util::getFieldVal($subscriber['plans'],array()) as  $subPlan) {
-				$substart = min($subPlan['plan_activation']->sec, $substart);
-			}
 			$subend = max($subscriber['sto'], $subend);
 			// Get the plans
 			$subscriberPlans= array_merge($subscriberPlans,Billrun_Util::getFieldVal($subscriber['plans'],array()));
@@ -454,7 +449,7 @@ class Billrun_Cycle_Subscriber extends Billrun_Cycle_Common {
 		foreach($services as $service) {
 				//Adjust serives that mistakenly started before the subscriber existed to start at the  same time  of the subscriber creation
 				$service['end'] =  min($subend, $service['end']);
-				$service['start'] =  max($substart, $service['start']);
+				$service['start'] =  max($subscriber['activation_date'], $service['start']);
 				$servicesAggregatorData[$service['end']][] = $service;
 		}
 
