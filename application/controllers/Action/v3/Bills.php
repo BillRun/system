596049c00aafa6ee5966c7d5e17c9e91e1f918e1
Yonatan <yonatan@billrun.com>
2019-03-31 15:16:53 +0300

    BRCD-1798: move Bills API to be under version 3 only

--- application/controllers/Action/v3/Bills.php
+++ application/controllers/Action/v3/Bills.php
@@ -9,9 +9,45 @@
 require_once APPLICATION_PATH . '/application/controllers/Action/Api.php';
 
 class V3_paymentHistoryAction extends ApiAction {
+	use Billrun_Traits_Api_UserPermissions;
 
 	public function execute() {
-		$this->forward('paymenthistory');
-		return false;
+		$this->allowed();
+		$request = $this->getRequest();
+		try {
+			switch ($request->get('action')) {
+				case 'search' :
+				default :
+					$payments = $this->searchPayments($request);
+			}
+
+			if ($payments !== FALSE) {
+				$this->getController()->setOutput(array(array(
+						'status' => 1,
+						'desc' => 'success',
+						'input' => $request->getPost(),
+						'details' => array(
+							'payments' => $payments,
+						)
+				)));
+			}
+		} catch (Exception $ex) {
+			$this->setError($ex->getMessage(), $request->getPost());
+			return;
+		}
+	}
+	
+	protected function searchPayments($request) {
+		$aid = filter_var($request->get('aid'), FILTER_VALIDATE_INT);
+		$aid = $aid === FALSE ? NULL : $aid;
+
+		//$months_back = $request->get('months_back');
+		$to = $request->get('to');
+		$from = $request->get('from');
+		return Billrun_Bill_Payment::getPayments($aid, array(), array(), date('Y/m/d',  strtotime($to)), date('Y/m/d',  strtotime($from)), null, true, true);
+	}
+	
+	protected function getPermissionLevel() {
+		return Billrun_Traits_Api_IUserPermissions::PERMISSION_READ;
 	}
 }
