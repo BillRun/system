896f66b7495c4f5274fc64952c09346b4f5ef574
Yonatan <yonatan@billrun.com>
2019-03-31 13:44:43 +0300

    BRCD-1798: redirect [HOST]/api/v/[VERSION] to the specific version's action

--- application/controllers/Action/v3/Bill.php
+++ application/controllers/Action/v3/Bill.php
@@ -2,7 +2,7 @@
 
 /**
  * @package         Billing
- * @copyright       Copyright (C) 2012-2016 BillRun Technologies Ltd. All rights reserved.
+ * @copyright       Copyright (C) 2012-2019 BillRun Technologies Ltd. All rights reserved.
  * @license         GNU Affero General Public License Version 3; see LICENSE.txt
  */
 require_once APPLICATION_PATH . '/application/controllers/Action/Api.php';
