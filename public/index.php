17cb2a2f9f10648a08a5829f25ecbe75452dfd8f
Roman <roman@billrun.net>
2019-04-30 17:55:09 +0300

    fic commit - remove local file changes

--- public/index.php
+++ public/index.php
@@ -7,13 +7,6 @@
  */
 
 defined('APPLICATION_PATH') || define('APPLICATION_PATH', dirname(__DIR__));
-
-error_reporting( E_ALL );
-header('Access-Control-Allow-Credentials: true');
-header("Access-Control-Allow-Origin: ".$_SERVER['HTTP_ORIGIN']);
-
-
-
 require_once(APPLICATION_PATH . DIRECTORY_SEPARATOR . 'conf' . DIRECTORY_SEPARATOR . 'config.php');
 $app = new Yaf_Application(BILLRUN_CONFIG_PATH);
 try {
