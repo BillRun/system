9a1873648a91bba82b894742852a860072cf535b
Roman <roman@billrun.net>
2019-07-31 18:48:20 +0300

    BRCD-1938(Export) - New bill API export

---
+++ application/modules/Billapi/Models/Action/Export.php
@@ -0,0 +1,120 @@
+<?php
+
+/**
+ * @package         Billing
+ * @copyright       Copyright (C) 2012-2016 BillRun Technologies Ltd. All rights reserved.
+ * @license         GNU Affero General Public License Version 3; see LICENSE.txt
+ */
+
+/**
+ * Billapi unique get operation
+ * Retrieve list of entities while the key or name field is unique
+ * This is accounts unique get
+ *
+ * @package  Billapi
+ * @since    5.5
+ */
+class Models_Action_Export extends Models_Action {
+
+	public function execute() {
+		$output = array();
+		$mapper = $this->getCsvMapper();
+		if (Billrun_Util::getIn($this->options, 'headers', true)) {
+			$output[] = $this->getCsvHeaders($mapper);
+		}
+		$entries = $this->getDataToExport($this->getExportQuery());
+		foreach ($entries as $entry) {
+			$output[] = $this->getRow($entry, $mapper);
+		}
+		return $output;
+	}
+
+	protected function getCsvMapper() {
+		$fields = Billrun_Factory::config()->getConfigValue('discounts.fields', array());
+		$exportable_fields = array_filter($fields, function($field) {
+			return Billrun_Util::getIn($field, 'exportable', true);
+		});
+		$mapper = array_reduce($exportable_fields, function ($acc, $field) {
+			$acc[$field['field_name']] = $field;
+			return $acc;
+		}, []);
+		return $mapper;
+	}
+
+	protected function getRowValue($data, $path, $params) {
+		$value = Billrun_Util::getIn($data, explode('.', $path), null);
+		$type = Billrun_Util::getIn($params, 'type', 'string');
+		switch ($type) {
+			case 'date':
+			case 'datetime':
+				return Billrun_Utils_Mongo::convertMongoDatesToReadable($value);
+			case 'daterange':
+				$values = [];
+				if (empty($value)) {
+					return '';
+				}
+				foreach ($value as $range) {
+					$from = Billrun_Utils_Mongo::convertMongoDatesToReadable($range['from']);
+					$to = Billrun_Utils_Mongo::convertMongoDatesToReadable($range['to']);
+					$values[] = "{$from} - $to";
+				}
+				return implode(" ,", $values);
+			case 'range':
+				$values = [];
+				if (!empty($value)) {
+					return '';
+				}
+				foreach ($value as $range) {
+					$values[] = "{$range['from']} - {$range['to']}";
+				}
+				return implode(" ,", $values);
+			case 'percentage':
+				return ($value * 100) . '%';
+			case 'boolean':
+				return $value ? 'yes' : 'no';
+			default:
+				if (!empty($value) && Billrun_Util::getIn($params, 'multiple', false)) {
+					return implode(" ,", $value);
+				}
+				return $value;
+		}
+	}
+
+	protected function getRow($data, $mapper) {
+		$line = array();
+		foreach ($mapper as $path => $map) {
+			$line[] = $this->getRowValue($data, $path, $map);
+		}
+		return $line;
+	}
+
+	protected function getCsvHeaders($mapper) {
+		$headers = [];
+		foreach ($mapper as $key => $map) {
+			$headers[] = !empty($map['title']) ? $map['title'] : $key;
+		}
+		return $headers;
+	}
+
+	protected function getExportQuery() {
+		$query = [];
+		foreach ($this->query as $key => $value) {
+			if ($key === 'from') {
+				$query[$key] = ['$gte' => $value];
+			} else {
+				$query[$key] = $value;
+			}
+		}
+		return $query;
+	}
+
+	protected function getDataToExport($query) {
+		$records = [];
+		$results = $this->collectionHandler->query($query)->cursor();
+		foreach ($results as $result) {
+			$records[] = $result->getRawData();
+		}
+		return $records;
+	}
+
+}
---
+++ application/modules/Billapi/controllers/Export.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * @package         Billing
+ * @copyright       Copyright (C) 2012-2016 BillRun Technologies Ltd. All rights reserved.
+ * @license         GNU Affero General Public License Version 3; see LICENSE.txt
+ */
+require_once APPLICATION_PATH . '/application/modules/Billapi/controllers/Billapi.php';
+
+/**
+ * Billapi controller for getting BillRun entities
+ *
+ * @package  Billapi
+ * @since    5.5
+ */
+class ExportController extends BillapiController {
+
+	protected function runOperation() {
+		$this->action = Models_Action::getInstance($this->params);
+		if (!$this->action) {
+			throw new Billrun_Exceptions_Api(999999, array(), 'Action cannot be found');
+		}
+		$this->output->status = 1;
+		try {
+			$results = $this->action->execute();
+			$this->output->details = $results;
+		} catch (Exception $ex) {
+			$this->output->status = 0;
+			$this->output->errorCode = $ex->getCode();
+			$this->output->desc = $ex->getMessage();
+			Billrun_Factory::log($this->output->errorCode . ': ' . $this->output->desc, Zend_Log::ERR);
+		}
+	}
+
+			/**
+	 *
+	 * @param string $tpl the default tpl the controller used; this will be override to use the general admin layout
+	 * @param array $parameters parameters of the view
+	 *
+	 * @return string the render layout including the page (component)
+	 */
+	protected function render($tpl, array $parameters = null) {
+		$filename = isset($this->params['options']['file_name']) ? $this->params['options']['file_name'] : 'export_' . date('Ymd');
+		if (isset($this->params['options']['delimiter'])) {
+			$this->getView()->delimiter = $this->params['options']['delimiter'];
+		} else if (isset($this->settings['delimiter'])) {
+			$this->getView()->delimiter = $this->settings['delimiter'];
+		} else {
+			$this->getView()->delimiter = ',';
+		}
+		$resp = $this->getResponse();
+		$resp->setHeader("Cache-Control", "max-age=0");
+		$resp->setHeader("Content-type",  "application/csv; charset=UTF-8");
+		$resp->setHeader('Content-disposition', 'inline; filename="' . $filename . '.csv"');
+		return $this->getView()->render('csv.phtml', $parameters);
+	}
+
+
+
+}
