5502a7e11517910902932fc03a24cb582a7a48b6
Eran Uzan <eran.uzan@billrun.com>
2019-09-10 11:44:30 +0300

    BRGT-302 : added recorsive  file receiption to support multiple directories

---
+++ library/Billrun/Receiver/Recursive.php
@@ -0,0 +1,122 @@
+<?php
+
+/**
+ * @package         Billing
+ * @copyright       Copyright (C) 2012-2013 S.D.O.C. LTD. All rights reserved.
+ * @license         GNU Affero General Public License Version 3; see LICENSE.txt
+ */
+
+/**
+ * Billing relocate receiver class
+ *
+ * @package  Billing
+ * @since    0.5
+ */
+class Billrun_Receiver_Recursive extends Billrun_Receiver_Relocate {
+
+	/**
+	 * Type of object
+	 *
+	 * @var string
+	 */
+	static protected $type = 'recursive';
+
+
+	protected $depth = 2;
+	/**
+	 * Flag to check if the relocate should moved the received files or copy them.
+	 * @var boolean defaults to FALSE
+	 */
+	protected $moveFiles = false;
+
+	public function __construct($options) {
+		parent::__construct($options);
+		if(!is_array($this->srcPath)) {
+			$this->srcPath = [$this->srcPath];
+		}
+
+		$this->depth = Billrun_Util::getFieldVal($options['depth'],Billrun_Util::getFieldVal($options['receiver']['depth'],$this->depth));
+	}
+/**
+	 * General function to receive
+	 *
+	 * @return array list of files received
+	 */
+	public function receive() {
+
+		Billrun_Factory::dispatcher()->trigger('beforeLocalFilesReceive', array($this));
+		$ret = array();
+		$type = static::$type;
+		$ret = $this->receiveRecursive($this->srcPath,$type);
+		Billrun_Factory::dispatcher()->trigger('afterLocalFilesReceived', array($this, $ret));
+
+		return $ret;
+	}
+
+	protected function receiveRecursive($paths, $type, $depth = 0) {
+		$ret = [];
+		foreach($paths as $dirPath) {
+			if (!file_exists($dirPath) ) {
+				if(!empty($resolvedDirPaths = glob($dirPath))) {//Support for glob escaped paths
+					$ret = array_merge($ret,$this->receiveRecursive($resolvedDirPaths, $type, $depth));
+				} else {
+					Billrun_Factory::log()->log("NOTICE : SKIPPING $type !!! directory " . $dirPath . " not found!!", Zend_Log::NOTICE);
+				}
+				continue;
+			}
+			$files = $this->getFiles($dirPath, $this->sort, $this->order);
+			$files = array_filter($files, function($i) { return $i != '.' && $i != '..';});
+			$receivedCount = 0;
+			foreach ($files as $file) {
+				$path = $dirPath . DIRECTORY_SEPARATOR . $file;
+				if(is_dir($path) && $this->depth > $depth) {
+					$ret = array_merge($ret,$this->receiveRecursive([$path], $type, $depth+1));
+					continue;
+				}
+
+				if (!$this->isFileValid($file, $path) || is_dir($path)) {
+					Billrun_Factory::log('File ' . $file . ' is not valid', Zend_Log::INFO);
+					continue;
+				}
+				if($fileData = $this->receiveFile($file,$type) === FALSE) {
+					continue;
+				}
+
+				$ret[] = $fileData;
+				if (( ++$receivedCount) >= $this->limit) {
+					break;
+				}
+			}
+		}
+		return $ret;
+	}
+
+	protected function receiveFile($file, $type) {
+		if ( !$this->lockFileForReceive($file, $type) ) {
+			Billrun_Factory::log('File ' . $file . ' has been received already', Zend_Log::INFO);
+			return FALSE;
+		}
+		Billrun_Factory::log()->log("Billrun_Receiver_Base_LocalFiles::receive - handle file {$file}", Zend_Log::DEBUG);
+
+		$fileData = $this->getFileLogData($file, $type);
+		$fileData['path'] = $this->handleFile($path, $file);
+
+		if (!$fileData['path']) {
+			Billrun_Factory::log()->log("NOTICE : Couldn't relocate file from  $path.", Zend_Log::NOTICE);
+			return FALSE;
+		}
+		if(!empty($this->backupPaths)) {
+			$backedTo = $this->backup($fileData['path'], $file, $this->backupPaths, FALSE, FALSE);
+			Billrun_Factory::dispatcher()->trigger('beforeReceiverBackup', array($this, &$fileData['path']));
+			$fileData['backed_to'] = $backedTo;
+			Billrun_Factory::dispatcher()->trigger('afterReceiverBackup', array($this, &$fileData['path']));
+		}
+		if ($this->logDB($fileData) !== FALSE) {
+			return $fileData['path'];
+
+
+		}
+		return FALSE;
+	}
+
+}
