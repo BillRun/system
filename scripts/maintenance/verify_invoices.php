e7300f4c1a668dee7f8f41fe522f8f18656e3fec
Eran Uzan <eran.uzan@billrun.com>
2019-09-19 10:42:53 +0300

    BRGT-1820 : Made the invoice verification alittle faster

--- scripts/maintenance/verify_invoices.php
+++ scripts/maintenance/verify_invoices.php
@@ -39,26 +39,23 @@ foreach(glob($pdfsPath."*.pdf") as $filePath ) {
 $MAX_DPI = 120;
 $MIN_DPI = 105;
 foreach($brokenPdfs as $brokenFile) {
-	$dpi=$MIN_DPI;
+	$dpi=$MAX_DPI;
 	$highestDpi = FALSE;
 	$basePdfGenCmd = 'php -t '.APPLICATION_PATH.'/ '.APPLICATION_PATH.'/public/index.php --env '.Billrun_Factory::config()->getEnv()." --generate --type invoice_export --stamp {$billrunKey}";
 	$aid = preg_replace("/\d+_(\d+)_\d+.*/",'$1',basename($brokenFile));
-	for(;$dpi <= $MAX_DPI;$dpi++) {
+	for(;$dpi >= $MIN_DPI;$dpi--) {
 		$pdfParameters = "--page-size A4 -R 0 -L 0 -T 45 -B 27 --dpi {$dpi} --print-media-type";
 		$log->log('Generating Invoice '.basename($brokenFile).' with dpi of : '.$dpi);
 		exec("$basePdfGenCmd accounts={$aid},{$aid} exporter_flags='{$pdfParameters}' 2>/dev/null");
 		if(!empty(exec("{$pdfVerifyExec} {$brokenFile} 2>/dev/null"))) {
 			$highestDpi = $dpi;
 			$log->log('Invoice generation succesful');
+			break;
 		} else {
 		$log->log("Invoice generation failed for : {$brokenFile}",Zend_Log::WARN);
 		}
 	}
-	if($highestDpi) {
-		$pdfParameters = "--page-size A4 -R 0 -L 0 -T 45 -B 27 --dpi {$highestDpi} --print-media-type";
-		$log->log("Generating final Invoice {$brokenFile} with DPI of :{$highestDpi}");
-		exec("$basePdfGenCmd accounts={$aid},{$aid} exporter_flags='{$pdfParameters}'");
-	} else {
+	if(!$highestDpi) {
 		$log->log("Failed when trying the fix {$brokenFile} invoice",Zend_Log::CRIT);
 	}
 }
