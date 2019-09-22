34a19827192ab0259a5a01d18c4832449368f4a3
killawatt <shani@billrun.net>
2019-09-22 18:08:56 +0300

    Fix collection entrance date (view) + update version

--- library/Billrun/Version.php
+++ library/Billrun/Version.php
@@ -27,10 +27,10 @@ class Billrun_Version {
 	public static $DEV_STATUS = '';
 
 	/** @var  string  Build number. */
-	public static $BUILD = '';
+	public static $BUILD = '2';
 
 	/** @var  string  Release date. */
-	public static $RELDATE = '28-July-2019';
+	public static $RELDATE = '22-September-2019';
 
 	/** @var  string  Link text. */
 	public static $URL = '<a href="https://bill.run">BillRun</a>';
