171f8eead301b63337717af3985ad8969914c8dc
sahar <sahar.leviad@billrun.com>
2019-10-03 10:24:29 +0300

    Make sure no update is made, if "Txid" field is missing - denials case.

--- library/Billrun/Processor/PaymentGateway/Custom.php
+++ library/Billrun/Processor/PaymentGateway/Custom.php
@@ -165,7 +165,8 @@ class Billrun_Processor_PaymentGateway_Custom extends Billrun_Processor_Updater 
 		}
 	}
 	}
-	protected function getOriginalFileStamp($correlatedField) {
+	
+        protected function getOriginalFileStamp($correlatedField) {
 		$query = array(
 			$correlatedField => $this->correlatedValue,
 		);
@@ -175,14 +176,22 @@ class Billrun_Processor_PaymentGateway_Custom extends Billrun_Processor_Updater 
 	}
 	
 	protected function updatePaymentsByRows($data, $currentProcessor) {
+                $No_TxId_counter = 0;
 		foreach ($data['data'] as $row) {
+                    if($row[$this->tranIdentifierField] !== ""){
 			$bill = (static::$type != 'payments') ?  Billrun_Bill_Payment::getInstanceByid($row[$this->tranIdentifierField]) : null;
 			if (is_null($bill) && static::$type != 'payments') {
 				Billrun_Factory::log('Unknown transaction ' . $row[$this->tranIdentifierField] . ' in file ' . $this->filePath, Zend_Log::ALERT);
 				continue;
 			}
 			$this->updatePayments($row, $bill, $currentProcessor);
+                    }else{
+                        $No_TxId_counter++;
+                    }
 		}
+                if($No_TxId_counter > 0){
+                    Billrun_Factory::log()->log('In ' .$No_TxId_counter . ' lines, ' . $this->tranIdentifierField . ' field is empty. No update was made for these lines.', Zend_Log::ALERT);
+                }
 	}
 	
 	protected function updateLogCollection($fileCorrelation) {
