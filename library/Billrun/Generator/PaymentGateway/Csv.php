85e96e6647a6061838186e66a969759deb7c8190
sahar <sahar.leviad@billrun.com>
2019-10-06 14:05:59 +0300

    Removed redundant return statement. Added phpdoc to "validateOptions" function.

--- library/Billrun/Generator/PaymentGateway/Csv.php
+++ library/Billrun/Generator/PaymentGateway/Csv.php
@@ -32,12 +32,17 @@ class Billrun_Generator_PaymentGateway_Csv {
 		}
 		if (!$this->validateOptions($options)) {
 			throw new Exception("Missing options when generating payment gateways csv file for file type " . $options['file_type']);
-			return false;
 		}
 		
 		$this->filePath = $options['local_dir'] . DIRECTORY_SEPARATOR . $options['file_name'];
 	}
-	
+        
+	/**
+	 * validate the config.
+	 *
+	 * @param  array   $options   Relevant params from the config
+	 * @return true in case all the expected config params exist, false otherwise.
+	 */
 	protected function validateOptions($options) {
 		if (isset($options['type']) && !in_array($options['type'], array('fixed', 'separator'))) {
 			return false;
