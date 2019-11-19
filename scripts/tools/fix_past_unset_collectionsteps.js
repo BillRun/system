b8ced7186a22a9bcaf18d664af8ad65b7e87ac77
Yedaya <yedaya.schwalm@billrun.com>
2019-11-19 15:26:33 +0200

    added multiplier & accounts per day

--- scripts/tools/fix_past_unset_collectionsteps.js
+++ scripts/tools/fix_past_unset_collectionsteps.js
@@ -1,7 +1,35 @@
 
-
 var accounts = db.collection_steps.aggregate({$match: {"returned_value.success": {$ne: true}, trigger_date: {$lt: ISODate()}}}, {$group: {_id: "$extra_params.aid", count: {$sum: 1}}}, {$match: {count: {$gte: 1}}});
+var accounts_per_day = 100; //'UNLIMITED';
+var multiplier = 1;
+var acc_arr =[];
+var acc_array_per_day =[];
 
+accounts.forEach(function (acc) {
+
+acc_arr.push(acc);
+
+});
+var acc_sum = acc_arr.length;
+
+
+function calcDaysOfOperation(acc_sum){
+    var sum =0;
+    for (var i = 1; i <=acc_sum; i=(accounts_per_day)*(multiplier)) {
+        sum++;
+        multiplier = multiplier*2;
+        acc_array_per_day.push(i);
+    }
+    return sum;
+}
+function setAlert(stepId, cur_date, type, stepcode) {
+
+    var data = new Date(cur_date.getTime() + type * 24 * 60 * 60000);
+    print("set step " + stepcode + " to " + data);
+
+    db.collection_steps.update({"_id": stepId}, {$set: {"trigger_date": data}});
+
+}
 function DaysToSkip(type) {
 
     switch (type) {
@@ -26,36 +54,45 @@ function DaysToSkip(type) {
     }
 }
 
-function setAlert(stepId, cur_date, type, stepcode) {
+var num_of_days = calcDaysOfOperation(acc_sum);
 
-    var data = new Date(cur_date.getTime() + type * 24 * 60 * 60000);
-    print("set step " + stepcode + " to " + data);
+var pivot = 0;
 
-    db.collection_steps.update({"_id": stepId}, {$set: {"trigger_date": data}});
+for (var cur_day = 0; cur_day < num_of_days; cur_day++) {
 
+    var day_delay = (cur_day+1);
+     
+    for (cur_acc=0; cur_acc < ((acc_array_per_day[cur_day])); cur_acc++) {
+        if(pivot<acc_sum){
+        operateOnacc(acc_arr[pivot]._id,day_delay);
+        pivot++;}
+    }
 }
-accounts.forEach(function (acc) {
-    
-    var acc_aid = acc["_id"];
-    print ("setting steps for account: " +acc_aid);
+
+function operateOnacc(account,day_delay){
+        
+    print ("setting steps for account: " +account);
     var first = true;
 
-    var currnet_acc = db.collection_steps.find({"returned_value.success": {$ne: true}, "extra_params.aid": {$in: [acc_aid]}}).sort({step_code: 1});
-
-    currnet_acc.forEach(function (step) {
+    var currnet_acc = db.collection_steps.find({"returned_value.success": {$ne: true}, "extra_params.aid": account}).sort({step_code: 1});
+    
+        currnet_acc.forEach(function (step) {
+            
         newDate = ISODate();
-        var prevDate = new Date(newDate.getTime() + 1 * 24 * 60 * 60000);
+        var prevDate = new Date(newDate.getTime() + day_delay * 24 * 60 * 60000);
         var nextSkip;
         var stepcode = step["step_code"];
         var id = step["_id"];
         if (first === true) {
-            nextSkip = 1;
-            prevDate = new Date(newDate.getTime());
+            nextSkip = 0;
             first = false;
         } else {
             nextSkip = DaysToSkip(stepcode);
         }
         setAlert(id, prevDate, nextSkip, stepcode);
     });
-});
+}
+
+print("number of accounts " + acc_sum);
+print("process is divided to "+num_of_days +" days");
 
