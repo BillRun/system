13fae12e9836470c04a3851e318a3ce990ec99d5
sahar <sahar.leviad@billrun.com>
2019-11-28 15:12:20 +0200

    BRCD-2125: removed  "getInvoicesInRange" from billrun_billrun - because of duplication

--- library/Billrun/Billrun.php
+++ library/Billrun/Billrun.php
@@ -1066,35 +1066,6 @@ class Billrun_Billrun {
 		return @$this->data['invoice_id'];
 	}
         
-                /**
-         * Function gets start + end time, as Unix Timestamp,aid, and invoice type, and returns an array of this aid's relevant invoices, between those dates.
-         * @param string $startTime
-         * @param string $endTime
-         * @param integer $aid
-         * @param string $type
-         * @return array of the wanted account's invoices, from the requested type, and in the time rang. 
-         */
-        public static function getInvoicesInRange($aid, $startTime, $endTime, $type) {
-            if(!in_array($type, ['immediate', 'regular'])) {
-                Billrun_Factory::log()->log($type . " isn't a valid value of invoice type.", Zend_Log::ERR);
-                return false;
-            }
-            $convertedStartTime = date('YmdHis', $startTime);
-            $convertedEndTime = date('YmdHis', $endTime);
-            $query = array(
-                        'aid' => $aid,
-			'invoice_type' => array('$eq' => $type),
-                        'billrun_key' => array('$gte' => $convertedStartTime, '$lt' => $convertedEndTime)
-		);
-            $sort = array(
-			'billrun_key' => -1,
-		);
-            $fields = array(
-			'billrun_key' => 1,
-                );  
-            $billruns = Billrun_Factory::db()->billsCollection()->query($query)->cursor()->sort($sort);
-            return iterator_to_array($billruns, true);
-        }
 }
 
 // TODO: Why is this here? this is the Billrun class code, this should be in some excute script file.
