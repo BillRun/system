33d4bf65ef43d9841f2de9ee3936b864f9259aeb
or <or@snir-Latitude-E5570>
2020-03-29 18:59:03 +0300

    BRCD-2364: indentation

--- mongo/migration/script.js
+++ mongo/migration/script.js
@@ -1,5 +1,5 @@
 /* 
- * General (idempotent) DB migration script goes here.
+  * General (idempotent) DB migration script goes here.
  * Please try to avoid using migration script and instead make special treatment in the code!
  */
 
@@ -21,16 +21,6 @@ function addFieldToConfig(lastConf, fieldConf, entityName) {
 	}
 	lastConf[entityName]['fields'] = fields;
 
-	return lastConf;
-}
-
-function removeFieldFromConfig(lastConf, field_names, entityName) {
-	if (typeof lastConf[entityName] === 'undefined') {
-		return lastConf;
-	}
-	field_names_to_delete = Array.isArray(field_names) ? field_names : [field_names];
-	var fields = lastConf[entityName]['fields'];
-	lastConf[entityName]['fields'] = fields.filter(field => !field_names_to_delete.includes(field.field_name));
 	return lastConf;
 }
 
@@ -215,8 +205,22 @@ if(lastConfig.invoice_export) {
 	}
 }
 
-// BRCD-2251 remove old vatable filed
-lastConfig = removeFieldFromConfig(lastConfig, 'vatable', 'services');
+//BRCD-1374 : Add taxation support services 
+var vatableField ={
+					"system":true,
+					"select_list" : false,
+					"display" : true,
+					"editable" : true,
+					"multiple" : false,
+					"field_name" : "vatable",
+					"unique" : false,
+					"default_value" : true,
+					"title" : "This service is taxable",
+					"mandatory" : false,
+					"type" : "boolean",
+					"select_options" : ""
+	};
+lastConfig = addFieldToConfig(lastConfig, vatableField, 'services')
 
 //BRCD-1272 - Generate Creditguard transactions in csv file + handle rejections file
 for (var i in lastConfig['payment_gateways']) {
@@ -269,19 +273,27 @@ if(!lastConfig.email_templates) {
 }
 // BRCD-2364: Customer invoicing_day field, Should be a system field, not visible for editing by default.
 //The possible values are 1-28 - should be enforced using the existing "Select list" feature
-var invoicingDayField = {
-	"field_name": "invoicing_day",
-	"title": "Invoicing Day",
-	"mandatory": false,
-	"system": true,
-	"show_in_list": true,
-	"select_list": true,
-	"editable": true,
-	"display": true,
-	"select_options": "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28",
-};
-
-lastConfig['subscribers'] = addFieldToConfig(lastConfig['subscribers'], invoicingDayField, 'account');
+var fields = lastConfig['subscribers']['account']['fields'];
+var found = false;
+for (var field_key in fields) {
+	if (fields[field_key].field_name === "invoicing_day") {
+		found = true;
+	}
+}
+if (!found) {
+	fields.push({
+		"field_name": "invoicing_day",
+		"title": "Invoicing Day",
+		"mandatory": true,
+		"system": true,
+		"show_in_list": true,
+		"select_list": true,
+		"editable": true,
+		"display": true,
+		"select_options": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28]
+	});
+}
+lastConfig['subscribers']['account']['fields'] = fields;
 
 // BRCD-1415 - add system field to account (invoice_shipping_method)
 var fields = lastConfig['subscribers']['account']['fields'];
@@ -347,23 +359,6 @@ db.subscribers.find({type: 'subscriber', 'services.creation_time.sec': {$exists:
 		db.subscribers.save(obj);
 	}
 );
-
-// BRCD-1552 collection
-if (typeof lastConfig['collection']['min_debt'] !== 'undefined' && lastConfig['collection']['settings']['min_debt'] === 'undefined') {
-    lastConfig['collection']['settings']['min_debt'] = lastConfig['collection']['min_debt'];
-}
-delete lastConfig['collection']['min_debt'];
-// BRCD-1562 - steps trigget time
-if (typeof lastConfig['collection']['settings']['run_on_holidays'] === 'undefined') {
-    lastConfig['collection']['settings']['run_on_holidays'] = true;
-}
-if (typeof lastConfig['collection']['settings']['run_on_days'] === 'undefined') {
-    lastConfig['collection']['settings']['run_on_days'] = [true,true,true,true,true,true,true];
-}
-if (typeof lastConfig['collection']['settings']['run_on_hours'] === 'undefined') {
-    lastConfig['collection']['settings']['run_on_hours'] = [];
-}
-
 db.counters.dropIndex("coll_1_oid_1");
 db.counters.ensureIndex({coll: 1, key: 1}, { sparse: false, background: true});
 
@@ -870,28 +865,3 @@ if (db.serverStatus().ok == 0) {
 	sh.shardCollection("billing.billrun", { "aid" : 1, "billrun_key" : 1 } );
 	sh.shardCollection("billing.balances",{ "aid" : 1, "sid" : 1 }  );
 }
-
-db.subscribers.ensureIndex({'invoicing_day': 1 }, { unique: false, sparse: false, background: true });
-db.billrun.ensureIndex( { 'billrun_key': -1, 'attributes.invoicing_day': -1 },{unique: false, background: true });
-db.billrun.dropIndex('billrun_key_-1');
-
-
-//BRCD-2042 - charge.not_before migration script
-db.bills.find({'charge.not_before':{$exists:0}, 'due_date':{$exists:1}}).forEach(
-	function(obj) {
-		if (typeof obj['charge'] === 'undefined') {
-			obj['charge'] = {};
-		}
-		obj['charge']['not_before'] = obj['due_date'];
-		db.bills.save(obj);
-	}
-)
-db.billrun.find({'charge.not_before':{$exists:0}, 'due_date':{$exists:1}}).forEach(
-	function(obj) {
-		if (typeof obj['charge'] === 'undefined') {
-			obj['charge'] = {};
-		}
-		obj['charge']['not_before'] = obj['due_date'];
-		db.billrun.save(obj);
-	}
-)
