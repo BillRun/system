72ff67732ee4b79d282ca0a74c846c9ea035b0bf
or <or@snir-Latitude-E5570>
2020-03-31 17:24:22 +0300

    BRCD-1650: Incorrect monthly upfront charge

--- library/Billrun/Plans/Charge/Upfront/Month.php
+++ library/Billrun/Plans/Charge/Upfront/Month.php
@@ -19,18 +19,19 @@ class Billrun_Plans_Charge_Upfront_Month extends Billrun_Plans_Charge_Upfront {
 	 * @return int
 	 */
 	protected function getFractionOfMonth() {
+		
 		if (empty($this->deactivation) ) {
 			return 1;
 		} 
 
 		// subscriber activates in the middle of the cycle and should be charged for a partial month and should be charged for the next month (upfront) 
 		if ($this->activation > $this->cycle->start() && $this->deactivation > $this->cycle->end()) {
-			$endActivation = strtotime('-1 day', $this->deactivation);
+			$endActivation = strtotime('-1 second', $this->deactivation);
 			return 1 + Billrun_Plan::calcFractionOfMonthUnix($this->cycle->key(), $this->activation, $endActivation);
 		}
 		// subscriber activates in the middle of the cycle and should be charged for a partial month
 		if ($this->activation > $this->cycle->start() && $this->deactivation <= $this->cycle->end()) {
-			$endActivation = strtotime('-1 day', $this->deactivation);
+			$endActivation = strtotime('-1 second', $this->deactivation);
 			return Billrun_Plan::calcFractionOfMonthUnix($this->cycle->key(), $this->activation, $endActivation);
 		}
 
@@ -56,7 +57,7 @@ class Billrun_Plans_Charge_Upfront_Month extends Billrun_Plans_Charge_Upfront {
 		}
 		
 		$lastUpfrontCharge = $this->getPriceForcycle($cycle);
-		$endActivation  = strtotime('-1 day', $this->deactivation);
+		$endActivation  = strtotime('-1 second', $this->deactivation);
 		$refundFraction = 1- Billrun_Plan::calcFractionOfMonthUnix($cycle->key(), $this->activation, $endActivation);
 		
 		return array( 'value' => -$lastUpfrontCharge * $refundFraction, 
