350ad6a9b6b359da3d2d581858426b811067868c
Roman <roman.edneral@billrun.com>
2020-04-28 01:12:21 +0300

    BRCD-2527 feat(InputProcessor) - added option to save URT as rate mapping KEY

--- application/models/Config.php
+++ application/models/Config.php
@@ -1337,7 +1337,7 @@ class ConfigModel {
 //			if ($uniqueFields != array_unique($uniqueFields)) {
 //				throw new Exception('Cannot use same field for different configurations');
 //			}
-			$billrunFields = array('type', 'usaget', 'file', 'connection_type');
+			$billrunFields = array('type', 'usaget', 'file', 'connection_type', 'urt');
 			$customFields = array_merge($customFields, array_map(function($field) {
 				return 'uf.' . $field;
 			}, $customFields));
