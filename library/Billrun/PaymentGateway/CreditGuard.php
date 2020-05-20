a59b50fa5417b2915462ed1086ca2be21f278232
or <or.shaashua@billrun.com>
2020-05-20 13:10:03 +0300

    BRCD-2569: add aid custom data to CreditGuard

--- library/Billrun/PaymentGateway/CreditGuard.php
+++ library/Billrun/PaymentGateway/CreditGuard.php
@@ -226,6 +226,9 @@ class Billrun_PaymentGateway_CreditGuard extends Billrun_PaymentGateway {
 										<user>' . $addonData['txid'] . '</user>
 										 ' . $ZParameter . '
 										<validation>AutoComm</validation>
+										 <customerData>
+											<userData1>' . $addonData['aid'] . '</userData1>
+	                                     </customerData>
 									</doDeal>
 								</request>
 						</ashrait>'
