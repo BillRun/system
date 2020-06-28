fb9dd3279c7d96d57313a0a63f16c000fbf9d003
killawatt <shani@billrun.net>
2020-06-28 11:04:14 +0300

    Update version

--- library/Billrun/Version.php
+++ library/Billrun/Version.php
@@ -27,10 +27,10 @@ class Billrun_Version {
 	public static $DEV_STATUS = '';
 
 	/** @var  string  Build number. */
-	public static $BUILD = '3';
+	public static $BUILD = '4';
 
 	/** @var  string  Release date. */
-	public static $RELDATE = '22-June-2020';
+	public static $RELDATE = '28-June-2020';
 
 	/** @var  string  Link text. */
 	public static $URL = '<a href="https://bill.run">BillRun</a>';
