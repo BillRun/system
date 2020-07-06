ad9f25611bbdc70442e570076ae77736744b6a3a
sahar <sahar.leviad@billrun.com>
2020-07-06 11:36:48 +0300

    BRCD-2643:Added input validations and relevant log messages, to avoid CLI injection

--- application/controllers/Action/CustomPaymentGateway.php
+++ application/controllers/Action/CustomPaymentGateway.php
@@ -33,8 +33,11 @@ require_once APPLICATION_PATH . '/application/controllers/Action/Collect.php';
 		$options['params']['created_by'] = Billrun_Factory::user()->getUsername();
 
 		$options['pay_mode'] = !empty($request->get('pay_mode')) ? $request->get('pay_mode') : null;
-		if ((in_array($options['file_type'], ["transactions_request"])) && (empty($options['gateway_name']) || empty($options['file_type']))) {
+		if ((in_array($options['cpg_type'], ["transactions_request"])) && (empty($options['gateway_name']) || empty($options['file_type']))) {
 			return $this->setError("Action " . $options['cpg_type'] . " must be transferred with both file type and payment gateway.");
+		}
+		if (!$this->validateOptions($options)) {
+			return $this->setError("One or more of the input parameters are not valid. ");
 		}
 		$cmd = 'php ' . APPLICATION_PATH . '/public/index.php ' . Billrun_Util::getCmdEnvParams() . ' --generate --type ' . $options['cpg_type'] . ' payment_gateway=' . $options['gateway_name'] . ' file_type=' . $options['file_type'];
 		if (!is_null($options['pay_mode'])) {
@@ -59,4 +62,28 @@ require_once APPLICATION_PATH . '/application/controllers/Action/Collect.php';
 	protected function getPermissionLevel() {
 		return Billrun_Traits_Api_IUserPermissions::PERMISSION_WRITE;
 	}
+	
+	protected function validateOptions($options) {
+		if (isset($options['pay_mode']) && !in_array($options['pay_mode'], ['one_payment', 'multiple_payments'])) {
+			Billrun_Factory::log("pay_mode parameter's value isn't valid" , Zend_Log::ERR);
+			return false;
+		}
+		if (!isset($options['cpg_type']) || !in_array($options['cpg_type'], ['transactions_request'])) {
+			Billrun_Factory::log("cpg_type parameter's value isn't valid" , Zend_Log::ERR);
+			return false;
+		}
+		$paymentsGatewaysConfig = Billrun_Factory::config()->getConfigValue('payment_gateways', []);
+		if (empty($paymentsGatewaysConfig)) {
+			Billrun_Factory::log("Payment gateways configuration is empty" , Zend_Log::ERR);
+			return false;
+		} else {
+			$gatewaysOptions = array_column($paymentsGatewaysConfig, 'name');
+			if (!in_array($options['gateway_name'], $gatewaysOptions)) {
+				Billrun_Factory::log("gateway_name parameter's value isn't valid" , Zend_Log::ERR);
+				return false;
+			}
+		}
+		return true;
+	}
+
 }
