4379d8280bf7a45bf86589caebd4d6b362e2b274
sahar <sahar.leviad@billrun.com>
2020-08-10 12:15:34 +0300

    BRCD-2752:Updated credit API to support charging customer by rate's usage (in case no aprice was sent), and not only refund.

--- application/controllers/Action/Credit.php
+++ application/controllers/Action/Credit.php
@@ -204,7 +204,7 @@ class CreditAction extends ApiAction {
 	
 	protected function getCreditUsaget($row) {
 		if (!isset($row['aprice'])) {
-			return 'refund';
+			return isset($row['credit_type']) ? $row['credit_type'] : 'refund';
 		}
 		return ($row['aprice'] >= 0 ? 'charge' : 'refund');
 	}
