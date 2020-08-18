dea9b26bb1a71f7132c08876326e49eb0fc63f63
or <or.shaashua@billrun.com>
2020-08-18 15:58:18 +0300

    BRCD-2738: Refund invoice isn't linked to a charge invoice after rejection/denial of payment - insert $payment->save() to deny method

--- library/Billrun/Processor/PaymentGateway/CreditGuard/Denials.php
+++ library/Billrun/Processor/PaymentGateway/CreditGuard/Denials.php
@@ -55,13 +55,6 @@ class Billrun_Processor_PaymentGateway_CreditGuard_Denials extends Billrun_Proce
 			if (!is_null($payment)) {
 				Billrun_Factory::log()->log("Denial was created successfully for payment: " . $newRow['transaction_id'], Zend_Log::NOTICE);
 				$payment->deny($denial);
-				$paymentSaved = $payment->save();
-				if (!$paymentSaved) {
-					Billrun_Factory::log()->log("Denied flagging failed for rec " . $newRow['transaction_id'], Zend_Log::ALERT);
-				} else {
-					$payment->updatePastRejectionsOnProcessingFiles();
-					Billrun_Bill::payUnpaidBillsByOverPayingBills($payment->getAid());
-				}
 			} else {
 				Billrun_Factory::log()->log("Denial was created successfully without matching payment", Zend_Log::NOTICE);
 			}
