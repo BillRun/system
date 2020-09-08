5b15e79479982aa52d6abd9565c85e6a2611150f
killawatt <shani@billrun.net>
2020-09-08 16:23:41 +0300

    Update js

---
+++ public/precache-manifest.4df9d1cce2de621eb35b0b63de0e6090.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "424e306499adfedbe125fa582a654519",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72e3d9be829f0e796014",
+    "url": "/static/css/2.ea29ef5f.chunk.css"
+  },
+  {
+    "revision": "15c8635a8f7033fc9834",
+    "url": "/static/css/main.c0c5715b.chunk.css"
+  },
+  {
+    "revision": "72e3d9be829f0e796014",
+    "url": "/static/js/2.8c64521e.chunk.js"
+  },
+  {
+    "revision": "c0c6914b93e48aa4d589",
+    "url": "/static/js/3.7ab04ba7.chunk.js"
+  },
+  {
+    "revision": "9a5da95e588e38a006a2",
+    "url": "/static/js/4.a33b7050.chunk.js"
+  },
+  {
+    "revision": "15c8635a8f7033fc9834",
+    "url": "/static/js/main.0c874475.chunk.js"
+  },
+  {
+    "revision": "2b6267b240e60768f871",
+    "url": "/static/js/runtime~main.a560c8a1.js"
+  },
+  {
+    "revision": "1f38670a7d7ae21d3c7e2099513f5114",
+    "url": "/static/media/billRun-cloud-logo.1f38670a.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "1f38670a7d7ae21d3c7e2099513f5114",
+    "url": "/static/media/footer-logo.1f38670a.png"
+  }
+]);
