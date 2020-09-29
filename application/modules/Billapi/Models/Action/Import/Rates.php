7b18ee88af73b15f451d71a49478921e5ed4cbba
Roman <roman.edneral@billrun.com>
2020-09-29 13:32:57 +0300

    BRCD-2766 feat(Import) - add support for service price override

--- application/modules/Billapi/Models/Action/Import/Rates.php
+++ application/modules/Billapi/Models/Action/Import/Rates.php
@@ -33,17 +33,26 @@ class Models_Action_Import_Rates extends Models_Action_Import {
 	 */
 	
 	protected function runQuery() {
-		$rates = array();
+		$plan_rates = array();
+		$service_rates = array();
 		foreach ($this->update as $rate) {
 			$keyField = !empty($rate['__UPDATER__']['value']) ? $rate['__UPDATER__']['value'] : 'key';
 			$rates[$keyField] = array();
 			if(!empty($rate['rates'])) {
 				foreach ($rate['rates'] as $usaget => $pricing) {
-					$rates[$keyField] = array_merge($rates[$keyField], array_keys($pricing));
+					if (!isset($plan_rates[$keyField])) {
+						$plan_rates[$keyField] = [];
+					}
+					$plan_rates[$keyField] = array_merge($plan_rates[$keyField], array_keys($pricing['plans']));
+					if (!isset($service_rates[$keyField])) {
+						$service_rates[$keyField] = [];
+					}
+					$service_rates[$keyField] = array_merge($service_rates[$keyField], array_keys($pricing['services']));
 				}
 			}
 		}
-		$this->rates_by_plan = $rates;
+		$this->rates_by_plan = $plan_rates;
+		$this->rates_by_service = $service_rates;
 		return parent::runQuery();
 	}
 	
@@ -59,10 +68,13 @@ class Models_Action_Import_Rates extends Models_Action_Import {
 	
 	protected function importEntity($entity) {
 		$keyField = !empty($entity['__UPDATER__']['value']) ? $entity['__UPDATER__']['value'] : 'key';
-		$plans_names = isset($this->rates_by_plan[$entity[$keyField]]) ? $this->rates_by_plan[$entity[$keyField]] : [];
+		$plans_names = isset($this->rates_by_plan[$keyField]) ? $this->rates_by_plan[$keyField] : [];
 		$unique_plan_rates_count = count(array_unique($plans_names));
 		$plan_rates_count = count($plans_names);
-		if($this->getImportOperation() == 'create' && $unique_plan_rates_count != $plan_rates_count){
+		$services_names = isset($this->rates_by_service[$keyField]) ? $this->rates_by_service[$keyField] : [];
+		$unique_service_rates_count = count(array_unique($services_names));
+		$service_rates_count = count($services_names);
+		if($this->getImportOperation() == 'create' && ($unique_plan_rates_count != $plan_rates_count || $unique_service_rates_count != $service_rates_count)){
 			return 'Create revision not allowd with import action Create, please use Update';
 		}
 		
@@ -81,6 +93,7 @@ class Models_Action_Import_Rates extends Models_Action_Import {
 			if(!$existingRate || $existingRate->isEmpty()) {
 				throw new Exception("Product {$entity['__UPDATER__']['value']} does not exist");
 			}
+			$existingRate = $existingRate->getRawData();
 			$key = $existingRate['key']; // get rate key in update
 			
 			if(!empty($entity['__MULTI_FIELD_ACTION__'])) {
@@ -108,15 +121,43 @@ class Models_Action_Import_Rates extends Models_Action_Import {
 					if($usaget == "_KEEP_SOURCE_USAGE_TYPE_") {
 						$uom_display = $existingRate['rates'][$usagetype]['BASE']['rate'][0]['uom_display'];
 						$entity['rates'][$usagetype] = $rates;
-						foreach ($entity['rates'][$usagetype] as $plan_name => &$plan_rates) {
-							foreach ($plan_rates['rate'] as $key => &$rate) {
-								if ($rate['uom_display']['interval'] === '_KEEP_SOURCE_USAGE_TYPE_UNIT_') {
-									$rate['uom_display']['interval'] = $uom_display['interval'];
-								}
-								if ($rate['uom_display']['range'] === '_KEEP_SOURCE_USAGE_TYPE_UNIT_') {
-									$rate['uom_display']['range'] = $uom_display['range'];
+						if (isset($entity['rates'][$usaget]['plans'])) {
+							foreach ($entity['rates'][$usaget]['plans']  as $plan_name => &$plan_rates) {
+								foreach ($plan_rates['rate'] as $key => &$rate) {
+									if ($rate['uom_display']['interval'] === '_KEEP_SOURCE_USAGE_TYPE_UNIT_') {
+										$rate['uom_display']['interval'] = $uom_display['interval'];
+									}
+									if ($rate['uom_display']['range'] === '_KEEP_SOURCE_USAGE_TYPE_UNIT_') {
+										$rate['uom_display']['range'] = $uom_display['range'];
+									}
 								}
 							}
+						}
+						if (isset($entity['rates'][$usaget]['services'])) {
+							foreach ($entity['rates'][$usaget]['services']  as $service_name => &$service_rates) {
+								foreach ($service_rates['rate'] as $key => &$rate) {
+									if ($rate['uom_display']['interval'] === '_KEEP_SOURCE_USAGE_TYPE_UNIT_') {
+										$rate['uom_display']['interval'] = $uom_display['interval'];
+									}
+									if ($rate['uom_display']['range'] === '_KEEP_SOURCE_USAGE_TYPE_UNIT_') {
+										$rate['uom_display']['range'] = $uom_display['range'];
+									}
+								}
+							}
+							
+						}
+						if (isset($entity['rates'][$usaget]['product'])) {
+							foreach ($entity['rates'][$usaget]['product']  as $product_name => &$product_rates) {
+								foreach ($product_rates['rate'] as $key => &$rate) {
+									if ($rate['uom_display']['interval'] === '_KEEP_SOURCE_USAGE_TYPE_UNIT_') {
+										$rate['uom_display']['interval'] = $uom_display['interval'];
+									}
+									if ($rate['uom_display']['range'] === '_KEEP_SOURCE_USAGE_TYPE_UNIT_') {
+										$rate['uom_display']['range'] = $uom_display['range'];
+									}
+								}
+							}
+							
 						}
 						unset($entity['rates']["_KEEP_SOURCE_USAGE_TYPE_"]);
 					}
@@ -171,61 +212,77 @@ class Models_Action_Import_Rates extends Models_Action_Import {
 		$plan_updates = true;
 		foreach ($entity['rates'] as $usaget => $rates) {
 			if($rates) {
-				$plans = array_keys($rates);
-				$withBase = in_array('BASE',$plans);
-				if (count($plans) > 1 || !$withBase) {
-					foreach ($plans as $plan_name) {
-						if($plan_name !== 'BASE'){
-							// Check if update Plan or Service by serching for plan if not exist update service
-							$planQuery = Billrun_Utils_Mongo::getDateBoundQuery(strtotime($from));
-							$planQuery['name'] = $plan_name;
-							$existingPlan = Billrun_Factory::db()->plansCollection()->query($planQuery)->cursor()->current();
-							$collection = (!$existingPlan || $existingPlan->isEmpty()) ? 'services' : 'plans' ;
-							$rateKey = $existingRate['key'];
-							$existingRateRates = $existingPlan['rates'];
-							$query = array(
-								'effective_date' => $from,
-								'name' => $plan_name
-							);
-							$update = [
-								'from' => $from,
-								'rates' => $existingRateRates,
-							];
-							if (!empty($rates[$plan_name]['rate'])) {
-								$update['rates'][$rateKey][$usaget] = ['rate' => $rates[$plan_name]['rate']];
-							} else if (!empty($rates[$plan_name]['percentage'])) {
-								$update['rates'][$rateKey][$usaget] = ['percentage' => (float)$rates[$plan_name]['percentage']];
-							}
-							$params = array(
-								'collection' => $collection,
-								'request' => array(
-									'action' => 'permanentchange',
-									'update' => json_encode($update),
-									'query' => json_encode($query)
-								)
-							);
-							$entityModel = $this->getEntityModel($params);
-							try {
-								$result = $entityModel->permanentchange();
-								if($result !== true) {
-									$plan_updates = $result;
-								}	
-							} catch (Exception $exc) {
-								$plan_updates = $exc->getMessage();
-							}
-							// Remove plan rates
-							unset($entity['rates'][$usaget][$plan_name]);
-							if (empty($entity['rates'][$usaget])) {
-								unset($entity['rates'][$usaget]);
+				$plans = !empty($rates['plans']) ? array_keys($rates['plans']) : [];// to do add plans or servives
+				$services = !empty($rates['services']) ? array_keys($rates['services']) : [];
+				$withBase = in_array('BASE', $plans) ||  in_array('BASE', $services);
+				if (count($plans) > 1 || count($services) > 1 || !$withBase) {
+					$entities_to_update = [
+						'services' => $services,
+						'plans' => $plans,
+					];
+					foreach ($entities_to_update as $collection => $entity_names) {
+						foreach ($entity_names as $entity_name) {
+							if($entity_name !== 'BASE'){
+								// Check if update Plan or Service by serching for plan if not exist update service
+								$entityQuery = Billrun_Utils_Mongo::getDateBoundQuery(strtotime($from));
+								$entityQuery['name'] = $entity_name;
+								$collectionName = "{$collection}Collection";
+								$existingEntity = Billrun_Factory::db()->{$collectionName}()->query($entityQuery)->cursor()->current();
+								$rateKey = $existingRate['key'];
+								$existingRateRates = isset($existingEntity['rates']) ? $existingEntity['rates'] : [];
+								$query = array(
+									'effective_date' => $from,
+									'name' => $entity_name
+								);
+								$update = [
+									'from' => $from,
+									'rates' => $existingRateRates,
+								];
+								if (!empty($rates[$collection][$entity_name]['rate'])) {
+									$update['rates'][$rateKey][$usaget] = ['rate' => $rates[$collection][$entity_name]['rate']];
+								} else if (!empty($rates[$collection][$entity_name]['percentage'])) {
+									$update['rates'][$rateKey][$usaget] = ['percentage' => (float)$rates[$collection][$entity_name]['percentage']];
+								}
+								$params = array(
+									'collection' => $collection,
+									'request' => array(
+										'action' => 'permanentchange',
+										'update' => json_encode($update),
+										'query' => json_encode($query)
+									)
+								);
+								
+								$entityModel = $this->getEntityModel($params);
+								try {
+									error_log("permanentchange : " . print_r(json_encode($params), 1));
+									$result = $entityModel->permanentchange();
+									if($result !== true) {
+										return $result;
+									}	
+								} catch (Exception $exc) {
+									return $exc->getMessage();
+								}
+								// Remove plan rates
+								unset($entity['rates'][$usaget]['services']);
+								unset($entity['rates'][$usaget]['plan']);
+								if (empty($entity['rates'][$usaget])) {
+									unset($entity['rates'][$usaget]);
+								}
 							}
 						}
 					}
 				}
 			}
 		}
-		// case when request include only plan rates update
-		if(isset($entity['rates']) && empty($entity['rates']) && !in_array('BASE',$plans)) {
-			return $plan_updates;
+		foreach ($entity['rates'] as $usaget => $rates) {
+			if (isset($entity['rates'][$usaget]['product'])) {
+				$entity['rates'][$usaget] = $entity['rates'][$usaget]['product'];
+			} else {
+				unset($entity['rates'][$usaget]);
+			}
+		}
+		if (empty($entity['rates'])) {
+			unset($entity['rates']);
 		}
 		return parent::importEntity($entity);
 	}
