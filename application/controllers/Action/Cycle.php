0ebd556012f8094eff1b2ec753d826827dd6ee35
Ofer Cohen <ofer.cohen@billrun.com>
2020-11-02 00:47:09 +0200

    adapt new yaf convention  is null on yaf action abstract

--- application/controllers/Action/Cycle.php
+++ application/controllers/Action/Cycle.php
@@ -33,7 +33,7 @@ class CycleAction extends Action_Base {
 			'fetchonly' => true,
 		);
 
-		$options = $this->_controller->getInstanceOptions($possibleOptions);
+		$options = $this->getController()->getInstanceOptions($possibleOptions);
 		if ($options === false) {
 			return false;
 		}
@@ -73,7 +73,7 @@ class CycleAction extends Action_Base {
 	 */
 	public function execute() {		
 		$options = $this->buildOptions();
-		$extraParams = $this->_controller->getParameters();
+		$extraParams = $this->getController()->getParameters();
 		if (!empty($extraParams)) {
 			$options = array_merge($extraParams, $options);
 		}
@@ -86,7 +86,7 @@ class CycleAction extends Action_Base {
         $allowPrematureRun = (int)Billrun_Factory::config()->getConfigValue('cycle.allow_premature_run');
         // Check if we should cycle.
         if (!$allowPrematureRun && time() < Billrun_Billingcycle::getEndTime($stamp)) {
-			$this->_controller->addOutput("Can't run billing cycle before the cycle end time.");
+			$this->getController()->addOutput("Can't run billing cycle before the cycle end time.");
             return;
 		} 
 
@@ -99,7 +99,7 @@ class CycleAction extends Action_Base {
 					die('could not fork');
 				}
 
-				$this->_controller->addOutput("Running on PID " . $pid);
+				$this->getController()->addOutput("Running on PID " . $pid);
 
 				// Parent process.
 				if ($pid) {
@@ -121,7 +121,7 @@ class CycleAction extends Action_Base {
 	}
 	
 	protected function executeParentProcess($processInterval) {
-		$this->_controller->addOutput("Going to sleep for " . $processInterval . " seconds");
+		$this->getController()->addOutput("Going to sleep for " . $processInterval . " seconds");
 		sleep($processInterval);
 		pcntl_signal(SIGCHLD, SIG_IGN);
 	}
@@ -139,16 +139,16 @@ class CycleAction extends Action_Base {
 			return;
 		}
 		
-		$this->_controller->addOutput("Loading data to Aggregate...");
+		$this->getController()->addOutput("Loading data to Aggregate...");
 		$aggregator->load();
 		if (isset($options['fetchonly'])) {
-			$this->_controller->addOutput("Only fetched aggregate accounts info. Exit...");
+			$this->getController()->addOutput("Only fetched aggregate accounts info. Exit...");
 			return;
 		}
 
-		$this->_controller->addOutput("Starting to Aggregate. This action can take a while...");
+		$this->getController()->addOutput("Starting to Aggregate. This action can take a while...");
 		$aggregator->aggregate();
-		$this->_controller->addOutput("Finish to Aggregate.");
+		$this->getController()->addOutput("Finish to Aggregate.");
 	}
 	
 	/**
@@ -158,18 +158,18 @@ class CycleAction extends Action_Base {
 	 * @todo getAggregator might be common in actions, maybe create a basic aggregate action class?
 	 */
 	protected function getAggregator($options) {
-		$this->_controller->addOutput("Loading aggregator");
+		$this->getController()->addOutput("Loading aggregator");
 		if(!Billrun_Factory::config()->getConfigValue('customer.aggregator.should_fork',TRUE)) {
 			$options = array_merge($options,['rand'=>  microtime(true)]);
 		}
 		$aggregator = Billrun_Aggregator::getInstance($options);
 		
 		if(!$aggregator || !$aggregator->isValid()) {
-			$this->_controller->addOutput("Aggregator cannot be loaded");
+			$this->getController()->addOutput("Aggregator cannot be loaded");
 			return false;
 		}
 		
-		$this->_controller->addOutput("Aggregator loaded");
+		$this->getController()->addOutput("Aggregator loaded");
 		return $aggregator;
 	}
 }
