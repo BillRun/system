72de16ab30b90c26a559aa31cc181307afb2e2b4
or <or.shaashua@billrun.com>
2021-01-07 16:31:49 +0200

    BRCD-2955: Deprecate Alcaeus MongoDB adapter

--- library/Mongodloid/Cursor.php
+++ library/Mongodloid/Cursor.php
@@ -100,7 +100,7 @@ class Mongodloid_Cursor implements Iterator, Countable {
 	public function next() {
 		if ($this->_iterator === null) {
           $this->doQuery();
-		  return $this;
+		  return;
         }
 		return $this->_iterator->next();
 	}
