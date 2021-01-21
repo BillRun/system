e970a362c0a462a08c22e054fad244f66d1a659d
or <or.shaashua@billrun.com>
2021-01-21 16:59:00 +0200

    BRCD-3001: Unit test to the Mongodloid layer

--- library/Tests/Mongodloidtest.php
+++ library/Tests/Mongodloidtest.php
@@ -251,30 +251,31 @@ class Tests_Mongodloid extends UnitTestCase{
 			),
 			'expected' => array('result' => true, 'dbValues'=> array('aid' => '13853', '_id' => '5b34a78d05e77c6ae62d4a77'))
 		),
-		//27 check findAndModify operator - retEntity= true
-		array('function' => 'checkFindAndModify', 'description' => 'check findAndModify operator - retEntity= true', 'collection' => 'balances',
-			'params'=> array(
-				'query' => array(
-					'sid' => array('$gt' => 10)
-				),
-				'update' => array('$set' => array('aidGt' => true)),
-				'fields' => array(),
-				'options' => array(),
-				'retEntity' => true
-			)
-		),
-		//28 check findAndModify operator - retEntity= false
-		array('function' => 'checkFindAndModify', 'description' => 'check findAndModify operator - retEntity= false', 'collection' => 'balances',
-			'params'=> array(
-				'query' => array(
-					'sid' => 10
-				),
-				'update' => array('$set' => array('findAndModdify' => true)),
-				'fields' => array(),
-				'options' => array(),
-				'retEntity' => false
-			)
-		),
+		//not working on sharding collection
+//		//27 check findAndModify operator - retEntity= true
+//		array('function' => 'checkFindAndModify', 'description' => 'check findAndModify operator - retEntity= true', 'collection' => 'balances',
+//			'params'=> array(
+//				'query' => array(
+//					'sid' => array('$gt' => 10)
+//				),
+//				'update' => array('$set' => array('aidGt' => true)),
+//				'fields' => array(),
+//				'options' => array(),
+//				'retEntity' => true
+//			)
+//		),
+//		//28 check findAndModify operator - retEntity= false
+//		array('function' => 'checkFindAndModify', 'description' => 'check findAndModify operator - retEntity= false', 'collection' => 'balances',
+//			'params'=> array(
+//				'query' => array(
+//					'sid' => 10
+//				),
+//				'update' => array('$set' => array('findAndModdify' => true)),
+//				'fields' => array(),
+//				'options' => array(),
+//				'retEntity' => false
+//			)
+//		),
 		//29 check batchInsert operator
 		array('function' => 'checkBatchInsert', 'description' => 'check batchInsert operator', 'collection' => 'subscribers',
 			'params'=> array(
@@ -305,6 +306,70 @@ class Tests_Mongodloid extends UnitTestCase{
 				'options' => array(),
 			),
 			'expected' => array('result' => array('ok' => 1.0))
+		),
+		//31 check createAutoIncForEntity operator - check min_id
+		array('function' => 'checkAutoIncForEntity', 'description' => 'check createAutoIncForEntity operator - check min_id', 'collection' => 'billrun',
+			'params'=> array(
+				'values' => array(
+					'_id' => '5aeee57b463d4f12759b7634',
+					'aid' => 10006,
+					'sid' => 10007	
+				),
+				'field' => 'invoice_id',
+				'min_id' => 5,
+			),
+			'expected' => array('result' => 5)
+		),
+		//32 check createAutoIncForEntity operator - check increment
+		array('function' => 'checkAutoIncForEntity', 'description' => 'check createAutoIncForEntity operator - check increment', 'collection' => 'billrun',
+			'params'=> array(
+				'values' => array(
+					'_id' => '5aeee57b463d4f12759b7680',
+					'aid' => 10008,
+					'sid' => 10009	
+				),
+				'field' => 'invoice_id',
+				'min_id' => 5,
+			),
+			'expected' => array('result' => 6)
+		),
+		//33 check createAutoIncForEntity operator - check value exist
+		array('function' => 'checkAutoIncForEntity', 'description' => 'check createAutoIncForEntity operator - check value exist', 'collection' => 'billrun',
+			'params'=> array(
+				'values' => array(
+					'_id' => '5aeee57b463d4f12759b7681',
+					'aid' => 10010,
+					'sid' => 10011,
+					'invoice_id' => 20
+				),
+				'field' => 'invoice_id',
+				'min_id' => 5,
+			),
+			'expected' => array('result' => 20)
+		),
+		//34 check distinct operator - without query
+		array('function' => 'checkDistinct', 'description' => 'check distinct operator- without query', 'collection' => 'lines',
+			'params'=> array(
+				'item' => 'source',
+				'query' => array()
+			),
+			'expected' => array('result' => array('realtime', 'billrun'))
+		),
+		//35 check distinct operator
+		array('function' => 'checkDistinct', 'description' => 'check distinct operator', 'collection' => 'lines',
+			'params'=> array(
+				'item' => 'billrun',
+				'query' => array('billrun'=> array('$lte' =>'201807'))
+			),
+			'expected' => array('result' => array('201806', '201807'))
+		),
+		//36 check distinct operator - use date query
+		array('function' => 'checkDistinct', 'description' => 'check distinct operator- use date query', 'collection' => 'lines',
+			'params'=> array(
+				'item' => 'billrun',
+				'query' => array('urt' => array('$gte' => '2018-05-13T07:10:10Z'))
+			),
+			'expected' => array('result' => array('201806', '201807', '202003'))
 		),
 		
 //		//TODO:: need to restore also indexes after finish run unit test
@@ -340,12 +405,88 @@ class Tests_Mongodloid extends UnitTestCase{
 //			'expected' => array('fields' => array('_id', 'aid', 'sid', 'from', 'to', 'priority', 'aid', 'sid'))
 //		)
 		
+		//37 check Create MongodloidDate With String
+		array('function' => 'checkCreateMongodloidDateWithString', 'description' => 'check Create MongodloidDate With String',
+			'params'=> array(
+				'sec' => '1234567890',
+				'usec' => '123456'
+			),
+			'expected' => array('result' => array('sec' => 1234567890,'usec' => 123000))
+		),
+		//38 check Create MongodloidDate With BsonDate
+		array('function' => 'checkCreateMongodloidDateWithBsonDate', 'description' => 'check Create MongodloidDate With BsonDate',
+			'params'=> array(
+				'bsonDate' => 1234567890123,
+			),
+			'expected' => array('result' => array('sec' => 1234567890,'usec' => 123000))
+		),
+		//39 check MongodloidDate Convert To Bson And Create With String
+		array('function' => 'checkMongodloidDateConvertToBsonAndCreateWithString', 'description' => 'check MongodloidDate Convert To Bson And Create With String',
+			'params'=> array(
+				'sec' => '1234567890',
+				'usec' => '123456'
+			),
+			'expected' => array('result' => array('date' => '1234567890123'))
+		),
+		//40 check MongodloidDate Convert To Bson And Create With Bson Date
+		array('function' => 'checkMongodloidDateConvertToBsonAndCreateWithBsonDate', 'description' => 'check MongodloidDate Convert To Bson And Create With Bson Date',
+			'params'=> array(
+				'bsonDate' => 1234567890123,
+			),
+			'expected' => array('result' => array('date' => '1234567890123'))
+		),
+		//41 check Create MongodloidId With String
+		array('function' => 'checkCreateMongodloidIdWithString', 'description' => 'check Create MongodloidId With String',
+			'params'=> array(
+				'id' => '54203e08d51d4a1f868b456e',
+			)
+		),
+		//42 check Create MongodloidId With Invalid String
+		array('function' => 'checkCreateMongodloidIdWithInvalidString', 'description' => 'check Create MongodloidId With Invalid String'),
+		//41 check Create MongodloidId With ObjectId
+		array('function' => 'checkCreateMongodloidIdWithObjectId', 'description' => 'check Create MongodloidId With ObjectId',
+			'params'=> array(
+				'id' => '54203e08d51d4a1f868b456e',
+			)
+		),
+		//43 check Create MongodloidId Without Parameter
+		array('function' => 'checkCreateMongodloidIdWithoutParameter', 'description' => 'check Create MongodloidId Without Parameter'),
+		
+		//44 check Mongodloid Regex Convert To Bson
+		array('function' => 'checkMongodloidRegexConvertToBson', 'description' => 'check Mongodloid Regex Convert To Bson',
+			'params'=> array(
+				'regex' => '/abc/i',
+			),
+			'expected' => array('result' => array('regex' => 'abc', 'flags' => 'i'))
+		),
+		//45 check Mongodloid Regex Convert To Bson And Create With Bson Type
+		array('function' => 'checkMongodloidRegexConvertToBsonAndCreateWithBsonType', 'description' => 'check Mongodloid Regex Convert To Bson And Create With Bson Type',
+			'params'=> array(
+				'pattern' => 'abc',
+				'flags' => 'i'
+			),
+			'expected' => array('result' => array('regex' => 'abc', 'flags' => 'i'))
+		),
+		//46 check MongodloidRef Create 
+		array('function' => 'checkMongodloidRefCreate', 'description' => 'check MongodloidRef Create',
+			'params'=> array(
+				'ref' => 'foo',
+			)
+		),
+		//47 check MongodloidRef Create with database
+		array('function' => 'checkMongodloidRefCreate', 'description' => 'check MongodloidRef Create with database',
+			'params'=> array(
+				'ref' => 'foo',
+				'database' => 'database',
+			)
+		),
+		
 	);
 
 
 	public function __construct($label = 'Mongodloid layer') {
          parent::__construct("test mongodloid layer");
-         $this->construct(basename(__FILE__, '.php'), []);
+         $this->construct(basename(__FILE__, '.php'), ['counters']);
          $this->setColletions();
          $this->loadDbConfig();
      }
@@ -579,7 +720,231 @@ class Tests_Mongodloid extends UnitTestCase{
 			$this->checkDb($collection, $document, $document);
 	}
 	
-	////////////////////////////////////////////////////////////////////////////////////////
+	protected function checkAutoIncForEntity($test) {
+		$collection = $test['collection'];
+		$values = $test['params']['values'];
+		$this->changeValues($values);
+		$field = $test['params']['field'];
+		$min_id = $test['params']['min_id'];
+		$result = Billrun_Factory::db()->{$collection . 'Collection'}()->createAutoIncForEntity(new Mongodloid_Entity($values), $field, $min_id);
+		return $this->basicCompare($result, $test['expected']['result'], $field);
+	}
+	
+	protected function checkDistinct($test) {
+		$collection = $test['collection'];
+		$item = $test['params']['item'];
+		$query = $test['params']['query'];
+		$this->changeValues($query);
+		$result = Billrun_Factory::db()->{$collection . 'Collection'}()->distinct($item, $query);
+		return $this->basicCompare($result, $test['expected']['result'], $item);
+	}
+
+	
+//////////////////////////Mongodloid_Date tests//////////////////////////////////////////
+	
+	
+	protected function checkCreateMongodloidDateWithString($test)
+    {
+		$class = $this->getMongodloidDateClass();
+        $date = new $class($test['params']['sec'], $test['params']['usec']);
+		return $this->basicCompare($date->sec, $test['expected']['result']['sec'], 'sec') &&
+			$this->basicCompare($date->usec, $test['expected']['result']['usec'], 'usec');
+    }
+	
+	protected function checkCreateMongodloidDateWithBsonDate($test)
+    {
+
+		$class = $this->getMongodloidDateClass();
+        $bsonDate = new \MongoDB\BSON\UTCDateTime($test['params']['bsonDate']);
+        $date = new $class($bsonDate);
+		return $this->basicCompare($date->sec, $test['expected']['result']['sec'], 'sec') &&
+			$this->basicCompare($date->usec, $test['expected']['result']['usec'], 'usec');
+    }
+	
+	protected function getMongodloidDateClass() {
+		if(@class_exists('MongoDate')){
+			$class = 'MongoDate';
+		}else{
+			$class = 'Mongodloid_Date';
+		}
+		return $class;
+	}
+	
+    public function checkMongodloidDateConvertToBsonAndCreateWithString($test)
+    {
+		
+		$class = $this->getMongodloidDateClass();
+        $date = new $class($test['params']['sec'], $test['params']['usec']);
+        $dateTime = $date->toDateTime();
+
+        $bsonDate = $date->toBSONType();
+        $bsonDateTime = $bsonDate->toDateTime();
+
+        // Compare timestamps to avoid issues with DateTime
+        $timestamp = $dateTime->format('U') . '.' . $dateTime->format('U');
+        $bsonTimestamp = $bsonDateTime->format('U') . '.' . $bsonDateTime->format('U');
+		
+		return $this->basicCompare((string) $bsonDate, $test['expected']['result']['date'], 'date') &&
+			$this->basicCompare($bsonTimestamp, $timestamp, 'timestamp') &&
+			$this->basicCompare(get_class($bsonDate), 'MongoDB\BSON\UTCDateTime', 'instance');
+    }
+	
+	public function checkMongodloidDateConvertToBsonAndCreateWithBsonDate($test)
+    {
+		
+		$class = $this->getMongodloidDateClass();
+        $bsonDate = new \MongoDB\BSON\UTCDateTime($test['params']['bsonDate']);
+        $date = new $class($bsonDate);
+        $dateTime = $date->toDateTime();
+
+        $newBsonDate = $date->toBSONType();
+        $bsonDateTime = $newBsonDate->toDateTime();
+
+        // Compare timestamps to avoid issues with DateTime
+        $timestamp = $dateTime->format('U') . '.' . $dateTime->format('U');
+        $bsonTimestamp = $bsonDateTime->format('U') . '.' . $bsonDateTime->format('U');
+		
+		return $this->basicCompare((string) $newBsonDate, $test['expected']['result']['date'], 'date') &&
+			$this->basicCompare($bsonTimestamp, $timestamp, 'timestamp') &&
+			$this->basicCompare(get_class($newBsonDate), 'MongoDB\BSON\UTCDateTime', 'instance');
+    }
+
+//////////////////////////Mongodloid_Id tests//////////////////////////////////////////
+	
+	protected function getMongodloidIdClass() {
+		if(@class_exists('MongoId')){
+			$class = 'MongoId';
+		}else{
+			$class = 'Mongodloid_Id';
+		}
+		return $class;
+	}
+
+	protected function checkCreateMongodloidIdWithString($test)
+    {
+        $original = $test['params']['id'];
+		$class = $this->getMongodloidIdClass();
+        $id = new $class($original);
+		return $this->basicCompare((string) $id, $original, 'id');
+    }
+
+    protected function checkCreateMongodloidIdWithInvalidString()
+    {
+		$class = $this->getMongodloidIdClass();
+		try{
+			new $class('invalid');
+			return false;
+		} catch (Exception $e){
+			return true; 
+		}
+    }
+
+    protected function checkCreateMongodloidIdWithObjectId($test)
+    {
+		$class = $this->getMongodloidIdClass();
+        $original = $test['params']['id'];
+        $objectId = new  MongoDB\BSON\ObjectID($original);
+        $id = new $class($objectId);
+		return $this->basicCompare((string) $id, $original, 'id');
+    }
+	
+	protected function checkCreateMongodloidIdWithoutParameter()
+    {
+		$class = $this->getMongodloidIdClass();
+        $id = new $class();
+        $stringId = (string) $id;
+        $serialized = serialize($id);
+        $unserialized = unserialize($serialized);
+        $json = json_encode($id);
+		return $this->basicCompare(strlen($stringId), 24,  'id length') &&
+			$this->basicCompare($stringId, $id->{'$id'}, 'id') &&
+			$this->basicCompare($serialized, sprintf('C:'. strlen($class).':"' . $class . '":24:{%s}', $stringId), 'serialize') &&
+			$this->basicCompare((string) $unserialized, $stringId, 'unserialize') &&
+				$this->basicCompare(get_class($unserialized) , $class, 'instance') &&
+				$this->basicCompare($json, sprintf('{"$id":"%s"}', $stringId), 'json');
+    }
+
+
+
+//////////////////////////Mongodloid_Regex tests//////////////////////////////////////////
+
+
+	protected function getMongodloidRegexClass() {
+		if(@class_exists('MongoRegex')){
+			$class = 'MongoRegex';
+		}else{
+			$class = 'Mongodloid_Regex';
+		}
+		return $class;
+	}
+	
+
+    protected function checkMongodloidRegexConvertToBson($test)
+    {
+        $class = $this->getMongodloidRegexClass();
+		$regexString = $test['params']['regex'];
+		$regex = new $class($regexString);
+
+		$bsonRegex = $regex->toBSONType();
+		$instance = $bsonRegex instanceof MongoDB\BSON\Regex;
+		return $this->basicCompare($bsonRegex->getPattern(), $test['expected']['result']['regex'], 'regex') &&
+			$this->basicCompare($bsonRegex->getFlags(), $test['expected']['result']['flags'], 'flags') &&
+			$this->basicCompare((string) $regex, $regexString,  'regex string') && 
+			$this->basicCompare($instance, true, 'instanceof MongoDB\BSON\Regex');
+
+    }
+	
+	protected function checkMongodloidRegexConvertToBsonAndCreateWithBsonType($test)
+    {
+        $class = $this->getMongodloidRegexClass();
+		$pattern = $test['params']['pattern'];
+		$flags = $test['params']['flags'];
+		$bsonRegex = new \MongoDB\BSON\Regex($pattern, $flags);
+		$regex = new $class($bsonRegex);
+
+		$newBsonRegex = $regex->toBSONType();
+		$instance = $newBsonRegex instanceof MongoDB\BSON\Regex;
+		return $this->basicCompare($newBsonRegex->getPattern(), $test['expected']['result']['regex'], 'regex') &&
+			$this->basicCompare($newBsonRegex->getFlags(), $test['expected']['result']['flags'], 'flags') &&
+			$this->basicCompare((string) $newBsonRegex, (string) $regex,  'regex string') && 
+			$this->basicCompare($instance, true, 'instanceof MongoDB\BSON\Regex');
+
+    }
+
+
+
+
+
+//////////////////////////Mongodloid_Ref tests//////////////////////////////////////////
+
+	protected function getMongodloidRefClass() {
+		if(@class_exists('MongoDBRef')){
+			$class = 'MongoDBRef';
+		}else{
+			$class = 'Mongodloid_Ref';
+		}
+		return $class;
+	} 
+	
+	public function checkMongodloidRefCreate($test)
+    {
+		$idClass = $this->getMongodloidIdClass();
+        $id = new $idClass();
+		$refClass = $this->getMongodloidRefClass();
+		$refParam = $test['params']['ref'];
+		$database = $test['params']['database'] ?? null;
+		if(!isset($database)){
+			$ref = $refClass::create($refParam, $id);
+			return $this->checkResult($ref, ['$ref' => $refParam, '$id' => $id]);
+		}else{
+			$ref = $refClass::create($refParam, $id, $database);
+			return $this->checkResult($ref, ['$ref' => $refParam, '$id' => $id, '$db' => $database]);
+		}
+    }
+
+
+	///////////////////////////////////////////////////////////////////////////////////
+	
 	
 	protected function checkDb($collection, $query, $expectedDbValues) {
 		$this->message .= '<br><b>compare db values</b><br>';
@@ -629,9 +994,15 @@ class Tests_Mongodloid extends UnitTestCase{
 	}
 	
 	protected function basicCompare($testResult, $expectedResult , $title) {
-		$ret = ($expectedResult === $testResult);
-		$expectedResult = is_array($expectedResult) ? implode (", ", $expectedResult) : $expectedResult;
-		$testResult = is_array($testResult) ? implode (", ", $testResult): $testResult;
+		if(is_array($expectedResult)){
+			sort($expectedResult);
+			$expectedResult = implode (", ", $expectedResult);
+		}
+		if(is_array($testResult)){
+			sort($testResult);
+			$testResult = implode (", ", $testResult);
+		}
+		$ret = ($expectedResult == $testResult);
 		$this->message .= '<p style="font: 14px arial; color: rgb(0, 0, 80);"> ' . '<b> Expected: </b><br> ' .$title . ' : ' . $expectedResult;
         $this->message .= '<br><b> Result: </b> <br> ' . $title . ' : ' . $testResult;
 		$this->message .= ($ret ? $this->pass : $this->fail);
@@ -640,46 +1011,36 @@ class Tests_Mongodloid extends UnitTestCase{
 	
 	protected function changeValues(&$values) {
 		if(isset($values['_id'])){
-			if(class_exists('MongoId')){
+			if(@class_exists('MongoId')){
 				$values['_id'] =  new MongoId($values['_id']);//for now check mongo
 			}
 			else{
 				$values['_id'] =  new Mongodloid_Id($values['_id']);
 			}
 		}
+		$datesFields =['urt', 'time'];
+		foreach ($datesFields as $field){
+			if(isset($values[$field])){
+				if(@class_exists('MongoDate')){
+					if(is_array($values[$field])){
+						$key = key($values[$field]);
+						$values[$field][$key] =  new MongoDate(strtotime($values[$field][$key]));//for now check mongo
+					}else{
+						$values[$field] =  new MongoDate(strtotime($values[$field]));//for now check mongo
+					}
+				}
+				else{
+					if(is_array($values[$field])){
+						$key = key($values[$field]);
+						$values[$field][$key] =  new Mongodloid_Date(strtotime($values[$field][$key]));
+					}else{
+						$values[$field] =  new Mongodloid_Date(strtotime($values[$field]));
+					}
+				}
+			}
+		}
 	}
 }
 
-
-class Mongodloid_Cursor_Test {
-	
-}
-
-class Mongodloid_Date_Test {
-	
-}
-class Mongodloid_Db_Test {
-	
-}
-
-class Mongodloid_Entity_Test{
-	
-}
-
-class Mongodloid_Id_Test{
-	
-}
-
-class Mongodloid_Query_Test{
-	
-}
-
-class Mongodloid_Connection_Test {
-	
-}
-
-class Mongodloid_Regex_Test {
-	
-}
 
 
