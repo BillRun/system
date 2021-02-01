1bd5bbe152f85d937252d634bfb3a7942bf76969
Nehemia Litterat <nlitterat@gmail.com>
2021-02-01 05:52:51 -0500

    Update gitlab-ci.sh

--- ci/scripts/gitlab-ci.sh
+++ ci/scripts/gitlab-ci.sh
@@ -5,13 +5,19 @@ function RESET_MONGO_DB() {
     local billing_db=$1
     local billing_db_port=$2
     mongo $billing_db --port $billing_db_port --eval "db.dropDatabase()"
+    echo "Import 1"
     mongo $billing_db --port $billing_db_port mongo/create.ini
+    echo "Import 2"
     mongo $billing_db --port $billing_db_port mongo/sharding.ini
+    echo "Import 3"
     mongoimport -d $billing_db --port $billing_db_port -c config mongo/base/config.export --batchSize 1
+    echo "Import 4"
     mongoimport -d $billing_db --port $billing_db_port -c users mongo/first_users.ini
-     echo "Import 1"
+     echo "Import 5"
     mongo $billing_db --port $billing_db_port --eval 'db.users.insert({ "username" : "$BILLING_DB_USER_NAME", "password" :  "$BILLING_DB_PASSWORD", "roles" : [ "read", "write", "admin" ], "from" : ISODate("2012-09-01T00:00:00Z"), "to" : ISODate("2168-03-25T09:43:10Z"), "creation_time" : ISODate("2012-09-01T00:00:00Z") })'
+    echo "Import 6"
     mongo $billing_db --port $billing_db_port --eval 'var lastConfig = db.config.find().sort({_id: -1}).limit(1).pretty()[0];delete lastConfig["_id"];lastConfig.shared_secret =  [{"key" : "@BILL_RUN_CLIENT_SECRET","crc" : "834fde09","name" : "@BILL_RUN_CLIENT_ID","from" : ISODate("2020-12-28T00:00:00Z"),"to" : ISODate("2222-12-28T00:00:00Z")}];db.config.insert(lastConfig);'
+    echo "Import 7"
     mongoimport -d $billing_db --port $billing_db_port -c taxes mongo/base/taxes.export
     echo "Done import"
      
