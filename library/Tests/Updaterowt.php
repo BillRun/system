b99694af2703da42f885b2e4e30eade885c5723d
yossi <yossi.tubul@billrun.com>
2021-02-09 16:50:03 +0200

    BRCD-3011 add test cases for multi day cycle

--- library/Tests/Updaterowt.php
+++ library/Tests/Updaterowt.php
@@ -30,6 +30,15 @@ class Tests_Updaterowt extends UnitTestCase {
 	protected $servicesToUse = ["SERVICE1", "SERVICE2"];
 	protected $fail = ' <span style="color:#ff3385; font-size: 80%;"> failed </span> <br>';
 	protected $pass = ' <span style="color:#00cc99; font-size: 80%;"> passed </span> <br>';
+	protected $defaultOptions = array(
+		"type" => "customer",
+		"stamp" => "201806",
+		"page" => 0,
+		"size" => 100,
+		'fetchonly' => true,
+		'generate_pdf' => 0,
+		"force_accounts" => array()
+	);
 
 	public function tests() {
 
@@ -516,13 +525,70 @@ class Tests_Updaterowt extends UnitTestCase {
 			 * Make sure there’s no regression -
 			  multi_day_cycle flag is “false“
 			  there’s no billrun object from the last 3 months  -remove all billruns
-			   process line and make sure it gets the runtime billrun key */
-			array('preTest' => ['removeBillruns'], 'row' => array('stamp' => 'z', "aid" => 10000, 'sid' => 20000, 'rates' => array('NEW-CALL-USA' => 'retail'), 'plan' => 'PLAN_A', 'usagev' => 40, 'services_data' => ['NEW-SERVICE1'], 'urt' => date('Y-m-d')),
+			  process line and make sure it gets the runtime billrun key */
+			array('preTest' => ['removeBillruns'], 'row' => array('stamp' => 'z1', "aid" => 10002, 'sid' => 20002, 'rates' => array('NEW-CALL-USA' => 'retail'), 'plan' => 'PLAN_A', 'usagev' => 40, 'services_data' => ['NEW-SERVICE1'], 'urt' => date('Y-m-d')),
+				'expected' => array('in_group' => 40, 'over_group' => 0, 'aprice' => 0, 'charge' => array('retail' => 0), 'billrun' => Billrun_Billingcycle::getBillrunKeyByTimestamp(time()))),
+			/* 	
+			 * multi_day_cycle flag is “false“ 
+			  exists billrun object from the last month - run last billrun
+			  process line and make sure it gets the billrun key -
+			  /according to the “urt“ time, only on future urt will affect on billrun */
+			array('preTest' => ['runCycle'], 'row' => array('runCycle' => ['stamp' => Billrun_Billingcycle::getBillrunKeyByTimestamp(strtotime('-1 month')), 'force_accounts' => [8]],
+					'stamp' => 'z2', "aid" => 10025, 'sid' => 20025, 'rates' => array('NEW-CALL-USA' => 'retail'), 'plan' => 'PLAN_A', 'usagev' => 40, 'services_data' => ['NEW-SERVICE1'], 'urt' => date('Y-m-d')),
+				'expected' => array('in_group' => 40, 'over_group' => 0, 'aprice' => 0, 'charge' => array('retail' => 0), 'billrun' => Billrun_Billingcycle::getBillrunKeyByTimestamp(time()))),
+			/* multi_day_cycle flag is “true” 
+			  exists billrun object from the last billrun cycle - run last billrun
+			  process line from the current cycle,
+			  and make sure it gets the right billrun key -
+			  according to the customer’s “invoicing_day“ field
+			  1. customer invoiceing day 1  urt from past in this montu-  billrun will be run time billrun
+			 */
+			array('preTest' => ['multiDayCycle', 'runCycle'], 'row' => array(
+					'multiDayCycle' => true, 'runCycle' => ['stamp' => Billrun_Billingcycle::getBillrunKeyByTimestamp(strtotime('-1 month')), 'force_accounts' => [8]],
+					'stamp' => 'z3', "aid" => 10000, 'sid' => 20000, 'rates' => array('NEW-CALL-USA' => 'retail'), 'plan' => 'PLAN_A', 'usagev' => 40, 'services_data' => ['NEW-SERVICE1'], 'urt' => date('Y-m-1'), "foreign" => ["account" => ["invoicing_day" => "1"]]),
+				'expected' => array('in_group' => 40, 'over_group' => 0, 'aprice' => 0, 'charge' => array('retail' => 0), 'billrun' => Billrun_Billingcycle::getBillrunKeyByTimestamp(time()))),
+			/* 	  2. customer invoiceing day 1  urt from futurre in this month - billrun will be run time billrun */
+			array('preTest' => ['multiDayCycle', 'runCycle'], 'row' => array(
+					'multiDayCycle' => true, 'runCycle' => ['stamp' => Billrun_Billingcycle::getBillrunKeyByTimestamp(strtotime('-1 month')), 'force_accounts' => [8]],
+					'stamp' => 'z4', "aid" => 11000, 'sid' => 21000, 'rates' => array('NEW-CALL-USA' => 'retail'), 'plan' => 'PLAN_A', 'usagev' => 40, 'services_data' => ['NEW-SERVICE1'], 'urt' => date('Y-m-28'), "foreign" => ["account" => ["invoicing_day" => "1"]]),
+				'expected' => array('in_group' => 40, 'over_group' => 0, 'aprice' => 0, 'charge' => array('retail' => 0), 'billrun' => Billrun_Billingcycle::getBillrunKeyByTimestamp(time()))),
+			/*
+			  3. customer invoiceing day 28  urt from past in this montu, befor currunt cycle - will take the prev cycle */
+			array('preTest' => ['removeBillruns', 'multiDayCycle', 'runCycle'], 'row' => array(
+					'multiDayCycle' => true, 'runCycle' => ['stamp' => Billrun_Billingcycle::getBillrunKeyByTimestamp(strtotime('-1 month')), 'force_accounts' => [8]],
+					'stamp' => 'z5', "aid" => 10027, 'sid' => 20027, 'rates' => array('NEW-CALL-USA' => 'retail'), 'plan' => 'PLAN_A', 'usagev' => 40, 'services_data' => ['NEW-SERVICE1'], 'urt' => date('Y-m-10'), "foreign" => ["account" => ["invoicing_day" => "28"]],),
+				'expected' => array('in_group' => 40, 'over_group' => 0, 'aprice' => 0, 'charge' => array('retail' => 0), 'billrun' => Billrun_Billingcycle::getBillrunKeyByTimestamp(strtotime('-1 month')))),
+			/*
+			  4. customer invoiceing day 24 urt from futurre in this month  - get currnt billrun*/
+			array('preTest' => ['multiDayCycle'], 'row' => array(
+					'multiDayCycle' => true,
+					'stamp' => 'z6', "aid" => 10024, 'sid' => 20024, 'rates' => array('NEW-CALL-USA' => 'retail'), 'plan' => 'PLAN_A', 'usagev' => 40, 'services_data' => ['NEW-SERVICE1'], 'urt' => date('Y-m-28'), "foreign" => ["account" => ["invoicing_day" => "24"]],),
+				'expected' => array('in_group' => 40, 'over_group' => 0, 'aprice' => 0, 'charge' => array('retail' => 0), 'billrun' => $this->calcBillrun(24, 28))),
+			/*-- no prev cycle run --*/
+			array('preTest' => ['removeBillruns'], 'row' => array(
+					'multiDayCycle' => true,
+					'stamp' => 'z7', "aid" => 10023, 'sid' => 20023, 'rates' => array('NEW-CALL-USA' => 'retail'), 'plan' => 'PLAN_A', 'usagev' => 40, 'services_data' => ['NEW-SERVICE1'], 'urt' => date('Y-m-28',strtotime('-1 month')), "foreign" => ["account" => ["invoicing_day" => "22"]],),
+				'expected' => array('in_group' => 40, 'over_group' => 0, 'aprice' => 0, 'charge' => array('retail' => 0), 'billrun' =>  Billrun_Billingcycle::getBillrunKeyByTimestamp(strtotime('-1 month')))),
+			/*multi_day_cycle flag is “true” + past  cycle is run(for example : 
+			 * now is 26/05/2020 ,the customer invoicing_day is 25, and cycle of billrun 202005 is run ),
+			 * process cdr with urt before 26/05/2020 - the billrun will be 202006. 
+			 */
+			array('preTest' => ['removeBillruns', 'multiDayCycle', 'runCycle'], 'row' => array(
+					'multiDayCycle' => true, 'runCycle' => ['stamp' => Billrun_Billingcycle::getBillrunKeyByTimestamp(strtotime('-1 month')), 'force_accounts' => [8]],
+					'stamp' => 'z8', "aid" => 10020, 'sid' => 20020, 'rates' => array('NEW-CALL-USA' => 'retail'), 'plan' => 'PLAN_A', 'usagev' => 40, 'services_data' => ['NEW-SERVICE1'], 'urt' => date('Y-m-21'), "foreign" => ["account" => ["invoicing_day" => date('d',strtotime('-1 day'))]],),
+				'expected' => array('in_group' => 40, 'over_group' => 0, 'aprice' => 0, 'charge' => array('retail' => 0), 'billrun' => Billrun_Billingcycle::getBillrunKeyByTimestamp(time()))),
+			/*multi_day_cycle flag is “true” + past  cycle is run(for example :
+			 *  now is 26/05/2020 ,the customer invoicing_day is 25,
+			 *  and cycle of billrun 202005 is run ) ,
+			 * process cdr with urt between  26/05/2020 to 26/06/2020 - the billrun will be 202006. */
+			array('preTest' => ['removeBillruns', 'multiDayCycle', 'runCycle'], 'row' => array(
+					'multiDayCycle' => true, 'runCycle' => ['stamp' => Billrun_Billingcycle::getBillrunKeyByTimestamp(strtotime('-1 month')), 'force_accounts' => [8]],
+					'stamp' => 'z9', "aid" => 10021, 'sid' => 20021, 'rates' => array('NEW-CALL-USA' => 'retail'), 'plan' => 'PLAN_A', 'usagev' => 40, 'services_data' => ['NEW-SERVICE1'], 'urt' => date('Y-m-d',strtotime('-1 day')), "foreign" => ["account" => ["invoicing_day" => date('d',strtotime('-1 day'))]],),
 				'expected' => array('in_group' => 40, 'over_group' => 0, 'aprice' => 0, 'charge' => array('retail' => 0), 'billrun' => Billrun_Billingcycle::getBillrunKeyByTimestamp(time()))),
 		];
 	}
 
-	protected $stampsToRun = ['z'];
+	protected $stampsToRun = [];
 
 	public function __construct($label = false) {
 		parent::__construct("test UpdateRow");
@@ -536,7 +602,7 @@ class Tests_Updaterowt extends UnitTestCase {
 		$this->billingCyclr = Billrun_Factory::db()->billing_cycleCollection();
 		$this->billrunCol = Billrun_Factory::db()->billrunCollection();
 		$this->calculator = Billrun_Calculator::getInstance(array('type' => 'customerPricing', 'autoload' => false));
-		$this->construct(null, ['lines', 'balances', 'rebalance_queue', 'billrun', 'billing_cycle']);
+		$this->construct(null, ['lines', 'balances', 'rebalance_queue', 'billrun', 'billing_cycle', 'taxes']);
 		$this->setColletions();
 		$this->loadDbConfig();
 	}
@@ -886,6 +952,42 @@ class Tests_Updaterowt extends UnitTestCase {
 		return $row;
 	}
 
+	protected function runCycle($row) {
+		$options = $this->defaultOptions;
+		$options['stamp'] = isset($row['row']['runCycle']['stamp']) ? $row['row']['runCycle']['stamp'] : $options['stamp'];
+		$options['force_accounts'] = isset($row['row']['runCycle']['force_accounts']) ? $row['row']['runCycle']['force_accounts'] : $options['force_accounts'];
+		$aggregator = Billrun_Aggregator::getInstance($options);
+		$aggregator->load();
+		$aggregator->aggregate();
+	}
+
+	public function multiDayCycle($row) {
+		$mdc = isset($row['row']['multiDayCycle']) ? $row['row']['multiDayCycle'] : false;
+		if ($mdc) {
+			Billrun_Factory::config()->addConfig(APPLICATION_PATH . '/library/Tests/conf/allow_premature_run.ini');
+		} else {
+			Billrun_Factory::config()->addConfig(APPLICATION_PATH . '/library/Tests/conf/multi_day_cycle_false.ini');
+		}
+	}
+
+	/**
+	 * 
+	 * @param type $invoiceDay
+	 * @param type $urt
+	 */
+	public function calcBillrun($invoiceDay, $urt) {
+		$now = date('d');
+		if (time($urt) >= time($invoiceDay)) {
+			return date('Ym', strtotime('+1 month'));
+		} else {
+			return date('Ym');
+		}
+	}
+
+	/**
+	 * 
+	 * @param type $row
+	 */
 	public function removeBillruns($row) {
 		$this->billingCyclr->remove(['billrun_key' => ['$ne' => 'abc']]);
 		$this->billrunCol->remove(['billrun_key' => ['$ne' => 'abc']]);
