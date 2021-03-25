4ea2fdc886e32394d2a8e430d12ca92978a94b0c
Yonatan Rosemarin <yonatan.r@billrun.com>
2021-03-25 10:05:13 +0200

    handle rebalance of free line for prepaid cases

--- library/Billrun/Calculator/Row/Customerpricing/Prepaid.php
+++ library/Billrun/Calculator/Row/Customerpricing/Prepaid.php
@@ -149,9 +149,9 @@ class Billrun_Calculator_Row_Customerpricing_Prepaid extends Billrun_Calculator_
 				'in_balance_usage' => $rebalanceUsagev,
 			);
 			
-			$rebalanceCost = abs($this->getRebalanceCost($lineToRebalance, $realUsagev, $rebalanceUsagev));
+			$rebalanceCost = $this->getRebalanceCost($lineToRebalance, $realUsagev, $rebalanceUsagev);
 			if (!is_null($balance['balance.totals.' . $balance_totals_key . '.usagev'])) {
-				if ($rebalanceCost > 0) { // in case of a free line - no reservation was made
+				if (abs($rebalanceCost) > 0) { // in case of a free line - no reservation was made
 					$balance['balance.totals.' . $balance_totals_key . '.usagev'] += $rebalanceUsagev;
 				}
 			} else {
