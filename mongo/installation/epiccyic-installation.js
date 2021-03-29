63cfe7be229d6079b49b5eff01ed592a1fccf83d
Or Shaashua <or.shaashua@billrun.com>
2021-03-29 15:27:12 +0300

    EPICIC-42: Create more rate custom fields and add them to the mapper

--- mongo/installation/epiccyic-installation.js
+++ mongo/installation/epiccyic-installation.js
@@ -3213,7 +3213,25 @@ lastConfig["rates"]["fields"] =
 				"title" : "Object Id",
 				"editable" : true,
 				"display" : true
-			}
+			},
+                        {
+				"field_name" : "gl_account_Description",
+				"title" : "Account Description",
+				"editable" : true,
+				"display" : true
+			},
+                        {
+				"field_name" : "mtn_ind",
+				"title" : "Mtn/Ind",
+				"editable" : true,
+				"display" : true
+			},
+                        {
+				"field_name" : "prod_serv",
+				"title" : "Prod/Serv",
+				"editable" : true,
+				"display" : true
+			},
 		];
 
 //foreign fields
@@ -3869,73 +3887,90 @@ lastConfig["import"]["mapping"] = [
 		]
 	},
         {
-        "label": "Missing Mapping",
-        "map": [
-            {
-                "field": "price_from",
-                "value": "0"
-            },
-            {
-                "field": "params.product",
-                "value": "__csvindex__1"
-            },
-            {
-                "field": "usage_type_value",
-                "value": "erp_report"
-            },
-            {
-                "field": "usage_type_unit",
-                "value": ""
-            },
-            {
-                "field": "params.operator",
-                "value": "__csvindex__5"
-            },
-            {
-                "field": "price_to",
-                "value": "UNLIMITED"
-            },
-            {
-                "field": "params.cash_flow",
-                "value": "__csvindex__3"
-            },
-            {
-                "field": "gl_account",
-                "value": "__csvindex__7"
-            },
-            {
-                "field": "params.component",
-                "value": "__csvindex__2"
-            },
-            {
-                "field": "params.scenario",
-                "value": "__csvindex__0"
-            },
-            {
-                "field": "tariff_category",
-                "value": "retail"
-            },
-            {
-                "field": "object_id",
-                "value": "__csvindex__6"
-            },
-            {
-                "field": "price_interval",
-                "value": "1"
-            },
-            {
-                "field": "price_value",
-                "value": "0"
-            },
-            {
-                "field": "key",
-                "value": "__csvindex__8"
-            }
-        ],
-        "updater": [],
-        "linker": [],
-        "multiFieldAction": []
-    }
+                "label" : "Missing Mapping",
+                "map" : [
+                        {
+                                "field" : "price_from",
+                                "value" : "0"
+                        },
+                        {
+                                "field" : "params.product",
+                                "value" : "__csvindex__1"
+                        },
+                        {
+                                "field" : "mtn_ind",
+                                "value" : "__csvindex__9"
+                        },
+                        {
+                                "field" : "usage_type_value",
+                                "value" : "erp_report"
+                        },
+                        {
+                                "field" : "usage_type_unit",
+                                "value" : "counter"
+                        },
+                        {
+                                "field" : "params.operator",
+                                "value" : "__csvindex__5"
+                        },
+                        {
+                                "field" : "gl_account_Description",
+                                "value" : "__csvindex__8"
+                        },
+                        {
+                                "field" : "gl_account_description",
+                                "value" : "__csvindex__8"
+                        },
+                        {
+                                "field" : "price_to",
+                                "value" : "UNLIMITED"
+                        },
+                        {
+                                "field" : "params.cash_flow",
+                                "value" : "__csvindex__3"
+                        },
+                        {
+                                "field" : "gl_account",
+                                "value" : "__csvindex__7"
+                        },
+                        {
+                                "field" : "params.component",
+                                "value" : "__csvindex__2"
+                        },
+                        {
+                                "field" : "params.scenario",
+                                "value" : "__csvindex__0"
+                        },
+                        {
+                                "field" : "tariff_category",
+                                "value" : "retail"
+                        },
+                        {
+                                "field" : "object_id",
+                                "value" : "__csvindex__6"
+                        },
+                        {
+                                "field" : "price_interval",
+                                "value" : "1"
+                        },
+                        {
+                                "field" : "price_value",
+                                "value" : "0"
+                        },
+                        {
+                                "field" : "prod_serv",
+                                "value" : "__csvindex__10"
+                        },
+                        {
+                                "field" : "key",
+                                "value" : "__csvindex__11"
+                        }
+                ],
+                "updater" : [ ],
+                "linker" : [ ],
+                "multiFieldAction" : [ ]
+        }
+
 ];
 
 var report_MissingMapping = {
