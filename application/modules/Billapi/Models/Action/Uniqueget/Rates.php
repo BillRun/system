99e4bf66c8ae6eabd7fb41435f1a9d2de084e1e7
Roman <roman.edneral@billrun.com>
2021-05-04 13:22:05 +0300

    BRCD-3060 feat(Rates) - allow query by rates.* fields

---
+++ application/modules/Billapi/Models/Action/Uniqueget/Rates.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @package         Billing
+ * @copyright       Copyright (C) 2012-2016 BillRun Technologies Ltd. All rights reserved.
+ * @license         GNU Affero General Public License Version 3; see LICENSE.txt
+ */
+
+/**
+ * Billapi get Reports operation
+ * Retrieve list of entities
+ *
+ * @package  Billapi
+ * @since    5.3
+ */
+class Models_Action_Uniqueget_Rates extends Models_Action_Uniqueget {
+
+	public function getConfigParams($params) {
+		$settings = parent::getConfigParams($params);
+		if (in_array('rates.*', array_column($settings['query_parameters'], 'name'))) {
+			$usage_types = Billrun_Factory::config()->getConfigValue('usage_types');
+			$usage_types_names = array_column($usage_types, 'usage_type');
+			foreach ($usage_types_names as $usage_type_name) {
+				$settings['query_parameters'][] = [
+					'name' => "rates.$usage_type_name",
+					'type' => 'array'
+				];
+			}
+		}
+		return $settings;
+	}
+}
