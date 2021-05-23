f44ff7dcf06daf85fd301933321270d987a693a4
killawatt <shani@billrun.net>
2021-05-23 22:25:45 +0300

    Update version

--- library/Billrun/Version.php
+++ library/Billrun/Version.php
@@ -21,7 +21,7 @@ class Billrun_Version {
 	public static $RELEASE = '5.13';
 
 	/** @var  string  Maintenance version. */
-	public static $DEV_LEVEL = '6';
+	public static $DEV_LEVEL = '7';
 
 	/** @var  string  Development STATUS. */
 	public static $DEV_STATUS = '';
@@ -30,7 +30,7 @@ class Billrun_Version {
 	public static $BUILD = '';
 
 	/** @var  string  Release date. */
-	public static $RELDATE = '15-Mar-2021';
+	public static $RELDATE = '23-May-2021';
 
 	/** @var  string  Link text. */
 	public static $URL = '<a href="https://bill.run">BillRun</a>';
