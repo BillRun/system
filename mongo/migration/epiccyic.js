b5114646f921f927f69578572ff864d899905f4e
alon.s <alon.shalom@billrun.com>
2021-06-14 15:28:44 +0300

    Insert into runOnce function

--- mongo/migration/epiccyic.js
+++ mongo/migration/epiccyic.js
@@ -5017,16 +5017,18 @@ for (var i = 0; i < lastConfig.file_types.length; i++) {
 
 //EPICIC-83: Unify "tier title" is directed to the wrong path
 //EPICIC-86: Add 'cf.anaa' to unified fields
-for (var i = 0; i < lastConfig.file_types.length; i++) {
-	if (lastConfig.file_types[i].file_type === "ICT") {
-		const index = lastConfig["file_types"][i]["unify"]["unification_fields"]["fields"][0]["update"][0]["data"].indexOf("cf.tier_title");
-		if (index > -1) {
-			lastConfig["file_types"][i]["unify"]["unification_fields"]["fields"][0]["update"][0]["data"].splice(index, 1);
+lastConfig = runOnce(lastConfig, 'EPICIC-83', function () {
+	for (var i = 0; i < lastConfig.file_types.length; i++) {
+		if (lastConfig.file_types[i].file_type === "ICT") {
+			const index = lastConfig["file_types"][i]["unify"]["unification_fields"]["fields"][0]["update"][0]["data"].indexOf("cf.tier_title");
+			if (index > -1) {
+				lastConfig["file_types"][i]["unify"]["unification_fields"]["fields"][0]["update"][0]["data"].splice(index, 1);
+			}
+			lastConfig["file_types"][i]["unify"]["unification_fields"]["fields"][0]["update"][0]["data"].push('foreign');
+			lastConfig["file_types"][i]["unify"]["unification_fields"]["fields"][0]["update"][0]["data"].push('cf.anaa');
 		}
-		lastConfig["file_types"][i]["unify"]["unification_fields"]["fields"][0]["update"][0]["data"].push('foreign');
-		lastConfig["file_types"][i]["unify"]["unification_fields"]["fields"][0]["update"][0]["data"].push('cf.anaa');
 	}
-}
+});
 
 db.config.insert(lastConfig);
 
