80d2901bcf7080ce50ed0551ee7500ebec8b8bcd
Yonatan Rosemarin <yonatan.r@billrun.com>
2021-07-04 17:48:48 +0300

    BRCD-3159: add log and error in case of failure generating OAuth 2.0 access token

--- library/Billrun/Http/Authentication/Oauth2.php
+++ library/Billrun/Http/Authentication/Oauth2.php
@@ -44,6 +44,12 @@ class Billrun_Http_Authentication_Oauth2 extends Billrun_Http_Authentication_Bas
         $request = new Billrun_Http_Request($url, $data, $params);
         $response = json_decode((string)$request->send(), JSON_OBJECT_AS_ARRAY);
         $accessToken = $response['access_token'] ?? '';
+
+        if (empty($accessToken)) {
+            unset($data['client_secret']);
+            Billrun_Factory::log("OAuth 2.0 - failed to generate access token. URL: {$url}, Data: " . $data, Zend_Log::ERR);
+            throw new Exception("Failed to generate access token for OAuth 2.0");
+        }
         
         if ($useCache && !empty($cache)) {
             $cacheTtl = $response['expires_in'] ?? false;
