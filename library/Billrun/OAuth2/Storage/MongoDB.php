9a58f6ac078da2c37085a47c3ff2f7c6c5d0c97d
Yonatan Rosemarin <yonatan.r@billrun.com>
2021-07-19 16:14:48 +0300

    BRCD-3188: add PHP header

--- library/Billrun/OAuth2/Storage/MongoDB.php
+++ library/Billrun/OAuth2/Storage/MongoDB.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * Wrapper class for MongoDB storage used by OAuth2.0
+ * Adding required functionality to the original (vendor) class.
+ * 
+ * Allows updating the vendor class without affecting the functionality added by BillRun
+ *
+ * @author Yonatan
+ */
 class Billrun_OAuth2_Storage_MongoDB extends OAuth2\Storage\MongoDB {
     public function unsetClientDetails($client_id, $client_secret = null) {
 		if (is_null($client_id) && is_null($client_secret)) {
