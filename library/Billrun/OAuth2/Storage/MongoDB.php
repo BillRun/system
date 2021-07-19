435ab0d5fc5e336528b883141c465fde87d491be
Yonatan Rosemarin <yonatan.r@billrun.com>
2021-07-19 16:28:44 +0300

    BRCD-3188: update header with license info

--- library/Billrun/OAuth2/Storage/MongoDB.php
+++ library/Billrun/OAuth2/Storage/MongoDB.php
@@ -6,9 +6,12 @@
  * 
  * Allows updating the vendor class without affecting the functionality added by BillRun
  *
- * @author Yonatan
+ * @package         Billing
+ * @copyright       Copyright (C) 2012-2021 BillRun Technologies Ltd. All rights reserved.
+ * @license         GNU Affero General Public License Version 3; see LICENSE.txt
  */
 class Billrun_OAuth2_Storage_MongoDB extends OAuth2\Storage\MongoDB {
+    
     public function unsetClientDetails($client_id, $client_secret = null) {
 		if (is_null($client_id) && is_null($client_secret)) {
 			return false;
@@ -18,4 +21,5 @@ class Billrun_OAuth2_Storage_MongoDB extends OAuth2\Storage\MongoDB {
 		}
 		return $this->collection('client_table')->deleteOne(array('client_secret' => $client_secret));
 	}
+
 }
