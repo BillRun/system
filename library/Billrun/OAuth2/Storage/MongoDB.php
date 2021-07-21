5436b50ae02076dd553f89057f2f7d66f004b239
Yonatan Rosemarin <yonatan.r@billrun.com>
2021-07-21 12:50:26 +0300

    BRCD-3188: update PHP doc

--- library/Billrun/OAuth2/Storage/MongoDB.php
+++ library/Billrun/OAuth2/Storage/MongoDB.php
@@ -1,14 +1,18 @@
 <?php
 
 /**
+ * @package         Billing
+ * @copyright       Copyright (C) 2012-2021 BillRun Technologies Ltd. All rights reserved.
+ * @license         GNU Affero General Public License Version 3; see LICENSE.txt
+ */
+
+/**
  * Wrapper class for MongoDB storage used by OAuth2.0
  * Adding required functionality to the original (vendor) class.
  * 
  * Allows updating the vendor class without affecting the functionality added by BillRun
  *
- * @package         Billing
- * @copyright       Copyright (C) 2012-2021 BillRun Technologies Ltd. All rights reserved.
- * @license         GNU Affero General Public License Version 3; see LICENSE.txt
+ * @package     OAuth2
  */
 class Billrun_OAuth2_Storage_MongoDB extends OAuth2\Storage\MongoDB {
     
