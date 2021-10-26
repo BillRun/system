bbdf6ed6e3cf5ad8b3e2d966ffa9d4493e6de96b
Eran Uzan <eran.uzan@billrun.com>
2021-10-27 00:03:26 +0300

    Unrelated change to docker to remove  existing  orphand containers

--- docker/billrun-docker/run_docker.sh
+++ docker/billrun-docker/run_docker.sh
@@ -5,4 +5,4 @@ mkdir ${DEBUG_LOG_DIR} -p
 touch ${DEBUG_LOG_DIR}/debug.log && chmod 666 ${DEBUG_LOG_DIR}/debug.log
 
 docker-compose  -f docker-compose-php73.yml  build
-docker-compose  -f docker-compose-php73.yml  up
+docker-compose  -f docker-compose-php73.yml  up --remove-orphans
