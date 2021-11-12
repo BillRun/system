fb4752ffa823b96059049fc285b6e12caccc6940
Ofer Cohen <ofer.cohen@billrun.com>
2021-11-13 01:44:27 +0200

    add docker support for PHP 7.4/8.0 and MongoDB 4.2/4.4 accordingly

--- docker/billrun-docker/run_docker.sh
+++ docker/billrun-docker/run_docker.sh
@@ -4,5 +4,5 @@ DEBUG_LOG_DIR=../../logs/container
 mkdir ${DEBUG_LOG_DIR} -p
 touch ${DEBUG_LOG_DIR}/debug.log && chmod 666 ${DEBUG_LOG_DIR}/debug.log
 
-docker-compose  -f docker-compose-php73.yml  build
-docker-compose  -f docker-compose-php73.yml  up --remove-orphans
+docker-compose -f docker-compose-php74.yml build
+docker-compose -f docker-compose-php74.yml up -d
