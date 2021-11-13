ba2b529257192da139ed41b2e4235be63dba4d29
Ofer Cohen <ofer.cohen@billrun.com>
2021-11-13 15:06:40 +0200

    make the bash script with argument to the PHP version

--- docker/billrun-docker/run_docker.sh
+++ docker/billrun-docker/run_docker.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+ver=$1
+
+if [ -n "$ver" ]; then
+    echo "PHP version selected is" $ver
+else
+    ver=74
+    echo "No input for PHP version; Selected default" $ver
+fi
+
+cd `dirname "$0"`
+
 DEBUG_LOG_DIR=../../logs/container
 mkdir ${DEBUG_LOG_DIR} -p
 touch ${DEBUG_LOG_DIR}/debug.log && chmod 666 ${DEBUG_LOG_DIR}/debug.log
 
-docker-compose -f docker-compose-php74.yml build
-docker-compose -f docker-compose-php74.yml up -d
+docker-compose -f docker-compose-php$ver.yml build
+docker-compose -f docker-compose-php$ver.yml up -d
