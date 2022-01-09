91f08876b31874e2d4b85d7e1f9e1cbd170ea940
or <or.shaashua@billrun.com>
2022-01-09 10:02:44 +0200

    BRCD-3412: fix error "Only variables can be passed by reference"

--- library/Billrun/EmailSender/Base.php
+++ library/Billrun/EmailSender/Base.php
@@ -134,7 +134,7 @@ abstract class Billrun_EmailSender_Base {
                     $value = Billrun_Util::getIn($data, $placeholder['path']);
                     if(!empty($value)){
                         $warningMessages = [];
-                        $replaces["[[". $name ."]]"] = Billrun_Util::formattingValue($placeholder, $value, $warningMessages , Billrun_Base::base_dateformat);
+                        $replaces["[[". $name ."]]"] = Billrun_Util::formattingValue($placeholder, $value, $warningMessages, Billrun_Base::base_dateformat);
                     }
                 }
                 return str_replace(array_keys($replaces), array_values($replaces), $msg);
