c60b381b3f09849fbbffc07024a60b577defac6d
killawatt <shani@billrun.net>
2022-01-11 12:06:58 +0200

    Update version

--- library/Billrun/Version.php
+++ library/Billrun/Version.php
@@ -21,7 +21,7 @@ class Billrun_Version {
 	public static $RELEASE = '5.13';
 
 	/** @var  string  Maintenance version. */
-	public static $DEV_LEVEL = '14';
+	public static $DEV_LEVEL = '15';
 
 	/** @var  string  Development STATUS. */
 	public static $DEV_STATUS = '';
@@ -30,7 +30,7 @@ class Billrun_Version {
 	public static $BUILD = '';
 
 	/** @var  string  Release date. */
-	public static $RELDATE = '06-Jan-2022';
+	public static $RELDATE = '11-Jan-2022';
 
 	/** @var  string  Link text. */
 	public static $URL = '<a href="https://bill.run">BillRun</a>';
