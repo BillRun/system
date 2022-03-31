a25553659f4bd22e28da20bcbaa9f18fd7818dff
alon.s <alon.shalom@billrun.com>
2022-03-31 14:24:37 +0300

    EPICIC-147: Add a month to the print

--- mongo/migration/epiccyic.js
+++ mongo/migration/epiccyic.js
@@ -9711,10 +9711,10 @@ lastConfig = runOnce(lastConfig, 'EPICIC-145', function () {
 
 lastConfig = runOnce(lastConfig, 'EPICIC-147', function () {
     var dates = [
-        {"from": ISODate("2022-02-01T00:00:00+0200"), "to": ISODate("2022-03-01T00:00:00+0200")},
-        {"from": ISODate("2022-01-01T00:00:00+0200"), "to": ISODate("2022-02-01T00:00:00+0200")},
-        {"from": ISODate("2022-03-01T00:00:00+0200"), "to": ISODate("2022-04-01T00:00:00+0200")},
-        {"from": ISODate("2022-04-01T00:00:00+0200"), "to": ISODate("2022-05-01T00:00:00+0200")}
+        {"from": ISODate("2022-02-01T00:00:00+0200"), "to": ISODate("2022-03-01T00:00:00+0200"), "month": "February"},
+        {"from": ISODate("2022-01-01T00:00:00+0200"), "to": ISODate("2022-02-01T00:00:00+0200"), "month": "January"},
+        {"from": ISODate("2022-03-01T00:00:00+0200"), "to": ISODate("2022-04-01T00:00:00+0200"), "month": "March"},
+        {"from": ISODate("2022-04-01T00:00:00+0200"), "to": ISODate("2022-05-01T00:00:00+0200"), "month": "April"}
     ];
     dates.forEach(period => {
         var valid_archive_lines = db.archive.find({urt: {$gte: period.from, $lt: period.to}, 'cf.cusagev': {$exists: false}}).noCursorTimeout();
@@ -9726,10 +9726,10 @@ lastConfig = runOnce(lastConfig, 'EPICIC-147', function () {
                 cusagev = 0;
             }
             line.cusagev = cusagev;
-            print("Iteration " + counter + " set cusagev as " + cusagev + " for archived line " + line.stamp);
+            print("Iteration " + counter + " set cusagev as " + cusagev + " for archived line " + line.stamp + " from " + period.month);
             db.archive.save(line);
             db.lines.update({stamp: line.u_s}, {$inc: {'cf.cusagev': line.cf.cusagev}});
-            print("Iteration " + counter + " added " + cusagev + " to unified line " + line.u_s);
+            print("Iteration " + counter + " added " + cusagev + " to unified line " + line.u_s + " from " + period.month);
             counter++;
         });
     });
