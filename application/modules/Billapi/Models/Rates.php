3d7cad6076055ea9d034dac93ee9dbf5444d3e57
Roman <roman.edneral@billrun.com>
2022-05-24 17:48:16 +0300

    BRCD-3662 fix(Import) - rates with different FROM date but same TO date create incorrect revision

--- application/modules/Billapi/Models/Rates.php
+++ application/modules/Billapi/Models/Rates.php
@@ -50,11 +50,16 @@ class Models_Rates extends Models_Entity {
 		// to allow create revisions - if OK can be moved to the parrent
 		if ($this->is_import) {
 			$duplicate_check_fields = Billrun_Util::getFieldVal($this->config['duplicate_check'], []);
-			$duplicate_check_fields['date'] = 'from';
 			$query = array();
 			foreach ($duplicate_check_fields as $fieldName) {
 				$query[$fieldName] = $data[$fieldName];
 			}
+			$query['from'] = [
+				'$gte' => $data['from']
+			];
+			$query['to'] = [
+				'$lte' => $data['to']
+			];
 			if ($ignoreIds) {
 				$query['_id'] = array(
 					'$nin' => $ignoreIds,
