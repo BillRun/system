a037678f626b3d740494dd2cf0f54431ef71b395
Eran Uzan <eran.uzan@billrun.com>
2022-06-09 15:53:45 +0300

    BRCD-3686 : improvments to the API calls response time

--- application/modules/Billapi/Models/Entity.php
+++ application/modules/Billapi/Models/Entity.php
@@ -1258,7 +1258,7 @@ class Models_Entity {
 			$query[$fieldName] = $record[$fieldName];
 		}
 		$recordCollection = Billrun_Factory::db()->{$collection . 'Collection'}();
-		return $recordCollection->query($query)->count() === 0;
+		return $recordCollection->query($query)->cursor()->limit(1)->current()->isEmpty();
 	}
 
 	/**
