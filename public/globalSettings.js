5e4394abfbf8c1ddef376b6382e5150cba18bc21
killawatt <shani@billrun.net>
2022-07-07 23:23:24 +0300

    Update version

--- public/globalSettings.js
+++ public/globalSettings.js
@@ -5,7 +5,7 @@ var globalSetting = {
   // serverUrl : "http://10.162.20.247", // Shani
   serverUrl: "http://billrun",
   //serverUrl: "",
-  serverApiVersion: '5.12.12',
+  serverApiVersion: '5.12.18',
   serverApiTimeOut: 300000, // 5 minutes
   serverApiDebug: false,
   serverApiDebugQueryString: 'XDEBUG_SESSION_START=netbeans-xdebug',
