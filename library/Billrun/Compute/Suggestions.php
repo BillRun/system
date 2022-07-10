52d39addfd1dc3eba82bdf294b50957c9c3c1706
or <or.shaashua@billrun.com>
2022-07-10 14:57:15 +0300

    BRCD-3731: add suggestion grouping fields (if exists) to row

--- library/Billrun/Compute/Suggestions.php
+++ library/Billrun/Compute/Suggestions.php
@@ -626,7 +626,7 @@ abstract class Billrun_Compute_Suggestions extends Billrun_Compute {
             'usagev' => 1,
             'credit_time' => date("Y-m-d\TH:i:s\Z"),
             'label' => $creditSuggestion['invoice_label'] ?? $creditSuggestion['description'] . ' - correction',
-//            'recalculation_type' => $creditSuggestion['recalculation_type']
+            'recalculation_type' => $creditSuggestion['recalculation_type']
         );
         $recalculation_type = $creditSuggestion['recalculation_type'];
         $groupingKeys = static::getGroupingFieldsByRecalculationType($recalculation_type);
