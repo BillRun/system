caa0816b6a76e724b04a8bba381dbb9141329909
or <or.shaashua@billrun.com>
2022-07-31 15:02:21 +0300

    BRCD-2913: remove logo images

--- library/Billrun/ActionManagers/Realtime/Responder/Data/ClearCall.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-/**
- * Response to ClearCall request
- */
-class Billrun_ActionManagers_Realtime_Responder_Data_ClearCall extends Billrun_ActionManagers_Realtime_Responder_Call_ClearCall {
-	
-}
