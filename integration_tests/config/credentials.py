58526ecc56fae9e37a561ea80aad11ea57c68bec
Serhii <serhii.hres@nixsolutions.com>
2022-09-06 17:44:28 +0300

    add credential

---
+++ integration_tests/config/credentials.py
@@ -0,0 +1,7 @@
+import os
+
+USERNAME = os.environ.get("USERNAME", "admin")
+PASSWORD = os.environ.get("PASSWORD", "12345678")
+MONGO_CONNECTION = "mongodb://localhost:27017/"
+MONGO_BILLING_DB_NAME = "billing_container"
+LOCAL_HOST = 'http://localhost:8074'
