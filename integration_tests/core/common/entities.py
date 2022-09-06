738feccc89312df078d0bf61923d0b5ae85c8185
Serhii <serhii.hres@nixsolutions.com>
2022-09-06 17:43:26 +0300

    add entries

---
+++ integration_tests/core/common/entities.py
@@ -0,0 +1,30 @@
+from core.common.helpers.mixins import GetAttributes
+
+
+class APIStatus:
+    SUCCESSFUL = 1
+    FAILED = 0
+
+
+class APIPath:
+    PRODUCTS = 'billapi/rates'
+    CUSTOMERS = 'billapi/accounts'
+    PLANS = 'billapi/plans'
+    SUBSCRIBERS = 'billapi/subscribers'
+
+
+class RevisionStatus:
+    EXPIRED = 'expired'
+    ACTIVE = 'active'
+
+
+class Recurrence(GetAttributes):
+    MONTHLY = 1
+    BIMONTHLY = 2
+    QUARTERLY = 3
+    SEMIANNUAL = 6
+    ANNUAL = 12
+
+
+DATE_PATTERN = '%Y-%m-%d'
+DATE_TIME_PATTERN = '%Y-%m-%dT%H:%M:%S+%f'
