8832cd58fa94524ac56a0e4bf82a66837ab92106
killawatt <shani@billrun.net>
2022-11-08 15:29:47 +0200

    Copy uf fields from billrun object to its bill (BRCD-3930)

--- application/helpers/Generator/BillrunToBill.php
+++ application/helpers/Generator/BillrunToBill.php
@@ -129,6 +129,9 @@ class Generator_BillrunToBill extends Billrun_Generator {
 		if (!empty($invoice['invoicing_day'])) {
 			$bill['invoicing_day'] = $invoice['invoicing_day'];
 		}
+		if (!empty($invoice['uf'])) {
+			$bill['uf'] = $invoice['uf'];
+		}
 		if ($bill['due'] < 0) {
 			$bill['left'] = $bill['amount'];
 		}
