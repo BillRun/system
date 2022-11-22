e897714bc38a35b3d0b95dcf378bbdc5d1e6ce66
Serhii <sergiygres@gmail.com>
2022-11-22 13:42:34 +0200

    add integration tests

---
+++ integration_tests/steps/assertion_steps/backend_assertion_steps/base_api_assertion_steps.py
@@ -0,0 +1,110 @@
+from abc import ABC, abstractmethod
+from datetime import datetime
+
+from hamcrest import assert_that, less_than_or_equal_to, equal_to, not_none
+
+from core.common.entities import RevisionStatus, DATE_PATTERN
+from core.common.matchers import check_that, has_not_existing_entity
+from core.common.helpers.utils import (
+    to_float_date_obj_form_get, convert_datetime_str_to_timestamp,
+    convert_date_str_to_datetime_str
+)
+from core.common.helpers.api_helpers import get_id_from_response, get_details, get_entity
+from core.testlib.common_assertions import check_http_code_and_status, check_json_schema
+
+
+class APIAssertionSteps(ABC):
+    def __init__(self, instance):
+        self.instance = instance
+
+    @abstractmethod
+    def check_response(self, actual_response, expected_response, schema, method):
+        pass
+
+    def validate_get_response_is_correct(self, schema, expected_response=None):
+        self.check_response(
+            actual_response=self.instance.get_response,
+            expected_response=expected_response,
+            schema=schema,
+            method='GET'
+        )
+
+    def validate_post_response_is_correct(self, schema, expected_response=None):
+        self.check_response(
+            actual_response=self.instance.create_response,
+            expected_response=expected_response,
+            schema=schema,
+            method='POST'
+        )
+
+    def check_post_response_is_successful(self):
+        check_http_code_and_status(self.instance.create_response)
+
+    def check_update_response_is_successful(self):
+        check_http_code_and_status(self.instance.update_response)
+
+    def check_close_response_is_successful(self):
+        check_http_code_and_status(self.instance.close_response)
+
+        to_from_payload = self.instance.close_payload.get('to') if self.instance.close_payload else None
+        to_from_response = to_float_date_obj_form_get(
+            get_entity(self.instance.close_response).get('to'), msec=False
+        )
+        if not to_from_payload:
+            # If no to date supplied it will be now
+            # we omit msec due to it can vary greatly
+            time_now = datetime.now().replace(microsecond=0).timestamp()
+            assert_that(
+                to_from_response, less_than_or_equal_to(time_now),
+                f"to param should be less or equal to {time_now}"
+            )
+        else:
+            assert_that(
+                to_from_response, equal_to(convert_datetime_str_to_timestamp(to_from_payload)),
+                f"to param should be equal to {to_from_payload}"
+            )
+
+    def check_close_and_new_response_is_successful(self):
+        check_http_code_and_status(self.instance.close_and_new_response)
+        assert_that(get_entity(
+            self.instance.close_and_new_response),
+            not_none(),
+            f"Close and New response is incorrect with {self.instance.close_and_new_response.json()} content")
+
+    def check_revision_has_new_to_date_after_close_and_new(self):
+        assert_that(
+            get_details(self.instance.get_by_id())[0].get('to'),
+            equal_to(
+                convert_date_str_to_datetime_str(
+                    self.instance.close_and_new_payload.get('from'))),
+            '"to" param is not changed to "from" param from new revision'
+        )
+
+    def check_object_revision_status(self, status=None):
+        if not status:
+            to = self.instance.close_payload.get('to')
+
+            if not to or datetime.strptime(to, DATE_PATTERN) < datetime.now():
+                status = RevisionStatus.EXPIRED
+            else:
+                status = RevisionStatus.ACTIVE
+
+        check_that(
+            lambda: get_details(
+                self.instance.get_by_id())[0].get('revision_info').get('status'),
+            equal_to(status),
+            f"revision status should be {status}", timeout=3
+        )
+
+    def check_object_is_deleted_successfully(self):
+        check_http_code_and_status(self.instance.delete_response)
+
+        assert_that(
+            self.instance.get_all(), has_not_existing_entity(
+                get_id_from_response(self.instance.create_response))
+        )
+
+    @staticmethod
+    def check_json_schema_and_http_code_and_status(schema, actual_response):
+        check_http_code_and_status(actual_response)
+        check_json_schema(actual_response, schema)
---
+++ integration_tests/tests/backend/test_customers.py
@@ -0,0 +1,41 @@
+import allure
+import pytest
+from pytest_testrail.plugin import pytestrail
+
+from core.common.helpers.utils import skip_test
+from steps.backend_steps.customers_steps import Customers, CustomerAssertionSteps
+
+
+@pytestrail.case('C2672', 'C2693')
+@allure.title('Create customer by API')
+@allure.description('CUSTOMERS-API-1')
+@pytest.mark.parametrize('optional_params', [
+    {'invoice_detailed': False, 'invoice_shipping_method': 'email'},  # 1
+    skip_test(case={'invoice_detailed': True, 'invoice_shipping_method': None},  # 2
+              reason='https://billrun.atlassian.net/browse/BRCD-3826')
+])
+@pytest.mark.smoke
+def test_create_customer(optional_params):
+    customer = Customers()
+    assertion_steps = CustomerAssertionSteps(customer)
+
+    customer.compose_create_payload(**optional_params).create()
+    assertion_steps.validate_post_response_is_correct()
+
+    customer.get_by_id()
+    assertion_steps.validate_get_response_is_correct()
+
+
+@allure.title('Delete customer by API')
+@allure.description('CUSTOMERS-API-2')
+@pytestrail.case('C2673')
+@pytest.mark.smoke
+def test_delete_customer():
+    customer = Customers()
+    assertion_steps = CustomerAssertionSteps(customer)
+
+    customer.compose_create_payload().create()
+    assertion_steps.validate_post_response_is_correct()
+
+    customer.delete()
+    assertion_steps.check_object_is_deleted_successfully()
