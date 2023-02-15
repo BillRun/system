4f49f28b3227e42bbe48875a02f74504d78732b2
Tim Lyzhenkov <timofey.lyzhenkov@billrun.com>
2023-02-15 15:31:21 +0300

    BRCD-2766 - Revert Front-End files commit

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.14b71d2d61a9f73821332cd7a9faadcb.js"
+  "/precache-manifest.1aba700e4ac5fc291d18b00db0c38c45.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
