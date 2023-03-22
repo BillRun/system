bc7aff319924f86a234b02ba53925f8efec68319
Keval <keval@iamops.io>
2023-03-22 10:55:10 +0200

    Update test.py

--- docker/billrun-docker/Docker-Testing/test.py
+++ docker/billrun-docker/Docker-Testing/test.py
@@ -1,17 +1,23 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
-driver.get("http://localhost:8074/index.html#/")
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--disable-gpu")
+driver = webdriver.Chrome(options=chrome_options)
+
+# driver.get("https://www.google.com/")
+# print(driver.title)
+driver.get("http://billrun/index.html#/")
 driver.implicitly_wait(100)
 username = driver.find_element(By.XPATH, "//input[@placeholder='Email address']")
 username.send_keys("admin")
 password = driver.find_element(By.XPATH, "//input[@placeholder='Password']")
 password.send_keys("12345678")
 driver.find_element(By.XPATH, "//button[@type='submit']").click()
-driver.get("http://localhost:8074/test/updaterowt?rebalance=1")
+driver.get("http://billrun/test/updaterowt?rebalance=1")
 logs = driver.find_element(By.XPATH, "/html/body").text
 print(logs)
 # file1 = open("Logs.txt", "w")  # write mode
