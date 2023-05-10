05d1991b364d902122c225ca221d175cf5b80305
Priyanka J <priyanka.j@iamops.io>
2023-05-10 16:39:05 +0300

    Replace test.py

--- docker/billrun-docker/Docker-Testing/test.py
+++ docker/billrun-docker/Docker-Testing/test.py
@@ -4,17 +4,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-gpu")
-chrome_options.add_argument("--disable-dev-shm-usage")
+# chrome_options.add_argument("--no-sandbox")
+# chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--disable-gpu")
 driver = webdriver.Chrome(options=chrome_options)
 
-driver.get("https://www.google.com/")
-a = driver.title
-print(a)
-
-driver.get("http://46.101.14.10:8074")
+driver.get("http://46.101.14.10:8074/index.html#/")
+driver.maximize_window()
 driver.implicitly_wait(100)
 
 # Encoded username and password
@@ -52,4 +48,92 @@ except:
         file1.write(logs)
         print("Logs uploaded to file successfully")
 
+print(
+    "----------------------------------------------------------------------------------------UpdateRowt Rebalance--------------------------------------------------------------------------------")
+
+driver.get("http://46.101.14.10:8074/test/Aggregatortest?skip=1,2,40")
+result = driver.find_element(By.XPATH, "//div[contains(text(),'1')]").text
+print(result)
+logs = driver.find_element(By.XPATH, "/html/body").text
+print(logs)
+
+# Write logs to a file
+with open("Logs_1.txt", "w") as file2:
+    file2.write(logs)
+print(
+    "----------------------------------------------------------------------------------------Aggregatortest--------------------------------------------------------------------------------")
+driver.get("http://46.101.14.10:8074/test/updaterowt?skip=k12,d81")
+result = driver.find_element(By.XPATH, "//div[contains(text(),'1')]").text
+print(result)
+logs = driver.find_element(By.XPATH, "/html/body").text
+print(logs)
+
+# Write logs to a file
+with open("Logs_2.txt", "w") as file3:
+    file3.write(logs)
+print(
+    "----------------------------------------------------------------------------------------updaterowt--------------------------------------------------------------------------------")
+
+driver.get("http://46.101.14.10:8074/test/RateTest?skip=k12,d81")
+result = driver.find_element(By.XPATH, "//div[contains(text(),'1')]").text
+print(result)
+logs = driver.find_element(By.XPATH, "/html/body").text
+print(logs)
+
+# Write logs to a file
+with open("Logs_3.txt", "w") as file4:
+    file4.write(logs)
+print(
+    "----------------------------------------------------------------------------------------Rate Test--------------------------------------------------------------------------------")
+
+driver.get("http://46.101.14.10:8074/test/monthsdifftest?skip1,4")
+result = driver.find_element(By.XPATH, "//div[contains(text(),'1')]").text
+print(result)
+logs = driver.find_element(By.XPATH, "/html/body").text
+print(logs)
+
+# Write logs to a file
+with open("Logs_4.txt", "w") as file5:
+    file5.write(logs)
+print(
+    "----------------------------------------------------------------------------------------get month diff test --------------------------------------------------------------------------------")
+
+driver.get("http://46.101.14.10:8074/test/CustomerCalculatorTest?skip=k12,d81")
+result = driver.find_element(By.XPATH, "//div[contains(text(),'1')]").text
+print(result)
+logs = driver.find_element(By.XPATH, "/html/body").text
+print(logs)
+
+# Write logs to a file
+with open("Logs_5.txt", "w") as file6:
+    file6.write(logs)
+
+print(
+    "----------------------------------------------------------------------------------------CustomerCalculatorTest --------------------------------------------------------------------------------")
+
+driver.get("http://46.101.14.10:8074/test/Taxmappingtest?skip=1,2,40")
+result = driver.find_element(By.XPATH, "//div[contains(text(),'1')]").text
+print(result)
+logs = driver.find_element(By.XPATH, "/html/body").text
+print(logs)
+
+# Write logs to a file
+with open("Logs_6.txt", "w") as file7:
+    file7.write(logs)
+
+print(
+    "----------------------------------------------------------------------------------------Taxmappingtest --------------------------------------------------------------------------------")
+
+driver.get("http://46.101.14.10:8074/test/discounttest?skip=1,2,40")
+result = driver.find_element(By.XPATH, "//div[contains(text(),'1')]").text
+print(result)
+logs = driver.find_element(By.XPATH, "/html/body").text
+print(logs)
+
+# Write logs to a file
+with open("Logs_7.txt", "w") as file8:
+    file8.write(logs)
+
+print(
+    "----------------------------------------------------------------------------------------discounttest --------------------------------------------------------------------------------")
 print("Code is successfully working")
