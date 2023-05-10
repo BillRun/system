d477d7923cb8f141fcb1a536e020384901bd86e3
Keval <keval@iamops.io>
2023-05-10 15:46:20 +0300

    Update services_steps.py

--- integration_tests/steps/backend_steps/services_steps.py
+++ integration_tests/steps/backend_steps/services_steps.py
@@ -34,7 +34,8 @@ class Services(BaseAPI):
             prorated=None,
             quantitative=None,
             include=None,  # ?
-            recurrence=None
+            recurrence_frequency=None,
+            recurrence_start=None,
     ):
         self.create_payload = {
             'description': description or get_random_str(),
@@ -45,7 +46,10 @@ class Services(BaseAPI):
             'to': to or get_random_past_or_future_date_str(past=False, start_range_from=5),
             'prorated': prorated or get_true_or_false(),
             'quantitative': quantitative or get_true_or_false(),
-            'recurrence': recurrence(frequency=1, start=1),
+            'recurrence': {
+                "frequency": 1,
+                "start": 1
+            },
         }
 
         return self
