5883078c0c2d16b28c5c41e9ea5a742ee26b166f
Eran Uzan <eran.uzan@billrun.com>
2023-05-23 14:14:38 +0300

    BRGT-881 : Fixed  values that were removed from version_29 be mistake

--- scripts/use_without_agreement_last_hour.sh
+++ scripts/use_without_agreement_last_hour.sh
@@ -5,8 +5,20 @@ LAST_HOUR="`date -d '-1 hours' +'%Y-%m-%dT%H:%M:%S%:z'`"
 NOW="`date +'%Y-%m-%dT%H:%M:%S%:z'`"
 
 if [ -z $SENDING_SOURCE_LIST ];then
-    SENDING_SOURCE_LIST='"AUTPT", "BGR01", "BLRMD", "HRVVI", "LIEMK", "SVNSM", "MKDNO", "SRBNO", "CHNCT", "THACA", "THACT", "USAW6", "GEOGC", "ARGTM", "BRAV1", "BRAV2", "BRAV3", "MEXMS", "BRATC", "UKRUM", "DNKDM", "NORTM", "SWEEP", "CHEDX", "RUSNW", "MNEPM", "PHLSR", "CYPPT", "HKGSM", "ALBVF", "CZECM", "DEUD2", "ESPAT", "GBRVF", "GHAGT", "GRCPF", "HUNVR", "IRLEC", "ITAOM", "NLDLT", "NZLBS", "PRTTL", "ROMMF", "TURTS", "ZAFVC", "BELFT", "FRAFR", "LUXFT", "POLFT", "SVKFT", "SVKGT","LTUMT", "LVABT", "ESTRE", "FINRL", "AREDU", "USACG", "CANBM", "CANTS", "URYAN", "AZEAC", "IND10", "IND11", "IND12", "IND14", "INDA1", "INDA2", "INDA3", "INDA4", "INDA5", "INDA6", "INDA7", "INDA8", "INDA9", "INDAT", "INDBL", "INDH1", "INDJB", "INDJH", "INDMT", "INDSC", "IND15", "IND16", "AUSVF", "BELHB", "PERVG", "VNMVT", "ISLNO", "ISRCL", "LUXVM"'
+    SENDING_SOURCE_LIST='"POL03", "BELMO", "FRAF1", "AUTPT", "BGR01", "BLRMD", "HRVVI", "LIEMK", "SVNSM",
+                        "MKDNO", "SRBNO", "CHNCT", "THACA", "THACT", "USAW6", "GEOGC", "ARGTM", "BRAV1", "BRAV2",
+                        "BRAV3", "MEXMS", "BRATC", "UKRUM", "DNKDM", "NORTM", "SWEEP", "CHEDX", "RUSNW", "MNEPM",
+                        "PHLSR", "CYPPT", "HKGSM", "ALBVF", "CZECM", "DEUD2", "ESPAT", "GBRVF", "GHAGT", "GRCPF",
+                        "HUNVR", "IRLEC", "ITAOM", "NLDLT", "NZLBS", "PRTTL", "ROMMF", "TURTS", "ZAFVC", "BELFT",
+                        "FRAFR", "LUXFT", "POLFT", "SVKFT","SVKGT", "LTUMT", "LVABT", "ESTRE", "FINRL",
+                        "AREDU", "USACG", "CANBM", "CANTS", "URYAN", "AZEAC", "IND10", "IND11", "IND12", "IND14",
+                        "INDA1", "INDA2", "INDA3", "INDA4", "INDA5", "INDA6", "INDA7", "INDA8", "INDA9", "INDAT",
+                        "INDBL", "INDH1", "INDJB", "INDJH", "INDMT", "INDSC", "IND15", "IND16", "AUSVF", "BELHB",
+                        "PERVG", "VNMVT", "ISLNO", "ISRCL", "LUXVM"'
 fi
+
+BASE_PATH=`dirname $0`;
+. $BASE_PATH/../conf/bash_conf.sh
 
 OUTPUT=`mongo billing -ureading -p$PWD --quiet --eval 'rs.slaveOk();db.getCollection("lines").find({ source : "nrtrde",
     urt : {        $gte : ISODate("'$LAST_HOUR'"),
@@ -17,7 +29,7 @@ OUTPUT=`mongo billing -ureading -p$PWD --quiet --eval 'rs.slaveOk();db.getCollec
          print(l.urt+","+(l.sid ? l.sid : "" )+","+l.imsi+","+l.sender);
         })'`;
 
-
+echo $OUTPUT;
 HEADER="urt,sid,imsi,sender"
 if [ -n "$OUTPUT" ]; then
     FILENAME="`date +%Y%m%d-%H%I`"_NRTRDE_last_hour.csv
