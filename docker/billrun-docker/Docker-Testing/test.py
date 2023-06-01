6122c7977b1869eb6454340fdd826be09c1d9315
Keval <keval@iamops.io>
2023-06-01 10:26:06 +0300

    Update test.py

--- docker/billrun-docker/Docker-Testing/test.py
+++ docker/billrun-docker/Docker-Testing/test.py
@@ -13,7 +13,6 @@ driver.get("http://46.101.14.10:8074/index.html#/")
 driver.maximize_window()
 driver.implicitly_wait(100)
 
-# Encoded username and password
 encoded_credentials = 'YWRtaW46MTIzNDU2Nzg='
 
 # Decode the username and password from base64
@@ -29,7 +28,6 @@ password_field = driver.find_element(By.XPATH, "//input[@placeholder='Password']
 password_field.send_keys(password)
 print("Password entered")
 
-# Click on the submit button
 driver.find_element(By.XPATH, "//button[@type='submit']").click()
 
 try:
@@ -37,102 +35,58 @@ try:
     print("Login using admin credential didn't work ERROR:- " + abc)
 except:
     print("Successfully Login using admin Credentials")
-    driver.get("http://46.101.14.10:8074/test/updaterowt?rebalance=1")
-    result = driver.find_element(By.XPATH, "//div[contains(text(),'1')]").text
-    print(result)
-    logs = driver.find_element(By.XPATH, "/html/body").text
-    print(logs)
 
-    # Write logs to a file
-    with open("Logs.txt", "w") as file1:
-        file1.write(logs)
-        print("Logs uploaded to file successfully")
+    driver.get("http://46.101.14.10:8074/test/updaterowt?rebalance=1")
+    UpdateRowt_Rebalance_logs = driver.find_element(By.XPATH, "/html/body").text
+    print(UpdateRowt_Rebalance_logs)
 
 print(
     "----------------------------------------------------------------------------------------UpdateRowt Rebalance--------------------------------------------------------------------------------")
 
 driver.get("http://46.101.14.10:8074/test/Aggregatortest?skip=1,2,40")
-result = driver.find_element(By.XPATH, "//div[contains(text(),'1')]").text
-print(result)
-logs = driver.find_element(By.XPATH, "/html/body").text
-print(logs)
+Aggregatortest_logs = driver.find_element(By.XPATH, "/html/body").text
+print(Aggregatortest_logs)
 
-# Write logs to a file
-with open("Logs_1.txt", "w") as file2:
-    file2.write(logs)
 print(
     "----------------------------------------------------------------------------------------Aggregatortest--------------------------------------------------------------------------------")
 driver.get("http://46.101.14.10:8074/test/updaterowt?skip=k12,d81")
-result = driver.find_element(By.XPATH, "//div[contains(text(),'1')]").text
-print(result)
-logs = driver.find_element(By.XPATH, "/html/body").text
-print(logs)
+updaterowt_logs = driver.find_element(By.XPATH, "/html/body").text
+print(updaterowt_logs)
 
-# Write logs to a file
-with open("Logs_2.txt", "w") as file3:
-    file3.write(logs)
 print(
     "----------------------------------------------------------------------------------------updaterowt--------------------------------------------------------------------------------")
 
 driver.get("http://46.101.14.10:8074/test/RateTest?skip=k12,d81")
-result = driver.find_element(By.XPATH, "//div[contains(text(),'1')]").text
-print(result)
-logs = driver.find_element(By.XPATH, "/html/body").text
-print(logs)
+Rate_Test_logs = driver.find_element(By.XPATH, "/html/body").text
+print(Rate_Test_logs)
 
-# Write logs to a file
-with open("Logs_3.txt", "w") as file4:
-    file4.write(logs)
 print(
     "----------------------------------------------------------------------------------------Rate Test--------------------------------------------------------------------------------")
 
 driver.get("http://46.101.14.10:8074/test/monthsdifftest?skip1,4")
-result = driver.find_element(By.XPATH, "//div[contains(text(),'1')]").text
-print(result)
-logs = driver.find_element(By.XPATH, "/html/body").text
-print(logs)
+month_diff_test_logs = driver.find_element(By.XPATH, "/html/body").text
+print(month_diff_test_logs)
 
-# Write logs to a file
-with open("Logs_4.txt", "w") as file5:
-    file5.write(logs)
 print(
     "----------------------------------------------------------------------------------------get month diff test --------------------------------------------------------------------------------")
 
 driver.get("http://46.101.14.10:8074/test/CustomerCalculatorTest?skip=k12,d81")
-result = driver.find_element(By.XPATH, "//div[contains(text(),'1')]").text
-print(result)
-logs = driver.find_element(By.XPATH, "/html/body").text
-print(logs)
-
-# Write logs to a file
-with open("Logs_5.txt", "w") as file6:
-    file6.write(logs)
+CustomerCalculatorTest_logs = driver.find_element(By.XPATH, "/html/body").text
+print(CustomerCalculatorTest_logs)
 
 print(
     "----------------------------------------------------------------------------------------CustomerCalculatorTest --------------------------------------------------------------------------------")
 
 driver.get("http://46.101.14.10:8074/test/Taxmappingtest?skip=1,2,40")
-result = driver.find_element(By.XPATH, "//div[contains(text(),'1')]").text
-print(result)
-logs = driver.find_element(By.XPATH, "/html/body").text
-print(logs)
-
-# Write logs to a file
-with open("Logs_6.txt", "w") as file7:
-    file7.write(logs)
+Taxmappingtest_logs = driver.find_element(By.XPATH, "/html/body").text
+print(Taxmappingtest_logs)
 
 print(
     "----------------------------------------------------------------------------------------Taxmappingtest --------------------------------------------------------------------------------")
 
 driver.get("http://46.101.14.10:8074/test/discounttest?skip=1,2,40")
-result = driver.find_element(By.XPATH, "//div[contains(text(),'1')]").text
-print(result)
-logs = driver.find_element(By.XPATH, "/html/body").text
-print(logs)
-
-# Write logs to a file
-with open("Logs_7.txt", "w") as file8:
-    file8.write(logs)
+discounttest_logs = driver.find_element(By.XPATH, "/html/body").text
+print(discounttest_logs)
 
 print(
     "----------------------------------------------------------------------------------------discounttest --------------------------------------------------------------------------------")
