bb2c605753f1b44264f083a57e70e4afa06263a2
Ravid Billrun <ravid.hansav@billrun.com>
2023-07-20 12:54:09 +0300

    BRCD-4185 Added sid and breakdown key to addLineVatableData log

--- library/Billrun/Cycle/Subscriber/Invoice.php
+++ library/Billrun/Cycle/Subscriber/Invoice.php
@@ -324,7 +324,7 @@ class Billrun_Cycle_Subscriber_Invoice {
 			}
 			return $newPrice;
 		} else if( empty($taxData) ) {
-			Billrun_Factory::log('addLineVatableData failed: Tax data missing. account: ' . $this->data['aid'] . ', billrun: ' . $this->data['key'], Zend_Log::CRIT);
+			Billrun_Factory::log('addLineVatableData failed: Tax data missing. account: ' . $this->data['aid'] . ', subscriber: ' . $this->data['sid'] . ', billrun: ' . $this->data['key'] . ', breakdown key: ' . $breakdownKey, Zend_Log::CRIT);
 		}
 		//else 
 		return $pricingData['aprice'];
