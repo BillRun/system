37e9144779bc58161121d7d903cd4e4873155686
Ravid Billrun <ravid.hansav@billrun.com>
2023-10-16 10:57:38 +0300

    BRCD-4231 Added cf to save fields of customer pricing

--- library/Billrun/Calculator/CustomerPricing.php
+++ library/Billrun/Calculator/CustomerPricing.php
@@ -336,7 +336,7 @@ class Billrun_Calculator_CustomerPricing extends Billrun_Calculator {
 	}
 
 	public function getPossiblyUpdatedFields() {
-		return array_merge(parent::getPossiblyUpdatedFields(), array($this->pricingField, 'billrun', 'over_plan', 'in_plan', 'out_plan', 'plan_ref', 'usagesb', 'arategroups', 'over_arate', 'over_group', 'in_group', 'in_arate', 'rates', 'out_group'));
+		return array_merge(parent::getPossiblyUpdatedFields(), array($this->pricingField, 'billrun', 'over_plan', 'in_plan', 'out_plan', 'plan_ref', 'usagesb', 'arategroups', 'over_arate', 'over_group', 'in_group', 'in_arate', 'rates', 'out_group', 'cf'));
 	}
 
 	public function getPricingField() {
