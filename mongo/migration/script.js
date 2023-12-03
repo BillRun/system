24b02bacaf8cbd785c1979934671bc9922f4d901
alon.s <alon.shalom@billrun.com>
2023-12-03 19:32:14 +0200

    BRCD-4297: Filter to only prorated and fix type

--- mongo/migration/script.js
+++ mongo/migration/script.js
@@ -1403,12 +1403,12 @@ lastConfig = runOnce(lastConfig, 'BRCD-4297', function () {
 		const newDate = new Date(fromDate); // Create a new Date object from the provided fromDate
 	  
 		// Add the specified number of months to the newDate
-		newDate.setMonth(newDate.getMonth() + monthsToAdd);
+		newDate.setMonth(newDate.getMonth() + parseInt(monthsToAdd));
 	  
 		return newDate.toISOString(); // Return the new date as an ISO string
 	  }
 	
-	var limited_cycle_services = db.services.aggregate([{$match: {balance_period: {$exists: false}, price: {$size: 1, $elemMatch: {to: {$ne: "UNLIMITED"}}}}}, {$group: {_id: "$name", month_limit: {$addToSet: "$price.to"}}}, {$match: {month_limit: {$size: 1}}}, {$unwind: "$month_limit"},{$unwind: "$month_limit"}])
+	var limited_cycle_services = db.services.aggregate([{$match: {balance_period: {$exists: false}, prorated: true, price: {$size: 1, $elemMatch: {to: {$ne: "UNLIMITED"}}}}}, {$group: {_id: "$name", month_limit: {$addToSet: "$price.to"}}}, {$match: {month_limit: {$size: 1}}}, {$unwind: "$month_limit"},{$unwind: "$month_limit"}])
 	var today = new Date();
 	var lastYear = new Date(today.getFullYear() - 1, today.getMonth(), today.getDate());
 	var lastYearISO = lastYear.toISOString();
@@ -1436,7 +1436,7 @@ lastConfig = runOnce(lastConfig, 'BRCD-4297', function () {
 		db.subscribers.save(subscriber);
 	})
 	
-	var services_with_revisions_with_differernt_cycles = db.services.aggregate([{$match: {balance_period: {$exists: false}, price: {$elemMatch: {to: {$ne: "UNLIMITED"}}}}}, {$group: {_id: "$name", month_limit: {$addToSet: "$price.to"}}}, {$match: {$expr: {$gt: [{$size: "$month_limit"}, 1]}}}])
+	var services_with_revisions_with_differernt_cycles = db.services.aggregate([{$match: {balance_period: {$exists: false}, prorated: true, price: {$elemMatch: {to: {$ne: "UNLIMITED"}}}}}, {$group: {_id: "$name", month_limit: {$addToSet: "$price.to"}}}, {$match: {$expr: {$gt: [{$size: "$month_limit"}, 1]}}}])
 	services_with_revisions_with_differernt_cycles.forEach(service => {
 		printjson("BRCD-4297: Service with that the month limit has been changed and will require a more complex fix: " + service._id);
 	});
