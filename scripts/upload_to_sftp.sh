7db76819d3e0231b32d925f36764c95e27070ba0
Eran Uzan <eran.uzan@billrun.com>
2023-12-25 13:11:30 +0200

    BRGT-927 : Configuration fixes and ftp upload improvments
    - change backup  to the same place like other cellcom files under
    /mnt/billing/backup.
    - main backup library  :  international calls
    - under international calls :  from_golan , to_golan

--- scripts/upload_to_sftp.sh
+++ scripts/upload_to_sftp.sh
@@ -5,13 +5,18 @@
 OLD_UPLOAD_FILE=/var/www/billrun/workspace/last_ftp_upload_time_file
 CURRENT_UPLOAD_FILE=/var/www/billrun/workspace/ftp_upload_time_file
 
+DEST_DIRECTORY="/data/tmp/International_calls/from_golan"
+
+if[ -n $2 ]; then
+	DEST_DIRECTORY=$2;
+fi
 
 touch $CURRENT_UPLOAD_FILE
 
 if [ -f $OLD_UPLOAD_FILE ]; then  
-	find $1 -type f -mmin -30 -mmin +0 -cnewer $OLD_UPLOAD_FILE   -exec bash -c 'FULL_PATH="{}"; FILE_NAME="`basename {}`" ; /var/www/billrun/scripts/sftp_upload.expct $FULL_PATH $FILE_NAME '$UPLOAD_FTP_PASS'' \;
+	find $1 -type f -mmin -30 -mmin +0 -cnewer $OLD_UPLOAD_FILE   -exec bash -c 'FULL_PATH="{}"; FILE_NAME="`basename {}`" ; /var/www/billrun/scripts/sftp_upload.expct $FULL_PATH $FILE_NAME '$UPLOAD_FTP_PASS' '$DEST_DIRECTORY'' \;
 else 
-        find $1 -type f -mmin -30 -mmin +0  -exec bash -c 'FULL_PATH="{}"; FILE_NAME="`basename {}`" ; /var/www/billrun/scripts/sftp_upload.expct $FULL_PATH $FILE_NAME '$UPLOAD_FTP_PASS'' \;
+        find $1 -type f -mmin -30 -mmin +0  -exec bash -c 'FULL_PATH="{}"; FILE_NAME="`basename {}`" ; /var/www/billrun/scripts/sftp_upload.expct $FULL_PATH $FILE_NAME '$UPLOAD_FTP_PASS' '$DEST_DIRECTORY'' \;
 fi
  
 mv -f $CURRENT_UPLOAD_FILE $OLD_UPLOAD_FILE
