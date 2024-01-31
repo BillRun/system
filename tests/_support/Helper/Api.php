4c0a3e183135e6dbf86bf707173306b41e6f2711
Yossi Tubul <yossi.tubul@billrun.com>
2024-01-31 17:32:27 +0200

    Update Api.php

--- tests/_support/Helper/Api.php
+++ tests/_support/Helper/Api.php
@@ -3,10 +3,9 @@ namespace Helper;
 // here you can define custom actions
 // all public methods declared in helper class will be available in $I
 
-use Codeception\Module;
 use Codeception\Module\REST;
 
-class Api extends Module
+class Api extends  \Codeception\Module
 {
     public function getO2Token() {
         $testUser = $_ENV['APP_TEST_USER'];
