5a73ed15f7e22213ab21256b498426538bb9fa30
yossi <yossi.tubul@billrun.com>
2024-02-01 12:09:14 +0200

    add data generators (account, susbcriber, plan, service)

--- tests/_support/Helper/Api.php
+++ tests/_support/Helper/Api.php
@@ -1,13 +1,15 @@
 <?php
 namespace Helper;
+
 // here you can define custom actions
 // all public methods declared in helper class will be available in $I
 
 use Codeception\Module\REST;
 
-class Api extends  \Codeception\Module
+class Api extends \Codeception\Module
 {
-    public function getO2Token() {
+    public function getO2Token()
+    {
         $testUser = $_ENV['APP_TEST_USER'];
         $testSecret = $_ENV['APP_TEST_SECRET'];
 
@@ -27,30 +29,118 @@ class Api extends  \Codeception\Module
         return $rest->grabDataFromResponseByJsonPath('$.access_token')[0];
     }
 
-    public function generateAccount(array $override=[]) {
+    public function generateAccount(array $override = [])
+    {
         $account = array_merge([
-            "invoice_shipping_method"=>"email",
-            "lastname"=>"test",
-            "invoice_detailed"=>false,
-            "invoice_language"=>"en_GB",
-            "from"=>"2024-01-31",
-            "zip_code"=>"123ab",
-            "payment_gateway"=>"",
-            "address"=>"hshalom 7",
-            "country"=>"Israel",
-            "salutation"=>"",
-            "firstname"=>"yossi",
-            "email"=>"test@gmail.com"
-          ], $override);
-        
+            "invoice_shipping_method" => "email",
+            "lastname" => "test",
+            "invoice_detailed" => false,
+            "invoice_language" => "en_GB",
+            "from" => "2024-01-31",
+            "zip_code" => "123ab",
+            "payment_gateway" => "",
+            "address" => "hshalom 7",
+            "country" => "Israel",
+            "salutation" => "",
+            "firstname" => "yossi",
+            "email" => "test@gmail.com"
+        ], $override);
+
         // Get the REST module to send requests
         /** @var REST $rest */
         $rest = $this->getModule('REST');
         $rest->amBearerAuthenticated($this->getO2Token());
         $rest->sendPOST('/billapi/accounts/create', [
-           'update' => json_encode($account)
+            'update' => json_encode($account)
 
         ]);
     }
+
+    public function generateSubscriber(array $override = [])
+    {
+        $subscriber = array_merge([
+            
+                "lastname" => "test",
+                "plan" => "D",
+                "from" => "2024-01-31",
+                "play" => "Default",
+                "address" => "hshalom 7",
+                "country" => "Israel",
+                "firstname" => "yossi",
+                "aid" => 1,
+                "services" => []
+            
+        ], $override);
+
+        // Get the REST module to send requests
+        /** @var REST $rest */
+        $rest = $this->getModule('REST');
+        $rest->amBearerAuthenticated($this->getO2Token());
+        $rest->sendPOST('/billapi/subscribers/create', [
+            'update' => json_encode($subscriber)
+        ]);
+    }
+
+    public function generatePlan(array $override = [])
+    {
+        $plan = array_merge([
+            
+                "price" => [
+                    [
+                        "price" => 100,
+                        "from" => 0,
+                        "to" => "UNLIMITED"
+                    ]
+                ],
+                "from" => "2024-02-02",
+                "name" => "PLAN",
+                "tax" => [
+                    [
+                        "type" => "vat",
+                        "taxation" => "global"
+                    ]
+                ],
+                "upfront" => false,
+                "recurrence" => ["periodicity" => "month"],
+                "prorated_end" => true,
+                "rates" => [],
+                "prorated_start" => true,
+                "connection_type" => "postpaid",
+                "prorated_termination" => true,
+                "description" => "plan"
+            
+        ], $override);
+
+        // Get the REST module to send requests
+        /** @var REST $rest */
+        $rest = $this->getModule('REST');
+        $rest->amBearerAuthenticated($this->getO2Token());
+        $rest->sendPOST('/billapi/plans/create', [
+            'update' => json_encode($plan)
+        ]);
+    }
+    public function generateService(array $override = [])
+    {
+        $service = array_merge([
+            "description"=>"service_",
+            "name"=>"SERVICE_",
+            "price"=>[["from"=>0,
+            "to"=>"UNLIMITED",
+            "price"=>10]],
+            "tax"=>[["type"=>"vat",
+            "taxation"=>"global"]],
+            "from"=>"2024-02-02",
+            "prorated"=>true
+        ], $override);
+
+        // Get the REST module to send requests
+        /** @var REST $rest */
+        $rest = $this->getModule('REST');
+        $rest->amBearerAuthenticated($this->getO2Token());
+        $rest->sendPOST('/billapi/services/create', [
+            'update' => json_encode($service)
+        ]);
+    }
+
 
 }
--- tests/api/apiSanityBCest.php
+++ tests/api/apiSanityBCest.php
@@ -3,25 +3,82 @@
 class apiSanityBCest
 {
 
+    public $accountDetails;
+    public $planDetails;
+    public $serviceDetails;
     public function _before(ApiTester $I)
     {
         $I->amBearerAuthenticated($I->getO2Token());
-    }   
+    }
 
-  
-    
+
+
     public function testCreateAccount(ApiTester $I)
     {
-        
-        $I->generateAccount(['firstname'=>'yossi_test']);
-        $I->seeResponseCodeIsSuccessful();
-        $I->seeResponseIsJson();
 
+        $I->generateAccount(['firstname' => 'yossi_test']);
+        $I->seeResponseIsJson();
         $I->seeResponseContains('{"status":1');
-        
         $I->seeResponseContainsJson([
             'firstname' => 'yossi_test'
-          ]);
+        ]);
+        $this->accountDetails = json_decode($I->grabResponse(), true)['entity'];
+    }
+
+    public function testCreatePlan(ApiTester $I)
+    {
+
+        $I->generatePlan(['name' => 'TEST_PLAN_2']);
+        $I->seeResponseIsJson();
+        $I->seeResponseContains('{"status":1');
+        $I->seeResponseContainsJson([
+            'name' => 'TEST_PLAN_2'
+        ]);
+        $this->planDetails = json_decode($I->grabResponse(), true)['entity'];
+    }
+
+    public function testCreateService(ApiTester $I)
+    {
+
+        $I->generateService(['name' => 'TEST_SERVICE']);
+        $I->seeResponseIsJson();
+        $I->seeResponseContains('{"status":1');
+        $I->seeResponseContainsJson([
+            'name' => 'TEST_SERVICE'
+        ]);
+        $this->serviceDetails = json_decode($I->grabResponse(), true)['entity'];
+    }
+
+    /**
+     * @depends testCreateAccount
+     * @depends testCreatePlan
+     * @depends testCreateService
+     */
+    public function testCreateSubscriber(ApiTester $I)
+    {
+
+        $I->generateSubscriber(
+            [
+                'firstname' => 'yossi_test',
+                'aid' => $this->accountDetails['aid'],
+                'plan' => $this->planDetails['name'],
+                'services'=>[
+                       [
+                        'name'=>$this->serviceDetails['name'],
+                        'from'=>'2024-02-05',
+                        'to'=>'2124-02-05',
+                       ]
+                ]
+            ]
+        );
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeResponseIsJson();
+        $I->seeResponseContains('{"status":1');
+        $I->seeResponseContainsJson([
+            'firstname' => 'yossi_test',
+            'aid' => $this->accountDetails['aid'],
+            'plan' => $this->planDetails['name']
+        ]);
     }
 
 }
