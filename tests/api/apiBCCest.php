e00f9f53176333673c505b891a8cfeeac71ab1dd
killawatt <shani@billrun.net>
2024-03-06 17:27:15 +0200

    Add all SimpleTest test functions to Codeception bcCest

--- tests/api/apiBCCest.php
+++ tests/api/apiBCCest.php
@@ -6,6 +6,48 @@ class bcCest
     public function updateRowT(ApiTester $I)
     {
         $I->sendAuthenticatedGET('/test/updaterowt?rebalance=1');
+        $response = substr($I->grabResponse(), -110);
+        $I->assertStringContainsString('<strong>0</strong> fails and <strong>0</strong> exceptions', $response);
+    }
+
+    public function Aggregatortest(ApiTester $I)
+    {
+        $I->sendAuthenticatedGET('/test/Aggregatortest?skip=1,2,40');
+        $response = $I->grabResponse();
+        $I->assertStringContainsString('<strong>0</strong> fails and <strong>0</strong> exceptions', $response);
+    }
+
+    public function RateTest(ApiTester $I)
+    {
+        $I->sendAuthenticatedGET('/test/RateTest');
+        $response = $I->grabResponse();
+        $I->assertStringContainsString('<strong>0</strong> fails and <strong>0</strong> exceptions', $response);
+    }
+
+    public function monthsdifftest(ApiTester $I)
+    {
+        $I->sendAuthenticatedGET('/test/monthsdifftest');
+        $response = $I->grabResponse();
+        $I->assertStringContainsString('<strong>0</strong> fails and <strong>0</strong> exceptions', $response);
+    }
+
+    public function CustomerCalculatorTest(ApiTester $I)
+    {
+        $I->sendAuthenticatedGET('/test/CustomerCalculatorTest');
+        $response = $I->grabResponse();
+        $I->assertStringContainsString('<strong>0</strong> fails and <strong>0</strong> exceptions', $response);
+    }
+
+    public function Taxmappingtest(ApiTester $I)
+    {
+        $I->sendAuthenticatedGET('/test/Taxmappingtest');
+        $response = $I->grabResponse();
+        $I->assertStringContainsString('<strong>0</strong> fails and <strong>0</strong> exceptions', $response);
+    }
+
+    public function discounttest(ApiTester $I)
+    {
+        $I->sendAuthenticatedGET('/test/discounttest');
         $response = $I->grabResponse();
         $I->assertStringContainsString('<strong>0</strong> fails and <strong>0</strong> exceptions', $response);
     }
