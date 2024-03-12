6d96f98e14b8273e802a52f6897134fa8ede0515
Ofer Cohen <ofer.cohen@billrun.com>
2024-03-12 11:14:53 +0200

    run sharding also on create.js

--- mongo/migration/script.js
+++ mongo/migration/script.js
@@ -1243,17 +1243,24 @@ _dropIndex("archive", "call_reference_1");
 if (db.serverStatus().ok == 0) {
 	print('Cannot shard archive collection - no permission')
 } else if (db.serverStatus().process == 'mongos') {
-    // please run manually the sharding.js instead of the lagacy commands below
-//	var _dbName = db.getName();
-//	sh.shardCollection(_dbName + ".archive", {"stamp": 1});
-//	// BRCD-2099 - sharding rates, billrun and balances
-//	sh.shardCollection(_dbName + ".rates", { "key" : 1 } );
-//	sh.shardCollection(_dbName + ".billrun", { "aid" : 1, "billrun_key" : 1 } );
-//	sh.shardCollection(_dbName + ".balances",{ "aid" : 1, "sid" : 1 }  );
-//        // BRCD-2244 audit sharding
-//	sh.shardCollection(_dbName + ".audit",  { "stamp" : 1 } );
-//        // BRCD-2185 sharding queue as added support for sharded collection transaction
-//	sh.shardCollection(_dbName + ".queue", { "stamp" : 1 } );
+    // sharding/cluster/mongos support - taken from sharding.js
+    var _dbName = db.getName();
+    print('running sharding on db: ' + _dbName);
+    sh.enableSharding(_dbName);
+    sh.shardCollection(_dbName + ".lines", {"stamp": 1});
+    sh.shardCollection(_dbName + ".archive", {"stamp": 1});
+    sh.shardCollection(_dbName + ".rates", {"key": 1});
+    sh.shardCollection(_dbName + ".billrun", {"aid": "hashed", "billrun_key": 1});
+    sh.shardCollection(_dbName + ".balances", {"aid": "hashed", sid: 1});
+    if (Number(db.version().charAt(0)) >= 6) {
+        sh.shardCollection(_dbName + ".bills", {"aid": "hashed"});
+    }
+    sh.shardCollection(_dbName + ".audit", {"stamp": 1});
+    sh.shardCollection(_dbName + ".queue", {"stamp": 1});
+    //sh.shardCollection(_dbName + ".events", { "stamp" : 1 } );
+    sh.shardCollection(_dbName + ".subscribers", {"aid": "hashed", "sid": 1});
+    //sh.shardCollection(_dbName + ".cards", { "batch_number":1, "serial_number":1 } );
+    //sh.shardCollection(_dbName + ".plans", { "name" : 1 } );
 }
 /*** BRCD-2634 Fix limited cycle(s) service (addon) align to the cycle. ***/
 lastConfig = runOnce(lastConfig, 'BRCD-2634', function () {
