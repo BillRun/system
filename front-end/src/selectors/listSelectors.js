5fbf612328496da9c77e9ebcb0cb285941276934
ahmad.khalafbayli <ahmad.khalafbayli@kibrit.tech>
2024-07-06 21:03:22 +0400

    added firstname and lastname type validation in selectAccountsOptions function

--- front-end/src/selectors/listSelectors.js
+++ front-end/src/selectors/listSelectors.js
@@ -39,8 +39,12 @@ const selectAccountsOptions = (options) => {
   }
   return options.map(option => {
     let name = '';
-    name += option.get('firstname', '').trim() !== '' ? option.get('firstname', '').trim() : '';
-    name += option.get('lastname', '').trim() !== '' ? ` ${option.get('lastname', '').trim()}` : '';
+
+    if (typeof option.get('firstname','') === 'string' ) 
+      name += option.get('firstname', '').trim() !== '' ? option.get('firstname', '').trim() : '';
+    if (typeof option.get('lastname','') === 'string' )
+      name += option.get('lastname', '').trim() !== '' ? ` ${option.get('lastname', '').trim()}` : '';
+
     return Immutable.Map({
       label: `${name.trim()} [${option.get('aid', '')}]`,
       value: option.get('aid', ''),
