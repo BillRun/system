bc2a60e5e8fcff3e212e89a6521806f081f2e4d3
killawatt <shani@billrun.net>
2024-10-13 16:16:42 +0300

    Update version

--- library/Billrun/Version.php
+++ library/Billrun/Version.php
@@ -21,7 +21,7 @@ class Billrun_Version {
 	public static $RELEASE = '5.13';
 
 	/** @var  string  Maintenance version. */
-	public static $DEV_LEVEL = '38';
+	public static $DEV_LEVEL = '39';
 
 	/** @var  string  Development STATUS. */
 	public static $DEV_STATUS = '';
@@ -30,7 +30,7 @@ class Billrun_Version {
 	public static $BUILD = '';
 
 	/** @var  string  Release date. */
-	public static $RELDATE = '16-Sep-2024';
+	public static $RELDATE = '13-Oct-2024';
 
 	/** @var  string  Link text. */
 	public static $URL = '<a href="https://bill.run">BillRun</a>';
