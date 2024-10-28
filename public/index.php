25db855dba399487bb7b3fa87c907b40cc9406a7
or <or.shaashua@billrun.com>
2024-10-28 10:49:19 +0200

    Revert "BRCD-4617- fix log of"
    This reverts commit 19773f71ad87edd7a064c203c0b0f6c882c96c76.

--- public/index.php
+++ public/index.php
@@ -8,16 +8,6 @@
 
 defined('APPLICATION_PATH') || define('APPLICATION_PATH', dirname(__DIR__));
 require_once(APPLICATION_PATH . DIRECTORY_SEPARATOR . 'conf' . DIRECTORY_SEPARATOR . 'config.php');
-
-if( isset($_SERVER['HTTP_ORIGIN']) ) {
-	header('Access-Control-Allow-Origin: '.$_SERVER['HTTP_ORIGIN']); // cross domain
-	header('Access-Control-Allow-Methods: GET,POST,PUT,DELETE,OPTIONS');
-	header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
-	header('Access-Control-Allow-Credentials: true');
-}
-//        ini_set('session.cookie_samesite', 'None');
-//        session_start();
-//        setcookie('samesite-test'
 $app = new Yaf_Application(BILLRUN_CONFIG_PATH);
 try {
 	$app->bootstrap()->run();
