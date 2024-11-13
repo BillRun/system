5812873f117d3ecb4d9233c7397b23e1f78c505b
Eran Uzan <eran.uzan@billrun.com>
2024-11-13 21:35:36 +0200

    Cleanup the changes a little

--- library/Billrun/Calculator/CustomerPricing.php
+++ library/Billrun/Calculator/CustomerPricing.php
@@ -203,7 +203,7 @@ class Billrun_Calculator_CustomerPricing extends Billrun_Calculator {
 	}
 
 	public function updateRow($row) {
-        if (isset($this->aidsQueuedForRebalance[$row['aid']]) && $row['type'] !== "credit") {
+		if (isset($this->aidsQueuedForRebalance[$row['aid']]) && $row['type'] !== "credit") {
 			return false;
 		}
                 
