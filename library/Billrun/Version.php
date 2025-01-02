39e21a1475cb4bf055cad244cb0a3150fb9dad14
killawatt <shani@billrun.net>
2025-01-02 11:08:21 +0200

    Update version

--- library/Billrun/Version.php
+++ library/Billrun/Version.php
@@ -21,7 +21,7 @@ class Billrun_Version {
 	public static $RELEASE = '5.13';
 
 	/** @var  string  Maintenance version. */
-	public static $DEV_LEVEL = '42';
+	public static $DEV_LEVEL = '43';
 
 	/** @var  string  Development STATUS. */
 	public static $DEV_STATUS = '';
