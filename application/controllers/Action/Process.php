9c99c5ad0bad893b424fc41f4627d77bd936e609
or <or.shaashua@billrun.com>
2025-02-18 16:25:38 +0200

    BRCD-4759-Refactoring to allow testing (processing) CDRs file’s records by a dedicated method + process a cpf.

--- application/controllers/Action/Process.php
+++ application/controllers/Action/Process.php
@@ -30,6 +30,7 @@ class ProcessAction extends Action_Base {
 		$possibleOptions = array(
 			'type' => false,
 			'path' => true,
+			'text' => true,
 			'backup' => true, // backup path
 		);
 
@@ -60,6 +61,9 @@ class ProcessAction extends Action_Base {
 		ob_start();
 		if (isset($options['path']) && $options['path']) {
 			$linesProcessedCount = $processor->process();
+		} else if(isset($options['text']) && $options['text']){
+			 $processor->loadCdrsText($options['text']);
+			 $linesProcessedCount = $processor->process();
 		} else {
 			$linesProcessedCount = $processor->process_files();
 		}
