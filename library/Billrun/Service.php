f303eb939116327f5f9694f046abcdf4614de4a0
Ofer Cohen <ofer.cohen@billrun.com>
2025-05-15 19:08:41 +0300

    add support to cost includes service groups

--- library/Billrun/Service.php
+++ library/Billrun/Service.php
@@ -82,6 +82,10 @@ class Billrun_Service {
 		$this->groupSelected = null;
 		$this->strongestGroup = null;
 	}
+	
+	public function setGroups($groups) {
+		Billrun_Util::setIn($this->data, 'include.groups', $groups);
+	}
 
 	/**
 	 * load the service from DB
@@ -252,14 +256,21 @@ class Billrun_Service {
 	 * method to receive all group rates of the current plan
 	 * @param array $rate the rate to check
 	 * @param string $usageType usage type to check
+	 * @param string $counters option to include or exclude counter services; available values: with, without, only
 	 * @return false when no group rates, else array list of the groups
 	 * @since 2.6
 	 */
-	public function getRateGroups($rate, $usageType) {
+	public function getRateGroups($rate, $usageType, $counters = 'without') {
 		$groups = array();
 		if (isset($this->data['include']['groups']) && is_array($this->data['include']['groups'])) {
 			foreach ($this->data['include']['groups'] as $groupName => $groupIncludes) {
-				if ((array_key_exists($usageType, $groupIncludes) || array_key_exists('cost', $groupIncludes) || isset($groupIncludes['usage_types'][$usageType])) && !empty($groupIncludes['rates']) && in_array($rate['key'], $groupIncludes['rates'])) {
+				if (
+					(array_key_exists($usageType, $groupIncludes) || array_key_exists('cost', $groupIncludes) || isset($groupIncludes['usage_types'][$usageType]) || !empty($groupIncludes['counter_only'])) && 
+					!empty($groupIncludes['rates']) && 
+					(in_array($rate['key'], $groupIncludes['rates']) || (is_string($groupIncludes['rates']) && 
+						(strtoupper($groupIncludes['rates']) === 'ALL_RATES' || preg_match($groupIncludes['rates'], $rate['key']) === 1))) &&
+					($counters == 'with' || ($counters == 'without' && empty($groupIncludes['counter_only'])) || ($counters == 'only' && !empty($groupIncludes['counter_only'])))
+				) {
 					$groups[] = $groupName;
 				}
 			}
@@ -304,7 +315,7 @@ class Billrun_Service {
 	 * @return true when the rate is part of group else false
 	 */
 	public function isRateInEntityGroup($rate, $usageType) {
-		if (count($this->getRateGroups($rate, $usageType))) {
+		if (count($this->getRateGroups($rate, $usageType, 'without'))) {
 			return true;
 		}
 		return false;
@@ -324,7 +335,7 @@ class Billrun_Service {
 	 */
 	protected function setNextStrongestGroup($rate, $usageType, $reset = FALSE) {
 		if (is_null($this->groups)) {
-			$this->groups = $this->getRateGroups($rate, $usageType);
+			$this->groups = $this->getRateGroups($rate, $usageType, 'without');
 		}
 		if (!count($this->groups)) {
 			$this->setEntityGroup(FALSE);
