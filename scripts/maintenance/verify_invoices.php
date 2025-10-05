d0131afb5ea25fee3f9f16b69aef369002416d9d
Eran Uzan <eran.uzan@billrun.com>
2025-10-05 14:56:34 +0300

    BRCD-5007 : Fix for MongoDB moving to vendor and compose  logic

--- scripts/maintenance/verify_invoices.php
+++ scripts/maintenance/verify_invoices.php
@@ -3,7 +3,11 @@
 defined('APPLICATION_PATH') || define('APPLICATION_PATH', dirname(__DIR__)."/..");
 require_once(APPLICATION_PATH. DIRECTORY_SEPARATOR . 'conf' . DIRECTORY_SEPARATOR . 'config.php');
 $app = new Yaf_Application(BILLRUN_CONFIG_PATH);
-
+try {
+	$app->bootstrap();
+} catch(\Throwable $th) {
+	// Ignore application issues as we only need the configuration to  be loaded
+}
 $jsonConf = '{
 
 			"cli":{
