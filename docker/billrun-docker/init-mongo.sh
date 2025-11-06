490b46d47bf3ab3645757bf2625b1bbeb4331f49
killawatt <shani@billrun.net>
2025-11-06 13:28:23 +0200

    Run migration scripts with mongosh whenever possible (BRCD-5046)

--- docker/billrun-docker/init-mongo.sh
+++ docker/billrun-docker/init-mongo.sh
@@ -1,4 +1,10 @@
-mongo billing_container /billrun/mongo/create.ini
+if command -v mongosh &>/dev/null; then
+  MONGOC=mongosh
+else
+  MONGOC=mongo
+fi
+
+$MONGOC billing_container /billrun/mongo/create.ini
 
 mongoimport -d billing_container -c config /billrun/mongo/base/config.export --batchSize 1
 mongoimport -d billing_container -c taxes /billrun/mongo/base/taxes.export --batchSize 1
@@ -11,18 +17,18 @@ if test -f "$FILE"; then
     mongoimport -d billing_container -c users $FILE
 fi 
 sleep 1 # remove once BRCD-4430 is done
-mongo billing_container /billrun/mongo/migration/script.js
+$MONGOC billing_container /billrun/mongo/migration/script.js
 
 for f in /plugin/mongo/installation/*.js
 do
     [ -f "$f" ] || break
     sleep 1 # remove once BRCD-4430 is done
-    mongo billing_container $f
+    $MONGOC billing_container $f
 done
 
 for f in /plugin/mongo/migration/*.js
 do
     [ -f "$f" ] || break
     sleep 1 # remove once BRCD-4430 is done
-    mongo billing_container $f
+    $MONGOC billing_container $f
 done
