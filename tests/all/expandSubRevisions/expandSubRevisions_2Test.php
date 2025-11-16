89c68ce64a3a99b857ac4e0f8511b3b52ec14200
Or Chen <or.chen@billrun.com>
2025-11-16 09:52:50 +0200

    BRCD-5044-fix Code duplication of cleanDb

--- tests/all/expandSubRevisions/expandSubRevisions_2Test.php
+++ tests/all/expandSubRevisions/expandSubRevisions_2Test.php
@@ -10,22 +10,7 @@ class expandSubRevisions_2Test extends \Codeception\Test\Unit
 
     protected function _before()
     {
-      $this->cleanDB();
-    }
-
-    protected function cleanDB(){
-
-        $subs = Billrun_Factory::db()->subscribersCollection();
-        $subs->remove(['_id'=>['$exists' => true]]);
-        $lines = Billrun_Factory::db()->linesCollection();
-        $lines->remove(['_id'=>['$exists' => true]]);
-        $queue = Billrun_Factory::db()->queueCollection();
-        $queue->remove(['_id'=>['$exists' => true]]);
-        $services = Billrun_Factory::db()->servicesCollection();
-        $services->remove(['_id'=>['$exists' => true]]);
-        $plans = Billrun_Factory::db()->plansCollection();
-        $plans->remove(['_id'=>['$exists' => true]]);
-
+      $this->tester->cleanDB();
     }
 
     protected function _after()
