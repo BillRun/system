e5a2c456a9cf2d55fbf518477f743a77f3a2235e
killawatt <shani@billrun.net>
2026-05-04 12:27:53 +0300

    Update version

--- public/precache-manifest.5fcd2c6a87dc9fb219e54b9461634c81.js
+++ public/precache-manifest.5fcd2c6a87dc9fb219e54b9461634c81.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3595a84633d22bfcbad21f026ed7c004",
+    "revision": "257d44e0acdcf61be0eeb724e7026571",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.f19ff9ae.chunk.css"
   },
   {
-    "revision": "4b2b815441407040361a",
+    "revision": "4f99e4c7b336859b1923",
     "url": "/static/css/main.634a4414.chunk.css"
   },
   {
@@ -24,8 +24,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/4.84b05e85.chunk.js"
   },
   {
-    "revision": "4b2b815441407040361a",
-    "url": "/static/js/main.a729f280.chunk.js"
+    "revision": "4f99e4c7b336859b1923",
+    "url": "/static/js/main.05fc7444.chunk.js"
   },
   {
     "revision": "7bd7827d7849a04e1975",
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d8c2bdcc3800ac9b3e5efca0637a2166.js"
+  "/precache-manifest.5fcd2c6a87dc9fb219e54b9461634c81.js"
 );
 
 self.addEventListener('message', (event) => {
