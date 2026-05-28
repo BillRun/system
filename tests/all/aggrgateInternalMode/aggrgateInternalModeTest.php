ff3fe0b5767b73906030be25aa25f9193da850e4
yossi <yossi.tubul@billrun.com>
2026-05-28 14:40:16 +0300

    Update test setup to use internal subscribers mode

--- tests/all/aggrgateInternalMode/aggrgateInternalModeTest.php
+++ tests/all/aggrgateInternalMode/aggrgateInternalModeTest.php
@@ -20,7 +20,7 @@ class aggrgateInternalModeTest extends \Codeception\Test\Unit
     protected function _before()
     {
         ini_set('error_reporting', E_ALL & ~E_WARNING & ~E_NOTICE);
-        $this->tester->enableDBModeSettings();
+        Billrun_Factory::config()->setInternalSubscribersMode();
     }
 
     protected function _after()
