dbce251e4507e463684c6b1df22a8ab0552ca0f4
Shani <shani.dalal@billrun.com>
2026-08-17 15:46:16 +0300

    Update version & build js (BRCD-5345)

--- public/globalSettings.js
+++ public/globalSettings.js
@@ -5,7 +5,7 @@ var globalSetting = {
   // serverUrl : "http://10.162.20.247", // Shani
   serverUrl: "http://billrun",
   //serverUrl: "",
-  serverApiVersion: '5.39.0',
+  serverApiVersion: '5.40.0',
   serverApiTimeOut: 300000, // 5 minutes
   serverApiDebug: false,
   serverApiDebugQueryString: 'XDEBUG_SESSION_START=netbeans-xdebug',
--- public/precache-manifest.702deae9c0fe18990883532e938cd590.js
+++ public/precache-manifest.702deae9c0fe18990883532e938cd590.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0eaecf29145830f07e441b21ae2c2b03",
+    "revision": "688f9a37affc9b53ed20ac9bd2f0ad40",
     "url": "/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/3.da7176fa.chunk.css"
   },
   {
-    "revision": "9857400c96278b92e343",
+    "revision": "0ba06df5ff4b2644d19a",
     "url": "/static/css/main.84a4e576.chunk.css"
   },
   {
@@ -24,8 +24,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/4.02521e5f.chunk.js"
   },
   {
-    "revision": "9857400c96278b92e343",
-    "url": "/static/js/main.ff913ca2.chunk.js"
+    "revision": "0ba06df5ff4b2644d19a",
+    "url": "/static/js/main.cde6f8e8.chunk.js"
   },
   {
     "revision": "132464f73bb36f12ce80",
