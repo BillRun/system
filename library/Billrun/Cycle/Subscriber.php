bb597a41eb642908955ae0007a89002fe4c81021
Dekel Schreiber <dekel.sch@billrun.com>
2026-08-20 10:54:32 +0300

    BRCD-5472: WIP - take service overrides from the service's own revision

--- library/Billrun/Cycle/Subscriber.php
+++ library/Billrun/Cycle/Subscriber.php
@@ -273,7 +273,8 @@ class Billrun_Cycle_Subscriber extends Billrun_Cycle_Common {
 
 		Billrun_Factory::dispatcher()->trigger('beforeConstructServices',array($this,&$services,&$stumpLine));
 		foreach ($services as &$arrService) {
-			$overrideData['overrides'] = array_filter($data['overrides'], function($override) use ($arrService) {
+			$revisionOverrides = isset($arrService['overrides']) ? $arrService['overrides'] : $data['overrides'];
+			$overrideData['overrides'] = array_filter($revisionOverrides, function($override) use ($arrService) {
 				return $override['type'] != 'service' || empty($override['id']) || $arrService['service_id'] == $override['id'];
 			});
 			$localMongoServices = $this->cycleAggregator->getServices($this->parentAccount,$overrideData);
@@ -473,6 +474,7 @@ class Billrun_Cycle_Subscriber extends Billrun_Cycle_Common {
 											'plan' => $subscriber['sid'] != 0 ? $subscriber['plan'] : null,
 											'start'=> max($tmpService['from']->sec + ($tmpService['from']->usec/ 1000000), $activationDate),
 										'end'=> min($tmpService['to']->sec +($tmpService['to']->usec/ 1000000),  $deactivationDate),
+										'overrides' => Billrun_Util::getFieldVal($subscriber['overrides'], []),
 										'compare_fields' => $srvStampFields)
 					);
 
